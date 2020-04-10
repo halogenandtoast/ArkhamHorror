13aa79d5053864e1d4aa98b87076bccbcb646e07
Matthew Mongeau <halogenandtoast@gmail.com>
2020-04-10 10:14:12 -0500

    WIP

--- arkham-horror-backend/src/Application.hs
+++ arkham-horror-backend/src/Application.hs
@@ -48,6 +48,7 @@ import Handler.Home
 
 import Handler.Api.Authentication
 import Handler.Api.CurrentUser
+import Handler.Api.Games
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ arkham-horror-backend/src/Handler/Api/Games.hs
@@ -0,0 +1,6 @@
+module Handler.Api.Games where
+
+import Import
+
+postApiV1GamesR :: Handler Int
+postApiV1GamesR = pure 1

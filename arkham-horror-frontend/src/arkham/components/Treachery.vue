f878f9c9d425bf6cfb2ac995f2abbc79f5000e98
Matthew Mongeau <halogenandtoast@gmail.com>
2020-07-30 09:18:29 -0500

    Fix discard and some treacheries

---
+++ arkham-horror-frontend/src/arkham/components/Treachery.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img :src="image" class="card" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Game } from '@/arkham/types/Game';
+import * as Arkham from '@/arkham/types/Treachery';
+
+@Component
+export default class Asset extends Vue {
+  @Prop(Object) readonly game!: Game
+  @Prop(Object) readonly treachery!: Arkham.Treachery
+
+  get image() {
+    return `/img/arkham/cards/${this.treachery.contents.cardCode}.jpg`;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 200px;
+}
+</style>

57a3d95fe1e7c0cb297fba9b608fa54c5f76c307
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-02 16:44:05 -0500

    Generalize AttrsWithMetadata to With

--- backend/arkham-core/library/Arkham/Types/Helpers.hs
+++ backend/arkham-core/library/Arkham/Types/Helpers.hs
@@ -1,11 +1,40 @@
 module Arkham.Types.Helpers where
 
-import ClassyPrelude
-import Data.Aeson
+import ClassyPrelude hiding (unpack)
+import Arkham.Json
 import qualified Data.HashMap.Strict as HashMap
+import Data.Text.Lazy (unpack)
+import Data.Text.Lazy.Builder
 
 without :: Int -> [a] -> [a]
 without n as = [ a | (i, a) <- zip [0 ..] as, i /= n ]
+
+data With a b = With a b
+
+instance (ToJSON a, ToJSON b) => ToJSON (a `With` b) where
+  toJSON (a `With` b) = case (toJSON a, toJSON b) of
+    (Object o, Object m) -> Object $ HashMap.union m o
+    (a, b) -> metadataError a b
+   where
+    metadataError a b =
+      error
+        . unpack
+        . toLazyText
+        $ "With failed to serialize to object: "
+        <> "\nattrs: "
+        <> encodeToTextBuilder a
+        <> "\nmetadata: "
+        <> encodeToTextBuilder b
+
+instance (FromJSON a, FromJSON b) => FromJSON (a `With` b) where
+  parseJSON = withObject "With" $ \o ->
+    With <$> parseJSON (Object o) <*> parseJSON (Object o)
+
+instance (Show a, Show b) => Show (a `With` b) where
+  show (With a b) = show a <> " WITH " <> show b
+
+with :: a -> b -> a `With`  b
+with a b = With a b
 
 infix 9 !!?
 (!!?) :: [a] -> Int -> Maybe a

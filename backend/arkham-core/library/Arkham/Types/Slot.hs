bf574a57d5f118eb0003a138ec86bc3dd95862d9
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-05 15:04:39 -0500

    Fill slots

--- backend/arkham-core/library/Arkham/Types/Slot.hs
+++ backend/arkham-core/library/Arkham/Types/Slot.hs
@@ -9,6 +9,11 @@ isEmptySlot :: Slot -> Bool
 isEmptySlot (Slot masset) = isNothing masset
 isEmptySlot (TraitRestrictedSlot _ masset) = isNothing masset
 
+putIntoSlot :: AssetId -> Slot -> Slot
+putIntoSlot aid = \case
+  Slot _ -> Slot (Just aid)
+  TraitRestrictedSlot t _ -> TraitRestrictedSlot t (Just aid)
+
 data Slot = Slot (Maybe AssetId) | TraitRestrictedSlot Trait (Maybe AssetId)
   deriving stock (Show, Generic)
   deriving anyclass (ToJSON, FromJSON)
@@ -19,5 +24,5 @@ data SlotType
   | AllySlot
   | AccessorySlot
   | ArcaneSlot
-  deriving stock (Show, Generic)
-  deriving anyclass (ToJSON, FromJSON)
+  deriving stock (Show, Generic, Eq)
+  deriving anyclass (ToJSON, FromJSON, Hashable, ToJSONKey, FromJSONKey)

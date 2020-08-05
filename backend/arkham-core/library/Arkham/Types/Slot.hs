f44ac47d6d541573ba7040c049d1c550a7a5ee9c
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-05 15:58:31 -0500

    Basic slots

--- backend/arkham-core/library/Arkham/Types/Slot.hs
+++ backend/arkham-core/library/Arkham/Types/Slot.hs
@@ -14,6 +14,18 @@ putIntoSlot aid = \case
   Slot _ -> Slot (Just aid)
   TraitRestrictedSlot t _ -> TraitRestrictedSlot t (Just aid)
 
+slotItem :: Slot -> Maybe AssetId
+slotItem = \case
+  Slot masset -> masset
+  TraitRestrictedSlot _ masset -> masset
+
+removeIfMatches :: AssetId -> Slot -> Slot
+removeIfMatches aid = \case
+  Slot masset -> if masset == Just aid then Slot Nothing else Slot masset
+  TraitRestrictedSlot trait masset -> if masset == Just aid
+    then TraitRestrictedSlot trait Nothing
+    else TraitRestrictedSlot trait masset
+
 data Slot = Slot (Maybe AssetId) | TraitRestrictedSlot Trait (Maybe AssetId)
   deriving stock (Show, Generic)
   deriving anyclass (ToJSON, FromJSON)

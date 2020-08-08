67b55a1f4eafca4d44e7549cd0e296c0596b3f8f
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-07 20:00:44 -0500

    Break out locations

---
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/Study.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE UndecidableInstances #-}
+module Arkham.Types.Location.Cards.Study where
+
+import Arkham.Json
+import Arkham.Types.Classes
+import Arkham.Types.GameValue
+import Arkham.Types.Location.Attrs
+import Arkham.Types.Location.Runner
+import Arkham.Types.LocationSymbol
+import ClassyPrelude
+
+newtype Study = Study Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+study :: Study
+study = Study $ baseAttrs "01111" "Study" 2 (PerPlayer 2) Circle []
+
+instance (LocationRunner env) => RunMessage env Study where
+  runMessage msg (Study attrs) = Study <$> runMessage msg attrs

7ed6734c2375fe8c7e756874ed6a637f713335c4
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-08 16:16:54 -0500

    Simplify shrivelling

--- backend/arkham-core/library/Arkham/Types/Asset/Cards/Shrivelling.hs
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/Shrivelling.hs
@@ -10,85 +10,68 @@ import Arkham.Types.Asset.Uses (Uses(..))
 import qualified Arkham.Types.Asset.Uses as Resource
 import Arkham.Types.AssetId
 import Arkham.Types.Classes
-import Arkham.Types.Helpers
 import Arkham.Types.Message
 import Arkham.Types.Modifier
 import Arkham.Types.SkillType
 import Arkham.Types.Slot
 import Arkham.Types.Source
 import qualified Arkham.Types.Token as Token
+import Arkham.Types.TokenResponse
 import ClassyPrelude
 import Lens.Micro
 
-newtype ShrivellingMetadata = ShrivellingMetadata { inUse :: Bool }
-  deriving stock (Show, Generic)
-  deriving anyclass (ToJSON, FromJSON) -- must parse to object
-
-newtype Shrivelling = Shrivelling (Attrs `With` ShrivellingMetadata)
+newtype Shrivelling = Shrivelling Attrs
   deriving stock (Show, Generic)
   deriving anyclass (ToJSON, FromJSON)
 
 shrivelling :: AssetId -> Shrivelling
 shrivelling uuid =
-  Shrivelling
-    $ ((baseAttrs uuid "01060") { assetSlots = [ArcaneSlot] })
-    `with` ShrivellingMetadata False
+  Shrivelling $ (baseAttrs uuid "01060") { assetSlots = [ArcaneSlot] }
 
 instance (AssetRunner env) => RunMessage env Shrivelling where
-  runMessage msg a@(Shrivelling (attrs@Attrs {..} `With` metadata@ShrivellingMetadata {..}))
-    = case msg of
-      InvestigatorPlayAsset _ aid _ _ | aid == assetId -> do
-        let
-          attrs' =
-            attrs
-              & (uses .~ Uses Resource.Charge 4)
-              & (abilities
-                .~ [ ( AssetSource aid
-                     , Nothing
-                     , 1
-                     , ActionAbility 1 (Just Action.Fight)
-                     , NoLimit
-                     )
-                   ]
-                )
-        Shrivelling . (`with` metadata) <$> runMessage msg attrs'
-      DrawToken token
-        | inUse
-          && token
-          `elem` [ Token.Skull
-                 , Token.Cultist
-                 , Token.Tablet
-                 , Token.ElderThing
-                 , Token.AutoFail
+  runMessage msg a@(Shrivelling attrs@Attrs {..}) = case msg of
+    InvestigatorPlayAsset _ aid _ _ | aid == assetId -> do
+      let
+        attrs' =
+          attrs
+            & (uses .~ Uses Resource.Charge 4)
+            & (abilities
+              .~ [ ( AssetSource aid
+                   , Nothing
+                   , 1
+                   , ActionAbility 1 (Just Action.Fight)
+                   , NoLimit
+                   )
                  ]
-        -> do
-          unshiftMessage
-            (InvestigatorDamage
-              (getInvestigator attrs)
-              (AssetSource assetId)
-              0
-              1
-            )
-          pure $ Shrivelling (attrs `with` ShrivellingMetadata False)
-      UseCardAbility iid (AssetSource aid, _, 1, _, _) | aid == assetId ->
-        case assetUses of
-          Uses Resource.Charge n -> do
-            when
-              (n == 1)
-              (unshiftMessage (RemoveAbilitiesFrom (AssetSource assetId)))
-            unshiftMessage
-              (ChooseFightEnemy
-                iid
-                SkillWillpower
-                [DamageDealt 1 (AssetSource aid)]
-                mempty -- TODO: Add metadata response here
-                False
               )
-            pure
-              $ Shrivelling
-              . (`with` ShrivellingMetadata True)
-              $ attrs
-              & uses
-              .~ Uses Resource.Charge (n - 1)
-          _ -> pure a
-      _ -> Shrivelling . (`with` metadata) <$> runMessage msg attrs
+      Shrivelling <$> runMessage msg attrs'
+    UseCardAbility iid (AssetSource aid, _, 1, _, _) | aid == assetId ->
+      case assetUses of
+        Uses Resource.Charge n -> do
+          when
+            (n == 1)
+            (unshiftMessage (RemoveAbilitiesFrom (AssetSource assetId)))
+          unshiftMessage
+            (ChooseFightEnemy
+              iid
+              SkillWillpower
+              [DamageDealt 1 (AssetSource aid)]
+              [ OnAnyToken
+                  [ Token.Skull
+                  , Token.Cultist
+                  , Token.Tablet
+                  , Token.ElderThing
+                  , Token.AutoFail
+                  ]
+                  [ InvestigatorDamage
+                      (getInvestigator attrs)
+                      (AssetSource assetId)
+                      0
+                      1
+                  ]
+              ]
+              False
+            )
+          pure $ Shrivelling $ attrs & uses .~ Uses Resource.Charge (n - 1)
+        _ -> pure a
+    _ -> Shrivelling <$> runMessage msg attrs

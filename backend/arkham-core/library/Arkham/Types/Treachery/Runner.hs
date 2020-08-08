21ed16f1a824c5567ca811199eae9787cb5a8396
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-08 11:02:51 -0500

    try to fix obscuring fog

--- backend/arkham-core/library/Arkham/Types/Treachery/Runner.hs
+++ backend/arkham-core/library/Arkham/Types/Treachery/Runner.hs
@@ -1,13 +1,16 @@
 module Arkham.Types.Treachery.Runner where
 
 import Arkham.Types.AssetId
+import Arkham.Types.Card
 import Arkham.Types.Classes
 import Arkham.Types.InvestigatorId
 import Arkham.Types.LocationId
+import Arkham.Types.Query
 
 type TreacheryRunner env
   = ( HasQueue env
     , HasSet AssetId InvestigatorId env
     , HasId LocationId InvestigatorId env
+    , HasCount TreacheryCount (LocationId, CardCode) env
     )
 

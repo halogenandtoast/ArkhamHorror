06089c3f4813903c4aa1356f36dd4755685511d2
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-12 09:23:21 -0500

    Add editor

--- frontend/src/arkham/api.ts
+++ frontend/src/arkham/api.ts
@@ -13,3 +13,10 @@ export const fetchGame = (gameId: string) => api
 export const updateGame = (gameId: string, choice: number) => api
   .put(`arkham/games/${gameId}`, { choice })
   .then((resp) => gameDecoder.decodePromise(resp.data));
+
+export const fetchGameRaw = (gameId: string) => api
+  .get(`arkham/games/${gameId}`)
+  .then((resp) => resp.data);
+
+export const updateGameRaw = (gameId: string, gameJson: string) => api
+  .put(`arkham/games/${gameId}/raw`, { gameJson });
--- frontend/src/arkham/routes/index.ts
+++ frontend/src/arkham/routes/index.ts
@@ -1,10 +1,18 @@
 import Game from '@/arkham/views/Game.vue';
+import EditGame from '@/arkham/views/EditGame.vue';
 
 export default [
   {
     path: '/games/:gameId',
     name: 'Game',
     component: Game,
+    meta: { requiresAuth: true },
+    props: true,
+  },
+  {
+    path: '/games/:gameId/edit',
+    name: 'EditGame',
+    component: EditGame,
     meta: { requiresAuth: true },
     props: true,
   },

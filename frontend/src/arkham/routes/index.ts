0115711ceb2fccc6a5ff60b7137ca82181e44716
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-14 00:54:35 -0500

    WIP

--- frontend/src/arkham/routes/index.ts
+++ frontend/src/arkham/routes/index.ts
@@ -1,7 +1,15 @@
 import Game from '@/arkham/views/Game.vue';
 import EditGame from '@/arkham/views/EditGame.vue';
+import NewCampaign from '@/arkham/views/NewCampaign.vue';
 
 export default [
+  {
+    path: '/campaigns/new',
+    name: 'NewCampaign',
+    component: NewCampaign,
+    meta: { requiresAuth: true },
+    props: true,
+  },
   {
     path: '/games/:gameId',
     name: 'Game',

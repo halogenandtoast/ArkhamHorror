f0133be7c404cd48567fa0317f1991e4bf31ab8c
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-14 04:51:58 -0500

    Record things

--- frontend/src/arkham/types/Phase.ts
+++ frontend/src/arkham/types/Phase.ts
@@ -1,8 +1,9 @@
 import { JsonDecoder } from 'ts.data.json';
 
-export type Phase = 'InvestigationPhase' | 'EnemyPhase' | 'UpkeepPhase' | 'MythosPhase';
+export type Phase = 'CampaignPhase' | 'InvestigationPhase' | 'EnemyPhase' | 'UpkeepPhase' | 'MythosPhase';
 
 export const phaseDecoder = JsonDecoder.oneOf<Phase>([
+  JsonDecoder.isExactly('CampaignPhase'),
   JsonDecoder.isExactly('InvestigationPhase'),
   JsonDecoder.isExactly('EnemyPhase'),
   JsonDecoder.isExactly('UpkeepPhase'),

5b23443ad2ac685f3ea763541415e928bc1ea81a
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-21 10:09:09 -0500

    Uncovering the conspiracy

--- backend/arkham-core/library/Arkham/Types/Act/Cards/UncoveringTheConspiracy.hs
+++ backend/arkham-core/library/Arkham/Types/Act/Cards/UncoveringTheConspiracy.hs
@@ -5,6 +5,7 @@ import Arkham.Json
 import Arkham.Types.Ability
 import Arkham.Types.Act.Attrs
 import Arkham.Types.Act.Runner
+import Arkham.Types.Card.CardCode
 import Arkham.Types.Classes
 import Arkham.Types.FastWindow
 import Arkham.Types.Message
@@ -39,11 +40,24 @@ instance (ActionRunner env investigator) => HasActions env investigator Uncoveri
 instance (ActRunner env) => RunMessage env UncoveringTheConspiracy where
   runMessage msg a@(UncoveringTheConspiracy attrs@Attrs {..}) = case msg of
     AdvanceAct aid | aid == actId && actSequence == "Act 1a" -> do
+      leadInvestigatorId <- unLeadInvestigatorId <$> asks (getId ())
+      unshiftMessage (Ask leadInvestigatorId $ ChooseOne [AdvanceAct aid])
       pure
         $ UncoveringTheConspiracy
         $ attrs
         & (sequence .~ "Act 1b")
         & (flipped .~ True)
+    AdvanceAct aid | aid == actId && actSequence == "Act 1b" -> do
+      a <$ unshiftMessage (Resolution 1)
+    AddToVictory _ -> do
+      victoryDisplay <- HashSet.map unVictoryDisplayCardCode
+        <$> asks (getSet ())
+      let
+        cultists = HashSet.fromList
+          ["01121b", "01137", "01138", "01139", "01140", "01141"]
+      a <$ when
+        (cultists `HashSet.isSubsetOf` victoryDisplay)
+        (unshiftMessage (AdvanceAct actId))
     UseCardAbility iid _ (ActSource aid) 1 | aid == actId -> do
       investigatorIds <- HashSet.toList <$> asks (getSet ())
       a <$ unshiftMessages

316dd8eedaaa7030bc9489dbf9cc91984a6fbaae
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-24 10:50:48 -0500

    Goat Spawn

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/GoatSpawn.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/GoatSpawn.hs
@@ -7,7 +7,10 @@ import Arkham.Types.Enemy.Attrs
 import Arkham.Types.Enemy.Runner
 import Arkham.Types.EnemyId
 import Arkham.Types.GameValue
+import Arkham.Types.Message
+import Arkham.Types.Source
 import ClassyPrelude
+import qualified Data.HashSet as HashSet
 
 newtype GoatSpawn = GoatSpawn Attrs
   deriving newtype (Show, ToJSON, FromJSON)
@@ -24,4 +27,12 @@ instance (IsInvestigator investigator) => HasActions env investigator GoatSpawn 
   getActions i window (GoatSpawn attrs) = getActions i window attrs
 
 instance (EnemyRunner env) => RunMessage env GoatSpawn where
-  runMessage msg (GoatSpawn attrs) = GoatSpawn <$> runMessage msg attrs
+  runMessage msg (GoatSpawn attrs@Attrs {..}) = case msg of
+    EnemyDefeated eid _ _ _ | eid == enemyId -> do
+      investigatorIds <- HashSet.toList <$> asks (getSet enemyLocation)
+      unshiftMessages
+        [ InvestigatorAssignDamage iid (EnemySource eid) 0 1
+        | iid <- investigatorIds
+        ]
+      GoatSpawn <$> runMessage msg attrs
+    _ -> GoatSpawn <$> runMessage msg attrs

542ca2d372d09177808b212525b1f669e3d141a1
Matthew Mongeau <halogenandtoast@gmail.com>
2020-08-24 10:58:57 -0500

    Young Deep One

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/YoungDeepOne.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/YoungDeepOne.hs
@@ -7,6 +7,10 @@ import Arkham.Types.Enemy.Attrs
 import Arkham.Types.Enemy.Runner
 import Arkham.Types.EnemyId
 import Arkham.Types.GameValue
+import Arkham.Types.Message
+import Arkham.Types.Prey
+import Arkham.Types.SkillType
+import Arkham.Types.Source
 import ClassyPrelude
 
 newtype YoungDeepOne = YoungDeepOne Attrs
@@ -19,10 +23,18 @@ youngDeepOne uuid = YoungDeepOne $ (baseAttrs uuid "01181")
   , enemyFight = 3
   , enemyHealth = Static 3
   , enemyEvade = 3
+  , enemyPrey = LowestSkill SkillCombat
   }
 
 instance (IsInvestigator investigator) => HasActions env investigator YoungDeepOne where
   getActions i window (YoungDeepOne attrs) = getActions i window attrs
 
 instance (EnemyRunner env) => RunMessage env YoungDeepOne where
-  runMessage msg (YoungDeepOne attrs) = YoungDeepOne <$> runMessage msg attrs
+  runMessage msg (YoungDeepOne attrs@Attrs {..}) = case msg of
+    EnemyEngageInvestigator eid iid | eid == enemyId -> do
+      unshiftMessage (InvestigatorAssignDamage iid (EnemySource eid) 0 1)
+      YoungDeepOne <$> runMessage msg attrs
+    EngageEnemy iid eid False | eid == enemyId -> do
+      unshiftMessage (InvestigatorAssignDamage iid (EnemySource eid) 0 1)
+      YoungDeepOne <$> runMessage msg attrs
+    _ -> YoungDeepOne <$> runMessage msg attrs

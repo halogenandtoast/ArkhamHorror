a6a376a6f71aa40d0f36f4cde723dcd4e25fecd6
Matthew Mongeau <halogenandtoast@gmail.com>
2020-09-20 08:47:45 -0500

    Start showing campaign log

--- frontend/src/arkham/types/Campaign.ts
+++ frontend/src/arkham/types/Campaign.ts
@@ -5,14 +5,26 @@ export interface Campaign {
   contents: CampaignContents;
 }
 
+export interface LogContents {
+  recorded: string[];
+  recordedSets: any[]; // eslint-disable-line
+}
+
 export interface CampaignContents {
   name: string;
   id: string;
+  log: LogContents;
 }
+
+export const logContentsDecoder = JsonDecoder.object<LogContents>({
+  recorded: JsonDecoder.array<string>(JsonDecoder.string, 'recorded[]'),
+  recordedSets: JsonDecoder.array<any>(JsonDecoder.succeed, 'recordedSets[]'), // eslint-disable-line
+}, 'LogContents');
 
 export const campaignContentsDecoder = JsonDecoder.object<CampaignContents>({
   name: JsonDecoder.string,
   id: JsonDecoder.string,
+  log: logContentsDecoder,
 }, 'CampaignContents');
 
 export const campaignDecoder = JsonDecoder.object<Campaign>({

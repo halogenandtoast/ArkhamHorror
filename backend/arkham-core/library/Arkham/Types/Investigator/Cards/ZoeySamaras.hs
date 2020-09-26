ca0bd839a5453098fd85bf1bfb1caa75170e5e5f
Matthew Mongeau <halogenandtoast@gmail.com>
2020-09-26 10:06:29 -0500

    WIP

--- backend/arkham-core/library/Arkham/Types/Investigator/Cards/ZoeySamaras.hs
+++ backend/arkham-core/library/Arkham/Types/Investigator/Cards/ZoeySamaras.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Arkham.Types.Investigator.Cards.ZoeySamaras where
 
+import Arkham.Types.Ability
 import Arkham.Types.Classes
 import Arkham.Types.ClassSymbol
 import Arkham.Types.Investigator.Attrs
 import Arkham.Types.Investigator.Runner
 import Arkham.Types.Message
+import Arkham.Types.Source
 import Arkham.Types.Stats
 import Arkham.Types.Token
 import Arkham.Types.Trait
+import Arkham.Types.Window (Who(..))
+import qualified Arkham.Types.Window as Fast
 import ClassyPrelude
 import Data.Aeson
 
@@ -30,7 +34,16 @@ zoeySamaras = ZoeySamaras $ baseAttrs
     }
   [Believer, Hunter]
 
-instance HasActions env investigator ZoeySamaras where
+instance (ActionRunner env investigator) => HasActions env investigator ZoeySamaras where
+  getActions i (Fast.AfterEnemyEngageInvestigator You) (ZoeySamaras Attrs {..})
+    | getId () i == investigatorId
+    = let
+        ability = mkAbility
+          (InvestigatorSource investigatorId)
+          1
+          (ReactionAbility (Fast.AfterEnemyEngageInvestigator You))
+      in pure [ActivateCardAbilityAction investigatorId ability]
+
   getActions i window (ZoeySamaras attrs) = getActions i window attrs
 
 instance (InvestigatorRunner Attrs env) => RunMessage env ZoeySamaras where

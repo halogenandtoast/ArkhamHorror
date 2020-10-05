fde698e94c57db051c66e516c9421f3c6aab30e2
Matthew Mongeau <halogenandtoast@gmail.com>
2020-10-05 12:27:10 -0500

    Add Bulletproof Vest and Elder Sign Amulet

---
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/BulletproofVest3.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE UndecidableInstances #-}
+module Arkham.Types.Asset.Cards.BulletproofVest3 where
+
+import Arkham.Json
+import Arkham.Types.Asset.Attrs
+import Arkham.Types.Asset.Runner
+import Arkham.Types.AssetId
+import Arkham.Types.Classes
+import Arkham.Types.Slot
+import ClassyPrelude
+
+newtype BulletproofVest3 = BulletproofVest3 Attrs
+  deriving stock (Show, Generic)
+  deriving anyclass (ToJSON, FromJSON)
+
+bulletproofVest3 :: AssetId -> BulletproofVest3
+bulletproofVest3 uuid = BulletproofVest3
+  $ (baseAttrs uuid "01094") { assetSlots = [BodySlot], assetHealth = Just 4 }
+
+instance HasModifiersFor env investigator BulletproofVest3 where
+  getModifiersFor _ _ = pure []
+
+instance HasActions env investigator BulletproofVest3 where
+  getActions i window (BulletproofVest3 x) = getActions i window x
+
+instance (AssetRunner env) => RunMessage env BulletproofVest3 where
+  runMessage msg (BulletproofVest3 attrs) =
+    BulletproofVest3 <$> runMessage msg attrs
---
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/ElderSignAmulet3.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE UndecidableInstances #-}
+module Arkham.Types.Asset.Cards.ElderSignAmulet3 where
+
+import Arkham.Json
+import Arkham.Types.Asset.Attrs
+import Arkham.Types.Asset.Runner
+import Arkham.Types.AssetId
+import Arkham.Types.Classes
+import Arkham.Types.Slot
+import ClassyPrelude
+
+newtype ElderSignAmulet3 = ElderSignAmulet3 Attrs
+  deriving stock (Show, Generic)
+  deriving anyclass (ToJSON, FromJSON)
+
+elderSignAmulet3 :: AssetId -> ElderSignAmulet3
+elderSignAmulet3 uuid = ElderSignAmulet3 $ (baseAttrs uuid "01095")
+  { assetSlots = [AccessorySlot]
+  , assetSanity = Just 4
+  }
+
+instance HasModifiersFor env investigator ElderSignAmulet3 where
+  getModifiersFor _ _ = pure []
+
+instance HasActions env investigator ElderSignAmulet3 where
+  getActions i window (ElderSignAmulet3 x) = getActions i window x
+
+instance (AssetRunner env) => RunMessage env ElderSignAmulet3 where
+  runMessage msg (ElderSignAmulet3 attrs) =
+    ElderSignAmulet3 <$> runMessage msg attrs

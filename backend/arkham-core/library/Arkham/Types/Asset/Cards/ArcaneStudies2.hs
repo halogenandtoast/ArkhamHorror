5b8f16ad9442c2353c1a190e8e2b9ae97cc912bd
Matthew Mongeau <halogenandtoast@gmail.com>
2020-10-08 18:50:30 -0500

    Cleanup

--- backend/arkham-core/library/Arkham/Types/Asset/Cards/ArcaneStudies2.hs
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/ArcaneStudies2.hs
@@ -9,7 +9,6 @@ import Arkham.Types.Classes
 import Arkham.Types.Message
 import Arkham.Types.Modifier
 import Arkham.Types.SkillType
-import Arkham.Types.Source
 import Arkham.Types.Target
 import qualified Arkham.Types.Window as Fast
 import ClassyPrelude
@@ -24,44 +23,28 @@ instance HasModifiersFor env investigator ArcaneStudies2 where
   getModifiersFor _ _ _ = pure []
 
 instance (IsInvestigator investigator) => HasActions env investigator ArcaneStudies2 where
-  getActions i (Fast.WhenSkillTest SkillWillpower) (ArcaneStudies2 Attrs {..})
-    | Just (getId () i) == assetInvestigator = pure
-      [ UseCardAbility
-          (getId () i)
-          (AssetSource assetId)
-          (AssetSource assetId)
-          Nothing
-          1
+  getActions i (Fast.WhenSkillTest SkillWillpower) (ArcaneStudies2 a)
+    | ownedBy a i = pure
+      [ UseCardAbility (getId () i) (toSource a) (toSource a) Nothing 1
       | resourceCount i > 0
       ]
-  getActions i (Fast.WhenSkillTest SkillIntellect) (ArcaneStudies2 Attrs {..})
-    | Just (getId () i) == assetInvestigator = pure
-      [ UseCardAbility
-          (getId () i)
-          (AssetSource assetId)
-          (AssetSource assetId)
-          Nothing
-          2
+  getActions i (Fast.WhenSkillTest SkillIntellect) (ArcaneStudies2 a)
+    | ownedBy a i = pure
+      [ UseCardAbility (getId () i) (toSource a) (toSource a) Nothing 2
       | resourceCount i > 0
       ]
   getActions _ _ _ = pure []
 
 instance (AssetRunner env) => RunMessage env ArcaneStudies2 where
   runMessage msg a@(ArcaneStudies2 attrs@Attrs {..}) = case msg of
-    UseCardAbility iid _ (AssetSource aid) _ 1 | aid == assetId ->
+    UseCardAbility iid _ source _ 1 | isSource attrs source ->
       a <$ unshiftMessages
         [ SpendResources iid 1
-        , AddModifiers
-          SkillTestTarget
-          (AssetSource aid)
-          [SkillModifier SkillWillpower 1]
+        , AddModifiers SkillTestTarget source [SkillModifier SkillWillpower 1]
         ]
-    UseCardAbility iid _ (AssetSource aid) _ 2 | aid == assetId ->
+    UseCardAbility iid _ source _ 2 | isSource attrs source ->
       a <$ unshiftMessages
         [ SpendResources iid 1
-        , AddModifiers
-          SkillTestTarget
-          (AssetSource aid)
-          [SkillModifier SkillIntellect 1]
+        , AddModifiers SkillTestTarget source [SkillModifier SkillIntellect 1]
         ]
     _ -> ArcaneStudies2 <$> runMessage msg attrs

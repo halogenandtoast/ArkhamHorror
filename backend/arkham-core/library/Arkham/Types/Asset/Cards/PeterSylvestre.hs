e086dbbf43552497c554c2d45fad262bb0e4b1ae
Matthew Mongeau <halogenandtoast@gmail.com>
2020-10-09 17:45:34 -0500

    Cleanup

--- backend/arkham-core/library/Arkham/Types/Asset/Cards/PeterSylvestre.hs
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/PeterSylvestre.hs
@@ -1,20 +1,10 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Arkham.Types.Asset.Cards.PeterSylvestre where
 
-import Arkham.Json
-import Arkham.Types.Ability
+import Arkham.Import
+
 import Arkham.Types.Asset.Attrs
 import Arkham.Types.Asset.Runner
-import Arkham.Types.AssetId
-import Arkham.Types.Classes
-import Arkham.Types.Message
-import Arkham.Types.Modifier
-import Arkham.Types.SkillType
-import Arkham.Types.Slot
-import Arkham.Types.Source
-import Arkham.Types.Window
-import ClassyPrelude
-import Lens.Micro
 
 newtype PeterSylvestre = PeterSylvestre Attrs
   deriving newtype (Show, ToJSON, FromJSON)
@@ -27,24 +17,25 @@ peterSylvestre uuid = PeterSylvestre $ (baseAttrs uuid "02033")
   }
 
 instance IsInvestigator investigator => HasModifiersFor env investigator PeterSylvestre where
-  getModifiersFor _ i (PeterSylvestre Attrs {..}) = pure
-    [ SkillModifier SkillAgility 1 | Just (getId () i) == assetInvestigator ]
+  getModifiersFor _ i (PeterSylvestre a) =
+    pure [ SkillModifier SkillAgility 1 | ownedBy a i ]
+
+ability :: Attrs -> Ability
+ability attrs =
+  mkAbility (toSource attrs) 1 (ReactionAbility (AfterEndTurn You))
 
 instance (ActionRunner env investigator) => HasActions env investigator PeterSylvestre where
-  getActions i (AfterEndTurn You) (PeterSylvestre Attrs {..})
-    | Just (getId () i) == assetInvestigator = do
-      usedAbilities <- map unUsedAbility <$> asks (getList ())
-      let
-        ability =
-          mkAbility (AssetSource assetId) 1 (ReactionAbility (AfterEndTurn You))
-      pure
-        [ ActivateCardAbilityAction (getId () i) ability
-        | (getId () i, ability) `notElem` usedAbilities && assetSanityDamage > 0
-        ]
+  getActions i (AfterEndTurn You) (PeterSylvestre a) | ownedBy a i = do
+    let ability' = (getId () i, ability a)
+    unused <- asks $ notElem ability' . map unUsedAbility . getList ()
+    pure
+      [ uncurry ActivateCardAbilityAction ability'
+      | unused && assetSanityDamage a > 0
+      ]
   getActions _ _ _ = pure []
 
 instance (AssetRunner env) => RunMessage env PeterSylvestre where
-  runMessage msg (PeterSylvestre attrs@Attrs {..}) = case msg of
-    UseCardAbility _ (AssetSource aid) _ 1 | aid == assetId -> do
+  runMessage msg (PeterSylvestre attrs) = case msg of
+    UseCardAbility _ source _ 1 | isSource attrs source ->
       pure $ PeterSylvestre $ attrs & sanityDamage -~ 1
     _ -> PeterSylvestre <$> runMessage msg attrs

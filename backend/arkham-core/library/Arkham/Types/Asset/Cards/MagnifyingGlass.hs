8fe30e9cc2f4cc05b5cd6d7334b3ea72e6172458
Matthew Mongeau <halogenandtoast@gmail.com>
2020-10-09 17:18:02 -0500

    Cleaning

--- backend/arkham-core/library/Arkham/Types/Asset/Cards/MagnifyingGlass.hs
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/MagnifyingGlass.hs
@@ -1,16 +1,11 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Arkham.Types.Asset.Cards.MagnifyingGlass where
 
-import Arkham.Json
+import Arkham.Import
+
 import qualified Arkham.Types.Action as Action
 import Arkham.Types.Asset.Attrs
 import Arkham.Types.Asset.Runner
-import Arkham.Types.AssetId
-import Arkham.Types.Classes
-import Arkham.Types.Modifier
-import Arkham.Types.SkillType
-import Arkham.Types.Slot
-import ClassyPrelude
 
 newtype MagnifyingGlass = MagnifyingGlass Attrs
   deriving newtype (Show, ToJSON, FromJSON)
@@ -20,10 +15,8 @@ magnifyingGlass uuid =
   MagnifyingGlass $ (baseAttrs uuid "01030") { assetSlots = [HandSlot] }
 
 instance IsInvestigator investigator => HasModifiersFor env investigator MagnifyingGlass where
-  getModifiersFor _ i (MagnifyingGlass Attrs {..}) = pure
-    [ ActionSkillModifier Action.Investigate SkillIntellect 1
-    | Just (getId () i) == assetInvestigator
-    ]
+  getModifiersFor _ i (MagnifyingGlass a) = pure
+    [ ActionSkillModifier Action.Investigate SkillIntellect 1 | ownedBy a i ]
 
 instance HasActions env investigator MagnifyingGlass where
   getActions i window (MagnifyingGlass x) = getActions i window x

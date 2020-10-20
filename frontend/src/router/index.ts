9944522e107f49f91af322e8a71e711716be71c2
Matthew Mongeau <halogenandtoast@gmail.com>
2020-10-20 16:50:13 -0500

    Sign up/Sign in updates

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -12,4 +12,22 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta && record.meta.requiresAuth)) {
+    if (localStorage.getItem('token') === null) {
+      next({ path: '/sign-in', query: { nextUrl: to.fullPath } });
+    } else {
+      next();
+    }
+  } else if (to.matched.some((record) => record.meta && record.meta.guest)) {
+    if (localStorage.getItem('token') === null) {
+      next();
+    } else {
+      next({ path: '/' });
+    }
+  } else {
+    next();
+  }
+});
+
 export default router
--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,22 +1,25 @@
 <template>
   <form @submit.prevent="authenticate">
-    <div>
-      <input
-        v-model="credentials.email"
-        type="email"
-        placeholder="Email"
-      />
-    </div>
-    <div>
-      <input
-        v-model="credentials.password"
-        type="password"
-        placeholder="Password"
-      />
-    </div>
-    <div>
-      <button>Submit</button>
-    </div>
+    <header><i class="secret"></i></header>
+    <section>
+      <div>
+        <input
+          v-model="credentials.email"
+          type="email"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <input
+          v-model="credentials.password"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
+      <div>
+        <button>Log in</button>
+      </div>
+    </section>
   </form>
 </template>
 
@@ -50,3 +53,64 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped lang="scss">
+form {
+  margin: 0 auto;
+  margin-top: 10vh;
+  width: 50vw;
+  max-width: 400px;
+}
+
+section {
+  background-color: #333;
+  border-radius: 3px;
+  padding: 10px;
+}
+
+header {
+  text-align: center;
+}
+
+input {
+  outline: 0;
+  border: 1px solid #000;
+  padding: 15px;
+  background: #F2F2F2;
+  width: 100%;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+}
+
+button {
+  outline: 0;
+  padding: 15px;
+  background: #6E8640;
+  text-transform: uppercase;
+  color: white;
+  border: 0;
+  width: 100%;
+  &:hover {
+    background: darken(#6E8640, 7%);
+  }
+}
+
+i.secret {
+  font-family: 'Arkham';
+  speak: none;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  font-size: 5em;
+  color: #333;
+  -webkit-font-smoothing: antialiased;
+  position: relative;
+
+  &:before {
+    font-family: "Arkham";
+    content: "\0048";
+  }
+}
+</style>

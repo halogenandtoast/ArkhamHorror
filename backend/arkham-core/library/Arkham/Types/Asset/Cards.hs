52181c7af3ca6ec898650f8a01ccd52a6bac9722
Matthew Mongeau <halogenandtoast@gmail.com>
2020-10-22 16:33:09 -0500

    Celaeno Fragments

--- backend/arkham-core/library/Arkham/Types/Asset/Cards.hs
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards.hs
@@ -16,6 +16,7 @@ import Arkham.Types.Asset.Cards.BookOfShadows3 as X
 import Arkham.Types.Asset.Cards.BulletproofVest3 as X
 import Arkham.Types.Asset.Cards.Burglary as X
 import Arkham.Types.Asset.Cards.CatBurgler1 as X
+import Arkham.Types.Asset.Cards.CelaenoFragments as X
 import Arkham.Types.Asset.Cards.DaisysToteBag as X
 import Arkham.Types.Asset.Cards.DigDeep as X
 import Arkham.Types.Asset.Cards.DigDeep2 as X

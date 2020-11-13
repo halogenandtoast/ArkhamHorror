2d122e923e2d38bc3bada484f8a37d324ebf6f7a
Matthew Mongeau <halogenandtoast@gmail.com>
2020-11-12 19:44:49 -0600

    Bedroom

---
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/Bedroom.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE UndecidableInstances #-}
+module Arkham.Types.Location.Cards.Bedroom where
+
+import Arkham.Import
+import qualified Arkham.Types.Action as Action
+import Arkham.Types.Location.Attrs
+import Arkham.Types.Location.Runner
+
+newtype Bedroom = Bedroom Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+bedroom :: Bedroom
+bedroom =
+  Bedroom $ baseAttrs "50015" "Bedroom" 2 (PerPlayer 1) Heart [T] mempty
+
+instance HasModifiersFor env Bedroom where
+  getModifiersFor = noModifiersFor
+
+instance ActionRunner env => HasActions env Bedroom where
+  getActions i window (Bedroom attrs) = getActions i window attrs
+
+instance (LocationRunner env) => RunMessage env Bedroom where
+  runMessage msg l@(Bedroom attrs) = case msg of
+    After (FailedSkillTest iid (Just Action.Investigate) _ target _)
+      | isTarget attrs target -> l <$ unshiftMessage (RandomDiscard iid)
+    _ -> Bedroom <$> runMessage msg attrs

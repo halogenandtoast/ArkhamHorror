c37dc653327b7ea4bf8cbb5e6c8d5238c1a8a048
Matthew Mongeau <halogenandtoast@gmail.com>
2020-11-12 22:04:37 -0600

    Corpse-Hungry Ghoul and Ghoul from the Depths

---
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/CorpseHungryGhoul.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE UndecidableInstances #-}
+module Arkham.Types.Enemy.Cards.CorpseHungryGhoul where
+
+import Arkham.Import
+
+import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Enemy.Runner
+
+newtype CorpseHungryGhoul = CorpseHungryGhoul Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+corpseHungryGhoul :: EnemyId -> CorpseHungryGhoul
+corpseHungryGhoul uuid =
+  CorpseHungryGhoul
+    $ baseAttrs uuid "50022"
+    $ (healthDamage .~ 2)
+    . (sanityDamage .~ 2)
+    . (fight .~ 4)
+    . (health .~ Static 3)
+    . (evade .~ 3)
+
+instance HasModifiersFor env CorpseHungryGhoul where
+  getModifiersFor = noModifiersFor
+
+instance HasModifiers env CorpseHungryGhoul where
+  getModifiers _ (CorpseHungryGhoul Attrs {..}) =
+    pure . concat . toList $ enemyModifiers
+
+instance ActionRunner env => HasActions env CorpseHungryGhoul where
+  getActions i window (CorpseHungryGhoul attrs) = getActions i window attrs
+
+instance (EnemyRunner env) => RunMessage env CorpseHungryGhoul where
+  runMessage msg e@(CorpseHungryGhoul attrs@Attrs {..}) = case msg of
+    InvestigatorDrawEnemy _ _ eid | eid == enemyId ->
+      e <$ spawnAt enemyId "50015"
+    _ -> CorpseHungryGhoul <$> runMessage msg attrs
---
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/GhoulFromTheDepths.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE UndecidableInstances #-}
+module Arkham.Types.Enemy.Cards.GhoulFromTheDepths where
+
+import Arkham.Import
+
+import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Enemy.Runner
+
+newtype GhoulFromTheDepths = GhoulFromTheDepths Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+ghoulFromTheDepths :: EnemyId -> GhoulFromTheDepths
+ghoulFromTheDepths uuid =
+  GhoulFromTheDepths
+    $ baseAttrs uuid "50023"
+    $ (healthDamage .~ 1)
+    . (sanityDamage .~ 1)
+    . (fight .~ 3)
+    . (health .~ Static 4)
+    . (evade .~ 2)
+
+instance HasModifiersFor env GhoulFromTheDepths where
+  getModifiersFor = noModifiersFor
+
+instance HasModifiers env GhoulFromTheDepths where
+  getModifiers _ (GhoulFromTheDepths Attrs {..}) =
+    pure . concat . toList $ enemyModifiers
+
+instance ActionRunner env => HasActions env GhoulFromTheDepths where
+  getActions i window (GhoulFromTheDepths attrs) = getActions i window attrs
+
+instance (EnemyRunner env) => RunMessage env GhoulFromTheDepths where
+  runMessage msg e@(GhoulFromTheDepths attrs@Attrs {..}) = case msg of
+    InvestigatorDrawEnemy _ _ eid | eid == enemyId ->
+      e <$ spawnAt enemyId "50016"
+    _ -> GhoulFromTheDepths <$> runMessage msg attrs

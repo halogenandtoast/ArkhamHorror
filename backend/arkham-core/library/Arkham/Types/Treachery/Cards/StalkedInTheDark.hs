68417c854cc5a8b1262ea5cb6342d5acd5a656b3
Matthew Mongeau <halogenandtoast@gmail.com>
2020-12-29 13:08:26 -0600

    Stalked in the Dark

---
+++ backend/arkham-core/library/Arkham/Types/Treachery/Cards/StalkedInTheDark.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Arkham.Types.Treachery.Cards.StalkedInTheDark
+  ( stalkedInTheDark
+  , StalkedInTheDark(..)
+  )
+where
+
+import Arkham.Import
+
+import Arkham.Types.Treachery.Attrs
+import Arkham.Types.Treachery.Runner
+
+newtype StalkedInTheDark = StalkedInTheDark Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+stalkedInTheDark :: TreacheryId -> a -> StalkedInTheDark
+stalkedInTheDark uuid _ = StalkedInTheDark $ baseAttrs uuid "02143"
+
+instance HasModifiersFor env StalkedInTheDark where
+  getModifiersFor = noModifiersFor
+
+instance HasActions env StalkedInTheDark where
+  getActions i window (StalkedInTheDark attrs) = getActions i window attrs
+
+instance TreacheryRunner env => RunMessage env StalkedInTheDark where
+  runMessage msg t@(StalkedInTheDark attrs@Attrs {..}) = case msg of
+    Revelation iid source | isSource attrs source -> do
+      mHuntingHorrorId <- fmap unStoryEnemyId <$> getId (CardCode "02141")
+      case mHuntingHorrorId of
+        Just eid -> do
+          lid <- getId @LocationId iid
+          iids <- getSetList @InvestigatorId lid
+          t <$ unshiftMessages
+            ([Ready (EnemyTarget eid), EnemyEngageInvestigator eid iid]
+            <> [ EnemyAttack iid' eid | iid' <- iids ]
+            )
+        Nothing ->
+          t <$ unshiftMessages [Surge iid source, Discard $ toTarget attrs]
+    _ -> StalkedInTheDark <$> runMessage msg attrs

756be1ddb0a6d07926ee4db90634884fc6d1ec00
Matthew Mongeau <halogenandtoast@gmail.com>
2020-12-31 17:19:30 -0600

    Update uses cost

--- backend/arkham-core/library/Arkham/Types/Asset/Cards/Rolands38Special.hs
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/Rolands38Special.hs
@@ -12,7 +12,7 @@ import qualified Arkham.Types.Action as Action
 import Arkham.Types.Asset.Attrs
 import Arkham.Types.Asset.Helpers
 import Arkham.Types.Asset.Runner
-import Arkham.Types.Asset.Uses (Uses(..), useCount)
+import Arkham.Types.Asset.Uses (Uses(..))
 import qualified Arkham.Types.Asset.Uses as Resource
 
 newtype Rolands38Special = Rolands38Special Attrs
@@ -29,26 +29,26 @@ fightAbility :: Attrs -> Ability
 fightAbility attrs = mkAbility
   (toSource attrs)
   1
-  (ActionAbility (Just Action.Fight) (ActionCost 1))
+  (ActionAbility
+    (Just Action.Fight)
+    (Costs [ActionCost 1, UseCost (assetId attrs) Resource.Ammo 1])
+  )
 
 instance ActionRunner env => HasActions env Rolands38Special where
   getActions iid window (Rolands38Special a) | ownedBy a iid = do
     fightAvailable <- hasFightActions iid window
-    pure
-      [ ActivateCardAbilityAction iid (fightAbility a)
-      | useCount (assetUses a) > 0 && fightAvailable
-      ]
+    pure [ ActivateCardAbilityAction iid (fightAbility a) | fightAvailable ]
   getActions _ _ _ = pure []
 
 instance AssetRunner env => RunMessage env Rolands38Special where
-  runMessage msg (Rolands38Special attrs) = case msg of
+  runMessage msg a@(Rolands38Special attrs) = case msg of
     InvestigatorPlayAsset _ aid _ _ | aid == assetId attrs ->
       Rolands38Special
         <$> runMessage msg (attrs & usesL .~ Uses Resource.Ammo 4)
     UseCardAbility iid source _ 1 | isSource attrs source -> do
       locationId <- getId @LocationId iid
       anyClues <- (/= 0) . unClueCount <$> getCount locationId
-      unshiftMessages
+      a <$ unshiftMessages
         [ CreateSkillTestEffect
           (EffectModifiers $ toModifiers
             attrs
@@ -60,5 +60,4 @@ instance AssetRunner env => RunMessage env Rolands38Special where
           (InvestigatorTarget iid)
         , ChooseFightEnemy iid source SkillCombat False
         ]
-      pure $ Rolands38Special $ attrs & usesL %~ Resource.use
     _ -> Rolands38Special <$> runMessage msg attrs

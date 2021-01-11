5cfd264c17664d02677f8b107483d6a21184410c
Matthew Mongeau <halogenandtoast@gmail.com>
2021-01-11 07:53:01 -0600

    Museum fixes

---
+++ frontend/src/arkham/components/EncounterDiscard.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div class="discards">
+    <div v-for="card in cards" :key="card.id" class="discard">
+      <img
+        class="card"
+        :src="image(card)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Game } from '@/arkham/types/Game';
+import { EncounterCardContents } from '@/arkham/types/Card';
+
+export default defineComponent({
+  props: {
+    game: { type: Object as () => Game, required: true },
+    cards: { type: Array as () => EncounterCardContents[], required: true }
+  },
+  setup() {
+    const image = (card: EncounterCardContents) => {
+      const { cardCode } = card;
+      return `/img/arkham/cards/${cardCode}.jpg`;
+    }
+
+    return { image }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.discards {
+  background: white;
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+  width: 100%;
+  overflow-x: auto;
+}
+
+.discard {
+  padding-left: 10px;
+  position: relative;
+  width: 110px;
+  &::after {
+    pointer-events: none;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #FFF;
+    opacity: .85;
+    mix-blend-mode: saturation;
+  }
+}
+</style>

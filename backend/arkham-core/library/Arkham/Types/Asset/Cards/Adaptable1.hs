80981a59a0fe4ebae51d9d258a32ee07678b5e00
Caleb Hearth <caleb@calebhearth.com>
2021-01-12 14:33:22 -0600

    Adaptable
    [![](https://arkhamdb.com/bundles/cards/02110.png)](https://arkhamdb.com/card/02110)

---
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/Adaptable1.hs
@@ -0,0 +1,25 @@
+module Arkham.Types.Asset.Cards.Adaptable1
+  ( adaptable1
+  , Adaptable1(..)
+  )
+where
+
+import Arkham.Import
+
+import Arkham.Types.Asset.Attrs
+
+newtype Adaptable1 = Adaptable1 Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+adaptable1 :: AssetId -> Adaptable1
+adaptable1 uuid =
+  Adaptable1 $ baseAttrs uuid "02110"
+
+instance HasActions env Adaptable1 where
+  getActions iid window (Adaptable1 attrs) = getActions iid window attrs
+
+instance HasModifiersFor env Adaptable1 where
+  getModifiersFor = noModifiersFor
+
+instance (HasQueue env, HasModifiersFor env ()) => RunMessage env Adaptable1 where
+  runMessage msg (Adaptable1 attrs) = Adaptable1 <$> runMessage msg attrs

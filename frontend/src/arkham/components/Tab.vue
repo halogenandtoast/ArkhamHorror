3329270843d6197174bcf39c98939bee74fb00be
Matthew Mongeau <halogenandtoast@gmail.com>
2021-01-13 20:45:35 -0600

    Fix tabs

--- frontend/src/arkham/components/Tab.vue
+++ frontend/src/arkham/components/Tab.vue
@@ -5,19 +5,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, computed } from 'vue';
+import { defineComponent, computed } from 'vue';
 
 export default defineComponent({
   props: {
     title: { type: String, required: true },
     index: { type: String, required: true },
+    selectedTab: { type: String, required: true },
     activePlayer: { type: Boolean, required: true },
     playerClass: { type: String, required: true },
     investigatorId: { type: String, required: true },
   },
   setup(props) {
-    const selectedTab = inject('selectedTab', props.index)
-    const isActive = computed(() => selectedTab == props.index)
+    const isActive = computed(() => props.selectedTab == props.index)
     return { isActive }
   }
 })

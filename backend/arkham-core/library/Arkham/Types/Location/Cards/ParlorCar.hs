82b10ac8ade39b300f1eadbc6d1f70cd66827741
Matthew Mongeau <halogenandtoast@gmail.com>
2021-01-15 17:18:41 -0600

    Parlor Car

---
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/ParlorCar.hs
@@ -0,0 +1,61 @@
+module Arkham.Types.Location.Cards.ParlorCar
+  ( parlorCar
+  , ParlorCar(..)
+  ) where
+
+import Arkham.Import
+
+import qualified Arkham.Types.EncounterSet as EncounterSet
+import Arkham.Types.Location.Attrs
+import Arkham.Types.Location.Helpers
+import Arkham.Types.Location.Runner
+import Arkham.Types.Trait
+
+newtype ParlorCar = ParlorCar Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+parlorCar :: ParlorCar
+parlorCar = ParlorCar $ base
+  { locationConnectsTo = setFromList [LeftOf, RightOf]
+  , locationVictory = Just 1
+  }
+ where
+  base = baseAttrs
+    "02174"
+    (Name "Parlor Car" Nothing)
+    EncounterSet.TheEssexCountyExpress
+    3
+    (PerPlayer 1)
+    NoSymbol
+    []
+    (singleton Train)
+
+instance HasCount ClueCount env LocationId => HasModifiersFor env ParlorCar where
+  getModifiersFor _ target (ParlorCar location@Attrs {..})
+    | isTarget location target = do
+      let
+        leftLocation =
+          fromJustNote "no left location" $ lookup LeftOf locationDirections
+      clueCount <- unClueCount <$> getCount leftLocation
+      pure $ toModifiers
+        location
+        [ Blocked | not locationRevealed && clueCount > 0 ]
+  getModifiersFor _ _ _ = pure []
+
+ability :: Attrs -> Ability
+ability attrs = mkAbility
+  (toSource attrs)
+  1
+  (ActionAbility Nothing $ Costs [ActionCost 1, ResourceCost 3])
+
+instance ActionRunner env => HasActions env ParlorCar where
+  getActions iid NonFast (ParlorCar attrs) | locationRevealed attrs =
+    withBaseActions iid NonFast attrs
+      $ pure [ ActivateCardAbilityAction iid (ability attrs) | iid `on` attrs ]
+  getActions iid window (ParlorCar attrs) = getActions iid window attrs
+
+instance LocationRunner env => RunMessage env ParlorCar where
+  runMessage msg l@(ParlorCar attrs@Attrs {..}) = case msg of
+    UseCardAbility iid source _ 1 _ | isSource attrs source ->
+      l <$ unshiftMessage (DiscoverCluesAtLocation iid locationId 1)
+    _ -> ParlorCar <$> runMessage msg attrs
---
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/SleepingCar.hs
@@ -0,0 +1,61 @@
+module Arkham.Types.Location.Cards.SleepingCar
+  ( sleepingCar
+  , SleepingCar(..)
+  ) where
+
+import Arkham.Import
+
+import qualified Arkham.Types.EncounterSet as EncounterSet
+import Arkham.Types.Location.Attrs
+import Arkham.Types.Location.Helpers
+import Arkham.Types.Location.Runner
+import Arkham.Types.ScenarioLogKey
+import Arkham.Types.Trait
+
+newtype SleepingCar = SleepingCar Attrs
+  deriving newtype (Show, ToJSON, FromJSON)
+
+sleepingCar :: SleepingCar
+sleepingCar = SleepingCar
+  $ base { locationConnectsTo = setFromList [LeftOf, RightOf] }
+ where
+  base = baseAttrs
+    "02172"
+    (Name "Sleeping Car" Nothing)
+    EncounterSet.TheEssexCountyExpress
+    4
+    (Static 1)
+    NoSymbol
+    []
+    (singleton Train)
+
+instance HasCount ClueCount env LocationId => HasModifiersFor env SleepingCar where
+  getModifiersFor _ target (SleepingCar location@Attrs {..})
+    | isTarget location target = do
+      let
+        leftLocation =
+          fromJustNote "no left location" $ lookup LeftOf locationDirections
+      clueCount <- unClueCount <$> getCount leftLocation
+      pure $ toModifiers
+        location
+        [ Blocked | not locationRevealed && clueCount > 0 ]
+  getModifiersFor _ _ _ = pure []
+
+ability :: Attrs -> Ability
+ability attrs =
+  (mkAbility (toSource attrs) 1 (ActionAbility Nothing $ ActionCost 1))
+    { abilityLimit = GroupLimit PerGame 1
+    }
+
+instance ActionRunner env => HasActions env SleepingCar where
+  getActions iid NonFast (SleepingCar attrs) | locationRevealed attrs =
+    withBaseActions iid NonFast attrs
+      $ pure [ ActivateCardAbilityAction iid (ability attrs) | iid `on` attrs ]
+  getActions iid window (SleepingCar attrs) = getActions iid window attrs
+
+instance LocationRunner env => RunMessage env SleepingCar where
+  runMessage msg l@(SleepingCar attrs) = case msg of
+    UseCardAbility iid source _ 1 _ | isSource attrs source ->
+      l <$ unshiftMessages
+        [TakeResources iid 3 False, Remember StolenAPassengersLuggage]
+    _ -> SleepingCar <$> runMessage msg attrs

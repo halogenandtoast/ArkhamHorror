0c3d4fe5d3b3b547f278ee9449acfac9bdd8eec4
Matthew Mongeau <halogenandtoast@gmail.com>
2021-01-27 16:32:23 -0600

    Redundant exhaust and fix art gallery

--- backend/arkham-core/library/Arkham/Types/Location/Cards/ArtGallery.hs
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/ArtGallery.hs
@@ -1,8 +1,7 @@
 module Arkham.Types.Location.Cards.ArtGallery
   ( artGallery
   , ArtGallery(..)
-  )
-where
+  ) where
 
 import Arkham.Import
 
@@ -37,6 +36,6 @@ instance ActionRunner env => HasActions env ArtGallery where
 
 instance LocationRunner env => RunMessage env ArtGallery where
   runMessage msg l@(ArtGallery attrs@Attrs {..}) = case msg of
-    FailedSkillTest iid (Just Action.Investigate) _ (SkillTestInitiatorTarget _) _ _
+    After (FailedSkillTest iid (Just Action.Investigate) _ (SkillTestInitiatorTarget _) _ _)
       -> l <$ unshiftMessage (SpendResources iid 2)
     _ -> ArtGallery <$> runMessage msg attrs

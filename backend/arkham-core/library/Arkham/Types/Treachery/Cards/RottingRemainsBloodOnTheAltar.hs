99fc1c97243325383829808763b4c927b86b2285
Matthew Mongeau <halogenandtoast@gmail.com>
2021-02-01 09:03:24 -0600

    More rotting remains

---
+++ backend/arkham-core/library/Arkham/Types/Treachery/Cards/RottingRemainsBloodOnTheAltar.hs
@@ -0,0 +1,23 @@
+module Arkham.Types.Treachery.Cards.RottingRemainsBloodOnTheAltar
+  ( rottingRemainsBloodOnTheAltar
+  , RottingRemainsBloodOnTheAltar(..)
+  )
+where
+
+import Arkham.Import
+
+import Arkham.Types.Treachery.Attrs
+import Arkham.Types.Treachery.Cards.RottingRemains
+import Arkham.Types.Treachery.Runner
+
+newtype RottingRemainsBloodOnTheAltar = RottingRemainsBloodOnTheAltar RottingRemains
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasModifiersFor env, HasActions env)
+
+rottingRemainsBloodOnTheAltar
+  :: TreacheryId -> a -> RottingRemainsBloodOnTheAltar
+rottingRemainsBloodOnTheAltar uuid _ =
+  RottingRemainsBloodOnTheAltar . RottingRemains $ baseAttrs uuid "02223"
+
+instance TreacheryRunner env => RunMessage env RottingRemainsBloodOnTheAltar where
+  runMessage msg (RottingRemainsBloodOnTheAltar inner) =
+    RottingRemainsBloodOnTheAltar <$> runMessage msg inner

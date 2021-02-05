fb9a348582d8bf88bc2cc94abf8faea37e99bd83
Matthew Mongeau <halogenandtoast@gmail.com>
2021-02-05 16:52:49 -0600

    Bishop's Brook

--- backend/arkham-core/library/Arkham/Types/Location/Cards/BishopsBrook_203.hs
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/BishopsBrook_203.hs
@@ -6,6 +6,7 @@ module Arkham.Types.Location.Cards.BishopsBrook_203
 import Arkham.Import
 
 import qualified Arkham.Types.EncounterSet as EncounterSet
+import Arkham.Types.Game.Helpers
 import Arkham.Types.Location.Attrs
 import Arkham.Types.Location.Runner
 import Arkham.Types.Trait
@@ -25,11 +26,33 @@ bishopsBrook_203 = BishopsBrook_203 $ baseAttrs
   [Dunwich]
 
 instance HasModifiersFor env BishopsBrook_203 where
-  getModifiersFor = noModifiersFor
+  getModifiersFor (InvestigatorSource iid) target (BishopsBrook_203 attrs@LocationAttrs {..})
+    | isTarget attrs target
+    = pure $ toModifiers
+      attrs
+      [ Blocked
+      | iid `notElem` locationInvestigators && not (null locationInvestigators)
+      ]
+  getModifiersFor _ _ _ = pure []
+
+ability :: LocationAttrs -> Ability
+ability a = base { abilityLimit = GroupLimit PerGame 1 }
+  where base = mkAbility (toSource a) 1 (FastAbility Free)
 
 instance ActionRunner env => HasActions env BishopsBrook_203 where
+  getActions iid FastPlayerWindow (BishopsBrook_203 attrs@LocationAttrs {..}) =
+    withBaseActions iid FastPlayerWindow attrs $ pure
+      [ ActivateCardAbilityAction iid (ability attrs)
+      | locationClues == 0 && locationRevealed
+      ]
   getActions iid window (BishopsBrook_203 attrs) = getActions iid window attrs
 
 instance LocationRunner env => RunMessage env BishopsBrook_203 where
-  runMessage msg (BishopsBrook_203 attrs) =
-    BishopsBrook_203 <$> runMessage msg attrs
+  runMessage msg (BishopsBrook_203 attrs) = case msg of
+    UseCardAbility iid source _ 1 _ | isSource attrs source ->
+      case locationCardsUnderneath attrs of
+        (EncounterCard card : rest) -> do
+          unshiftMessage (InvestigatorDrewEncounterCard iid card)
+          pure $ BishopsBrook_203 $ attrs & cardsUnderneathL .~ rest
+        _ -> throwIO $ InvalidState "Not expecting a player card"
+    _ -> BishopsBrook_203 <$> runMessage msg attrs

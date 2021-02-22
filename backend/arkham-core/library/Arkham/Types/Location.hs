28e649a983cf0b63533a116677b2ba1dd3e71537
Matthew Mongeau <halogenandtoast@gmail.com>
2021-02-22 09:48:40 -0600

    Dimensional Gap

--- backend/arkham-core/library/Arkham/Types/Location.hs
+++ backend/arkham-core/library/Arkham/Types/Location.hs
@@ -118,6 +118,7 @@ data Location
   | WhateleyRuins_251' WhateleyRuins_251
   | DevilsHopYard_252' DevilsHopYard_252
   | DevilsHopYard_253' DevilsHopYard_253
+  | DimensionalGap' DimensionalGap
   | ATearInThePath' ATearInThePath
   | UprootedWoods' UprootedWoods
   | LostMemories' LostMemories
@@ -369,6 +370,7 @@ allLocations = mapFrom
   , WhateleyRuins_251' whateleyRuins_251
   , DevilsHopYard_252' devilsHopYard_252
   , DevilsHopYard_253' devilsHopYard_253
+  , DimensionalGap' dimensionalGap
   , ATearInThePath' aTearInThePath
   , UprootedWoods' uprootedWoods
   , LostMemories' lostMemories
---
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/DimensionalGap.hs
@@ -0,0 +1,63 @@
+module Arkham.Types.Location.Cards.DimensionalGap
+  ( dimensionalGap
+  , DimensionalGap(..)
+  ) where
+
+import Arkham.Prelude
+
+import Arkham.Types.Ability
+import Arkham.Types.Card
+import Arkham.Types.Card.EncounterCardMatcher
+import Arkham.Types.Classes
+import qualified Arkham.Types.EncounterSet as EncounterSet
+import Arkham.Types.GameValue
+import Arkham.Types.Location.Attrs
+import Arkham.Types.Location.Runner
+import Arkham.Types.LocationSymbol
+import Arkham.Types.Message
+import Arkham.Types.Name
+import Arkham.Types.Trait
+import Arkham.Types.Window
+
+newtype DimensionalGap = DimensionalGap LocationAttrs
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+dimensionalGap :: DimensionalGap
+dimensionalGap =
+  DimensionalGap
+    $ base
+    & (revealedSymbolL .~ T)
+    & (revealedConnectedSymbolsL .~ setFromList [Square, Moon])
+ where
+  base = baseAttrs
+    "02289"
+    (Name "Dimensional Gap" Nothing)
+    EncounterSet.WhereDoomAwaits
+    3
+    (PerPlayer 1)
+    NoSymbol
+    []
+    [Dunwich, Woods, Altered]
+
+instance HasModifiersFor env DimensionalGap where
+  getModifiersFor = noModifiersFor
+
+forcedAbility :: LocationAttrs -> Ability
+forcedAbility a = mkAbility (toSource a) 1 ForcedAbility
+
+instance ActionRunner env => HasActions env DimensionalGap where
+  getActions iid (AfterRevealLocation You) (DimensionalGap attrs)
+    | iid `on` attrs = pure
+      [ActivateCardAbilityAction iid (forcedAbility attrs)]
+  getActions iid window (DimensionalGap attrs) = getActions iid window attrs
+
+instance LocationRunner env => RunMessage env DimensionalGap where
+  runMessage msg l@(DimensionalGap attrs) = case msg of
+    UseCardAbility _ source _ 1 _ | isSource attrs source -> do
+      l <$ unshiftMessage
+        (DiscardEncounterUntilFirst source
+        $ EncounterCardMatchByType (EnemyType, Nothing)
+        )
+    RequestedEncounterCard source (Just ec) | isSource attrs source ->
+      l <$ unshiftMessage (SpawnEnemyAt (EncounterCard ec) (toId attrs))
+    _ -> DimensionalGap <$> runMessage msg attrs

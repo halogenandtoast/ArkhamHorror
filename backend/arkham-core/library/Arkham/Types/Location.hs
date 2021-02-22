241a3680f1fd6cf9c113aa5b023a15c0ca91f731
Matthew Mongeau <halogenandtoast@gmail.com>
2021-02-22 09:36:00 -0600

    A Tear in the Path

--- backend/arkham-core/library/Arkham/Types/Location.hs
+++ backend/arkham-core/library/Arkham/Types/Location.hs
@@ -118,6 +118,7 @@ data Location
   | WhateleyRuins_251' WhateleyRuins_251
   | DevilsHopYard_252' DevilsHopYard_252
   | DevilsHopYard_253' DevilsHopYard_253
+  | ATearInThePath' ATearInThePath
   | UprootedWoods' UprootedWoods
   | LostMemories' LostMemories
   | StudyAberrantGateway' StudyAberrantGateway
@@ -368,6 +369,7 @@ allLocations = mapFrom
   , WhateleyRuins_251' whateleyRuins_251
   , DevilsHopYard_252' devilsHopYard_252
   , DevilsHopYard_253' devilsHopYard_253
+  , ATearInThePath' aTearInThePath
   , UprootedWoods' uprootedWoods
   , LostMemories' lostMemories
   , StudyAberrantGateway' studyAberrantGateway

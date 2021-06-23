8c2680900dd5be1e07821017d15f66103d83b625
Matthew Mongeau <halogenandtoast@gmail.com>
2021-06-22 21:17:23 -0500

    General cleanup

--- backend/arkham-core/library/Arkham/Types/Classes/RunMessage.hs
+++ backend/arkham-core/library/Arkham/Types/Classes/RunMessage.hs
@@ -8,24 +8,25 @@ import Arkham.Prelude hiding (to)
 
 import Arkham.Types.Classes.HasQueue
 import Arkham.Types.Message
+import Control.Monad.State
 import GHC.Generics
 
-class (HasQueue env) => RunMessage1 env f where
-  runMessage1 :: (MonadIO m, MonadReader env m, MonadRandom m) => Message -> f p -> m (f p)
+class RunMessage1 env f where
+  runMessage1 :: (MonadIO m, MonadReader env m, MonadRandom m, HasQueue env) => Message -> f p -> m (f p)
 
-instance (HasQueue env, RunMessage1 env f) => RunMessage1 env (M1 i c f) where
+instance RunMessage1 env f => RunMessage1 env (M1 i c f) where
   runMessage1 msg (M1 x) = M1 <$> runMessage1 msg x
 
-instance (HasQueue env, RunMessage1 env l, RunMessage1 env r) => RunMessage1 env (l :+: r) where
+instance (RunMessage1 env l, RunMessage1 env r) => RunMessage1 env (l :+: r) where
   runMessage1 msg (L1 x) = L1 <$> runMessage1 msg x
   runMessage1 msg (R1 x) = R1 <$> runMessage1 msg x
 
-instance (HasQueue env, RunMessage env p) => RunMessage1 env (K1 R p) where
+instance RunMessage env p => RunMessage1 env (K1 R p) where
   runMessage1 msg (K1 x) = K1 <$> runMessage msg x
 
-class (HasQueue env) => RunMessage env a where
-  runMessage :: (HasCallStack, MonadIO m, MonadRandom m, MonadReader env m) => Message -> a -> m a
-  default runMessage :: (Generic a, RunMessage1 env (Rep a), MonadIO m, MonadRandom m, MonadReader env m) => Message -> a -> m a
+class RunMessage env a where
+  runMessage :: (MonadIO m, MonadRandom m, MonadReader env m, HasQueue env) => Message -> a -> m a
+  default runMessage :: (Generic a, RunMessage1 env (Rep a), MonadIO m, MonadRandom m, MonadReader env m, HasQueue env) => Message -> a -> m a
   runMessage = defaultRunMessage
 
 defaultRunMessage
@@ -34,8 +35,12 @@ defaultRunMessage
      , MonadIO m
      , MonadRandom m
      , MonadReader env m
+     , HasQueue env
      )
   => Message
   -> a
   -> m a
 defaultRunMessage msg = fmap to . runMessage1 msg . from
+
+runMessageState :: Monad m => (s -> StateT s m a) -> s -> m s
+runMessageState f s = execStateT (f s) s
--- backend/arkham-core/library/Arkham/Types/Investigator/Cards/SkidsOToole.hs
+++ backend/arkham-core/library/Arkham/Types/Investigator/Cards/SkidsOToole.hs
@@ -9,15 +9,15 @@ import Arkham.Types.Ability
 import Arkham.Types.Classes
 import Arkham.Types.ClassSymbol
 import Arkham.Types.Cost
-import Arkham.Types.Message
-import Arkham.Types.Source
-import Arkham.Types.Target
-import Arkham.Types.Token
-import Arkham.Types.Window
 import Arkham.Types.Investigator.Attrs
 import Arkham.Types.Investigator.Runner
+import Arkham.Types.Message
+import Arkham.Types.Source
 import Arkham.Types.Stats
+import Arkham.Types.Target
+import Arkham.Types.Token
 import Arkham.Types.Trait
+import Arkham.Types.Window
 
 newtype SkidsOToole = SkidsOToole InvestigatorAttrs
   deriving newtype (Show, ToJSON, FromJSON, Entity)
@@ -45,7 +45,7 @@ ability :: InvestigatorAttrs -> Ability
 ability attrs = base { abilityLimit = PlayerLimit PerTurn 1 }
   where base = mkAbility (toSource attrs) 1 (FastAbility $ ResourceCost 2)
 
-instance InvestigatorRunner env => HasActions env SkidsOToole where
+instance HasActions env SkidsOToole where
   getActions iid (DuringTurn You) (SkidsOToole a@InvestigatorAttrs {..})
     | iid == investigatorId = pure [ActivateCardAbilityAction iid (ability a)]
   getActions _ _ _ = pure []
@@ -56,7 +56,7 @@ instance HasTokenValue env SkidsOToole where
     $ TokenValue ElderSign (PositiveModifier 2)
   getTokenValue (SkidsOToole attrs) iid token = getTokenValue attrs iid token
 
-instance (InvestigatorRunner env) => RunMessage env SkidsOToole where
+instance InvestigatorRunner env => RunMessage env SkidsOToole where
   runMessage msg i@(SkidsOToole attrs@InvestigatorAttrs {..}) = case msg of
     UseCardAbility _ (InvestigatorSource iid) _ 1 _ | iid == investigatorId ->
       pure . SkidsOToole $ attrs & remainingActionsL +~ 1

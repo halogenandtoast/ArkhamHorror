39bd89a5b0f858919a5afad82c5c14fd424f9518
Goose <halogenandtoast@gmail.com>
2021-07-07 17:56:59 -0500

    Relic Hunter (3)

---
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/RelicHunter3.hs
@@ -0,0 +1,34 @@
+module Arkham.Types.Asset.Cards.RelicHunter3
+  ( relicHunter3
+  , RelicHunter3(..)
+  ) where
+
+import Arkham.Prelude
+
+import qualified Arkham.Asset.Cards as Cards
+import Arkham.Types.Asset.Attrs
+import Arkham.Types.Classes
+import Arkham.Types.Message
+import Arkham.Types.Slot
+
+newtype RelicHunter3 = RelicHunter3 AssetAttrs
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+relicHunter3 :: AssetCard RelicHunter3
+relicHunter3 = asset RelicHunter3 Cards.relicHunter3
+
+instance HasActions env RelicHunter3 where
+  getActions iid window (RelicHunter3 attrs) = getActions iid window attrs
+
+instance HasModifiersFor env RelicHunter3 where
+  getModifiersFor = noModifiersFor
+
+slot :: AssetAttrs -> Slot
+slot attrs = Slot (toSource attrs) Nothing
+
+instance HasModifiersFor env () => RunMessage env RelicHunter3 where
+  runMessage msg (RelicHunter3 attrs) = case msg of
+    InvestigatorPlayAsset iid aid _ _ | aid == assetId attrs -> do
+      push $ AddSlot iid AccessorySlot (slot attrs)
+      RelicHunter3 <$> runMessage msg attrs
+    _ -> RelicHunter3 <$> runMessage msg attrs

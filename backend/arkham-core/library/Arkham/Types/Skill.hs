2b2c56fc3768a0a61c6fbde1e192fa7f2f810131
Goose <halogenandtoast@gmail.com>
2021-07-08 18:31:47 -0500

    Emergency Cache (2), Rise to the Occasion

--- backend/arkham-core/library/Arkham/Types/Skill.hs
+++ backend/arkham-core/library/Arkham/Types/Skill.hs
@@ -24,6 +24,7 @@ data Skill
   | Opportunist' Opportunist
   | Overpower' Overpower
   | Perception' Perception
+  | RiseToTheOccasion' RiseToTheOccasion
   | SurvivalInstinct' SurvivalInstinct
   | TrueUnderstanding' TrueUnderstanding
   | UnexpectedCourage' UnexpectedCourage
@@ -71,6 +72,7 @@ allSkills = mapFromList $ map
   , Opportunist' <$> opportunist
   , Overpower' <$> overpower
   , Perception' <$> perception
+  , RiseToTheOccasion' <$> riseToTheOccasion
   , SurvivalInstinct' <$> survivalInstinct
   , TrueUnderstanding' <$> trueUnderstanding
   , UnexpectedCourage' <$> unexpectedCourage
---
+++ backend/arkham-core/library/Arkham/Types/Skill/Cards/RiseToTheOccasion.hs
@@ -0,0 +1,27 @@
+module Arkham.Types.Skill.Cards.RiseToTheOccasion
+  ( riseToTheOccasion
+  , RiseToTheOccasion(..)
+  ) where
+
+import Arkham.Prelude
+
+import qualified Arkham.Skill.Cards as Cards
+import Arkham.Types.Classes
+import Arkham.Types.Skill.Attrs
+import Arkham.Types.Skill.Runner
+
+newtype RiseToTheOccasion = RiseToTheOccasion SkillAttrs
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+riseToTheOccasion :: SkillCard RiseToTheOccasion
+riseToTheOccasion = skill RiseToTheOccasion Cards.riseToTheOccasion
+
+instance HasModifiersFor env RiseToTheOccasion where
+  getModifiersFor = noModifiersFor
+
+instance HasActions env RiseToTheOccasion where
+  getActions iid window (RiseToTheOccasion attrs) = getActions iid window attrs
+
+instance SkillRunner env => RunMessage env RiseToTheOccasion where
+  runMessage msg (RiseToTheOccasion attrs) =
+    RiseToTheOccasion <$> runMessage msg attrs

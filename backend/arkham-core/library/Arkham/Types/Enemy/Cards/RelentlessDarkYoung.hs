77e0f64dedabb2a79ad22d64fcff89a7dd872103
Matthew Mongeau <halogenandtoast@gmail.com>
2021-08-24 17:11:06 -0500

    Fixes

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/RelentlessDarkYoung.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/RelentlessDarkYoung.hs
@@ -6,16 +6,20 @@ module Arkham.Types.Enemy.Cards.RelentlessDarkYoung
 import Arkham.Prelude
 
 import qualified Arkham.Enemy.Cards as Cards
+import Arkham.Types.Ability
 import Arkham.Types.Classes
 import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Enemy.Helpers
 import Arkham.Types.Enemy.Runner
+import Arkham.Types.Matcher
 import Arkham.Types.Message
 import Arkham.Types.Prey
 import Arkham.Types.SkillType
+import qualified Arkham.Types.Timing as Timing
 
 newtype RelentlessDarkYoung = RelentlessDarkYoung EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor env)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities env)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 relentlessDarkYoung :: EnemyCard RelentlessDarkYoung
 relentlessDarkYoung = enemyWith
@@ -25,8 +29,13 @@ relentlessDarkYoung = enemyWith
   (2, 1)
   (preyL .~ LowestSkill SkillAgility)
 
+instance HasAbilities env RelentlessDarkYoung where
+  getAbilities i window (RelentlessDarkYoung attrs) =
+    withBaseAbilities i window attrs
+      $ pure [mkAbility attrs 1 $ ForcedAbility $ RoundEnds Timing.When]
+
 instance EnemyRunner env => RunMessage env RelentlessDarkYoung where
-  runMessage msg (RelentlessDarkYoung attrs) = case msg of
-    EndRound ->
-      pure $ RelentlessDarkYoung $ attrs & damageL %~ max 0 . subtract 2
+  runMessage msg e@(RelentlessDarkYoung attrs) = case msg of
+    UseCardAbility _ source _ 1 _ | isSource attrs source ->
+      e <$ push (HealDamage (toTarget attrs) 2)
     _ -> RelentlessDarkYoung <$> runMessage msg attrs

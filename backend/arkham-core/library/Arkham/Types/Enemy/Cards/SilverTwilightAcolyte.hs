ab0dc403e46962878aaf1c224114829d6faa224c
Goose <halogenandtoast@gmail.com>
2021-08-24 22:49:33 -0500

    Weaknesses

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/SilverTwilightAcolyte.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/SilverTwilightAcolyte.hs
@@ -6,15 +6,19 @@ module Arkham.Types.Enemy.Cards.SilverTwilightAcolyte
 import Arkham.Prelude
 
 import qualified Arkham.Enemy.Cards as Cards
+import Arkham.Types.Ability
 import Arkham.Types.Classes
 import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Enemy.Helpers
 import Arkham.Types.Enemy.Runner
-import Arkham.Types.Message
+import Arkham.Types.Matcher
+import Arkham.Types.Message hiding (EnemyAttacks)
 import Arkham.Types.Prey
+import qualified Arkham.Types.Timing as Timing
 
 newtype SilverTwilightAcolyte = SilverTwilightAcolyte EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor env)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities env)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 silverTwilightAcolyte :: EnemyCard SilverTwilightAcolyte
 silverTwilightAcolyte = enemyWith
@@ -24,9 +28,18 @@ silverTwilightAcolyte = enemyWith
   (1, 0)
   (preyL .~ SetToBearer)
 
+instance HasAbilities env SilverTwilightAcolyte where
+  getAbilities iid window (SilverTwilightAcolyte a) =
+    withBaseAbilities iid window a $ pure
+      [ mkAbility a 1
+        $ ForcedAbility
+        $ EnemyAttacks Timing.After Anyone
+        $ EnemyWithId
+        $ toId a
+      ]
+
 instance EnemyRunner env => RunMessage env SilverTwilightAcolyte where
-  runMessage msg (SilverTwilightAcolyte attrs@EnemyAttrs {..}) = case msg of
-    EnemyAttack _ eid _ | eid == enemyId -> do
-      push PlaceDoomOnAgenda
-      SilverTwilightAcolyte <$> runMessage msg attrs
+  runMessage msg e@(SilverTwilightAcolyte attrs) = case msg of
+    UseCardAbility _ source _ 1 _ | isSource attrs source -> do
+      e <$ push PlaceDoomOnAgenda
     _ -> SilverTwilightAcolyte <$> runMessage msg attrs

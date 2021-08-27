9d5cb75070ad76111abef2593bdcca8d94b587a7
Matthew Mongeau <halogenandtoast@gmail.com>
2021-08-27 07:06:40 -0500

    Gravedigger's Shovel

---
+++ backend/arkham-core/library/Arkham/Types/Asset/Cards/GravediggersShovel.hs
@@ -0,0 +1,47 @@
+module Arkham.Types.Asset.Cards.GravediggersShovel
+  ( gravediggersShovel
+  , GravediggersShovel(..)
+  ) where
+
+import Arkham.Prelude
+
+import qualified Arkham.Asset.Cards as Cards
+import Arkham.Types.Ability
+import qualified Arkham.Types.Action as Action
+import Arkham.Types.Asset.Attrs
+import Arkham.Types.Asset.Helpers
+import Arkham.Types.Asset.Runner
+import Arkham.Types.Classes
+import Arkham.Types.Cost
+import Arkham.Types.Criteria
+import Arkham.Types.Modifier
+import Arkham.Types.SkillType
+import Arkham.Types.Target
+
+newtype GravediggersShovel = GravediggersShovel AssetAttrs
+  deriving anyclass (IsAsset, HasModifiersFor env)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+gravediggersShovel :: AssetCard GravediggersShovel
+gravediggersShovel = hand GravediggersShovel Cards.gravediggersShovel
+
+instance HasAbilities env GravediggersShovel where
+  getAbilities _ _ (GravediggersShovel x) = pure
+    [ restrictedAbility x 1 OwnsThis
+      $ ActionAbility (Just Action.Fight) (ActionCost 1)
+    , restrictedAbility x 2 OwnsThis $ ActionAbility Nothing $ Costs
+      [ActionCost 1, DiscardCost (toTarget x)]
+    ]
+
+instance AssetRunner env => RunMessage env GravediggersShovel where
+  runMessage msg (GravediggersShovel attrs) = case msg of
+    UseCardAbility iid source _ 1 _ | isSource attrs source -> a <$ pushAll
+      [ skillTestModifiers
+        attrs
+        (InvestigatorTarget iid)
+        (SkillModifier SkillCombat 2)
+      , ChooseFightEnemy iid source SkillCombat mempty False
+      ]
+    UseCardAbility iid source _ 2 _ | isSource attrs source ->
+      a <$ push (InvestigatorDiscoverCluesAtTheirLocation iid 1 Nothing)
+    _ -> GravediggersShovel <$> runMessage msg attrs

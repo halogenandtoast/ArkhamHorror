fef1d1d941d194639462b8137b25adddccadab93
Goose <halogenandtoast@gmail.com>
2021-08-29 16:45:52 -0500

    Administration Building

--- backend/arkham-core/library/Arkham/Types/Location/Cards/AdministrationBuilding.hs
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/AdministrationBuilding.hs
@@ -3,14 +3,18 @@ module Arkham.Types.Location.Cards.AdministrationBuilding where
 import Arkham.Prelude
 
 import qualified Arkham.Location.Cards as Cards (administrationBuilding)
+import Arkham.Types.Ability
 import Arkham.Types.Classes
+import Arkham.Types.Criteria
+import Arkham.Types.Game.Helpers
 import Arkham.Types.GameValue
 import Arkham.Types.Location.Attrs
-import Arkham.Types.Matcher hiding (RevealLocation)
-import Arkham.Types.Message
+import Arkham.Types.Matcher
+import Arkham.Types.Message hiding (RevealLocation)
+import qualified Arkham.Types.Timing as Timing
 
 newtype AdministrationBuilding = AdministrationBuilding LocationAttrs
-  deriving anyclass IsLocation
+  deriving anyclass (IsLocation, HasModifiersFor env)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 administrationBuilding :: LocationCard AdministrationBuilding
@@ -22,17 +26,23 @@ administrationBuilding = location
   Circle
   [Plus, T]
 
-instance HasModifiersFor env AdministrationBuilding
-
 instance HasAbilities env AdministrationBuilding where
-  getAbilities i window (AdministrationBuilding attrs) =
-    getAbilities i window attrs
+  getAbilities i window (AdministrationBuilding x) =
+    withBaseAbilities i window x $ pure $ if locationRevealed x
+      then
+        [ restrictedAbility x 1 Here
+        $ ForcedAbility
+        $ RevealLocation Timing.After You
+        $ LocationWithId
+        $ toId x
+        , restrictedAbility x 2 Here $ ForcedAbility $ TurnEnds Timing.When You
+        ]
+      else []
 
 instance (LocationRunner env) => RunMessage env AdministrationBuilding where
   runMessage msg l@(AdministrationBuilding attrs) = case msg of
-    RevealLocation _ lid | lid == locationId attrs -> do
-      push $ PlaceLocationMatching (LocationWithTitle "Faculty Offices")
-      AdministrationBuilding <$> runMessage msg attrs
-    EndTurn iid | iid `elem` locationInvestigators attrs ->
+    UseCardAbility _ source _ 1 _ | isSource attrs source ->
+      l <$ push (PlaceLocationMatching $ LocationWithTitle "Faculty Offices")
+    UseCardAbility iid source _ 2 _ | isSource attrs source ->
       l <$ push (DiscardTopOfDeck iid 1 Nothing)
     _ -> AdministrationBuilding <$> runMessage msg attrs

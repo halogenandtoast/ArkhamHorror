aa8d4c21a464d187f1cfc11c9168b465eb01cccd
Matthew Mongeau <halogenandtoast@gmail.com>
2021-09-02 08:03:21 -0500

    Golden

--- backend/arkham-core/library/Arkham/Types/Treachery/Cards/Atychiphobia.hs
+++ backend/arkham-core/library/Arkham/Types/Treachery/Cards/Atychiphobia.hs
@@ -12,8 +12,8 @@ import Arkham.Types.Cost
 import Arkham.Types.Criteria
 import Arkham.Types.Matcher
 import Arkham.Types.Message
-import Arkham.Types.Source
 import Arkham.Types.Target
+import qualified Arkham.Types.Timing as Timing
 import Arkham.Types.Treachery.Attrs
 import Arkham.Types.Treachery.Runner
 
@@ -26,26 +26,21 @@ atychiphobia = treachery Atychiphobia Cards.atychiphobia
 
 instance HasAbilities env Atychiphobia where
   getAbilities _ _ (Atychiphobia a) = pure
-    [ restrictedAbility a 1 (InThreatAreaOf $ InvestigatorAt YourLocation)
-      $ ActionAbility Nothing
-      $ ActionCost 2
+    [ restrictedAbility a 1 (InThreatAreaOf You)
+    $ ForcedAbility
+    $ SkillTestResult Timing.After You AnySkillTest
+    $ FailureResult AnyValue
+    , restrictedAbility a 2 (InThreatAreaOf $ InvestigatorAt YourLocation)
+    $ ActionAbility Nothing
+    $ ActionCost 2
     ]
 
 instance TreacheryRunner env => RunMessage env Atychiphobia where
-  runMessage msg t@(Atychiphobia attrs@TreacheryAttrs {..}) = case msg of
+  runMessage msg t@(Atychiphobia attrs) = case msg of
     Revelation iid source | isSource attrs source ->
-      t <$ push (AttachTreachery treacheryId $ InvestigatorTarget iid)
-    FailedSkillTest iid _ _ SkillTestInitiatorTarget{} _ _
-      | treacheryOnInvestigator iid attrs
-      -> t
-        <$ push
-             (InvestigatorAssignDamage
-               iid
-               (TreacherySource treacheryId)
-               DamageAny
-               0
-               1
-             )
-    UseCardAbility _ (TreacherySource tid) _ 1 _ | tid == treacheryId ->
-      t <$ push (Discard (TreacheryTarget treacheryId))
+      t <$ push (AttachTreachery (toId attrs) $ InvestigatorTarget iid)
+    UseCardAbility iid source _ 1 _ | isSource attrs source ->
+      t <$ push (InvestigatorAssignDamage iid source DamageAny 0 1)
+    UseCardAbility _ source _ 2 _ | isSource attrs source ->
+      t <$ push (Discard $ toTarget attrs)
     _ -> Atychiphobia <$> runMessage msg attrs

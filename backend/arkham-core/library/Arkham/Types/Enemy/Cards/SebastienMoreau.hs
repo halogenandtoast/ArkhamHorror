8fe4eb11a763e9409d619e9de6a7fdb5fa125ee9
Matthew Mongeau <halogenandtoast@gmail.com>
2021-09-07 12:58:36 -0500

    Sebastien Moreau

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/SebastienMoreau.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/SebastienMoreau.hs
@@ -8,15 +8,22 @@ import Arkham.Prelude
 import qualified Arkham.Enemy.Cards as Cards
 import Arkham.Types.Classes
 import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Enemy.Helpers
 import Arkham.Types.Enemy.Runner
+import Arkham.Types.Modifier
 
 newtype SebastienMoreau = SebastienMoreau EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor env)
+  deriving anyclass IsEnemy
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 sebastienMoreau :: EnemyCard SebastienMoreau
 sebastienMoreau =
-  enemy SebastienMoreau Cards.sebastienMoreau (0, Static 1, 0) (0, 0)
+  enemy SebastienMoreau Cards.sebastienMoreau (3, Static 5, 3) (2, 2)
+
+instance HasModifiersFor env SebastienMoreau where
+  getModifiersFor _ target (SebastienMoreau a) | isTarget a target =
+    pure $ toModifiers a [AttacksCannotBeCancelled]
+  getModifiersFor _ _ _ = pure []
 
 instance EnemyRunner env => RunMessage env SebastienMoreau where
   runMessage msg (SebastienMoreau attrs) =

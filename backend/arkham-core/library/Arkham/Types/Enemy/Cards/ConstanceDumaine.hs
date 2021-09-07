c15a09756b7b83257c08e992a0b3be64f8997bd0
Matthew Mongeau <halogenandtoast@gmail.com>
2021-09-07 08:34:36 -0500

    Update constance

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/ConstanceDumaine.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/ConstanceDumaine.hs
@@ -8,17 +8,20 @@ import Arkham.Prelude
 import qualified Arkham.Enemy.Cards as Cards
 import Arkham.Types.Classes
 import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Enemy.Helpers
 import Arkham.Types.Enemy.Runner
+import Arkham.Types.Modifier
+import Arkham.Types.Target
 
 newtype ConstanceDumaine = ConstanceDumaine EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor env)
+  deriving anyclass IsEnemy
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 constanceDumaine :: EnemyCard ConstanceDumaine
 constanceDumaine =
   enemy ConstanceDumaine Cards.constanceDumaine (4, Static 6, 1) (2, 0)
 
-instance HasModifiersFor ConstanceDumaine where
+instance HasModifiersFor env ConstanceDumaine where
   getModifiersFor _ (EnemyTarget eid) (ConstanceDumaine a) | eid == toId a =
     pure $ toModifiers a [ EnemyFight 3 | enemyExhausted a ]
   getModifiersFor _ _ _ = pure []

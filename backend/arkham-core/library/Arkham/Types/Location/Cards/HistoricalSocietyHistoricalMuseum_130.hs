cb6fd8d318872c92dfe10af18982d9ebe46a1447
Matthew Mongeau <halogenandtoast@gmail.com>
2021-09-14 07:45:47 -0500

    WIP

--- backend/arkham-core/library/Arkham/Types/Location/Cards/HistoricalSocietyHistoricalMuseum_130.hs
+++ backend/arkham-core/library/Arkham/Types/Location/Cards/HistoricalSocietyHistoricalMuseum_130.hs
@@ -6,9 +6,16 @@ module Arkham.Types.Location.Cards.HistoricalSocietyHistoricalMuseum_130
 import Arkham.Prelude
 
 import qualified Arkham.Location.Cards as Cards
+import Arkham.Types.Ability
 import Arkham.Types.Classes
 import Arkham.Types.GameValue
 import Arkham.Types.Location.Attrs
+import Arkham.Types.Location.Helpers
+import Arkham.Types.Matcher hiding (RevealLocation)
+import Arkham.Types.Message
+import Arkham.Types.Modifier
+import Arkham.Types.Target
+import qualified Arkham.Types.Timing as Timing
 
 newtype HistoricalSocietyHistoricalMuseum_130 = HistoricalSocietyHistoricalMuseum_130 LocationAttrs
   deriving anyclass IsLocation
@@ -16,20 +23,32 @@ newtype HistoricalSocietyHistoricalMuseum_130 = HistoricalSocietyHistoricalMuseu
 
 historicalSocietyHistoricalMuseum_130
   :: LocationCard HistoricalSocietyHistoricalMuseum_130
-historicalSocietyHistoricalMuseum_130 = location
+historicalSocietyHistoricalMuseum_130 = locationWith
   HistoricalSocietyHistoricalMuseum_130
   Cards.historicalSocietyHistoricalMuseum_130
-  0
-  (Static 0)
+  2
+  (PerPlayer 1)
   NoSymbol
-  []
+  [Square]
+  ((revealedSymbolL .~ Heart)
+  . (revealedConnectedSymbolsL .~ setFromList [Square, Hourglass])
+  )
 
 instance HasModifiersFor env HistoricalSocietyHistoricalMuseum_130
 
 instance HasAbilities HistoricalSocietyHistoricalMuseum_130 where
   getAbilities (HistoricalSocietyHistoricalMuseum_130 attrs) =
-    getAbilities attrs
+    withBaseAbilities
+      attrs
+      [ mkAbility attrs 1 $ ForcedAbility $ EnemySpawns
+          Timing.When
+          (LocationWithId $ toId attrs)
+          AnyEnemy
+      | not (locationRevealed attrs)
+      ]
 
 instance LocationRunner env => RunMessage env HistoricalSocietyHistoricalMuseum_130 where
-  runMessage msg (HistoricalSocietyHistoricalMuseum_130 attrs) =
-    HistoricalSocietyHistoricalMuseum_130 <$> runMessage msg attrs
+  runMessage msg l@(HistoricalSocietyHistoricalMuseum_130 attrs) = case msg of
+    UseCardAbility _ source _ 1 _ | isSource attrs source ->
+      l <$ push (RevealLocation Nothing $ toId attrs)
+    _ -> HistoricalSocietyHistoricalMuseum_130 <$> runMessage msg attrs

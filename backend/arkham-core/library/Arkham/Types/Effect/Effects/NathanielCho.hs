d50bcb129b7c03220095ad2bf598ad03d25bc2f0
Caleb Hearth <caleb@calebhearth.com>
2021-09-26 14:48:02 -0500

    Nathaniel Cho

---
+++ backend/arkham-core/library/Arkham/Types/Effect/Effects/NathanielCho.hs
@@ -0,0 +1,31 @@
+module Arkham.Types.Effect.Effects.NathanielCho
+  ( NathanielCho(..)
+  , nathanielCho
+  ) where
+
+import Arkham.Prelude
+
+import Arkham.Types.Card
+import Arkham.Types.Classes
+import Arkham.Types.Effect.Attrs
+import Arkham.Types.Id
+import Arkham.Types.Message
+import Arkham.Types.Target
+
+newtype NathanielCho = NathanielCho EffectAttrs
+  deriving anyclass HasAbilities
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+nathanielCho :: EffectArgs -> NathanielCho
+nathanielCho = NathanielCho . uncurry4 (baseAttrs "60101")
+
+instance HasModifiersFor env NathanielCho
+
+instance (HasList DiscardedPlayerCard env InvestigatorId, HasQueue env) => RunMessage env NathanielCho where
+  runMessage msg e@(NathanielCho attrs) = case msg of
+    PassedSkillTest iid _ _ _ _ _ -> do
+      discardedCards <- map unDiscardedPlayerCard <$> getList iid
+      let events = filter ((== EventType) . toCardType) discardedCards
+      e <$ pushAll [chooseOne iid [TargetLabel (CardIdTarget $ toCardId event) [ReturnToHand iid (CardIdTarget $ toCardId event)] | event <- events], DisableEffect $ toId attrs]
+    SkillTestEnds _ -> e <$ push (DisableEffect $ toId attrs)
+    _ -> NathanielCho <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Types/Investigator/Cards/NathanielCho.hs
+++ backend/arkham-core/library/Arkham/Types/Investigator/Cards/NathanielCho.hs
@@ -3,9 +3,24 @@ module Arkham.Types.Investigator.Cards.NathanielCho where
 import Arkham.Prelude
 
 import Arkham.Types.Investigator.Attrs
+import qualified Arkham.Types.Timing as Timing
+import qualified Arkham.Types.Action as Action
+import Arkham.Types.Ability
+import Arkham.Types.Card.CardType
+import Arkham.Types.Cost
+import Arkham.Types.Criteria
+import Arkham.Types.DamageEffect
+import Arkham.Types.Matcher hiding (NonAttackDamageEffect)
+import Arkham.Types.Message
+import Arkham.Types.SkillTest
+import Arkham.Types.Source
+import Arkham.Types.Target
+import Arkham.Types.Window (Window(..))
+import qualified Arkham.Types.Window as Window
+
 
 newtype NathanielCho = NathanielCho InvestigatorAttrs
-  deriving anyclass (HasAbilities, HasModifiersFor env)
+  deriving anyclass (HasModifiersFor env)
   deriving newtype (Show, ToJSON, FromJSON, Entity)
 
 nathanielCho :: NathanielCho
@@ -23,5 +38,31 @@ nathanielCho = NathanielCho $ baseAttrs
     }
   [Criminal, Warden]
 
+instance HasAbilities NathanielCho where
+  getAbilities (NathanielCho x) =
+    [ restrictedAbility
+        x
+        1
+        Self
+        (ReactionAbility
+          (EnemyDealtDamage Timing.When AnyDamageEffect AnyEnemy (SourceOwnedBy You <> SourceIsType EventType))
+          Free
+        )
+    ]
+
+instance HasTokenValue env NathanielCho where
+  getTokenValue (NathanielCho attrs) iid ElderSign | iid == investigatorId attrs =
+    pure $ TokenValue ElderSign (PositiveModifier 1)
+  getTokenValue _ _ token = pure $ TokenValue token mempty
+
 instance (InvestigatorRunner env) => RunMessage env NathanielCho where
-  runMessage msg (NathanielCho attrs) = NathanielCho <$> runMessage msg attrs
+  runMessage msg a@(NathanielCho attrs) = case msg of
+    UseCardAbility iid source [Window _ (Window.DealtDamage _ _ (EnemyTarget eid))] 1 _ | isSource attrs source ->
+      a <$ push (DirectEnemyDamage eid iid source NonAttackDamageEffect 1)
+    ResolveToken _drawnToken ElderSign iid | iid == toId attrs -> do
+      mSource <- getSkillTestSource
+      case mSource of
+        Just (SkillTestSource _ _ _ _ (Just Action.Fight)) ->
+          a <$ push (CreateEffect "60101" Nothing (toSource attrs) (toTarget attrs))
+        _ -> pure a
+    _ -> NathanielCho <$> runMessage msg attrs

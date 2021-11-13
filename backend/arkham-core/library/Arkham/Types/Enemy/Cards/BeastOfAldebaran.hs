8cda645501e1f04bc85e7053b92883e6063bf06d
Matthew Mongeau <halogenandtoast@gmail.com>
2021-11-12 20:17:43 -0600

    Finish BeastOfAldebaran

--- backend/arkham-core/library/Arkham/Types/Enemy/Cards/BeastOfAldebaran.hs
+++ backend/arkham-core/library/Arkham/Types/Enemy/Cards/BeastOfAldebaran.hs
@@ -8,14 +8,19 @@ import Arkham.Prelude
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Types.Classes
 import Arkham.Types.Enemy.Attrs
+import Arkham.Types.Strategy
 
 newtype BeastOfAldebaran = BeastOfAldebaran EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor env)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 beastOfAldebaran :: EnemyCard BeastOfAldebaran
-beastOfAldebaran =
-  enemy BeastOfAldebaran Cards.beastOfAldebaran (3, Static 7, 5) (2, 1)
+beastOfAldebaran = enemyWith
+  BeastOfAldebaran
+  Cards.beastOfAldebaran
+  (3, Static 7, 5)
+  (2, 1)
+  (damageStrategyL .~ SingleTarget)
 
 instance EnemyRunner env => RunMessage env BeastOfAldebaran where
   runMessage msg (BeastOfAldebaran attrs) =

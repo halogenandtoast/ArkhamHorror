3878d9ea232232264c3b559956c56724b2d283e0
Goose <halogenandtoast@gmail.com>
2021-11-13 13:40:11 -0600

    Fix standlone story cards

--- backend/arkham-core/library/Arkham/Treachery/Cards.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards.hs
@@ -179,6 +179,7 @@ allEncounterTreacheryCards = mapFromList $ map
   , theCreaturesTracks
   , theCultsSearch
   , theKingsEdict
+  , thePaleMaskBeckons
   , theYellowSign
   , theZealotsSeal
   , toughCrowd

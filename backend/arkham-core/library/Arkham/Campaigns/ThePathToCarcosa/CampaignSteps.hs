76a2dc61a5d5f0ffa51368da03d7dd4a2664a9a8
Goose <halogenandtoast@gmail.com>
2021-11-14 15:23:16 -0600

    Hookup campaigns

--- backend/arkham-core/library/Arkham/Campaigns/ThePathToCarcosa/CampaignSteps.hs
+++ backend/arkham-core/library/Arkham/Campaigns/ThePathToCarcosa/CampaignSteps.hs
@@ -11,4 +11,5 @@ nextStep a = case campaignStep a of
   Just (ScenarioStep "03043") -> Just (UpgradeDeckStep $ ScenarioStep "03061")
   Just (ScenarioStep "03061") -> Just (UpgradeDeckStep $ ScenarioStep "03120")
   Just (InterludeStep 1 _) -> Just (UpgradeDeckStep $ ScenarioStep "03120")
+  Just (ScenarioStep "03120") -> Just (UpgradeDeckStep $ ScenarioStep "03159")
   _ -> Nothing

9ab7fe4e39383479737d5d3e97d929c72d8b8965
Matthew Mongeau <halogenandtoast@gmail.com>
2021-12-19 09:47:28 -0600

    First and Second Night

--- backend/arkham-core/library/Arkham/Scenarios/APhantomOfTruth/Helpers.hs
+++ backend/arkham-core/library/Arkham/Scenarios/APhantomOfTruth/Helpers.hs
@@ -4,8 +4,13 @@ import Arkham.Prelude
 
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Types.Classes
+import Arkham.Types.Cost
+import Arkham.Types.Game.Helpers
 import Arkham.Types.Id
-import Arkham.Types.Matcher
+import Arkham.Types.Matcher hiding (MoveAction)
+import Arkham.Types.Message
+import Arkham.Types.Query
+import Arkham.Types.Target
 
 investigatorsNearestToTheOrganist ::
   (HasList (InvestigatorId, Distance) env EnemyMatcher, MonadReader env m) =>
@@ -24,3 +29,87 @@ investigatorsNearestToTheOrganist = do
     filter
       ((== minDistance) . unDistance . snd)
       mappings
+
+moveOrganistAwayFromNearestInvestigator ::
+  ( MonadReader env m
+  , HasId LeadInvestigatorId env ()
+  , HasId LocationId env EnemyId
+  , HasList (LocationId, Distance) env InvestigatorId
+  , HasList (InvestigatorId, Distance) env EnemyMatcher
+  , Query LocationMatcher env
+  , Query EnemyMatcher env
+  ) =>
+  m Message
+moveOrganistAwayFromNearestInvestigator = do
+  organist <-
+    selectJust $
+      EnemyOneOf
+        [ enemyIs Cards.theOrganistDrapedInMystery
+        , enemyIs Cards.theOrganistHopelessIDefiedHim
+        ]
+  from <- getId organist
+  leadInvestigatorId <- getLeadInvestigatorId
+  (minDistance, iids) <- investigatorsNearestToTheOrganist
+  lids <-
+    setFromList . concat
+      <$> for
+        iids
+        ( \iid -> do
+            rs <- getList iid
+            pure $ map fst $ filter ((> minDistance) . snd) rs
+        )
+  withNoInvestigators <- select LocationWithoutInvestigators
+  let forced = lids `intersect` withNoInvestigators
+      targets = toList $ if null forced then lids else forced
+  pure $
+    chooseOne
+      leadInvestigatorId
+      [ TargetLabel (LocationTarget lid) [EnemyMove organist from lid]
+      | lid <- targets
+      ]
+
+disengageEachEnemyAndMoveToConnectingLocation ::
+  ( MonadReader env m
+  , HasSet InvestigatorId env ()
+  , HasId LeadInvestigatorId env ()
+  , Query EnemyMatcher env
+  , Query LocationMatcher env
+  ) =>
+  m [Message]
+disengageEachEnemyAndMoveToConnectingLocation = do
+  leadInvestigatorId <- getLeadInvestigatorId
+  iids <- getInvestigatorIds
+  enemyPairs <-
+    traverse
+      (traverseToSnd (selectList . EnemyIsEngagedWith . InvestigatorWithId))
+      iids
+  locationPairs <-
+    traverse
+      ( traverseToSnd
+          ( selectList
+              . AccessibleFrom
+              . LocationWithInvestigator
+              . InvestigatorWithId
+          )
+      )
+      iids
+  pure $
+    [ DisengageEnemy iid enemy
+    | (iid, enemies) <- enemyPairs
+    , enemy <- enemies
+    ]
+      <> [ chooseOneAtATime
+            leadInvestigatorId
+            [ TargetLabel
+              (InvestigatorTarget iid)
+              [ chooseOne
+                  iid
+                  [ TargetLabel
+                    (LocationTarget lid)
+                    [MoveAction iid lid Free False]
+                  | lid <- locations
+                  ]
+              ]
+            | (iid, locations) <- locationPairs
+            ]
+         ]
--- backend/arkham-core/library/Arkham/Types/Agenda/Cards/TheFirstNight.hs
+++ backend/arkham-core/library/Arkham/Types/Agenda/Cards/TheFirstNight.hs
@@ -6,19 +6,15 @@ module Arkham.Types.Agenda.Cards.TheFirstNight (
 import Arkham.Prelude
 
 import Arkham.Agenda.Cards qualified as Cards
-import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Scenarios.APhantomOfTruth.Helpers
 import Arkham.Types.Agenda.Attrs
 import Arkham.Types.Agenda.Helpers
 import Arkham.Types.Agenda.Runner
 import Arkham.Types.CampaignLogKey
 import Arkham.Types.Classes
-import Arkham.Types.Cost
 import Arkham.Types.GameValue
-import Arkham.Types.Matcher hiding (MoveAction)
 import Arkham.Types.Message
 import Arkham.Types.Modifier
-import Arkham.Types.Target
 
 newtype TheFirstNight = TheFirstNight AgendaAttrs
   deriving anyclass (IsAgenda, HasAbilities)
@@ -37,56 +33,10 @@ instance HasRecord env () => HasModifiersFor env TheFirstNight where
 instance AgendaRunner env => RunMessage env TheFirstNight where
   runMessage msg a@(TheFirstNight attrs) = case msg of
     AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
-      leadInvestigatorId <- getLeadInvestigatorId
-      iids <- getInvestigatorIds
-      enemyPairs <-
-        traverse
-          (traverseToSnd (selectList . EnemyIsEngagedWith . InvestigatorWithId))
-          iids
-      locationPairs <-
-        traverse
-          ( traverseToSnd
-              ( selectList
-                  . AccessibleFrom
-                  . LocationWithInvestigator
-                  . InvestigatorWithId
-              )
-          )
-          iids
-      pushAll $
-        [ DisengageEnemy iid enemy
-        | (iid, enemies) <- enemyPairs
-        , enemy <- enemies
-        ]
-          <> [ chooseOneAtATime
-                leadInvestigatorId
-                [ TargetLabel
-                  (InvestigatorTarget iid)
-                  [ chooseOne
-                      iid
-                      [ TargetLabel
-                        (LocationTarget lid)
-                        [MoveAction iid lid Free False]
-                      | lid <- locations
-                      ]
-                  ]
-                | (iid, locations) <- locationPairs
-                ]
-             ]
-          <> [NextAdvanceAgendaStep (toId attrs) 2]
-      a <$ pushAll [AdvanceAgendaDeck (agendaDeckId attrs) (toSource attrs)]
-    NextAdvanceAgendaStep aid 2 | aid == toId attrs && onSide B attrs -> do
-      organist <- selectJust $ EnemyOneOf [enemyIs Cards.theOrganistDrapedInMystery, enemyIs Cards.theOrganistHopelessIDefiedHim]
-      from <- getId organist
-      leadInvestigatorId <- getLeadInvestigatorId
-      (minDistance, iids) <- investigatorsNearestToTheOrganist
-      lids <- setFromList . concat <$> for iids (\iid -> do
-        rs <- getList iid
-        pure $ map fst $ filter ((> minDistance) . snd) rs)
-      withNoInvestigators <- select LocationWithoutInvestigators
-      let
-        forced = lids `intersect` withNoInvestigators
-        targets = toList $ if null forced then lids else forced
-      push $ chooseOne leadInvestigatorId [TargetLabel (LocationTarget lid) [EnemyMove organist from lid] | lid <- targets]
+      msgs <- disengageEachEnemyAndMoveToConnectingLocation
+      pushAll $ msgs <> [NextAdvanceAgendaStep (toId attrs) 2]
       pure a
+    NextAdvanceAgendaStep aid 2 | aid == toId attrs && onSide B attrs -> do
+      organistMsg <- moveOrganistAwayFromNearestInvestigator
+      a <$ pushAll (organistMsg : [AdvanceAgendaDeck (agendaDeckId attrs) (toSource attrs)])
     _ -> TheFirstNight <$> runMessage msg attrs

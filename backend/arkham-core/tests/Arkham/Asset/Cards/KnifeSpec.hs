95a5cec90a29c8bfc5e3d14fc36eea4f696395c7
Matthew Mongeau <halogenandtoast@gmail.com>
2022-05-28 07:40:21 -0500

    Formatting

--- backend/arkham-core/tests/Arkham/Asset/Cards/KnifeSpec.hs
+++ backend/arkham-core/tests/Arkham/Asset/Cards/KnifeSpec.hs
@@ -1,12 +1,11 @@
 module Arkham.Asset.Cards.KnifeSpec
   ( spec
-  )
-where
+  ) where
 
 import TestImport.Lifted
 
-import Arkham.Enemy.Attrs (EnemyAttrs(..))
-import Arkham.Investigator.Attrs (InvestigatorAttrs(..))
+import Arkham.Enemy.Attrs ( EnemyAttrs (..) )
+import Arkham.Investigator.Attrs ( InvestigatorAttrs (..) )
 
 spec :: Spec
 spec = describe "Knife" $ do
@@ -29,41 +28,43 @@ spec = describe "Knife" $ do
         . (entitiesL . locationsL %~ insertEntity location)
         )
       $ do
-        runMessages
-        [knifeFightAction, _] <- getAbilitiesOf knife
-        push $ UseAbility (toId investigator) knifeFightAction []
-        runMessages
-        chooseOnlyOption "Fight enemy"
-        chooseOnlyOption "Start skill test"
-        chooseOnlyOption "Apply Results"
-        updated enemy `shouldSatisfyM` hasDamage (1, 0)
-        isInDiscardOf investigator knife `shouldReturn` False
+          runMessages
+          [knifeFightAction, _] <- getAbilitiesOf knife
+          push $ UseAbility (toId investigator) knifeFightAction []
+          runMessages
+          chooseOnlyOption "Fight enemy"
+          chooseOnlyOption "Start skill test"
+          chooseOnlyOption "Apply Results"
+          updated enemy `shouldSatisfyM` hasDamage (1, 0)
+          isInDiscardOf investigator knife `shouldReturn` False
 
-  it "Discard Knife: Fight. You get +2 for this attack. This attack deals +1 damage." $ do
-    knife <- buildAsset "01086"
-    investigator <- testInvestigator
-      $ \attrs -> attrs { investigatorCombat = 1 }
-    enemy <- testEnemy
-      $ \attrs -> attrs { enemyHealth = Static 3, enemyFight = 3 }
-    location <- testLocation id
-    gameTest
-        investigator
-        [ SetTokens [Zero]
-        , playAsset investigator knife
-        , enemySpawn location enemy
-        , moveTo investigator location
-        ]
-        ((entitiesL . enemiesL %~ insertEntity enemy)
-        . (entitiesL . assetsL %~ insertEntity knife)
-        . (entitiesL . locationsL %~ insertEntity location)
-        )
-      $ do
-        runMessages
-        [_, knifeDiscardFightAction] <- getAbilitiesOf knife
-        push $ UseAbility (toId investigator) knifeDiscardFightAction []
-        runMessages
-        chooseOnlyOption "Fight enemy"
-        chooseOnlyOption "Start skill test"
-        chooseOnlyOption "Apply Results"
-        updated enemy `shouldSatisfyM` hasDamage (2, 0)
-        isInDiscardOf investigator knife `shouldReturn` True
+  it
+      "Discard Knife: Fight. You get +2 for this attack. This attack deals +1 damage."
+    $ do
+        knife <- buildAsset "01086"
+        investigator <- testInvestigator
+          $ \attrs -> attrs { investigatorCombat = 1 }
+        enemy <- testEnemy
+          $ \attrs -> attrs { enemyHealth = Static 3, enemyFight = 3 }
+        location <- testLocation id
+        gameTest
+            investigator
+            [ SetTokens [Zero]
+            , playAsset investigator knife
+            , enemySpawn location enemy
+            , moveTo investigator location
+            ]
+            ((entitiesL . enemiesL %~ insertEntity enemy)
+            . (entitiesL . assetsL %~ insertEntity knife)
+            . (entitiesL . locationsL %~ insertEntity location)
+            )
+          $ do
+              runMessages
+              [_, knifeDiscardFightAction] <- getAbilitiesOf knife
+              push $ UseAbility (toId investigator) knifeDiscardFightAction []
+              runMessages
+              chooseOnlyOption "Fight enemy"
+              chooseOnlyOption "Start skill test"
+              chooseOnlyOption "Apply Results"
+              updated enemy `shouldSatisfyM` hasDamage (2, 0)
+              isInDiscardOf investigator knife `shouldReturn` True

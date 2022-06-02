4458b88c8663ff67ce43e765b33bf4a600cf827f
Goose <halogenandtoast@gmail.com>
2022-06-01 20:19:51 -0500

    Guidance

---
+++ backend/arkham-core/library/Arkham/Event/Cards/Guidance.hs
@@ -0,0 +1,38 @@
+module Arkham.Event.Cards.Guidance
+  ( guidance
+  , Guidance(..)
+  ) where
+
+import Arkham.Prelude
+
+import Arkham.Classes
+import Arkham.Event.Attrs
+import Arkham.Event.Cards qualified as Cards
+import Arkham.Event.Runner
+import Arkham.Matcher
+import Arkham.Message
+
+newtype Guidance = Guidance EventAttrs
+  deriving anyclass (IsEvent, HasModifiersFor env, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+guidance :: EventCard Guidance
+guidance = event Guidance Cards.guidance
+
+instance EventRunner env => RunMessage env Guidance where
+  runMessage msg e@(Guidance attrs) = case msg of
+    InvestigatorPlayEvent iid eid _ _ _ | eid == toId attrs -> do
+      investigators <-
+        selectList $ NotYou <> InvestigatorAt YourLocation <> YetToTakeTurn
+      pushAll
+        [ chooseOne
+          iid
+          [ targetLabel
+              investigator
+              [GainActions investigator (toSource attrs) 1]
+          | investigator <- investigators
+          ]
+        , Discard (toTarget attrs)
+        ]
+      pure e
+    _ -> Guidance <$> runMessage msg attrs

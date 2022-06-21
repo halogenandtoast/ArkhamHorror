3f80e2915d320ed3994fb06d6d5568a3db2a9187
Matthew Mongeau <halogenandtoast@gmail.com>
2022-06-20 20:52:13 -0500

    Grand Rue and standalone settings

--- backend/arkham-core/library/Arkham/Location/Cards/GrandRue.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/GrandRue.hs
@@ -5,10 +5,19 @@ module Arkham.Location.Cards.GrandRue
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Agenda.Attrs ( Field (AgendaDoom) )
 import Arkham.Classes
+import Arkham.Criteria
 import Arkham.GameValue
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Helpers
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Target
+import Arkham.Timing qualified as Timing
+import Data.Semigroup
 
 newtype GrandRue = GrandRue LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -24,8 +33,24 @@ grandRue = location
   [Circle, Triangle, Diamond, Equals]
 
 instance HasAbilities GrandRue where
-  getAbilities (GrandRue attrs) = getAbilities attrs
-    -- withBaseAbilities attrs []
+  getAbilities (GrandRue a) = withBaseAbilities
+    a
+    [ restrictedAbility a 1 Here
+      $ ForcedAbility
+      $ SkillTestResult Timing.After You AnySkillTest
+      $ SuccessResult
+      $ AtMost
+      $ Static 1
+    ]
 
 instance RunMessage GrandRue where
-  runMessage msg (GrandRue attrs) = GrandRue <$> runMessage msg attrs
+  runMessage msg l@(GrandRue attrs) = case msg of
+    UseCardAbility iid source _ 1 _ | isSource attrs source -> do
+      maxDoom <- getMax <$> selectAgg Max AgendaDoom AnyAgenda
+      agendas <- selectListMap AgendaTarget $ AgendaWithDoom $ EqualTo $ Static
+        maxDoom
+      push $ chooseOrRunOne
+        iid
+        [ TargetLabel target [PlaceDoom target 1] | target <- agendas ]
+      pure l
+    _ -> GrandRue <$> runMessage msg attrs

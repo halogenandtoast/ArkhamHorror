cb9902184609af7bcc64d88a9c3a023422c620ba
Goose <halogenandtoast@gmail.com>
2022-06-20 22:21:44 -0500

    OuterWall 286

--- backend/arkham-core/library/Arkham/Location/Cards/OuterWall_286.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/OuterWall_286.hs
@@ -5,14 +5,21 @@ module Arkham.Location.Cards.OuterWall_286
 
 import Arkham.Prelude
 
+import Arkham.Agenda.Attrs (Field(AgendaDoom))
+import Arkham.Agenda.Sequence (AgendaSide(A, C))
 import Arkham.Classes
 import Arkham.GameValue
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Projection
+import Arkham.Source
+import Arkham.Target
 
 newtype OuterWall_286 = OuterWall_286 LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+  deriving anyclass IsLocation
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 outerWall_286 :: LocationCard OuterWall_286
 outerWall_286 = location
@@ -23,9 +30,19 @@ outerWall_286 = location
   Triangle
   [Squiggle, Diamond, Equals]
 
-instance HasAbilities OuterWall_286 where
-  getAbilities (OuterWall_286 attrs) = getAbilities attrs
-    -- withBaseAbilities attrs []
+instance HasModifiersFor OuterWall_286 where
+  getModifiersFor (SkillTestSource iid _ _ _) (CardIdTarget _) (OuterWall_286 a)
+    = do
+      mAgendaA <- selectOne $ AgendaWithSide A
+      mAgendaC <- selectOne $ AgendaWithSide C
+      case (mAgendaA, mAgendaC) of
+        (Just agendaA, Just agendaC) -> do
+          aDoom <- field AgendaDoom agendaA
+          cDoom <- field AgendaDoom agendaC
+          let atOuterWall = iid `member` locationInvestigators a
+          pure $ toModifiers a [ DoubleSkillIcons | atOuterWall && cDoom > aDoom ]
+        _ -> pure []
+  getModifiersFor _ _ _ = pure []
 
 instance RunMessage OuterWall_286 where
   runMessage msg (OuterWall_286 attrs) = OuterWall_286 <$> runMessage msg attrs

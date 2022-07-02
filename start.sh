9e946864bab0edf77fade22d8dd47550e806736a
Matthew Mongeau <halogenandtoast@gmail.com>
2022-07-01 20:19:57 -0500

    Some docker stuff

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sed -i -e 's/$PORT/'"$PORT"'/g' /opt/arkham/src/backend/prod.nginxconf
 PORT=3002 /opt/arkham/bin/arkham-api &
 nginx -c /opt/arkham/src/backend/prod.nginxconf -g 'daemon off;'

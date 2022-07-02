38085fab6e9a728caa37e4d1742db2279287abaf
Goose <halogenandtoast@gmail.com>
2022-07-01 22:11:41 -0500

    More style choices

--- frontend/src/arkham/views/CampaignLog.vue
+++ frontend/src/arkham/views/CampaignLog.vue
@@ -16,26 +16,48 @@ console.log(campaignLog)
 const { recorded, recordedSets } = campaignLog
 
 function toCapitalizedWords(name) {
-  console.log(name)
   const words = name.match(/[A-Za-z][a-z]*/g) || [];
-  return words.map(capitalize).join(" ");
+  return capitalize(words.map(lowercase).join(" "));
 }
 
 function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.substring(1);
 }
+
+function lowercase(word) {
+  return word.charAt(0).toLowerCase() + word.substring(1);
+}
 </script>
 
 <template>
-  <h1>Campaign Log: {{game.name}}</h1>
-  <ul>
-    <li v-for="record in recorded" :key="record">{{toCapitalizedWords(record)}}</li>
-  </ul>
-  <ul>
-    <li v-for="[setKey, setValues] in recordedSets" :key="setKey">{{toCapitalizedWords(setKey)}}
-      <ul>
-        <li v-for="setValue in setValues" :key="setValue">{{setValue}}</li>
-      </ul>
-    </li>
-  </ul>
+  <div class="campaign-log">
+    <h1>Campaign Log: {{game.name}}</h1>
+    <ul>
+      <li v-for="record in recorded" :key="record">{{toCapitalizedWords(record)}}.</li>
+    </ul>
+    <ul>
+      <li v-for="[setKey, setValues] in recordedSets" :key="setKey">{{toCapitalizedWords(setKey)}}
+        <ul>
+          <li v-for="setValue in setValues" :key="setValue">{{setValue}}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+  font-family: teutonic, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.campaign-log {
+  padding: 20px;
+  width: 80vw;
+  margin: 0 auto;
+  margin-top: 20px;
+  background-color: rgba(255,255,255, 0.5);
+  font-size: 1.8em;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,6 +6,7 @@ import { debugGame, deleteGame, fetchGames } from '@/arkham/api';
 import Prompt from '@/components/Prompt.vue'
 import type { Game } from '@/arkham/types/Game';
 import type { User } from '@/types';
+import GameRow from '@/arkham/components/GameRow';
 
 const router = useRouter()
 const store = useStore()
@@ -28,8 +29,6 @@ async function deleteGameEvent() {
   }
 }
 
-const baseUrl = process.env.NODE_ENV == 'production' ? "https://assets.arkhamhorror.app" : '';
-
 const debugFile = ref<HTMLInputElement | null>(null)
 const submitDebugUpload = async (e: Event) => {
   e.preventDefault()
@@ -51,65 +50,10 @@ const submitDebugUpload = async (e: Event) => {
     </div>
 
     <h2>Active Games</h2>
-    <div v-for="game in activeGames" class="game" :key="game.id">
-      <div class="campaign-icon-container" v-if="game.campaign">
-        <img class="campaign-icon" :src="`${baseUrl}/img/arkham/sets/${game.campaign.contents.id}.png`" />
-      </div>
-      <div class="campaign-icon-container" v-else-if="game.scenario">
-        <img class="campaign-icon" :src="`${baseUrl}/img/arkham/sets/${game.scenario.contents.id.replace('c', '').slice(0,2)}.png`" />
-      </div>
-      <div class="game-details">
-        <router-link class="title" :to="`/games/${game.id}`">{{game.name}}</router-link>
-        <div v-if="game.scenario" class="scenario-details">
-          <img class="scenario-icon" :src="`${baseUrl}/img/arkham/sets/${game.scenario.contents.id.replace('c', '')}.png`" />
-          <span>{{game.scenario.contents.name.title}}</span>
-        </div>
-        <div class="investigators">
-          <div
-            v-for="investigator in game.investigators"
-            :key="investigator.contents.id"
-            class="investigator"
-          >
-            <div class="investigator-portrait-container">
-              <img :src="`${baseUrl}/img/arkham/cards/${investigator.contents.id.replace('c', '')}.jpg`" class="investigator-portrait"/>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="game-delete">
-        <a href="#delete" @click.prevent="deleteId = game.id"><font-awesome-icon icon="trash" /></a>
-      </div>
-    </div>
+    <GameRow v-for="game in activeGames" :key="game.id" :game="game" />
 
     <h2>Finished Games</h2>
-    <div v-for="game in finishedGames" class="game finished-game" :key="game.id">
-      <div class="campaign-icon-container" v-if="game.campaign">
-        <img class="campaign-icon" :src="`${baseUrl}/img/arkham/sets/${game.campaign.contents.id}.png`" />
-      </div>
-      <div class="campaign-icon-container" v-else-if="game.scenario">
-        <img class="campaign-icon" :src="`${baseUrl}/img/arkham/sets/${game.scenario.contents.id.replace('c', '').slice(0,2)}.png`" />
-      </div>
-      <div class="game-details">
-        <router-link class="title" :to="`/games/${game.id}`">{{game.name}}</router-link>
-        <div v-if="game.scenario" class="scenario-details">
-          <img class="scenario-icon" :src="`${baseUrl}/img/arkham/sets/${game.scenario.contents.id.replace('c', '')}.png`" />
-          <span>{{game.scenario.contents.name.title}}</span>
-        </div>
-        <div>
-          <span>Investigators: </span>
-          <span
-            v-for="investigator in game.investigators"
-            :key="investigator.contents.id"
-            class="investigator-name"
-          >
-            {{investigator.contents.name.title}}
-          </span>
-        </div>
-      </div>
-      <div class="game-delete">
-        <a href="#delete" @click.prevent="deleteId = game.id"><font-awesome-icon icon="trash" /></a>
-      </div>
-    </div>
+    <GameRow v-for="game in finishedGames" :key="game.id" :game="game" />
 
     <Prompt
       v-if="deleteId"
@@ -130,88 +74,7 @@ h2 {
   color: #6E8644;
   font-size: 2em;
   text-transform: uppercase;
-}
-.game {
-  display: flex;
-  background-color: #15192C;
-  border-left: 10px solid #6e8640;
-  color: #f0f0f0;
-  margin: 10px;
-  padding: 10px;
-  border-radius: 3px;
-  a {
-    color: lighten(#365488, 10%);
-    font-weight: bolder;
-    &:hover {
-      color: lighten(#365488, 20%);
-    }
-  }
-}
-
-.finished-game {
-  border-left: 10px solid #999;
-  background: #333;
-  color: #999;
-
-  a {
-    color: #494949;
-  }
-
-  .game-delete a {
-    color: #111;
-  }
-
-  .campaign-icon {
-    filter: invert(28%) sepia(0%) hue-rotate(-180deg) saturate(3);
-    width: 50px;
-  }
-}
-
-.campaign-icon-container {
-  display: flex;
-  align-items: center;
-}
-
-.campaign-icon {
-  filter: invert(28%) sepia(100%) hue-rotate(-180deg) saturate(3);
-  width: 50px;
-}
-
-.scenario-icon {
-  width: 18px;
-  margin-right: 5px;
-  filter: invert(100%);
-}
-
-.game-details {
-  flex: 1;
-  padding-left: 20px;
-}
-
-.game-delete {
-  justify-self: flex-end;
-  align-self: flex-start;
-  a {
-    color: #660000;
-    &:hover {
-      color: #990000;
-    }
-  }
-}
-
-.scenario-details {
-  display: flex;
-  background-color: #333;
-  padding: 10px;
-  margin-top: 10px;
-  border-radius: 10px;
-  align-items: center;
-  img {
-    margin: 0 10px 0 5px;
-  }
-  span {
-    line-height: 25px;
-  }
+  font-family: teutonic, sans-serif;
 }
 
 .new-game {
@@ -233,43 +96,9 @@ h2 {
   }
 }
 
-.title {
-  font-family: teutonic, sans-serif;
-  font-size: 1.6em;
-  a {
-    text-decoration: none;
-  }
-}
-
 .home {
   width: 100%;
   max-width: 800px;
   margin: 0 auto;
 }
-
-.investigator {
-  display: inline;
-  padding: 5px;
-  border-radius: 10px;
-}
-
-.investigator-portrait-container {
-  width: 50px;
-  height:50px;
-  overflow: hidden;
-  border-radius: 5px;
-  border: 1px solid white;
-  margin-right: 10px;
-}
-
-.investigator-portrait {
-  width: 150px;
-  margin: -18px;
-}
-
-.investigators {
-  margin-top: 10px;
-  display: flex;
-}
-
 </style>

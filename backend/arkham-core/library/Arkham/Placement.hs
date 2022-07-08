5ff589b528c40085c7ec442ba203a95eb6c2fd37
Matthew Mongeau <halogenandtoast@gmail.com>
2022-07-08 17:31:41 -0500

    Placements

---
+++ backend/arkham-core/library/Arkham/Placement.hs
@@ -0,0 +1,19 @@
+module Arkham.Placement where
+
+import Arkham.Prelude
+
+import Arkham.Id
+
+data Placement
+  = AtLocation LocationId
+  | AttachedToLocation LocationId
+  | InPlayArea InvestigatorId
+  | InThreatArea InvestigatorId
+  | AttachedToEnemy EnemyId
+  | AttachedToAsset AssetId
+  | AttachedToAct ActId
+  | AttachedToAgenda ActId
+  | AttachedToInvestigator InvestigatorId
+  | Unplaced
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (ToJSON, FromJSON)

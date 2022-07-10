12648334766403e721379ff402347034c56b0a59
Goose <halogenandtoast@gmail.com>
2022-07-09 21:08:18 -0500

    Modifier data

--- backend/arkham-core/library/Arkham/ModifierData.hs
+++ backend/arkham-core/library/Arkham/ModifierData.hs
@@ -15,9 +15,35 @@ instance ToJSON ModifierData where
   toJSON = genericToJSON $ aesonOptions $ Just "md"
   toEncoding = genericToEncoding $ aesonOptions $ Just "md"
 
+data LocationMetadata = LocationMetadata
+  { lmConnectedLocations :: [LocationId]
+  , lmInvestigators :: HashSet InvestigatorId
+  , lmEnemies :: HashSet EnemyId
+  , lmTreacheries :: HashSet TreacheryId
+  , lmAssets :: HashSet AssetId
+  , lmEvents :: HashSet EventId
+  }
+  deriving stock (Show, Eq, Generic)
+
+instance ToJSON LocationMetadata where
+  toJSON = genericToJSON $ aesonOptions $ Just "lm"
+  toEncoding = genericToEncoding $ aesonOptions $ Just "lm"
+
 newtype ConnectionData = ConnectionData { cdConnectedLocations :: [LocationId] }
   deriving stock (Show, Eq, Generic)
 
 instance ToJSON ConnectionData where
   toJSON = genericToJSON $ aesonOptions $ Just "cd"
   toEncoding = genericToEncoding $ aesonOptions $ Just "cd"
+
+data EnemyMetadata = EnemyMetadata
+  { emEngagedInvestigators :: HashSet InvestigatorId
+  , emTreacheries :: HashSet TreacheryId
+  , emAssets :: HashSet AssetId
+  , emModifiers :: [Modifier]
+  }
+  deriving stock (Show, Eq, Generic)
+
+instance ToJSON EnemyMetadata where
+  toJSON = genericToJSON $ aesonOptions $ Just "em"
+  toEncoding = genericToEncoding $ aesonOptions $ Just "em"

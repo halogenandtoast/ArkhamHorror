b842f689c5bfeb8689ea01cdcb2042a0c949d610
Goose <halogenandtoast@gmail.com>
2022-07-13 21:32:17 -0500

    Evidence spec

---
+++ backend/arkham-core/tests/Arkham/Event/Cards/EvidenceSpec.hs
@@ -0,0 +1,42 @@
+module Arkham.Event.Cards.EvidenceSpec
+  ( spec
+  ) where
+
+import TestImport.Lifted
+
+import Arkham.Event.Cards qualified as Events
+import Arkham.Enemy.Attrs (healthL, fightL, Field(..))
+import Arkham.Investigator.Attrs (combatL, resourcesL, Field(..))
+import Arkham.Location.Attrs (cluesL, Field(..))
+import Arkham.Projection
+
+spec :: Spec
+spec = describe
+  "Evidence!"
+  do
+    it "discovers a clue at your location after you defeat an enemy" $ do
+      investigator <- testJenny ((combatL .~ 1) . (resourcesL .~ 1))
+      enemy <- testEnemy ((healthL .~ Static 1) . (fightL .~ 1))
+      location <- testLocation (cluesL .~ 1)
+      evidence <- genPlayerCard Events.evidence
+      gameTest
+          investigator
+          [ SetTokens [Zero]
+          , addToHand investigator (PlayerCard evidence)
+          , enemySpawn location enemy
+          , moveTo investigator location
+          ]
+          ((entitiesL . enemiesL %~ insertEntity enemy)
+          . (entitiesL . locationsL %~ insertEntity location)
+          )
+        $ do
+          runMessages
+          (fight : _) <- field EnemyAbilities (toId enemy)
+          pushAndRun $ UseAbility (toId investigator) fight []
+          chooseOnlyOption "Begin skill test"
+          chooseOnlyOption "Apply results"
+          chooseOptionMatching "Play evidence" $ \case
+            TargetLabel (CardIdTarget _) _ -> True
+            _ -> False
+          fieldAssert InvestigatorClues (== 1) investigator
+          fieldAssert LocationClues (== 0) location

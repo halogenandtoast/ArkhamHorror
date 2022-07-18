41cc626966de17aa949f46c1086738a3526f0dbe
Matthew Mongeau <halogenandtoast@gmail.com>
2022-07-17 21:35:39 -0500

    Token ui work

--- frontend/src/arkham/types/ChaosBag.ts
+++ frontend/src/arkham/types/ChaosBag.ts
@@ -1,11 +1,37 @@
 import { JsonDecoder } from 'ts.data.json';
 import { ChaosToken, chaosTokenDecoder } from '@/arkham/types/ChaosToken';
 
+type ChaosBagStep = { "tag": "Draw" } | { "tag": "ChooseMatch", "contents": [number, ChaosBagStep[], ] }
+type ChaosBagChoice = { "tag": "Decided", "contents": ChaosBagStep } | { "tag": "Undecided", "contents": ChaosBagStep }
+
 export interface ChaosBag {
   tokens: ChaosToken[]
+  choice: ChaosBagChoice | null
 }
 
+export const chaosBagStepDecoder: JsonDecoder.Decoder<ChaosBagStep> = JsonDecoder.oneOf<ChaosBagStep>([
+  JsonDecoder.object<ChaosBagStep>({
+    tag: JsonDecoder.isExactly("Draw"),
+  }, 'ChaosBagStep'),
+  JsonDecoder.object<ChaosBagStep>({
+    tag: JsonDecoder.isExactly("ChooseMatch"),
+    contents: JsonDecoder.tuple([JsonDecoder.number, JsonDecoder.lazy<ChaosBagStep[]>(() => JsonDecoder.array(chaosBagStepDecoder, 'ChaosBagStep[]')), JsonDecoder.succeed, JsonDecoder.succeed], '[number, ChaosBagStep, any, any]').map(([n, ss]) => [n, ss])
+  }, 'ChaosBagStep'),
+], 'ChaosBagStep');
+
+export const chaosBagChoiceDecoder = JsonDecoder.oneOf([
+  JsonDecoder.object<ChaosBagChoice>({
+    tag: JsonDecoder.isExactly("Decided"),
+    contents: chaosBagStepDecoder
+  }, 'ChaosBagChoice'),
+  JsonDecoder.object<ChaosBagChoice>({
+    tag: JsonDecoder.isExactly("Undecided"),
+    contents: chaosBagStepDecoder
+  }, 'ChaosBagChoice')
+], 'ChaosBagChoice')
+
 export const chaosBagDecoder = JsonDecoder.object<ChaosBag>({
-  tokens: JsonDecoder.array<ChaosToken>(chaosTokenDecoder, 'ChaosToken[]')
+  tokens: JsonDecoder.array<ChaosToken>(chaosTokenDecoder, 'ChaosToken[]'),
+  choice: JsonDecoder.nullable(chaosBagChoiceDecoder)
 }, 'ChaosBag');
 

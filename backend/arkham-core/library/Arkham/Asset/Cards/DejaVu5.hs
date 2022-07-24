1de48e397c9458c25b5eecdf44e4935fdd4226df
Goose <halogenandtoast@gmail.com>
2022-07-24 18:35:40 -0500

    Deja Vu (5)

---
+++ backend/arkham-core/library/Arkham/Asset/Cards/DejaVu5.hs
@@ -0,0 +1,21 @@
+module Arkham.Asset.Cards.DejaVu5
+  ( dejaVu5
+  , DejaVu5(..)
+  )
+where
+
+import Arkham.Prelude
+
+import qualified Arkham.Asset.Cards as Cards
+import Arkham.Asset.Runner
+
+newtype DejaVu5 = DejaVu5 AssetAttrs
+  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+dejaVu5 :: AssetCard DejaVu5
+dejaVu5 =
+  asset DejaVu5 Cards.dejaVu5
+
+instance RunMessage DejaVu5 where
+  runMessage msg (DejaVu5 attrs) = DejaVu5 <$> runMessage msg attrs

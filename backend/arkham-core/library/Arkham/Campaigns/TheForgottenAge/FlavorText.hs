13c3a0636974c8cf8c36d097a386a8442d4ebee9
Goose <halogenandtoast@gmail.com>
2022-07-25 08:18:32 -0500

    Start The Forgotten Age

---
+++ backend/arkham-core/library/Arkham/Campaigns/TheForgottenAge/FlavorText.hs
@@ -0,0 +1,37 @@
+module Arkham.Campaigns.TheForgottenAge.FlavorText where
+
+import Arkham.Prelude
+
+import Arkham.Message
+
+prologue :: Message
+prologue = FlavorText
+  (Just "Prologue")
+  [ "Tuesday, June 2nd, 1925"
+  , "There will be no rest for us this summer, it seems."
+  , "Today I met with Harlan Earnstone, a historical theorist at Miskatonic\
+    \ University and an old friend of mine. He’d said he had a job offer for\
+    \ me, but frankly, I didn’t know what to expect."
+  , "Harlan introduced me to an associate of his: Alejandro Vela, a\
+    \ distinguished historian from across the southern border. Mr. Vela is\
+    \ a graduate of a prestigious university in Mexico City, and has spent\
+    \ many years researching the Aztec Empire. He claims that he has found\
+    \ evidence regarding the existence of an unheard-of Aztec city-state called\
+    \ Eztli. His strange and unprecedented findings were not accepted by the\
+    \ university, and he’s become a pariah among his peers."
+  , "Alejandro came to Arkham to seek funding for an expedition into the\
+    \ rainforests of southern Mexico, where he believes that the ruins of the\
+    \ Eztli altepetl lie undiscovered. His description of this city-state’s origins\
+    \ defies modern understanding of Mesoamerican history. Despite this—\
+    \ or, knowing my old friend, because of this—Harlan is enthralled by\
+    \ Alejandro’s discoveries. Miskatonic University has agreed to fund the\
+    \ expedition (albeit meagerly), and Harlan wants me to go along."
+  , "The way I see it, there’s a good chance we’re looking for something that\
+    \ doesn’t even exist. But if there’s even a slight chance these Eztli people\
+    \ existed, the historical significance would be profound. We debated\
+    \ the issue for some time, but in the end, we accepted Alejandro’s offer.\
+    \ Preparations are underway, and we should be setting off by the end of\
+    \ the month. With any luck, we’ll have this journal filled with all manner\
+    \ of interesting discoveries. If not, perhaps it’ll be a nice wedge to place\
+    \ beneath Harlan’s lopsided desk."
+  ]
---
+++ backend/arkham-core/library/Arkham/Campaigns/TheForgottenAge/Import.hs
@@ -0,0 +1,7 @@
+module Arkham.Campaigns.TheForgottenAge.Import
+  ( module X
+  ) where
+
+import Arkham.Campaigns.TheForgottenAge.CampaignSteps as X
+import Arkham.Campaigns.TheForgottenAge.ChaosBag as X
+import Arkham.Campaigns.TheForgottenAge.FlavorText as X

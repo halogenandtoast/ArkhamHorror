db843001e2bdd5fb7e9ed2cbf1adbaed8d094109
Goose <halogenandtoast@gmail.com>
2022-07-30 19:43:55 -0500

    Handle expedition camp ability, still need labels

--- backend/arkham-core/library/Arkham/Deck.hs
+++ backend/arkham-core/library/Arkham/Deck.hs
@@ -3,11 +3,13 @@ module Arkham.Deck where
 import Arkham.Prelude
 
 import Arkham.Id
+import Arkham.Scenario.Deck
 
 data DeckSignifier
   = InvestigatorDeck InvestigatorId
   | InvestigatorDiscard InvestigatorId
   | EncounterDeck
   | EncounterDiscard
+  | ScenarioDeckByKey ScenarioDeckKey
   deriving stock (Show, Eq, Generic)
   deriving anyclass (ToJSON, FromJSON, Hashable)

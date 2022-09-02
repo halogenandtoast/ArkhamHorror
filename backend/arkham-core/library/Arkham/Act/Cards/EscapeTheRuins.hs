a8178b86a865e22467879b5c3671f2485e1baa3f
Goose <halogenandtoast@gmail.com>
2022-09-02 18:00:20 -0500

    Escape the ruins work

--- backend/arkham-core/library/Arkham/Act/Cards/EscapeTheRuins.hs
+++ backend/arkham-core/library/Arkham/Act/Cards/EscapeTheRuins.hs
@@ -5,16 +5,54 @@ module Arkham.Act.Cards.EscapeTheRuins
 
 import Arkham.Prelude
 
-import qualified Arkham.Act.Cards as Cards
+import Arkham.Ability
+import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Runner
+import Arkham.Campaigns.TheForgottenAge.Helpers
 import Arkham.Classes
+import Arkham.Criteria
+import Arkham.Enemy.Types ( Field (EnemyTraits) )
+import Arkham.Helpers.Ability
+import Arkham.Helpers.Modifiers
+import Arkham.Helpers.Scenario
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Projection
+import Arkham.Resolution
+import Arkham.Scenario.Types
+import Arkham.Target
+import Arkham.Trait
 
 newtype EscapeTheRuins = EscapeTheRuins ActAttrs
-  deriving anyclass (IsAct, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass (IsAct)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 escapeTheRuins :: ActCard EscapeTheRuins
 escapeTheRuins = act (3, A) EscapeTheRuins Cards.escapeTheRuins Nothing
 
+instance HasModifiersFor EscapeTheRuins where
+  getModifiersFor (EnemyTarget eid) (EscapeTheRuins a) = do
+    n <- getVengeanceInVictoryDisplay
+    isSerpent <- fieldP EnemyTraits (elem Serpent) eid
+    pure $ toModifiers a [ EnemyEvade 1 | n >= 3 && isSerpent ]
+  getModifiersFor _ _ = pure []
+
+instance HasAbilities EscapeTheRuins where
+  getAbilities (EscapeTheRuins x) = withBaseAbilities
+    x
+    [ restrictedAbility x 1 AllUndefeatedInvestigatorsResigned
+      $ Objective
+      $ ForcedAbility AnyWindow
+    | onSide A x
+    ]
+
 instance RunMessage EscapeTheRuins where
-  runMessage msg (EscapeTheRuins attrs) = EscapeTheRuins <$> runMessage msg attrs
+  runMessage msg a@(EscapeTheRuins attrs) = case msg of
+    AdvanceAct aid _ _ | aid == actId attrs && onSide B attrs -> do
+      resignedWithRelicOfAges <- scenarioFieldMap
+        ScenarioResignedCardCodes
+        (elem "04061")
+      let resolution = if resignedWithRelicOfAges then 1 else 3
+      push $ ScenarioResolution $ Resolution resolution
+      pure a
+    _ -> EscapeTheRuins <$> runMessage msg attrs

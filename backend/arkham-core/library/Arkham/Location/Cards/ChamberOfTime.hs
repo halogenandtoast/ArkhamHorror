25239eadf4242f0485d99a10cd07dadb5cd3b81d
Goose <halogenandtoast@gmail.com>
2022-09-03 09:58:10 -0500

    More location work

--- backend/arkham-core/library/Arkham/Location/Cards/ChamberOfTime.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/ChamberOfTime.hs
@@ -5,10 +5,18 @@ module Arkham.Location.Cards.ChamberOfTime
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Asset.Cards qualified as Assets
 import Arkham.Direction
 import Arkham.GameValue
+import Arkham.Helpers.Ability
+import Arkham.Helpers.Query
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Placement
+import Arkham.Timing qualified as Timing
 
 newtype ChamberOfTime = ChamberOfTime LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -23,8 +31,22 @@ chamberOfTime = locationWith
   (connectsToL .~ singleton RightOf)
 
 instance HasAbilities ChamberOfTime where
-  getAbilities (ChamberOfTime attrs) = getAbilities attrs
-    -- withBaseAbilities attrs []
+  getAbilities (ChamberOfTime attrs) = withBaseAbilities
+    attrs
+    [ mkAbility attrs 1
+      $ ForcedAbility
+      $ PutLocationIntoPlay Timing.After Anyone
+      $ LocationWithId
+      $ toId attrs
+    ]
 
 instance RunMessage ChamberOfTime where
-  runMessage msg (ChamberOfTime attrs) = ChamberOfTime <$> runMessage msg attrs
+  runMessage msg l@(ChamberOfTime attrs) = case msg of
+    UseCardAbility _ source _ 1 _ | isSource attrs source -> do
+      relicOfAges <- getSetAsideCard Assets.relicOfAgesADeviceOfSomeSort
+      pushAll
+        [ CreateAssetAt relicOfAges (AttachedToLocation $ toId attrs)
+        , PlaceDoom (toTarget attrs) 1
+        ]
+      pure l
+    _ -> ChamberOfTime <$> runMessage msg attrs

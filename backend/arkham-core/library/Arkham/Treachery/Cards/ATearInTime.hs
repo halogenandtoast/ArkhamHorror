7bec8415331771a02f13ff6aaab7cf1cc2218ad5
Matthew Mongeau <halogenandtoast@gmail.com>
2022-09-03 16:15:09 -0500

    A Tear in Time

--- backend/arkham-core/library/Arkham/Treachery/Cards/ATearInTime.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/ATearInTime.hs
@@ -1,14 +1,17 @@
 module Arkham.Treachery.Cards.ATearInTime
   ( aTearInTime
   , ATearInTime(..)
-  )
-where
+  ) where
 
 import Arkham.Prelude
 
-import qualified Arkham.Treachery.Cards as Cards
 import Arkham.Classes
+import Arkham.Investigator.Types ( Field (InvestigatorRemainingActions) )
 import Arkham.Message
+import Arkham.Projection
+import Arkham.SkillType
+import Arkham.Target
+import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
 newtype ATearInTime = ATearInTime TreacheryAttrs
@@ -20,5 +23,23 @@ aTearInTime = treachery ATearInTime Cards.aTearInTime
 
 instance RunMessage ATearInTime where
   runMessage msg t@(ATearInTime attrs) = case msg of
-    Revelation _iid source | isSource attrs source -> pure t
+    Revelation iid source | isSource attrs source -> do
+      push $ RevelationSkillTest iid source SkillWillpower 3
+      pure t
+    FailedSkillTest iid maction source target@SkillTestInitiatorTarget{} sType n
+      | isSource attrs source && n > 0 -> do
+        hasRemainingActions <- fieldP InvestigatorRemainingActions (> 0) iid
+        pushAll
+          $ chooseOrRunOne
+              iid
+              ([ Label "Lose 1 Action" [LoseActions iid source 1]
+               | hasRemainingActions
+               ]
+              <> [ Label
+                     "Take 1 Horror"
+                     [InvestigatorAssignDamage iid source DamageAny 0 1]
+                 ]
+              )
+          : [FailedSkillTest iid maction source target sType (n - 1)]
+        pure t
     _ -> ATearInTime <$> runMessage msg attrs

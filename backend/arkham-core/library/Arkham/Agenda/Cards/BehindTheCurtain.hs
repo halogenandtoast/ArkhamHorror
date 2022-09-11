26f3a88f9e0388948e4ddf559bfd07b7c0c3b98e
Goose <halogenandtoast@gmail.com>
2022-09-11 17:37:50 -0500

    Hidden Entanglements

--- backend/arkham-core/library/Arkham/Agenda/Cards/BehindTheCurtain.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/BehindTheCurtain.hs
@@ -5,22 +5,45 @@ module Arkham.Agenda.Cards.BehindTheCurtain
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Act.Sequence qualified as Act
+import Arkham.Action qualified as Action
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Runner
 import Arkham.Classes
+import Arkham.Cost
 import Arkham.GameValue
+import Arkham.Matcher
 import Arkham.Message
 
 newtype BehindTheCurtain = BehindTheCurtain AgendaAttrs
-  deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAgenda, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 behindTheCurtain :: AgendaCard BehindTheCurtain
 behindTheCurtain =
-  agenda (2, A) BehindTheCurtain Cards.behindTheCurtain (Static 12)
+  agenda (2, A) BehindTheCurtain Cards.behindTheCurtain (Static 6)
+
+instance HasAbilities BehindTheCurtain where
+  getAbilities (BehindTheCurtain attrs) =
+    [mkAbility attrs 1 $ ActionAbility (Just Action.Resign) (ActionCost 1)]
 
 instance RunMessage BehindTheCurtain where
   runMessage msg a@(BehindTheCurtain attrs) = case msg of
-    AdvanceAgenda aid | aid == toId attrs && onSide B attrs ->
-      a <$ pushAll [AdvanceAgendaDeck (agendaDeckId attrs) (toSource attrs)]
+    UseCardAbility iid source _ 1 _ | isSource attrs source -> do
+      push (Resign iid)
+      BehindTheCurtain <$> runMessage msg attrs
+    AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
+      hasDeckA <- selectAny $ ActOneOf [ActWithSide Act.A, ActWithSide Act.B]
+      hasDeckC <- selectAny $ ActOneOf [ActWithSide Act.C, ActWithSide Act.D]
+      hasDeckE <- selectAny $ ActOneOf [ActWithSide Act.E, ActWithSide Act.F]
+      let deckCount = count id [hasDeckA, hasDeckC, hasDeckE]
+
+      pushAll
+        $ [ ShuffleEncounterDiscardBackIn
+          , AdvanceAgendaDeck (agendaDeckId attrs) (toSource attrs)
+          ]
+        <> [ PlaceDoomOnAgenda | deckCount <= 2 ]
+        <> [ PlaceDoomOnAgenda | deckCount == 1 ]
+      pure a
     _ -> BehindTheCurtain <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Agenda/Cards/HiddenEntanglements.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/HiddenEntanglements.hs
@@ -5,22 +5,40 @@ module Arkham.Agenda.Cards.HiddenEntanglements
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Action qualified as Action
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Runner
 import Arkham.Classes
+import Arkham.Cost
 import Arkham.GameValue
+import Arkham.Matcher hiding ( WindowMatcher (InvestigatorDefeated) )
 import Arkham.Message
 
 newtype HiddenEntanglements = HiddenEntanglements AgendaAttrs
-  deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAgenda, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 hiddenEntanglements :: AgendaCard HiddenEntanglements
 hiddenEntanglements =
-  agenda (3, A) HiddenEntanglements Cards.hiddenEntanglements (Static 12)
+  agenda (3, A) HiddenEntanglements Cards.hiddenEntanglements (Static 6)
+
+instance HasAbilities HiddenEntanglements where
+  getAbilities (HiddenEntanglements attrs) =
+    [mkAbility attrs 1 $ ActionAbility (Just Action.Resign) (ActionCost 1)]
 
 instance RunMessage HiddenEntanglements where
   runMessage msg a@(HiddenEntanglements attrs) = case msg of
-    AdvanceAgenda aid | aid == toId attrs && onSide B attrs ->
-      a <$ pushAll [AdvanceAgendaDeck (agendaDeckId attrs) (toSource attrs)]
+    UseCardAbility iid source _ 1 _ | isSource attrs source -> do
+      push (Resign iid)
+      HiddenEntanglements <$> runMessage msg attrs
+    AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
+      iids <- selectList UneliminatedInvestigator
+      a <$ pushAll
+        (concatMap
+          (\iid ->
+            [SufferTrauma iid 0 1, InvestigatorDefeated (toSource attrs) iid]
+          )
+          iids
+        )
     _ -> HiddenEntanglements <$> runMessage msg attrs

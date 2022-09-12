2f92c5f8cc451e0e6840adfeda15609cb1854cd7
Goose <halogenandtoast@gmail.com>
2022-09-12 18:39:17 -0500

    Harlan enemy

--- backend/arkham-core/library/Arkham/Enemy/Cards/HarlanEarnstoneCrazedByTheCurse.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/HarlanEarnstoneCrazedByTheCurse.hs
@@ -5,13 +5,17 @@ module Arkham.Enemy.Cards.HarlanEarnstoneCrazedByTheCurse
 
 import Arkham.Prelude
 
+import Arkham.Ability
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Runner
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Timing qualified as Timing
 
 newtype HarlanEarnstoneCrazedByTheCurse = HarlanEarnstoneCrazedByTheCurse EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 harlanEarnstoneCrazedByTheCurse :: EnemyCard HarlanEarnstoneCrazedByTheCurse
 harlanEarnstoneCrazedByTheCurse = enemy
@@ -20,6 +24,19 @@ harlanEarnstoneCrazedByTheCurse = enemy
   (4, Static 2, 3)
   (1, 1)
 
+instance HasAbilities HarlanEarnstoneCrazedByTheCurse where
+  getAbilities (HarlanEarnstoneCrazedByTheCurse a) = withBaseAbilities
+    a
+    [ mkAbility a 1 $ ForcedAbility $ SkillTestResult
+        Timing.After
+        You
+        (WhileEvadingAnEnemy $ EnemyWithId $ toId a)
+        (SuccessResult $ AtLeast $ Static 3)
+    ]
+
 instance RunMessage HarlanEarnstoneCrazedByTheCurse where
-  runMessage msg (HarlanEarnstoneCrazedByTheCurse attrs) =
-    HarlanEarnstoneCrazedByTheCurse <$> runMessage msg attrs
+  runMessage msg e@(HarlanEarnstoneCrazedByTheCurse attrs) = case msg of
+    UseCardAbility _ source _ 1 _ | isSource attrs source -> do
+      push $ AddToVictory (toTarget attrs)
+      pure e
+    _ -> HarlanEarnstoneCrazedByTheCurse <$> runMessage msg attrs

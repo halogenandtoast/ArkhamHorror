dbca24f510ed3bf82a1858c0984e7aea6b9714e2
Matthew Mongeau <halogenandtoast@gmail.com>
2022-10-28 22:38:23 -0400

    Interview Rooms

--- backend/arkham-core/library/Arkham/Location/Cards/InterviewRoomArrivalChamber.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/InterviewRoomArrivalChamber.hs
@@ -5,9 +5,17 @@ module Arkham.Location.Cards.InterviewRoomArrivalChamber
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Cost
+import Arkham.Criteria
 import Arkham.GameValue
+import Arkham.Helpers.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Timing qualified as Timing
+import Arkham.Trait ( Trait (Yithian) )
 
 newtype InterviewRoomArrivalChamber = InterviewRoomArrivalChamber LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -21,9 +29,20 @@ interviewRoomArrivalChamber = location
   (Static 0)
 
 instance HasAbilities InterviewRoomArrivalChamber where
-  getAbilities (InterviewRoomArrivalChamber attrs) = getAbilities attrs
-    -- withBaseAbilities attrs []
+  getAbilities (InterviewRoomArrivalChamber attrs) = withBaseAbilities
+    attrs
+    [ restrictedAbility
+          attrs
+          1
+          (Here <> EnemyCriteria
+            (EnemyExists $ enemyAt (toId attrs) <> EnemyWithTrait Yithian)
+          )
+        $ ReactionAbility (TurnBegins Timing.After You) Free
+    ]
 
 instance RunMessage InterviewRoomArrivalChamber where
-  runMessage msg (InterviewRoomArrivalChamber attrs) =
-    InterviewRoomArrivalChamber <$> runMessage msg attrs
+  runMessage msg l@(InterviewRoomArrivalChamber attrs) = case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      push $ DrawCards iid 1 False
+      pure l
+    _ -> InterviewRoomArrivalChamber <$> runMessage msg attrs

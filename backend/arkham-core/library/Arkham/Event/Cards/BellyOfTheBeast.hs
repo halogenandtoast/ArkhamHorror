be08840ba8a5068bc99b5756759137a28b5ea26f
Goose <halogenandtoast@gmail.com>
2023-04-10 19:48:20 +0900

    Belly of the Beast

---
+++ backend/arkham-core/library/Arkham/Event/Cards/BellyOfTheBeast.hs
@@ -0,0 +1,36 @@
+module Arkham.Event.Cards.BellyOfTheBeast
+  ( bellyOfTheBeast
+  , BellyOfTheBeast(..)
+  ) where
+
+import Arkham.Prelude
+
+import Arkham.Classes
+import Arkham.Event.Cards qualified as Cards
+import Arkham.Event.Runner
+import Arkham.Id
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Window ( Window (..) )
+import Arkham.Window qualified as Window
+
+newtype BellyOfTheBeast = BellyOfTheBeast EventAttrs
+  deriving anyclass (IsEvent, HasModifiersFor, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+bellyOfTheBeast :: EventCard BellyOfTheBeast
+bellyOfTheBeast = event BellyOfTheBeast Cards.bellyOfTheBeast
+
+toEnemyId :: [Window] -> EnemyId
+toEnemyId [] = error "Invalid window"
+toEnemyId (Window _ (Window.SuccessfulEvadeEnemy _ enemyId _) : _) = enemyId
+toEnemyId (_ : xs) = toEnemyId xs
+
+instance RunMessage BellyOfTheBeast where
+  runMessage msg e@(BellyOfTheBeast attrs) = case msg of
+    InvestigatorPlayEvent iid eid _ (toEnemyId -> enemyId) _
+      | eid == toId attrs -> do
+        mlid <- selectOne $ locationWithEnemy enemyId
+        for_ mlid $ \lid -> push $ DiscoverCluesAtLocation iid lid 1 Nothing
+        pure e
+    _ -> BellyOfTheBeast <$> runMessage msg attrs

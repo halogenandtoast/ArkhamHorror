1a9032e7a343a04837ca2d6de9bbc1ffad1b10d9
Goose <halogenandtoast@gmail.com>
2023-04-17 21:53:36 +0900

    Fix locations

--- backend/arkham-core/library/Arkham/Location/Cards/KeziahsRoom.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/KeziahsRoom.hs
@@ -27,22 +27,54 @@ newtype KeziahsRoom = KeziahsRoom (LocationAttrs `With` Metadata)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 keziahsRoom :: LocationCard KeziahsRoom
-keziahsRoom = location (KeziahsRoom . (`with` Metadata False)) Cards.keziahsRoom 3 (Static 0)
+keziahsRoom = location
+  (KeziahsRoom . (`with` Metadata False))
+  Cards.keziahsRoom
+  3
+  (Static 0)
 
 instance HasAbilities KeziahsRoom where
-  getAbilities (KeziahsRoom (a `With` _)) = withBaseAbilities a [mkAbility a 1 $ ReactionAbility (SkillTestResult Timing.When You (WhileInvestigating $ LocationWithId $ toId a) (SuccessResult AnyValue)) Free]
+  getAbilities (KeziahsRoom (a `With` _)) = withBaseAbilities
+    a
+    [ mkAbility a 1 $ ReactionAbility
+        (SkillTestResult
+          Timing.When
+          You
+          (WhileInvestigating $ LocationWithId $ toId a)
+          (SuccessResult AnyValue)
+        )
+        Free
+    ]
 
 instance RunMessage KeziahsRoom where
   runMessage msg l@(KeziahsRoom (attrs `With` meta)) = case msg of
     UseCardAbility _iid (isSource attrs -> True) 1 _ _ -> do
       pure $ KeziahsRoom $ attrs `with` Metadata True
-    Successful (Action.Investigate, _) iid _ (isTarget attrs -> True) _ | revealTopCard meta -> do
-      push $ DrawFromScenarioDeck iid UnknownPlacesDeck (toTarget attrs) 1
-      pure $ KeziahsRoom $ attrs `with` Metadata False
+    Successful (Action.Investigate, _) iid _ (isTarget attrs -> True) _
+      | revealTopCard meta -> do
+        push $ DrawFromScenarioDeck iid UnknownPlacesDeck (toTarget attrs) 1
+        pure $ KeziahsRoom $ attrs `with` Metadata False
     DrewFromScenarioDeck iid _ target [card] | isTarget attrs target -> do
-      labels <- selectAgg (:[]) LocationLabel $ LocationWithUnrevealedTitle "Unknown Places" <> NotLocation RevealedLocation
-      let nextLabel = fromJustNote "too many locations" $ find (`notElem` labels) $ map (\n -> "unknownPlaces" <> tshow n) ([1..7] :: [Int])
+      labels <-
+        selectAgg (: []) LocationLabel
+        $ LocationWithUnrevealedTitle "Unknown Places"
+        <> NotLocation RevealedLocation
+      let
+        nextLabel =
+          fromJustNote "too many locations" $ find (`notElem` labels) $ map
+            (\n -> "unknownPlaces" <> tshow n)
+            ([1 .. 7] :: [Int])
       (lid, placement) <- placeLocation card
-      pushAll [placement, SetLocationLabel lid nextLabel, chooseOne iid [Label "Do not move" [], Label "Move to location" [MoveTo $ move (toSource attrs) (toTarget iid) lid]]]
+      pushAll
+        [ placement
+        , SetLocationLabel lid nextLabel
+        , chooseOne
+          iid
+          [ Label "Do not move" []
+          , Label
+            "Move to location"
+            [MoveTo $ move (toSource attrs) (toTarget iid) lid]
+          ]
+        ]
       pure l
     _ -> KeziahsRoom . (`with` meta) <$> runMessage msg attrs

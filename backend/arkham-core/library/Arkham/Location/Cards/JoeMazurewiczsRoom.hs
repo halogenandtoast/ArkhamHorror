919340f9335c67c5caa06ef6e670549b76d923d8
Matthew Mongeau <halogenandtoast@gmail.com>
2023-04-22 12:04:22 +0900

    [TSN] Joe Mazurewicz's Room

--- backend/arkham-core/library/Arkham/Location/Cards/JoeMazurewiczsRoom.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/JoeMazurewiczsRoom.hs
@@ -1,13 +1,20 @@
-module Arkham.Location.Cards.JoeMazurewiczsRoom
-  ( joeMazurewiczsRoom
-  , JoeMazurewiczsRoom(..)
-  ) where
+module Arkham.Location.Cards.JoeMazurewiczsRoom (
+  joeMazurewiczsRoom,
+  JoeMazurewiczsRoom (..),
+) where
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Cost
+import Arkham.Criteria
 import Arkham.GameValue
+import Arkham.Helpers.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Trait (Trait (Blessed, Item))
 
 newtype JoeMazurewiczsRoom = JoeMazurewiczsRoom LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -18,9 +25,42 @@ joeMazurewiczsRoom =
   location JoeMazurewiczsRoom Cards.joeMazurewiczsRoom 3 (PerPlayer 1)
 
 instance HasAbilities JoeMazurewiczsRoom where
-  getAbilities (JoeMazurewiczsRoom attrs) = getAbilities attrs
-    -- withBaseAbilities attrs []
+  getAbilities (JoeMazurewiczsRoom a) =
+    withBaseAbilities
+      a
+      [ limitedAbility (GroupLimit PerGame 1) $
+          restrictedAbility a 1 Here $
+            ActionAbility Nothing $
+              ActionCost 1
+      , haunted
+          "You must either take 1 horror, or choose and discard an asset you control."
+          a
+          2
+      ]
 
 instance RunMessage JoeMazurewiczsRoom where
-  runMessage msg (JoeMazurewiczsRoom attrs) =
-    JoeMazurewiczsRoom <$> runMessage msg attrs
+  runMessage msg l@(JoeMazurewiczsRoom attrs) = case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      push $
+        Search
+          iid
+          (toSource attrs)
+          (InvestigatorTarget iid)
+          [fromDeck]
+          (CardWithOneOf [CardWithTrait Blessed, CardWithTrait Item])
+          (DrawFound iid 1)
+      pure l
+    UseCardAbility iid (isSource attrs -> True) 2 _ _ -> do
+      hasAssets <- selectAny $ DiscardableAsset <> assetControlledBy iid
+      push $
+        chooseOrRunOne iid $
+          Label
+            "Take 1 horror"
+            [InvestigatorAssignDamage iid (toSource attrs) DamageAny 0 1]
+            : [ Label
+                "Choose and discard an asset you control"
+                [ChooseAndDiscardAsset iid (toSource attrs) AnyAsset]
+              | hasAssets
+              ]
+      pure l
+    _ -> JoeMazurewiczsRoom <$> runMessage msg attrs

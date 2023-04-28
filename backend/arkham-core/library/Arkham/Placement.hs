e685130e4ee4078873fbcc4a349cb1e12006f072
Matthew Mongeau <halogenandtoast@gmail.com>
2023-04-28 13:21:57 +0900

    WIP

--- backend/arkham-core/library/Arkham/Placement.hs
+++ backend/arkham-core/library/Arkham/Placement.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+
 module Arkham.Placement where
 
 import Arkham.Prelude
--- backend/arkham-core/library/Arkham/Treachery/Cards/MeddlesomeFamiliar.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/MeddlesomeFamiliar.hs
@@ -1,14 +1,20 @@
-module Arkham.Treachery.Cards.MeddlesomeFamiliar
-  ( meddlesomeFamiliar
-  , MeddlesomeFamiliar(..)
-  ) where
+module Arkham.Treachery.Cards.MeddlesomeFamiliar (
+  meddlesomeFamiliar,
+  MeddlesomeFamiliar (..),
+) where
 
 import Arkham.Prelude
 
+import Arkham.Card
 import Arkham.Classes
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Enemy.Creation
+import Arkham.Helpers.Enemy
+import Arkham.Matcher
 import Arkham.Message
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
+import Arkham.Zone
 
 newtype MeddlesomeFamiliar = MeddlesomeFamiliar TreacheryAttrs
   deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
@@ -19,5 +25,22 @@ meddlesomeFamiliar = treachery MeddlesomeFamiliar Cards.meddlesomeFamiliar
 
 instance RunMessage MeddlesomeFamiliar where
   runMessage msg t@(MeddlesomeFamiliar attrs) = case msg of
-    Revelation _iid source | isSource attrs source -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      brownJenkinInPlay <- getEnemyIsInPlay Enemies.brownJenkin
+      push $
+        findEncounterCard iid attrs [FromEncounterDeck, FromEncounterDiscard] $
+          if brownJenkinInPlay then Enemies.swarmOfRats else Enemies.brownJenkin
+      pure t
+    FoundEncounterCard iid (isTarget attrs -> True) (toCard -> card) | card `cardMatch` Enemies.brownJenkin -> do
+      spawnBrownJenkin <- createEnemy2 card (locationWithInvestigator iid)
+      pushAll [CreateEnemy spawnBrownJenkin, InvestigatorAssignDamage iid (toSource attrs) DamageAny 1 0]
+      pure t
+    FoundEncounterCard iid (isTarget attrs -> True) (toCard -> card) | card `cardMatch` Enemies.swarmOfRats -> do
+      spawnSwarmOfRats <- createEnemy2 card (locationWithInvestigator iid)
+      push $
+        CreateEnemy $
+          spawnSwarmOfRats
+            { enemyCreationAfter = [InvestigatorAssignDamage iid (toSource attrs) DamageAny 1 0]
+            }
+      pure t
     _ -> MeddlesomeFamiliar <$> runMessage msg attrs

2245ae981166a1d8233538e4e8558bba25d627f0
Matthew Mongeau <halogenandtoast@gmail.com>
2023-04-28 21:36:35 +0900

    [TSN] Extradimensional Visions, Ghostly Presence

--- backend/arkham-core/library/Arkham/Treachery/Cards/ExtradimensionalVisions.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/ExtradimensionalVisions.hs
@@ -1,12 +1,16 @@
-module Arkham.Treachery.Cards.ExtradimensionalVisions
-  ( extradimensionalVisions
-  , ExtradimensionalVisions(..)
-  ) where
+module Arkham.Treachery.Cards.ExtradimensionalVisions (
+  extradimensionalVisions,
+  ExtradimensionalVisions (..),
+) where
 
 import Arkham.Prelude
 
 import Arkham.Classes
+import Arkham.Helpers.Scenario
+import Arkham.Matcher
 import Arkham.Message
+import Arkham.Scenario.Types (Field (ScenarioDiscard))
+import Arkham.SkillType
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
@@ -20,5 +24,11 @@ extradimensionalVisions =
 
 instance RunMessage ExtradimensionalVisions where
   runMessage msg t@(ExtradimensionalVisions attrs) = case msg of
-    Revelation _iid source | isSource attrs source -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      n <- scenarioFieldMap ScenarioDiscard ((`div` 10) . length)
+      push $ revelationSkillTest iid attrs SkillWillpower (2 + n)
+      pure t
+    FailedSkillTest iid _ (isSource attrs -> True) SkillTestInitiatorTarget {} _ _ -> do
+      push $ ChooseAndDiscardAsset iid (toSource attrs) AnyAsset
+      pure t
     _ -> ExtradimensionalVisions <$> runMessage msg attrs

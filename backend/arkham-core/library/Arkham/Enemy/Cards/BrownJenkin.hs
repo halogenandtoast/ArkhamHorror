8dd4dcaefebc5981b6afb9e645a59ef9d6c519ce
Matthew Mongeau <halogenandtoast@gmail.com>
2023-04-29 19:58:50 +0900

    [TSN] Brown Jenkin

--- backend/arkham-core/library/Arkham/Enemy/Cards/BrownJenkin.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/BrownJenkin.hs
@@ -1,20 +1,57 @@
-module Arkham.Enemy.Cards.BrownJenkin
-  ( brownJenkin
-  , BrownJenkin(..)
-  ) where
+module Arkham.Enemy.Cards.BrownJenkin (
+  brownJenkin,
+  BrownJenkin (..),
+) where
 
 import Arkham.Prelude
 
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Runner
+import Arkham.Enemy.Runner hiding (EnemyFight)
+import Arkham.Game.Helpers
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Phase
+import Arkham.Timing qualified as Timing
+import Arkham.Trait (Trait (Creature))
 
 newtype BrownJenkin = BrownJenkin EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass (IsEnemy)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 brownJenkin :: EnemyCard BrownJenkin
 brownJenkin = enemy BrownJenkin Cards.brownJenkin (1, Static 1, 4) (1, 1)
 
+instance HasModifiersFor BrownJenkin where
+  getModifiersFor (EnemyTarget eid) (BrownJenkin attrs) = do
+    ready <- eid <=~> (ReadyEnemy <> EnemyWithTrait Creature)
+    pure $ toModifiers attrs [EnemyFight 2 | ready]
+  getModifiersFor _ _ = pure []
+
+instance HasAbilities BrownJenkin where
+  getAbilities (BrownJenkin x) =
+    withBaseAbilities x $
+      [ restrictedAbility
+          x
+          1
+          ( EnemyCriteria (ThisEnemy ReadyEnemy)
+              <> InvestigatorExists (InvestigatorAt (locationWithEnemy $ toId x) <> HandWith AnyCards)
+          )
+          $ ForcedAbility
+          $ PhaseEnds Timing.When
+          $ PhaseIs EnemyPhase
+      ]
+
 instance RunMessage BrownJenkin where
-  runMessage msg (BrownJenkin attrs) = BrownJenkin <$> runMessage msg attrs
+  runMessage msg e@(BrownJenkin attrs) = case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 _ _ -> do
+      investigatorsWithHand <-
+        selectWithField InvestigatorHand $
+          InvestigatorAt (locationWithEnemy $ toId attrs) <> HandWith AnyCards
+      msgs <- for investigatorsWithHand $ \(iid, hand) -> do
+        drawing <- drawCards iid attrs (length hand)
+        pure [DiscardHand iid (toSource attrs), drawing]
+      pushAll $ concat msgs
+      pure e
+    _ -> BrownJenkin <$> runMessage msg attrs

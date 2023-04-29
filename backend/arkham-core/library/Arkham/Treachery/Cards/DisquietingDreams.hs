fd43928caef5d2ed13ab30d004a6b958a9891492
Matthew Mongeau <halogenandtoast@gmail.com>
2023-04-29 19:58:50 +0900

    [TSN] Brown Jenkin

--- backend/arkham-core/library/Arkham/Treachery/Cards/DisquietingDreams.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/DisquietingDreams.hs
@@ -1,7 +1,7 @@
-module Arkham.Treachery.Cards.DisquietingDreams
-  ( disquietingDreams
-  , DisquietingDreams(..)
-  ) where
+module Arkham.Treachery.Cards.DisquietingDreams (
+  disquietingDreams,
+  DisquietingDreams (..),
+) where
 
 import Arkham.Prelude
 
@@ -9,10 +9,10 @@ import Arkham.Ability
 import Arkham.Classes
 import Arkham.Matcher
 import Arkham.Message
-import Arkham.Treachery.Cards qualified as Cards
-import Arkham.Treachery.Runner
 import Arkham.SkillType
 import Arkham.Timing qualified as Timing
+import Arkham.Treachery.Cards qualified as Cards
+import Arkham.Treachery.Runner
 
 newtype DisquietingDreams = DisquietingDreams TreacheryAttrs
   deriving anyclass (IsTreachery, HasModifiersFor)
@@ -23,12 +23,12 @@ disquietingDreams = treachery DisquietingDreams Cards.disquietingDreams
 
 instance HasAbilities DisquietingDreams where
   getAbilities (DisquietingDreams a) =
-    [ restrictedAbility a 1 (InThreatAreaOf You)
-      $ ForcedAbility
-      $ TurnEnds Timing.AtIf You
-    , restrictedAbility a 2 (InThreatAreaOf You)
-      $ ForcedAbility
-      $ EncounterDeckRunsOutOfCards
+    [ restrictedAbility a 1 (InThreatAreaOf You) $
+        ForcedAbility $
+          TurnEnds Timing.AtIf You
+    , restrictedAbility a 2 (InThreatAreaOf You) $
+        ForcedAbility $
+          EncounterDeckRunsOutOfCards
     ]
 
 instance RunMessage DisquietingDreams where
@@ -36,8 +36,8 @@ instance RunMessage DisquietingDreams where
     Revelation iid (isSource attrs -> True) -> do
       push $ revelationSkillTest iid attrs SkillWillpower 5
       pure t
-    FailedSkillTest iid _ (isSource attrs -> True) SkillTestInitiatorTarget{} _ _ -> do
-      push $ AttachTreachery(toId attrs) (toTarget iid)
+    FailedSkillTest iid _ (isSource attrs -> True) SkillTestInitiatorTarget {} _ _ -> do
+      push $ AttachTreachery (toId attrs) (toTarget iid)
       pure t
     UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
       push $ DiscardTopOfEncounterDeck iid 1 (toSource attrs) Nothing
@@ -45,7 +45,13 @@ instance RunMessage DisquietingDreams where
     UseCardAbility iid (isSource attrs -> True) 2 _ _ -> do
       pushAll
         [ Discard (toSource attrs) (toTarget attrs)
-        , Search iid (toSource attrs) (toTarget iid) [(FromTopOfDeck 10, DiscardRest)] WeaknessCard (DrawFound  iid 10)
+        , Search
+            iid
+            (toSource attrs)
+            (toTarget iid)
+            [(FromTopOfDeck 10, DiscardRest)]
+            WeaknessCard
+            (DrawFound iid 10)
         ]
       pure t
     _ -> DisquietingDreams <$> runMessage msg attrs

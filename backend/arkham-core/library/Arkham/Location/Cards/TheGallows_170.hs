538fabfe0117beb21bb588329bbe4eb9d1dcea05
Matthew Mongeau <halogenandtoast@gmail.com>
2023-05-14 22:02:49 +0900

    [WOS] The Gallows (170)

--- backend/arkham-core/library/Arkham/Location/Cards/TheGallows_170.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/TheGallows_170.hs
@@ -8,22 +8,25 @@ import Arkham.Prelude
 
 import Arkham.Card
 import Arkham.GameValue
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Cards qualified as Locations
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Trait (Trait (Geist))
 
 newtype TheGallows_170 = TheGallows_170 LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+  deriving anyclass (IsLocation)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 theGallows_170 :: LocationCard TheGallows_170
 theGallows_170 = location TheGallows_170 Cards.theGallows_170 0 (Static 0)
 
-instance HasAbilities TheGallows_170 where
-  getAbilities (TheGallows_170 attrs) =
-    getAbilities attrs
-
--- withRevealedAbilities attrs []
+instance HasModifiersFor TheGallows_170 where
+  getModifiersFor target (TheGallows_170 attrs) | isTarget attrs target = do
+    geistCount <- selectCount $ EnemyWithTrait Geist
+    pure $ toModifiers attrs [ShroudModifier geistCount | geistCount > 0]
+  getModifiersFor _ _ = pure []
 
 instance RunMessage TheGallows_170 where
   runMessage msg l@(TheGallows_170 attrs) = case msg of

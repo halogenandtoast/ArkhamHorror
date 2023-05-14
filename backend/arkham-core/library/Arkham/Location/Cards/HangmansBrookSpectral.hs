9017f496f546cf42e6b4032f3665264d57efe302
Matthew Mongeau <halogenandtoast@gmail.com>
2023-05-14 19:23:23 +0900

    [WOS] Location work

--- backend/arkham-core/library/Arkham/Location/Cards/HangmansBrookSpectral.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/HangmansBrookSpectral.hs
@@ -7,11 +7,12 @@ where
 import Arkham.Prelude
 
 import Arkham.Ability
+import Arkham.Card
 import Arkham.Constants
 import Arkham.GameValue
-import Arkham.Helpers.Ability
 import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Cards qualified as Locations
 import Arkham.Location.Runner
 import Arkham.Matcher
 
@@ -23,19 +24,17 @@ hangmansBrookSpectral :: LocationCard HangmansBrookSpectral
 hangmansBrookSpectral = location HangmansBrookSpectral Cards.hangmansBrookSpectral 1 (Static 0)
 
 instance HasAbilities HangmansBrookSpectral where
-  getAbilities (HangmansBrookSpectral a)
-    | locationRevealed a =
-        withBaseAbilities
+  getAbilities (HangmansBrookSpectral a) =
+    withRevealedAbilities
+      a
+      [ withTooltip
+          "\"Who's bright idea was this, anyway?\""
+          (locationResignAction a)
+      , haunted
+          "Take 1 damage. Until the end of the round, the above resign ability cannot be triggered."
           a
-          [ withTooltip
-              "\"Who's bright idea was this, anyway?\""
-              (locationResignAction a)
-          , haunted
-              "Take 1 damage. Until the end of the round, the above resign ability cannot be triggered."
-              a
-              1
-          ]
-  getAbilities _ = []
+          1
+      ]
 
 instance RunMessage HangmansBrookSpectral where
   runMessage msg l@(HangmansBrookSpectral attrs) = case msg of
@@ -44,5 +43,9 @@ instance RunMessage HangmansBrookSpectral where
         [ InvestigatorAssignDamage iid (toSource attrs) DamageAny 1 0
         , roundModifier attrs iid (CannotTriggerAbilityMatching $ AbilityIs (toSource attrs) ResignAbility)
         ]
+      pure l
+    Flip _ _ target | isTarget attrs target -> do
+      regular <- genCard Locations.hangmansBrook
+      push $ ReplaceLocation (toId attrs) regular Swap
       pure l
     _ -> HangmansBrookSpectral <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Location/Cards/HauntedFieldsSpectral.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/HauntedFieldsSpectral.hs
@@ -1,27 +1,55 @@
-module Arkham.Location.Cards.HauntedFieldsSpectral
-  ( hauntedFieldsSpectral
-  , HauntedFieldsSpectral(..)
-  )
+module Arkham.Location.Cards.HauntedFieldsSpectral (
+  hauntedFieldsSpectral,
+  HauntedFieldsSpectral (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Card
+import Arkham.Game.Helpers
 import Arkham.GameValue
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Cards qualified as Locations
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Trait (Trait (Spectral))
 
 newtype HauntedFieldsSpectral = HauntedFieldsSpectral LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 hauntedFieldsSpectral :: LocationCard HauntedFieldsSpectral
 hauntedFieldsSpectral = location HauntedFieldsSpectral Cards.hauntedFieldsSpectral 3 (Static 0)
 
+instance HasModifiersFor HauntedFieldsSpectral where
+  getModifiersFor (EnemyTarget eid) (HauntedFieldsSpectral attrs) = do
+    affected <- eid <=~> (enemyAt (toId attrs) <> EnemyWithTrait Spectral)
+    pure $ toModifiers attrs [HorrorDealt 1 | affected]
+  getModifiersFor _ _ = pure []
+
 instance HasAbilities HauntedFieldsSpectral where
-  getAbilities (HauntedFieldsSpectral attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+  getAbilities (HauntedFieldsSpectral a) =
+    withRevealedAbilities
+      a
+      [haunted "Move the nearest Spectral enemy once toward Haunted Fields." a 1]
 
 instance RunMessage HauntedFieldsSpectral where
-  runMessage msg (HauntedFieldsSpectral attrs) =
-    HauntedFieldsSpectral <$> runMessage msg attrs
+  runMessage msg l@(HauntedFieldsSpectral attrs) = case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      enemies <- selectList $ NearestEnemy $ EnemyWithTrait Spectral
+      unless (null enemies) $ do
+        push $
+          chooseOne
+            iid
+            [ targetLabel
+              enemy
+              [MoveToward (EnemyTarget enemy) (LocationWithId $ toId attrs)]
+            | enemy <- enemies
+            ]
+      pure l
+    Flip _ _ target | isTarget attrs target -> do
+      regular <- genCard Locations.hauntedFields
+      push $ ReplaceLocation (toId attrs) regular Swap
+      pure l
+    _ -> HauntedFieldsSpectral <$> runMessage msg attrs

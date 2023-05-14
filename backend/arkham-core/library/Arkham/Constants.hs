a7b665a0be504531ab6c5c33966c9d749d28ee9a
Matthew Mongeau <halogenandtoast@gmail.com>
2023-05-14 18:24:22 +0900

    [WOS] progress

--- backend/arkham-core/library/Arkham/Constants.hs
+++ backend/arkham-core/library/Arkham/Constants.hs
@@ -3,17 +3,26 @@ module Arkham.Constants where
 import Arkham.Prelude
 
 pattern AbilityAttack :: Int
-pattern AbilityAttack <- 100 where
-  AbilityAttack = 100
+pattern AbilityAttack <- 100
+  where
+    AbilityAttack = 100
 
 pattern AbilityEvade :: Int
-pattern AbilityEvade <- 101 where
-  AbilityEvade = 101
+pattern AbilityEvade <- 101
+  where
+    AbilityEvade = 101
 
 pattern AbilityEngage :: Int
-pattern AbilityEngage <- 102 where
-  AbilityEngage = 102
+pattern AbilityEngage <- 102
+  where
+    AbilityEngage = 102
 
 pattern ActAdvancement :: Int
-pattern ActAdvancement <- 999 where
-  ActAdvancement = 999
+pattern ActAdvancement <- 999
+  where
+    ActAdvancement = 999
+
+pattern ResignAbility :: Int
+pattern ResignAbility <- 99
+  where
+    ResignAbility = 99

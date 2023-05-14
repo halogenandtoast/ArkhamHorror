2073c62626e992be4382b4638395a643d810edd9
Goose <halogenandtoast@gmail.com>
2023-05-15 08:20:26 +0900

    [WOS] Heretics' Graves (170)

--- backend/arkham-core/library/Arkham/Location/Cards/HistoricalSocietyHistoricalMuseum_130.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/HistoricalSocietyHistoricalMuseum_130.hs
@@ -1,7 +1,7 @@
-module Arkham.Location.Cards.HistoricalSocietyHistoricalMuseum_130
-  ( historicalSocietyHistoricalMuseum_130
-  , HistoricalSocietyHistoricalMuseum_130(..)
-  ) where
+module Arkham.Location.Cards.HistoricalSocietyHistoricalMuseum_130 (
+  historicalSocietyHistoricalMuseum_130,
+  HistoricalSocietyHistoricalMuseum_130 (..),
+) where
 
 import Arkham.Prelude
 
@@ -12,47 +12,51 @@ import Arkham.GameValue
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Helpers
 import Arkham.Location.Runner
-import Arkham.Matcher hiding ( RevealLocation )
+import Arkham.Matcher hiding (RevealLocation)
 import Arkham.Message qualified as Msg
 import Arkham.SkillType
 import Arkham.Timing qualified as Timing
+import Control.Monad.Trans.Maybe
 
 newtype HistoricalSocietyHistoricalMuseum_130 = HistoricalSocietyHistoricalMuseum_130 LocationAttrs
-  deriving anyclass IsLocation
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 historicalSocietyHistoricalMuseum_130
   :: LocationCard HistoricalSocietyHistoricalMuseum_130
-historicalSocietyHistoricalMuseum_130 = location
-  HistoricalSocietyHistoricalMuseum_130
-  Cards.historicalSocietyHistoricalMuseum_130
-  2
-  (PerPlayer 1)
+historicalSocietyHistoricalMuseum_130 =
+  location
+    HistoricalSocietyHistoricalMuseum_130
+    Cards.historicalSocietyHistoricalMuseum_130
+    2
+    (PerPlayer 1)
 
 instance HasModifiersFor HistoricalSocietyHistoricalMuseum_130 where
-  getModifiersFor (InvestigatorTarget _) (HistoricalSocietyHistoricalMuseum_130 attrs)
-    = do
-      mtarget <- getSkillTestTarget
-      mAction <- getSkillTestAction
-      case (mAction, mtarget) of
-        (Just Action.Investigate, Just target) | isTarget attrs target ->
-          pure $ toModifiers attrs [SkillCannotBeIncreased SkillIntellect]
-        _ -> pure []
+  getModifiersFor (InvestigatorTarget iid) (HistoricalSocietyHistoricalMuseum_130 a) = do
+    mModifiers <- runMaybeT $ do
+      guardM $ isTarget a <$> MaybeT getSkillTestTarget
+      guardM $ (== Action.Investigate) <$> MaybeT getSkillTestAction
+      guardM $ (== iid) <$> MaybeT getSkillTestInvestigator
+      pure $ SkillCannotBeIncreased SkillIntellect
+    pure $ toModifiers a $ maybeToList mModifiers
   getModifiersFor _ _ = pure []
 
 instance HasAbilities HistoricalSocietyHistoricalMuseum_130 where
   getAbilities (HistoricalSocietyHistoricalMuseum_130 attrs) =
     withBaseAbilities
       attrs
-      [ mkAbility attrs 1 $ ForcedAbility $ EnemySpawns
-          Timing.When
-          (LocationWithId $ toId attrs)
-          AnyEnemy
+      [ mkAbility attrs 1 $
+        ForcedAbility $
+          EnemySpawns
+            Timing.When
+            (LocationWithId $ toId attrs)
+            AnyEnemy
       | not (locationRevealed attrs)
       ]
 
 instance RunMessage HistoricalSocietyHistoricalMuseum_130 where
   runMessage msg l@(HistoricalSocietyHistoricalMuseum_130 attrs) = case msg of
-    UseCardAbility _ source 1 _ _ | isSource attrs source ->
-      l <$ push (Msg.RevealLocation Nothing $ toId attrs)
+    UseCardAbility _ source 1 _ _
+      | isSource attrs source ->
+          l <$ push (Msg.RevealLocation Nothing $ toId attrs)
     _ -> HistoricalSocietyHistoricalMuseum_130 <$> runMessage msg attrs

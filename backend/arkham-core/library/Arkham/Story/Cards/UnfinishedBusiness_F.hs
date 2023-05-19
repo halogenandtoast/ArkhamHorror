77bc19314b259f9e91ef243128c25ef38ffdbaa6
Goose <halogenandtoast@gmail.com>
2023-05-19 09:10:00 +0900

    Unfinished Business progress

--- backend/arkham-core/library/Arkham/Story/Cards/UnfinishedBusiness_F.hs
+++ backend/arkham-core/library/Arkham/Story/Cards/UnfinishedBusiness_F.hs
@@ -5,18 +5,72 @@ module Arkham.Story.Cards.UnfinishedBusiness_F (
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Card
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.SkillTest
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Placement
+import Arkham.Projection
+import Arkham.SkillType
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Runner
+import Arkham.Timing qualified as Timing
 
 newtype UnfinishedBusiness_F = UnfinishedBusiness_F StoryAttrs
-  deriving anyclass (IsStory, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsStory, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 unfinishedBusiness_F :: StoryCard UnfinishedBusiness_F
 unfinishedBusiness_F = story UnfinishedBusiness_F Cards.unfinishedBusiness_F
 
+instance HasAbilities UnfinishedBusiness_F where
+  getAbilities (UnfinishedBusiness_F x) = case storyPlacement x of
+    InThreatArea _ ->
+      [ restrictedAbility x 1 (InThreatAreaOf You) $ ForcedAbility $ RoundEnds Timing.When
+      , restrictedAbility
+          x
+          2
+          ( OnSameLocation
+              <> LocationExists (YourLocation <> LocationWithTitle "Chapel Attic" <> LocationWithoutClues)
+          )
+          $ ActionAbility Nothing
+          $ ActionCost 1
+      ]
+    _ -> []
+
 instance RunMessage UnfinishedBusiness_F where
   runMessage msg s@(UnfinishedBusiness_F attrs) = case msg of
-    ResolveStory _ _ story' | story' == toId attrs -> do
+    ResolveStory iid ResolveIt story' | story' == toId attrs -> do
+      enemy <- selectJust $ enemyIs Enemies.heretic_E
+      push $ chooseOne iid [targetLabel (toTarget attrs) [RemoveEnemy enemy]]
+      pure . UnfinishedBusiness_F $ attrs & placementL .~ InThreatArea iid
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      hasEnoughResources <- fieldMap InvestigatorResources (>= 2) iid
+      push
+        $ chooseOne
+          iid
+        $ [Label "Lose 2 resources" [LoseResources iid (toSource attrs) 2] | hasEnoughResources]
+          <> [ Label "Flip this back over" [Flip iid (toAbilitySource attrs 1) (toTarget attrs)]
+             ]
+      pure s
+    UseCardAbility iid (isSource attrs -> True) 2 _ _ -> do
+      push $
+        chooseOne
+          iid
+          [ SkillLabel sType [beginSkillTest iid attrs attrs sType 4] | sType <- [SkillIntellect, SkillAgility]
+          ]
+      pure s
+    PassedSkillTest _ _ (isSource attrs -> True) SkillTestInitiatorTarget {} _ _ -> do
+      card <- genCard Enemies.heretic_E
+      send $ format card <> " is \"banished\""
+      push $ AddToVictory (toTarget attrs)
+      pure s
+    Flip _ _ (isTarget attrs -> True) -> do
+      heretic <- genCard Enemies.heretic_E
+      creation <- createEnemy heretic (storyPlacement attrs)
+      pushAll
+        [RemoveStory (toId attrs), toMessage creation]
       pure s
     _ -> UnfinishedBusiness_F <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Story/Cards/UnfinishedBusiness_J.hs
+++ backend/arkham-core/library/Arkham/Story/Cards/UnfinishedBusiness_J.hs
@@ -5,6 +5,13 @@ module Arkham.Story.Cards.UnfinishedBusiness_J (
 
 import Arkham.Prelude
 
+import Arkham.Ability
+import Arkham.Attack
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Enemy.Types (Field (..))
+import Arkham.Helpers.Modifiers
+import Arkham.Matcher
+import Arkham.Projection
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Runner
 
@@ -17,6 +24,24 @@ unfinishedBusiness_J = story UnfinishedBusiness_J Cards.unfinishedBusiness_J
 
 instance RunMessage UnfinishedBusiness_J where
   runMessage msg s@(UnfinishedBusiness_J attrs) = case msg of
-    ResolveStory _ _ story' | story' == toId attrs -> do
+    ResolveStory iid ResolveIt story' | story' == toId attrs -> do
+      alreadyResolved <- getAlreadyResolved attrs
+      enemy <- selectJust $ enemyIs Enemies.heretic_I
+      if alreadyResolved
+        then do
+          card <- field EnemyCard enemy
+          send $ format card <> " was \"Banished\""
+          pushAll [RemoveEnemy enemy, AddToVictory (toTarget attrs)]
+        else do
+          afterStoryResolution attrs $ InitiateEnemyAttack $ enemyAttack enemy iid
+          afterStoryResolution attrs $ gameModifier attrs enemy CannotBeDefeated
+      -- TODO: parley only at spectral, +1 per player clues costt
+      push $ chooseOne iid [targetLabel (toTarget attrs) []]
+      pure s
+    ResolveStory iid DoNotResolveIt story' | story' == toId attrs -> do
+      push $ chooseOne iid [AbilityLabel iid (mkAbility attrs 1 $ ForcedAbility AnyWindow) [] []]
+      pure s
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      push $ ResolveStory iid ResolveIt (toId attrs)
       pure s
     _ -> UnfinishedBusiness_J <$> runMessage msg attrs

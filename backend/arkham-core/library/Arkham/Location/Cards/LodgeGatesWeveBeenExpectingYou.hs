396dce6665797d5e74e774f747f73f039c5e87d8
Matthew Mongeau <halogenandtoast@gmail.com>
2023-06-10 12:25:58 +0900

    Lodge Gates We've Been Expecting You

--- backend/arkham-core/library/Arkham/Location/Cards/LodgeGatesWeveBeenExpectingYou.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/LodgeGatesWeveBeenExpectingYou.hs
@@ -1,26 +1,34 @@
-module Arkham.Location.Cards.LodgeGatesWeveBeenExpectingYou
-  ( lodgeGatesWeveBeenExpectingYou
-  , LodgeGatesWeveBeenExpectingYou(..)
-  )
+module Arkham.Location.Cards.LodgeGatesWeveBeenExpectingYou (
+  lodgeGatesWeveBeenExpectingYou,
+  LodgeGatesWeveBeenExpectingYou (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.GameValue
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
 
 newtype LodgeGatesWeveBeenExpectingYou = LodgeGatesWeveBeenExpectingYou LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 lodgeGatesWeveBeenExpectingYou :: LocationCard LodgeGatesWeveBeenExpectingYou
 lodgeGatesWeveBeenExpectingYou = location LodgeGatesWeveBeenExpectingYou Cards.lodgeGatesWeveBeenExpectingYou 2 (Static 0)
 
+instance HasModifiersFor LodgeGatesWeveBeenExpectingYou where
+  getModifiersFor (EnemyTarget e) (LodgeGatesWeveBeenExpectingYou attrs) =
+    pure $ toModifiers attrs [CannotSpawnIn (LocationWithId $ toId attrs)]
+  getModifiersFor _ _ = pure []
+
 instance HasAbilities LodgeGatesWeveBeenExpectingYou where
   getAbilities (LodgeGatesWeveBeenExpectingYou attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withRevealedAbilities
+      attrs
+      [withTooltip "On second thought, maybe coming here was a bad idea" (locationResignAction attrs)]
 
 instance RunMessage LodgeGatesWeveBeenExpectingYou where
   runMessage msg (LodgeGatesWeveBeenExpectingYou attrs) =

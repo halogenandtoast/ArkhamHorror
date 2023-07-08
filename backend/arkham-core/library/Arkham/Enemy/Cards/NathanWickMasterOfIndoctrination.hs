42e0a54cfdb72ec2cc95173fcd28bd65b60d696c
Goose <halogenandtoast@gmail.com>
2023-07-09 08:20:24 +0900

    Nathan Wick

--- backend/arkham-core/library/Arkham/Enemy/Cards/NathanWickMasterOfIndoctrination.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/NathanWickMasterOfIndoctrination.hs
@@ -1,7 +1,7 @@
-module Arkham.Enemy.Cards.NathanWickMasterOfIndoctrination
-  ( nathanWickMasterOfIndoctrination
-  , NathanWickMasterOfIndoctrination(..)
-  )
+module Arkham.Enemy.Cards.NathanWickMasterOfIndoctrination (
+  nathanWickMasterOfIndoctrination,
+  NathanWickMasterOfIndoctrination (..),
+)
 where
 
 import Arkham.Prelude
@@ -9,14 +9,38 @@ import Arkham.Prelude
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Runner
+import Arkham.Matcher
+import Arkham.Message
+import Arkham.Timing qualified as Timing
 
 newtype NathanWickMasterOfIndoctrination = NathanWickMasterOfIndoctrination EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 nathanWickMasterOfIndoctrination :: EnemyCard NathanWickMasterOfIndoctrination
-nathanWickMasterOfIndoctrination = enemy NathanWickMasterOfIndoctrination Cards.nathanWickMasterOfIndoctrination (4, Static 5, 3) (1, 1)
+nathanWickMasterOfIndoctrination =
+  enemy
+    NathanWickMasterOfIndoctrination
+    Cards.nathanWickMasterOfIndoctrination
+    (4, Static 5, 3)
+    (1, 1)
+
+instance HasAbilities NathanWickMasterOfIndoctrination where
+  getAbilities (NathanWickMasterOfIndoctrination attrs) =
+    withBaseAbilities
+      attrs
+      [ mkAbility attrs 1 $
+          ForcedAbility $
+            SkillTestResult
+              Timing.After
+              You
+              (WhileEvadingAnEnemy $ EnemyWithId $ toId attrs)
+              (SuccessResult $ AtLeast $ Static 3)
+      ]
 
 instance RunMessage NathanWickMasterOfIndoctrination where
-  runMessage msg (NathanWickMasterOfIndoctrination attrs) =
-    NathanWickMasterOfIndoctrination <$> runMessage msg attrs
+  runMessage msg e@(NathanWickMasterOfIndoctrination attrs) = case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 _ _ -> do
+      push $ AddToVictory (toTarget attrs)
+      pure e
+    _ -> NathanWickMasterOfIndoctrination <$> runMessage msg attrs

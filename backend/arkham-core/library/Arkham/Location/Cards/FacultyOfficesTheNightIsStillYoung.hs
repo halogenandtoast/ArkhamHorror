e2b9195b3a3c17903d9ec8d7945151bfc37a17bf
Goose <halogenandtoast@gmail.com>
2023-07-09 07:45:09 +0900

    Holding Cells, Inner Sanctum

--- backend/arkham-core/library/Arkham/Location/Cards/FacultyOfficesTheNightIsStillYoung.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/FacultyOfficesTheNightIsStillYoung.hs
@@ -1,7 +1,7 @@
-module Arkham.Location.Cards.FacultyOfficesTheNightIsStillYoung
-  ( facultyOfficesTheNightIsStillYoung
-  , FacultyOfficesTheNightIsStillYoung(..)
-  ) where
+module Arkham.Location.Cards.FacultyOfficesTheNightIsStillYoung (
+  facultyOfficesTheNightIsStillYoung,
+  FacultyOfficesTheNightIsStillYoung (..),
+) where
 
 import Arkham.Prelude
 
@@ -10,8 +10,9 @@ import Arkham.Card
 import Arkham.Classes
 import Arkham.Game.Helpers
 import Arkham.GameValue
-import Arkham.Location.Cards qualified as Cards
-  ( facultyOfficesTheNightIsStillYoung )
+import Arkham.Location.Cards qualified as Cards (
+  facultyOfficesTheNightIsStillYoung,
+ )
 import Arkham.Location.Runner
 import Arkham.Matcher
 import Arkham.Resolution
@@ -19,51 +20,56 @@ import Arkham.Timing qualified as Timing
 import Arkham.Trait
 
 newtype FacultyOfficesTheNightIsStillYoung = FacultyOfficesTheNightIsStillYoung LocationAttrs
-  deriving anyclass IsLocation
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 facultyOfficesTheNightIsStillYoung
   :: LocationCard FacultyOfficesTheNightIsStillYoung
-facultyOfficesTheNightIsStillYoung = location
-  FacultyOfficesTheNightIsStillYoung
-  Cards.facultyOfficesTheNightIsStillYoung
-  2
-  (PerPlayer 2)
+facultyOfficesTheNightIsStillYoung =
+  location
+    FacultyOfficesTheNightIsStillYoung
+    Cards.facultyOfficesTheNightIsStillYoung
+    2
+    (PerPlayer 2)
 
 instance HasModifiersFor FacultyOfficesTheNightIsStillYoung where
   getModifiersFor target (FacultyOfficesTheNightIsStillYoung attrs)
-    | isTarget attrs target = pure
-    $ toModifiers attrs [ Blocked | not (locationRevealed attrs) ]
+    | isTarget attrs target =
+        pure $
+          toModifiers attrs [Blocked | not (locationRevealed attrs)]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities FacultyOfficesTheNightIsStillYoung where
   getAbilities (FacultyOfficesTheNightIsStillYoung x) =
-    withBaseAbilities x $ if locationRevealed x
-      then
-        [ mkAbility x 1
-        $ ForcedAbility
-        $ RevealLocation Timing.After Anyone
-        $ LocationWithId
-        $ toId x
-        , restrictedAbility x 2 Here $ FastAbility $ GroupClueCost
-          (PerPlayer 2)
-          (LocationWithTitle "Faculty Offices")
-        ]
-      else []
+    withRevealedAbilities
+      x
+      [ mkAbility x 1 $
+          ForcedAbility $
+            RevealLocation Timing.After Anyone $
+              LocationWithId $
+                toId x
+      , restrictedAbility x 2 Here $
+          FastAbility $
+            GroupClueCost
+              (PerPlayer 2)
+              (LocationWithTitle "Faculty Offices")
+      ]
 
 instance RunMessage FacultyOfficesTheNightIsStillYoung where
   runMessage msg l@(FacultyOfficesTheNightIsStillYoung attrs) = case msg of
     UseCardAbility iid source 1 _ _ | isSource attrs source -> do
       push
         $ FindEncounterCard
-            iid
-            (toTarget attrs)
-            [FromEncounterDeck, FromEncounterDiscard]
+          iid
+          (toTarget attrs)
+          [FromEncounterDeck, FromEncounterDiscard]
         $ CardWithType EnemyType
-        <> CardWithTrait Humanoid
+          <> CardWithTrait Humanoid
       pure l
-    FoundEncounterCard _iid target card | isTarget attrs target ->
-      l <$ push (SpawnEnemyAt (EncounterCard card) (toId attrs))
-    UseCardAbility _ source 2 _ _ | isSource attrs source ->
-      l <$ push (ScenarioResolution $ Resolution 1)
+    FoundEncounterCard _iid target card
+      | isTarget attrs target ->
+          l <$ push (SpawnEnemyAt (EncounterCard card) (toId attrs))
+    UseCardAbility _ source 2 _ _
+      | isSource attrs source ->
+          l <$ push (ScenarioResolution $ Resolution 1)
     _ -> FacultyOfficesTheNightIsStillYoung <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Location/Cards/InnerSanctum.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/InnerSanctum.hs
@@ -1,27 +1,43 @@
-module Arkham.Location.Cards.InnerSanctum
-  ( innerSanctum
-  , InnerSanctum(..)
-  )
+module Arkham.Location.Cards.InnerSanctum (
+  innerSanctum,
+  InnerSanctum (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Game.Helpers
 import Arkham.GameValue
+import Arkham.Key
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Scenarios.ForTheGreaterGood.Helpers
+import Arkham.Timing qualified as Timing
 
 newtype InnerSanctum = InnerSanctum LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 innerSanctum :: LocationCard InnerSanctum
 innerSanctum = location InnerSanctum Cards.innerSanctum 4 (PerPlayer 1)
 
+instance HasModifiersFor InnerSanctum where
+  getModifiersFor (InvestigatorTarget iid) (InnerSanctum attrs) = do
+    hasElderThingKey <- iid <=~> InvestigatorWithKey ElderThingKey
+    pure $ toModifiers attrs [CannotEnter (toId attrs) | unrevealed attrs && not hasElderThingKey]
+  getModifiersFor _ _ = pure []
+
 instance HasAbilities InnerSanctum where
   getAbilities (InnerSanctum attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withRevealedAbilities
+      attrs
+      [mkAbility attrs 1 $ ForcedAbility $ RevealLocation Timing.After You $ LocationWithId $ toId attrs]
 
 instance RunMessage InnerSanctum where
-  runMessage msg (InnerSanctum attrs) =
-    InnerSanctum <$> runMessage msg attrs
+  runMessage msg l@(InnerSanctum attrs) = case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 _ _ -> do
+      key <- getRandomKey
+      push $ PlaceKey (toTarget attrs) key
+      pure l
+    _ -> InnerSanctum <$> runMessage msg attrs

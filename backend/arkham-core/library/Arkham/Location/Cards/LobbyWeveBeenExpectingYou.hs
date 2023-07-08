27d9a3cb4020ec4baa6e47f7b1e728813eafa280
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-08 16:04:03 +0900

    Location work

--- backend/arkham-core/library/Arkham/Location/Cards/LobbyWeveBeenExpectingYou.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/LobbyWeveBeenExpectingYou.hs
@@ -1,27 +1,53 @@
-module Arkham.Location.Cards.LobbyWeveBeenExpectingYou
-  ( lobbyWeveBeenExpectingYou
-  , LobbyWeveBeenExpectingYou(..)
-  )
+module Arkham.Location.Cards.LobbyWeveBeenExpectingYou (
+  lobbyWeveBeenExpectingYou,
+  LobbyWeveBeenExpectingYou (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.GameValue
+import Arkham.Helpers.Ability
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher hiding (RevealLocation)
+import Arkham.Message
 
 newtype LobbyWeveBeenExpectingYou = LobbyWeveBeenExpectingYou LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 lobbyWeveBeenExpectingYou :: LocationCard LobbyWeveBeenExpectingYou
 lobbyWeveBeenExpectingYou = location LobbyWeveBeenExpectingYou Cards.lobbyWeveBeenExpectingYou 3 (Static 0)
 
+instance HasModifiersFor LobbyWeveBeenExpectingYou where
+  getModifiersFor target (LobbyWeveBeenExpectingYou attrs)
+    | isTarget attrs target =
+        pure $ toModifiers attrs [Blocked | not (locationRevealed attrs)]
+  getModifiersFor _ _ = pure []
+
 instance HasAbilities LobbyWeveBeenExpectingYou where
   getAbilities (LobbyWeveBeenExpectingYou attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withBaseAbilities
+      attrs
+      [ withTooltip
+        "{action}: _Parley._ The guards recognize you from the Meiger estate and let you pass. Reveal the Lobby."
+        $ restrictedAbility
+          ( ProxySource
+              (LocationMatcherSource $ LocationWithTitle "Lodge Gates")
+              (toSource attrs)
+          )
+          1
+          (OnLocation $ LocationWithTitle "Lodge Gates")
+          (ActionAbility Nothing $ ActionCost 1)
+      | unrevealed attrs
+      ]
 
 instance RunMessage LobbyWeveBeenExpectingYou where
-  runMessage msg (LobbyWeveBeenExpectingYou attrs) =
-    LobbyWeveBeenExpectingYou <$> runMessage msg attrs
+  runMessage msg l@(LobbyWeveBeenExpectingYou attrs) = case msg of
+    UseCardAbility iid (ProxySource _ source) 1 _ _
+      | isSource attrs source && unrevealed attrs -> do
+          push $ RevealLocation (Just iid) (toId attrs)
+          pure l
+    _ -> LobbyWeveBeenExpectingYou <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Location/Cards/LodgeGatesMembersOnly.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/LodgeGatesMembersOnly.hs
@@ -1,26 +1,34 @@
-module Arkham.Location.Cards.LodgeGatesMembersOnly
-  ( lodgeGatesMembersOnly
-  , LodgeGatesMembersOnly(..)
-  )
+module Arkham.Location.Cards.LodgeGatesMembersOnly (
+  lodgeGatesMembersOnly,
+  LodgeGatesMembersOnly (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.GameValue
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
 
 newtype LodgeGatesMembersOnly = LodgeGatesMembersOnly LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 lodgeGatesMembersOnly :: LocationCard LodgeGatesMembersOnly
 lodgeGatesMembersOnly = location LodgeGatesMembersOnly Cards.lodgeGatesMembersOnly 2 (PerPlayer 1)
 
+instance HasModifiersFor LodgeGatesMembersOnly where
+  getModifiersFor (EnemyTarget _) (LodgeGatesMembersOnly attrs) =
+    pure $ toModifiers attrs [CannotSpawnIn (LocationWithId $ toId attrs)]
+  getModifiersFor _ _ = pure []
+
 instance HasAbilities LodgeGatesMembersOnly where
   getAbilities (LodgeGatesMembersOnly attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withRevealedAbilities
+      attrs
+      [withTooltip "On second thought, maybe coming here was a bad idea" (locationResignAction attrs)]
 
 instance RunMessage LodgeGatesMembersOnly where
   runMessage msg (LodgeGatesMembersOnly attrs) =

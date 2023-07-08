a1df46903f0a997e6be7cc5b63a24e2d4f778f4e
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-08 17:45:41 +0900

    Library

--- backend/arkham-core/library/Arkham/Location/Cards/Library.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/Library.hs
@@ -1,26 +1,39 @@
-module Arkham.Location.Cards.Library
-  ( library
-  , Library(..)
-  )
+module Arkham.Location.Cards.Library (
+  library,
+  Library (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Action qualified as Action
+import Arkham.Game.Helpers
 import Arkham.GameValue
+import Arkham.Key
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
 
 newtype Library = Library LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+  deriving anyclass (IsLocation)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 library :: LocationCard Library
 library = location Library Cards.library 6 (PerPlayer 1)
 
-instance HasAbilities Library where
-  getAbilities (Library attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+instance HasModifiersFor Library where
+  getModifiersFor (LocationTarget lid) (Library attrs) | lid == toId attrs = do
+    mskillTestSource <- getSkillTestSource
+    case mskillTestSource of
+      Just (SkillTestSource iid _ source (Just Action.Investigate))
+        | isSource attrs source -> do
+            hasTabletKey <- iid <=~> InvestigatorWithKey TabletKey
+            pure $
+              toModifiers
+                attrs
+                [ShroudModifier (-3) | locationRevealed attrs && hasTabletKey]
+      _ -> pure []
+  getModifiersFor _ _ = pure []
 
 instance RunMessage Library where
   runMessage msg (Library attrs) =

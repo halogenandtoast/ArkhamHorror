efc2f37880ef9edca0e5af5f2ae9676a0441b3e5
Goose <halogenandtoast@gmail.com>
2023-07-08 21:34:03 +0900

    Ceremony Room

--- backend/arkham-core/library/Arkham/Location/Cards/SanctumDoorwayCeremonyRoom.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/SanctumDoorwayCeremonyRoom.hs
@@ -1,14 +1,17 @@
-module Arkham.Location.Cards.SanctumDoorwayCeremonyRoom
-  ( sanctumDoorwayCeremonyRoom
-  , SanctumDoorwayCeremonyRoom(..)
-  )
+module Arkham.Location.Cards.SanctumDoorwayCeremonyRoom (
+  sanctumDoorwayCeremonyRoom,
+  SanctumDoorwayCeremonyRoom (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.GameValue
+import Arkham.Key
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Timing qualified as Timing
 
 newtype SanctumDoorwayCeremonyRoom = SanctumDoorwayCeremonyRoom LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -19,9 +22,27 @@ sanctumDoorwayCeremonyRoom = location SanctumDoorwayCeremonyRoom Cards.sanctumDo
 
 instance HasAbilities SanctumDoorwayCeremonyRoom where
   getAbilities (SanctumDoorwayCeremonyRoom attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withRevealedAbilities
+      attrs
+      [ restrictedAbility
+          attrs
+          1
+          ( AnyCriterion [AssetExists $ assetAt $ toId attrs, InvestigatorExists $ investigatorAt $ toId attrs]
+          )
+          $ ForcedAbility
+          $ RoundEnds Timing.When
+      ]
 
 instance RunMessage SanctumDoorwayCeremonyRoom where
-  runMessage msg (SanctumDoorwayCeremonyRoom attrs) =
-    SanctumDoorwayCeremonyRoom <$> runMessage msg attrs
+  runMessage msg l@(SanctumDoorwayCeremonyRoom attrs) = case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 _ _ -> do
+      cancelEffect <-
+        selectAny $ investigatorAt (toId attrs) <> InvestigatorWithKey SkullKey
+      unless cancelEffect $ do
+        investigators <- selectList $ investigatorAt $ toId attrs
+        assets <- selectList $ assetAt $ toId attrs
+        pushAll $
+          [InvestigatorDirectDamage iid (toAbilitySource attrs 1) 0 1 | iid <- investigators]
+            <> [AssetDamage aid (toAbilitySource attrs 1) 0 1 | aid <- assets]
+      pure l
+    _ -> SanctumDoorwayCeremonyRoom <$> runMessage msg attrs

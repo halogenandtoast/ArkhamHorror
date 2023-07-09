603fc8fb435baf092e72a3fe6fb482573dbeb0c4
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-09 19:17:14 +0900

    Expulsion

--- backend/arkham-core/library/Arkham/Treachery/Cards/Expulsion.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/Expulsion.hs
@@ -1,13 +1,18 @@
-module Arkham.Treachery.Cards.Expulsion
-  ( expulsion
-  , Expulsion(..)
-  )
+module Arkham.Treachery.Cards.Expulsion (
+  expulsion,
+  Expulsion (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Attack
 import Arkham.Classes
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Matcher
 import Arkham.Message
+import Arkham.Projection
+import Arkham.Trait (Trait (Cultist))
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
@@ -20,5 +25,25 @@ expulsion = treachery Expulsion Cards.expulsion
 
 instance RunMessage Expulsion where
   runMessage msg t@(Expulsion attrs) = case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      nearestCultists <- selectList $ NearestEnemy $ EnemyWithTrait Cultist
+      if null nearestCultists
+        then push $ gainSurge attrs
+        else do
+          mYourLocation <- selectOne $ locationWithInvestigator iid
+          ks <- fieldMap InvestigatorKeys toList iid
+          for_ mYourLocation $ \yourLocation -> do
+            push $
+              chooseOrRunOne
+                iid
+                [ targetLabel
+                  cultist
+                  $ [ MoveUntil yourLocation (toTarget cultist)
+                    , EnemyEngageInvestigator cultist iid
+                    , InitiateEnemyAttack $ enemyAttack cultist (toSource attrs) iid
+                    ]
+                    <> [PlaceKey (toTarget cultist) k | k <- ks]
+                | cultist <- nearestCultists
+                ]
+      pure t
     _ -> Expulsion <$> runMessage msg attrs

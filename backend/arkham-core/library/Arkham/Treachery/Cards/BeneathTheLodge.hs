87a2eae30c560baf90338ca474b5702ec29361c9
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-09 19:32:29 +0900

    Beneath the Lodge

--- backend/arkham-core/library/Arkham/Treachery/Cards/BeneathTheLodge.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/BeneathTheLodge.hs
@@ -1,24 +1,70 @@
-module Arkham.Treachery.Cards.BeneathTheLodge
-  ( beneathTheLodge
-  , BeneathTheLodge(..)
-  )
+module Arkham.Treachery.Cards.BeneathTheLodge (
+  beneathTheLodge,
+  BeneathTheLodge (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.Classes
+import Arkham.Helpers.Modifiers
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Keyword qualified as Keyword
 import Arkham.Message
+import Arkham.Projection
+import Arkham.SkillType
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
 newtype BeneathTheLodge = BeneathTheLodge TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsTreachery, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 beneathTheLodge :: TreacheryCard BeneathTheLodge
 beneathTheLodge = treachery BeneathTheLodge Cards.beneathTheLodge
 
+instance HasModifiersFor BeneathTheLodge where
+  getModifiersFor SkillTestTarget (BeneathTheLodge attrs) = do
+    mskillTestSource <- getSkillTestSource
+    case mskillTestSource of
+      Just (SkillTestSource iid _ (isSource attrs -> True) _) -> do
+        hasKey <- fieldMap InvestigatorKeys notNull iid
+        pure $ toModifiers attrs [Difficulty 1 | hasKey]
+      _ -> pure []
+  getModifiersFor target (BeneathTheLodge attrs) | isTarget attrs target = do
+    hasKey <- maybe (pure False) (fieldMap InvestigatorKeys notNull) (treacheryOwner attrs)
+    pure $ toModifiers attrs $ [AddKeyword Keyword.Peril | hasKey]
+  getModifiersFor _ _ = pure []
+
 instance RunMessage BeneathTheLodge where
   runMessage msg t@(BeneathTheLodge attrs) = case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      push $ revelationSkillTest iid attrs SkillIntellect 3
+      pure t
+    FailedSkillTest iid _ source SkillTestInitiatorTarget {} _ n
+      | isSource attrs source ->
+          t
+            <$ push (HandlePointOfFailure iid (toTarget attrs) n)
+    HandlePointOfFailure _ target 0 | isTarget attrs target -> pure t
+    HandlePointOfFailure iid target n | isTarget attrs target -> do
+      hasClues <- fieldMap InvestigatorClues (> 0) iid
+      pushAll $
+        if hasClues
+          then
+            [ chooseOne
+                iid
+                [ Label
+                    "Lose 1 clue"
+                    [RemoveClues (toSource attrs) (toTarget iid) 1]
+                , Label
+                    "Take 1 horror"
+                    [InvestigatorAssignDamage iid (toSource attrs) DamageAny 0 1]
+                ]
+            , HandlePointOfFailure iid (toTarget attrs) (n - 1)
+            ]
+          else
+            [ InvestigatorAssignDamage iid (toSource attrs) DamageAny 0 1
+            , HandlePointOfFailure iid (toTarget attrs) (n - 1)
+            ]
+      pure t
     _ -> BeneathTheLodge <$> runMessage msg attrs

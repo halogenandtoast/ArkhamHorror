f99e1c9cc2dd047f2983bc2f69d9a37627237f08
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-09 14:21:25 +0900

    Call to Order

--- backend/arkham-core/library/Arkham/Treachery/Cards/CallToOrder.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/CallToOrder.hs
@@ -1,13 +1,19 @@
-module Arkham.Treachery.Cards.CallToOrder
-  ( callToOrder
-  , CallToOrder(..)
-  )
+module Arkham.Treachery.Cards.CallToOrder (
+  callToOrder,
+  CallToOrder (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Card
 import Arkham.Classes
+import Arkham.Enemy.Creation
+import Arkham.Helpers.Scenario
+import Arkham.Matcher
 import Arkham.Message
+import Arkham.Scenario.Types (Field (..))
+import Arkham.Trait (Trait (Cultist))
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
@@ -20,5 +26,16 @@ callToOrder = treachery CallToOrder Cards.callToOrder
 
 instance RunMessage CallToOrder where
   runMessage msg t@(CallToOrder attrs) = case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      discards <- scenarioField ScenarioDiscard
+      let cultists = take 2 $ filter (`cardMatch` CardWithTrait Cultist) discards
+      if null cultists
+        then push $ gainSurge attrs
+        else do
+          locations <- selectList $ LocationWithMostClues EmptyLocation
+          choices <- for locations $ \location -> do
+            creations <- traverse (\cultist -> createEnemy cultist (SpawnAtLocation location)) cultists
+            pure $ targetLabel location $ map toMessage creations
+          push $ chooseOrRunOne iid choices
+      pure t
     _ -> CallToOrder <$> runMessage msg attrs

c46f98350e26686f43bce021a81cf39bee47d254
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-12 20:33:25 +0900

    [UAD] Intro text

--- backend/arkham-core/library/Arkham/Scenario/Scenarios/UnionAndDisillusion.hs
+++ backend/arkham-core/library/Arkham/Scenario/Scenarios/UnionAndDisillusion.hs
@@ -7,8 +7,11 @@ import Arkham.Prelude
 
 import Arkham.Classes
 import Arkham.Difficulty
+import Arkham.Helpers.Query
 import Arkham.Helpers.Scenario
+import Arkham.Message
 import Arkham.Scenario.Runner
+import Arkham.Scenarios.UnionAndDisillusion.Story
 import Arkham.Token
 
 newtype UnionAndDisillusion = UnionAndDisillusion ScenarioAttrs
@@ -33,5 +36,9 @@ instance HasTokenValue UnionAndDisillusion where
     otherFace -> getTokenValue iid otherFace attrs
 
 instance RunMessage UnionAndDisillusion where
-  runMessage msg (UnionAndDisillusion attrs) =
-    UnionAndDisillusion <$> runMessage msg attrs
+  runMessage msg s@(UnionAndDisillusion attrs) = case msg of
+    PreScenarioSetup -> do
+      iids <- allInvestigatorIds
+      pushAll [story iids intro]
+      pure s
+    _ -> UnionAndDisillusion <$> runMessage msg attrs

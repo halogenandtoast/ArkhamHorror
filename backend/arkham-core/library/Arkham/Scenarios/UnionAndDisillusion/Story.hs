ff4df9be1d08aab34e460d1643664e9c94819f68
Matthew Mongeau <halogenandtoast@gmail.com>
2023-07-12 20:33:25 +0900

    [UAD] Intro text

---
+++ backend/arkham-core/library/Arkham/Scenarios/UnionAndDisillusion/Story.hs
@@ -0,0 +1,41 @@
+module Arkham.Scenarios.UnionAndDisillusion.Story where
+
+import Arkham.Prelude
+
+import Arkham.Text
+
+intro :: FlavorText
+intro =
+  FlavorText
+    (Just "Union and Disillusion")
+    [ "The Unvisited Isle lies in the midst of the Miskatonic River, in between two of\
+      \ the bridges that span the river and connect the Downtown and Rivertown\
+      \ neighborhoods of Arkham. Overgrown with vines, thorns, and unnatural\
+      \ underbrush, the island is home to no residents and very few fauna. Rows of\
+      \ mysterious stone pillars stand sentry throughout the island, rising high above\
+      \ the treetops. Though many theories abound, none know for sure what purpose\
+      \ these stone pillars serve, or who built them. Very few dare to venture onto the\
+      \ shores of the enigmatic island, and those who do return with strange tales:\
+      \ bonfires in the woods at the darkest hours of night—stone altars with deep\
+      \ carvings impossible Setup to translate—empty woods devoid of life, save for the\
+      \ stark-black ravens and whippoorwills who perch on the tree branches and watch\
+      \ trespassers with eager eyes. Unfortunately for you, the stakes are too high for\
+      \ you to heed these many warnings."
+    , "The streets of Arkham are emptier and quieter than you have ever seen them\
+      \ before. A familiar grey mist has enveloped the town. The haze grows denser and\
+      \ thicker the closer you get to the river. You move with haste, not wanting to\
+      \ encounter any others on your way to the site of the ritual, whether they be\
+      \ living or dead. You find several rowboats moored by the docks and climb aboard\
+      \ one of them. With the waters unnaturally still and barely any current carrying\
+      \ the boat, rowing toward the island should be a simple endeavor. Even so, the\
+      \ dense fog makes it difficult for you to get your bearings. Soon, you are\
+      \ completely enclosed by the grey mist, unable to see the banks of the river or\
+      \ the coast of the island. Shapes emerge from the fog. Ripples appear on the\
+      \ surface of the water. The gaze of the watcher is upon you."
+    , "Just then, a pillar of spectral energy ascends into the sky, clear as day even\
+      \ through the haze. The shapes all turn their attention to this beacon, and you\
+      \ sense the watcher’s gaze slipping from your soul. The rite has begun. You row\
+      \ faster, using the light to guide you. It is time to decide once and for all why\
+      \ you have come—are you here to aid the Silver Twilight Lodge? Or to disrupt\
+      \ them?"
+    ]

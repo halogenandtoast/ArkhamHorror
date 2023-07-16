d78339a4fcff948be169f6b4c926e0d645c3f56a
Goose <halogenandtoast@gmail.com>
2023-07-16 22:05:02 +0900

    The Uncisited Isle work

--- backend/arkham-core/library/Arkham/Act/Cards/TheUnvisitedIsle.hs
+++ backend/arkham-core/library/Arkham/Act/Cards/TheUnvisitedIsle.hs
@@ -28,15 +28,15 @@ theUnvisitedIsle = act (1, A) TheUnvisitedIsle Cards.theUnvisitedIsle (Just $ Gr
 instance RunMessage TheUnvisitedIsle where
   runMessage msg a@(TheUnvisitedIsle attrs) = case msg of
     AdvanceAct aid _ _ | aid == toId attrs && onSide B attrs -> do
-      -- cards <- shuffleM <$> selectList (SetAsideCardMatch $ CardWithTitle "Unvisited Isle")
-      -- pairs <- zip cards <$> getInvestigators
-      -- sidedWithTheCoven <- getHasRecord TheInvestigatorsSidedWithTheCoven
-      -- for_ pairs $ \(unvisitedIsle, investigator) -> do
-      --   (lid, placement) <- placeLocation unvisitedIsle
-      --   pushAll $
-      --     placement
-      --       : [PutLocationInFrontOf investigator lid, MoveTo $ move attrs investigator lid]
-      --         <> [UpdateLocation lid (LocationBrazier ?=. Lit) | sidedWithTheCoven]
+      cards <- shuffleM =<< selectList (SetAsideCardMatch $ CardWithTitle "Unvisited Isle")
+      paired <- zip cards <$> getInvestigators
+      sidedWithTheCoven <- getHasRecord TheInvestigatorsSidedWithTheCoven
+      for_ paired $ \(unvisitedIsle, investigator) -> do
+        (lid, placement) <- placeLocation unvisitedIsle
+        pushAll $
+          placement
+            : [PutLocationInFrontOf investigator lid, MoveTo $ move attrs investigator lid]
+              <> [UpdateLocation lid (LocationBrazier ?=. Lit) | sidedWithTheCoven]
 
       pure a
     _ -> TheUnvisitedIsle <$> runMessage msg attrs

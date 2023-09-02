cd0b1a5556629b6cd1cb568235961ec31d4ccf76
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-02 11:17:52 +0900

    Handle more advanced scenario options

--- frontend/src/arkham/types/StandaloneSetting.ts
+++ frontend/src/arkham/types/StandaloneSetting.ts
@@ -1,8 +1,23 @@
 export type CrossOutContent = { label: string, key: string, content: boolean }
 export type RecordableType = 'RecordableCardCode' | 'RecordableMemento'
+type Predicate =
+  { type: "lte", value: number } |
+  { type: "gte", value: number }
+type SettingCondition =
+  { type: "key", key: string } |
+  { type: "inSet", key: string, recordable: string, content: string } |
+  { type: "crossedOut", key: string, recordable: string, content: string } |
+  { type: "count", key: string, predicate: Predicate } |
+  { type: "option", key: string } |
+  { type: "and", content: SettingCondition[] } |
+  { type: "or", content: SettingCondition[] } |
+  { type: "not", content: SettingCondition } |
+  { type: "nor", content: SettingCondition[] }
+export type Recordable = { key: string, content: string, ifRecorded?: SettingCondition}
 
 export type StandaloneSetting =
   { type: "ToggleCrossedOut", key: string, recordable: RecordableType, content: CrossOutContent[] } |
+  { type: "ToggleRecords", key: string, recordable: RecordableType, content: Recordable[] } |
   { type: "ToggleKey", key: string, content: boolean } |
   { type: "ToggleOption", key: string, content: boolean } |
   { type: "PickKey", key: string, keys: string[], content: string}

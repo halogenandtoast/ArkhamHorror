4f2be775ad70255a591b35b6f593f166e7c92cc9
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-04 17:33:42 +0900

    [UAD] Start story assets

--- backend/arkham-core/library/Arkham/Asset/Cards/GavriellaMizrah.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/GavriellaMizrah.hs
@@ -6,16 +6,35 @@ where
 
 import Arkham.Prelude
 
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.Matcher
+import Arkham.SkillType
+import Arkham.Timing qualified as Timing
 
 newtype GavriellaMizrah = GavriellaMizrah AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 gavriellaMizrah :: AssetCard GavriellaMizrah
 gavriellaMizrah =
   allyWith GavriellaMizrah Cards.gavriellaMizrah (4, 1) (isStoryL .~ True)
 
+instance HasModifiersFor GavriellaMizrah where
+  getModifiersFor (InvestigatorTarget iid) (GavriellaMizrah a) =
+    pure [toModifier a (SkillModifier SkillCombat 1) | controlledBy a iid]
+  getModifiersFor _ _ = pure []
+
+instance HasAbilities GavriellaMizrah where
+  getAbilities (GavriellaMizrah a) =
+    [ restrictedAbility a 1 (ControlsThis <> CanDiscoverCluesAt YourLocation) $
+        ReactionAbility (EnemyAttacksEvenIfCancelled Timing.After You AnyEnemyAttack AnyEnemy) (exhaust a)
+    ]
+
 instance RunMessage GavriellaMizrah where
-  runMessage msg (GavriellaMizrah attrs) = GavriellaMizrah <$> runMessage msg attrs
+  runMessage msg a@(GavriellaMizrah attrs) = case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      push $ InvestigatorDiscoverCluesAtTheirLocation iid (toSource attrs) 1 Nothing
+      pure a
+    _ -> GavriellaMizrah <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Asset/Cards/JeromeDavids.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/JeromeDavids.hs
@@ -6,16 +6,26 @@ where
 
 import Arkham.Prelude
 
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.SkillType
 
 newtype JeromeDavids = JeromeDavids AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 jeromeDavids :: AssetCard JeromeDavids
 jeromeDavids =
   allyWith JeromeDavids Cards.jeromeDavids (1, 4) (isStoryL .~ True)
+
+instance HasModifiersFor JeromeDavids where
+  getModifiersFor (InvestigatorTarget iid) (JeromeDavids a) =
+    pure [toModifier a (SkillModifier SkillIntellect 1) | controlledBy a iid]
+  getModifiersFor _ _ = pure []
+
+instance HasAbilities JeromeDavids where
+  getAbilities (JeromeDavids a) = [restrictedAbility a 1 ControlsThis $ ReactionAbility undefined (exhaust a)]
 
 instance RunMessage JeromeDavids where
   runMessage msg (JeromeDavids attrs) = JeromeDavids <$> runMessage msg attrs

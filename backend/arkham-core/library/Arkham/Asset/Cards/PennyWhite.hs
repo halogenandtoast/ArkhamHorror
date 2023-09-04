2fdd66faf0e9515d969865d9d0f528a7561bde71
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-04 17:33:42 +0900

    [UAD] Start story assets

--- backend/arkham-core/library/Arkham/Asset/Cards/PennyWhite.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/PennyWhite.hs
@@ -8,14 +8,20 @@ import Arkham.Prelude
 
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.SkillType
 
 newtype PennyWhite = PennyWhite AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 pennyWhite :: AssetCard PennyWhite
 pennyWhite =
   allyWith PennyWhite Cards.pennyWhite (3, 2) (isStoryL .~ True)
+
+instance HasModifiersFor PennyWhite where
+  getModifiersFor (InvestigatorTarget iid) (PennyWhite a) =
+    pure [toModifier a (SkillModifier SkillWillpower 1) | controlledBy a iid]
+  getModifiersFor _ _ = pure []
 
 instance RunMessage PennyWhite where
   runMessage msg (PennyWhite attrs) = PennyWhite <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Asset/Cards/ValentinoRivas.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/ValentinoRivas.hs
@@ -8,14 +8,20 @@ import Arkham.Prelude
 
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.SkillType
 
 newtype ValentinoRivas = ValentinoRivas AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 valentinoRivas :: AssetCard ValentinoRivas
 valentinoRivas =
   allyWith ValentinoRivas Cards.valentinoRivas (2, 3) (isStoryL .~ True)
+
+instance HasModifiersFor ValentinoRivas where
+  getModifiersFor (InvestigatorTarget iid) (ValentinoRivas a) =
+    pure [toModifier a (SkillModifier SkillAgility 1) | controlledBy a iid]
+  getModifiersFor _ _ = pure []
 
 instance RunMessage ValentinoRivas where
   runMessage msg (ValentinoRivas attrs) = ValentinoRivas <$> runMessage msg attrs

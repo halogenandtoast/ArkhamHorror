93f066ea53e59b3d227dc52f824769ca22573b2b
Goose <halogenandtoast@gmail.com>
2023-09-06 21:43:38 +0900

    [ICC] French Hill

--- backend/arkham-core/library/Arkham/Location/Cards/FrenchHill_290.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/FrenchHill_290.hs
@@ -1,14 +1,19 @@
-module Arkham.Location.Cards.FrenchHill_290
-  ( frenchHill_290
-  , FrenchHill_290(..)
-  )
+module Arkham.Location.Cards.FrenchHill_290 (
+  frenchHill_290,
+  FrenchHill_290 (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Card
 import Arkham.GameValue
+import Arkham.Location.BreachStatus
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Scenarios.InTheClutchesOfChaos.Helpers
+import Arkham.SkillType ()
 
 newtype FrenchHill_290 = FrenchHill_290 LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -19,9 +24,26 @@ frenchHill_290 = location FrenchHill_290 Cards.frenchHill_290 3 (Static 0)
 
 instance HasAbilities FrenchHill_290 where
   getAbilities (FrenchHill_290 attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withRevealedAbilities
+      attrs
+      [ restrictedAbility attrs 1 (withBreaches attrs Here) $
+          ActionAbility Nothing $
+            ActionCost 1 <> HandDiscardCost 1 AnyCard
+      ]
+
+getCardPayment :: Payment -> Card
+getCardPayment = \case
+  DiscardCardPayment [card] -> card
+  Payments (DiscardCardPayment [card] : _) -> card
+  Payments (_ : ps) -> getCardPayment (Payments ps)
+  _ -> error "invalid payment"
 
 instance RunMessage FrenchHill_290 where
-  runMessage msg (FrenchHill_290 attrs) =
-    FrenchHill_290 <$> runMessage msg attrs
+  runMessage msg l@(FrenchHill_290 attrs) = case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 _ (getCardPayment -> card) -> do
+      let icons = count (== #willpower) $ cdSkills $ toCardDef card
+          n = min (maybe 0 countBreaches $ locationBreaches attrs) icons
+      act <- selectJust AnyAct
+      pushAll $ cycleN n [RemoveBreach (toTarget attrs), PlaceBreach (toTarget act)]
+      pure l
+    _ -> FrenchHill_290 <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Location/Cards/FrenchHill_291.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/FrenchHill_291.hs
@@ -1,14 +1,16 @@
-module Arkham.Location.Cards.FrenchHill_291
-  ( frenchHill_291
-  , FrenchHill_291(..)
-  )
+module Arkham.Location.Cards.FrenchHill_291 (
+  frenchHill_291,
+  FrenchHill_291 (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.GameValue
+import Arkham.Location.BreachStatus
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
 
 newtype FrenchHill_291 = FrenchHill_291 LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -19,9 +21,16 @@ frenchHill_291 = location FrenchHill_291 Cards.frenchHill_291 4 (Static 0)
 
 instance HasAbilities FrenchHill_291 where
   getAbilities (FrenchHill_291 attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    withRevealedAbilities attrs [restrictedAbility attrs 1 Here $ ActionAbility Nothing $ ActionCost 1]
 
 instance RunMessage FrenchHill_291 where
-  runMessage msg (FrenchHill_291 attrs) =
-    FrenchHill_291 <$> runMessage msg attrs
+  runMessage msg l@(FrenchHill_291 attrs) = case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+      push $ beginSkillTest iid attrs attrs #willpower 2
+      pure l
+    PassedSkillTest _iid _ (isSource attrs -> True) SkillTestInitiatorTarget {} _ n -> do
+      let n' = min (maybe 0 countBreaches $ locationBreaches attrs) n
+      act <- selectJust AnyAct
+      pushAll $ cycleN n' [RemoveBreach (toTarget attrs), PlaceBreach (toTarget act)]
+      pure l
+    _ -> FrenchHill_291 <$> runMessage msg attrs

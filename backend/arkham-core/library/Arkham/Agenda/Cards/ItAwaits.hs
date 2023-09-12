ce5f76c0e555194b81696b51285b86aa4df6b46d
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-12 20:13:14 +0900

    The Final Countdown

--- backend/arkham-core/library/Arkham/Agenda/Cards/ItAwaits.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/ItAwaits.hs
@@ -1,26 +1,48 @@
-module Arkham.Agenda.Cards.ItAwaits
-  ( ItAwaits(..)
-  , itAwaits
-  ) where
+module Arkham.Agenda.Cards.ItAwaits (
+  ItAwaits (..),
+  itAwaits,
+) where
 
 import Arkham.Prelude
 
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Runner
+import Arkham.CampaignLogKey
+import Arkham.Campaigns.TheCircleUndone.Memento
 import Arkham.Classes
 import Arkham.GameValue
+import Arkham.Matcher
 import Arkham.Message
+import Arkham.Scenarios.BeforeTheBlackThrone.Helpers
+import Arkham.Treachery.Cards qualified as Treacheries
 
 newtype ItAwaits = ItAwaits AgendaAttrs
   deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 itAwaits :: AgendaCard ItAwaits
-itAwaits = agenda (1, A) ItAwaits Cards.itAwaits (Static 6)
+itAwaits =
+  agendaWith (2, A) ItAwaits Cards.itAwaits (Static 6)
+    $ removeDoomMatchersL
+      %~ (\m -> m {removeDoomEnemies = NotEnemy AnyEnemy})
 
 instance RunMessage ItAwaits where
   runMessage msg a@(ItAwaits attrs) =
     case msg of
-      AdvanceAgenda aid | aid == toId attrs && onSide B attrs ->
-        a <$ pushAll [advanceAgendaDeck attrs]
+      AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
+        ritualSuicideMessages <- commitRitualSuicide attrs
+        lead <- getLead
+        theWraithRecognizesTheCrucifix <- WornCrucifix `inRecordSet` MementosDiscovered
+        investigators <- getInvestigators
+        pushAll
+          $ ritualSuicideMessages
+            <> [findAndDrawEncounterCard lead (cardIs Treacheries.daemonicPiping)]
+            <> ( guard (not theWraithRecognizesTheCrucifix)
+                  *> [beginSkillTest iid attrs iid #willpower 4 | iid <- investigators]
+               )
+            <> [advanceAgendaDeck attrs]
+        pure a
+      FailedThisSkillTest iid (isSource attrs -> True) -> do
+        push $ assignDamageAndHorror iid attrs 1 1
+        pure a
       _ -> ItAwaits <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Agenda/Cards/TheFinalCountdown.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/TheFinalCountdown.hs
@@ -1,26 +1,44 @@
-module Arkham.Agenda.Cards.TheFinalCountdown
-  ( TheFinalCountdown(..)
-  , theFinalCountdown
-  ) where
+module Arkham.Agenda.Cards.TheFinalCountdown (
+  TheFinalCountdown (..),
+  theFinalCountdown,
+) where
 
 import Arkham.Prelude
 
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Runner
+import Arkham.CampaignLogKey
+import Arkham.Campaigns.TheCircleUndone.Memento
 import Arkham.Classes
+import Arkham.Enemy.Cards qualified as Enemies
 import Arkham.GameValue
+import Arkham.Matcher
 import Arkham.Message
+import Arkham.Scenarios.BeforeTheBlackThrone.Helpers
+import Arkham.Treachery.Cards qualified as Treacheries
 
 newtype TheFinalCountdown = TheFinalCountdown AgendaAttrs
   deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theFinalCountdown :: AgendaCard TheFinalCountdown
-theFinalCountdown = agenda (1, A) TheFinalCountdown Cards.theFinalCountdown (Static 8)
+theFinalCountdown =
+  agendaWith (3, A) TheFinalCountdown Cards.theFinalCountdown (Static 8)
+    $ removeDoomMatchersL
+      %~ (\m -> m {removeDoomEnemies = NotEnemy AnyEnemy})
 
 instance RunMessage TheFinalCountdown where
   runMessage msg a@(TheFinalCountdown attrs) =
     case msg of
-      AdvanceAgenda aid | aid == toId attrs && onSide B attrs ->
-        a <$ pushAll [advanceAgendaDeck attrs]
+      AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
+        ritualSuicideMessages <- commitRitualSuicide attrs
+        lead <- getLead
+        theCreatureRecognizesYou <- CornHuskDoll `inRecordSet` MementosDiscovered
+        azathoth <- selectJust $ IncludeOmnipotent $ enemyIs Enemies.azathoth
+        pushAll
+          $ ritualSuicideMessages
+            <> [findAndDrawEncounterCard lead (cardIs Treacheries.daemonicPiping)]
+            <> [RemoveDoom (toSource attrs) (toTarget azathoth) 1 | theCreatureRecognizesYou]
+            <> [advanceAgendaDeck attrs]
+        pure a
       _ -> TheFinalCountdown <$> runMessage msg attrs

d908046db652fba9943928c276871bed3e4e2e95
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-18 20:34:55 +0900

    The World XXI (3)

---
+++ backend/arkham-core/library/Arkham/Asset/Cards/TheWorldXXI3.hs
@@ -0,0 +1,38 @@
+module Arkham.Asset.Cards.TheWorldXXI3 (
+  theWorldXxi3,
+  TheWorldXXI3 (..),
+)
+where
+
+import Arkham.Prelude
+
+import Arkham.Ability
+import Arkham.Asset.Cards qualified as Cards
+import Arkham.Asset.Runner
+import Arkham.Card
+import Arkham.Matcher
+import Arkham.Window (defaultWindows)
+
+newtype TheWorldXXI3 = TheWorldXXI3 AssetAttrs
+  deriving anyclass (IsAsset, HasModifiersFor)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+theWorldXxi3 :: AssetCard TheWorldXXI3
+theWorldXxi3 = asset TheWorldXXI3 Cards.theWorldXxi3
+
+instance HasAbilities TheWorldXXI3 where
+  getAbilities (TheWorldXXI3 a) =
+    [ fastAbility a 1 (exhaust a)
+        $ ControlsThis <> InvestigatorExists (You <> HandWith (LengthIs $ atLeast 8))
+    , restrictedAbility a 2 InYourHand $ freeReaction (GameBegins #when)
+    ]
+
+instance RunMessage TheWorldXXI3 where
+  runMessage msg a@(TheWorldXXI3 attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      pushM $ drawCards iid (toAbilitySource attrs 1) 1
+      pure a
+    InHand _ (UseThisAbility iid (isSource attrs -> True) 2) -> do
+      push $ PutCardIntoPlay iid (toCard attrs) Nothing (defaultWindows iid)
+      pure a
+    _ -> TheWorldXXI3 <$> runMessage msg attrs

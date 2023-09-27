76f40d330d9a7b7a8656459a19d66ba8ff9d3d13
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-27 15:21:47 -0500

    Detached from Reality

--- backend/arkham-core/library/Arkham/Treachery/Cards/DetachedFromReality.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/DetachedFromReality.hs
@@ -6,8 +6,14 @@ where
 
 import Arkham.Prelude
 
+import Arkham.Card
 import Arkham.Classes
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Matcher
 import Arkham.Message
+import Arkham.Movement
+import Arkham.Projection
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
@@ -20,5 +26,22 @@ detachedFromReality = treachery DetachedFromReality Cards.detachedFromReality
 
 instance RunMessage DetachedFromReality where
   runMessage msg t@(DetachedFromReality attrs) = case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      mWondrousJourney <- selectOne $ locationIs Locations.dreamGateWondrousJourney
+      enemies <- selectList $ enemyEngagedWith iid
+      case mWondrousJourney of
+        Just wondrousJourney -> do
+          currentLocation <- field InvestigatorLocation iid
+          pointlessReality <- genCard Locations.dreamGatePointlessReality
+          pushAll $ ReplaceLocation wondrousJourney pointlessReality Swap
+            : map (DisengageEnemy iid) enemies
+              <> [ MoveTo $ move (toAbilitySource attrs 1) iid wondrousJourney
+                 | currentLocation /= Just wondrousJourney
+                 ]
+        Nothing -> do
+          (dreamGate, placement) <- placeLocationCard Locations.dreamGatePointlessReality
+          pushAll
+            $ map (DisengageEnemy iid) enemies
+            <> [placement, MoveTo $ move (toAbilitySource attrs 1) iid dreamGate]
+      pure t
     _ -> DetachedFromReality <$> runMessage msg attrs

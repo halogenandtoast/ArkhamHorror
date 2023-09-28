c61fb59108d1b9562031eabdaa1cc14bf9f8e020
Matthew Mongeau <halogenandtoast@gmail.com>
2023-09-28 00:31:57 -0500

    costModifier cleanup

--- backend/arkham-core/library/Arkham/Asset/Cards/DeVermisMysteriis2.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/DeVermisMysteriis2.hs
@@ -9,12 +9,9 @@ import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
 import Arkham.Card
-import Arkham.Effect.Window
-import Arkham.EffectMetadata
 import Arkham.Matcher
-import Arkham.Timing qualified as Timing
 import Arkham.Trait (Trait (Insight, Spell))
-import Arkham.Window (mkWindow)
+import Arkham.Window (mkWhen)
 import Arkham.Window qualified as Window
 
 newtype DeVermisMysteriis2 = DeVermisMysteriis2 AssetAttrs
@@ -26,67 +23,28 @@ deVermisMysteriis2 = asset DeVermisMysteriis2 Cards.deVermisMysteriis2
 
 instance HasAbilities DeVermisMysteriis2 where
   getAbilities (DeVermisMysteriis2 a) =
-    [ restrictedAbility
-        a
-        1
-        ( ControlsThis
-            <> ExtendedCardExists
-              ( PlayableCardWithCostReduction 1
-                  $ InDiscardOf You
-                  <> BasicCardMatch
-                    ( CardWithOneOf [CardWithTrait Spell, CardWithTrait Insight]
-                        <> CardWithType EventType
-                    )
-              )
-        )
-        $ ActionAbility Nothing
-        $ ActionCost 1
-        <> ExhaustCost (toTarget a)
-        <> DoomCost (toSource a) (toTarget a) 1
+    [ controlledAbility a 1 (exists $ cardMatcher You)
+        $ actionAbilityWithCost (exhaust a <> DoomCost (toSource a) (toTarget a) 1)
     ]
+
+cardMatcher :: InvestigatorMatcher -> ExtendedCardMatcher
+cardMatcher who =
+  PlayableCardWithCostReduction 1
+    $ InDiscardOf who
+    <> BasicCardMatch (#event <> CardWithOneOf [CardWithTrait Spell, CardWithTrait Insight])
 
 instance RunMessage DeVermisMysteriis2 where
   runMessage msg a@(DeVermisMysteriis2 attrs) = case msg of
     UseCardAbility iid (isSource attrs -> True) 1 windows' _ -> do
-      let
-        windows'' =
-          nub
-            $ windows'
-            <> [ mkWindow Timing.When (Window.DuringTurn iid)
-               , mkWindow Timing.When Window.FastPlayerWindow
-               ]
-      cards <-
-        selectList
-          $ PlayableCardWithCostReduction 1
-          $ InDiscardOf (InvestigatorWithId iid)
-          <> BasicCardMatch
-            (EventCard <> CardWithOneOf [CardWithTrait Spell, CardWithTrait Insight] <> EventCard)
+      let windows'' = nub $ windows' <> [mkWhen (Window.DuringTurn iid), mkWhen Window.FastPlayerWindow]
+      cards <- selectList $ cardMatcher (InvestigatorWithId iid)
 
       push
-        $ chooseOne
-          iid
-          [ targetLabel
+        $ chooseOne iid
+        $ [ targetLabel
             (toCardId card)
-            [ CreateWindowModifierEffect
-                (EffectCardCostWindow $ toCardId card)
-                ( EffectModifiers
-                    $ toModifiers
-                      (toSource attrs)
-                      [ ReduceCostOf (CardWithId $ toCardId card) 1
-                      ]
-                )
-                (toSource attrs)
-                (toTarget $ toCardId card)
-            , CreateWindowModifierEffect
-                EffectAbilityWindow
-                ( EffectModifiers
-                    $ toModifiers
-                      (toSource attrs)
-                      [ RemoveFromGameInsteadOfDiscard
-                      ]
-                )
-                (toSource attrs)
-                (toTarget $ toCardId card)
+            [ costModifier attrs (toCardId card) (ReduceCostOf (CardWithId $ toCardId card) 1)
+            , abilityModifier attrs (toCardId card) RemoveFromGameInsteadOfDiscard
             , AddToHand iid [card]
             , PayCardCost iid card windows''
             , RemoveFromGame (CardIdTarget $ toCardId card)

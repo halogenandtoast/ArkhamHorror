f051c3e7ed2fe97177cecf462fc63cca42c3aa5b
Matthew Mongeau <halogenandtoast@gmail.com>
2023-10-01 10:42:28 +0900

    Formatting for FAQ and Errata

--- backend/arkham-core/tests/Arkham/Investigator/Cards/DaisyWalkerSpec.hs
+++ backend/arkham-core/tests/Arkham/Investigator/Cards/DaisyWalkerSpec.hs
@@ -24,40 +24,41 @@ spec = describe "Daisy Walker" $ do
           [duringTurn $ toId self]
           (ActionCost 1)
 
-    it "is lost after other additional actions" . gameTestWith daisyWalker $ \self -> do
-      run BeginRound
-      run $ LoseActions (toId self) (TestSource mempty) 4
-      assert
-        $ not
-        <$> getCanAffordCost
-          (toId self)
-          (toAbilitySource (TestSource $ singleton Tome) 1)
-          Nothing
-          [duringTurn $ toId self]
-          (ActionCost 1)
+    faq "If you have no more of those actions to lose, then you start losing “additional” actions" $ do
+      it "is lost after other additional actions" . gameTestWith daisyWalker $ \self -> do
+        run BeginRound
+        run $ LoseActions (toId self) (TestSource mempty) 4
+        assert
+          $ not
+          <$> getCanAffordCost
+            (toId self)
+            (toAbilitySource (TestSource $ singleton Tome) 1)
+            Nothing
+            [duringTurn $ toId self]
+            (ActionCost 1)
 
-    it "is in the choices for additional actions to lose" . gameTestWith daisyWalker $ \self -> do
-      run BeginRound
-      self `putCardIntoPlay` Cards.expeditionJournal
-      run $ LoseActions (toId self) (TestSource mempty) 4
-      chooseOptionMatching "lose tome action" \case
-        Label _ [LoseAdditionalAction _ _ (TraitRestrictedAdditionalAction Tome AbilitiesOnly)] -> True
-        _ -> False
-      assert
-        $ not
-        <$> getCanAffordCost
-          (toId self)
-          (toAbilitySource (TestSource $ singleton Tome) 1)
-          Nothing
-          [duringTurn $ toId self]
-          (ActionCost 1)
-      assert
-        $ getCanAffordCost
-          (toId self)
-          (toAbilitySource (TestSource mempty) 2)
-          (Just #explore)
-          [duringTurn $ toId self]
-          (ActionCost 1)
+      it "is in the choices for additional actions to lose" . gameTestWith daisyWalker $ \self -> do
+        run BeginRound
+        self `putCardIntoPlay` Cards.expeditionJournal
+        run $ LoseActions (toId self) (TestSource mempty) 4
+        chooseOptionMatching "lose tome action" \case
+          Label _ [LoseAdditionalAction _ _ (TraitRestrictedAdditionalAction Tome AbilitiesOnly)] -> True
+          _ -> False
+        assert
+          $ not
+          <$> getCanAffordCost
+            (toId self)
+            (toAbilitySource (TestSource $ singleton Tome) 1)
+            Nothing
+            [duringTurn $ toId self]
+            (ActionCost 1)
+        assert
+          $ getCanAffordCost
+            (toId self)
+            (toAbilitySource (TestSource mempty) 2)
+            (Just #explore)
+            [duringTurn $ toId self]
+            (ActionCost 1)
 
   context "elder sign" $ do
     it "forces you to draw one card for each Tome you control" $ gameTestWith daisyWalker $ \self -> do

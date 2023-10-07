c2ccaba9210d318d946ed17724f4f5588a7c6e76
Goose <halogenandtoast@gmail.com>
2023-10-07 15:28:17 +0900

    Dream Gate Spec

---
+++ backend/arkham-core/tests/Arkham/Location/Cards/DreamGatePointlessRealitySpec.hs
@@ -0,0 +1,56 @@
+module Arkham.Location.Cards.DreamGatePointlessRealitySpec (spec) where
+
+import Arkham.Event.Cards qualified as Events
+import Arkham.Investigator.Cards (lukeRobinson, rolandBanks)
+import Arkham.Location.Cards qualified as Locations
+import TestImport.New
+
+spec :: Spec
+spec = fdescribe "Dream Gate (Pointless Reality)" do
+  it "Enemies and investigators other than Luke Robinson cannot enter Dream-Gate."
+    . gameTestWith lukeRobinson
+    $ \self -> do
+      (location1, location2) <- testConnectedLocations id id
+      (dreamGate, placement) <- placeLocationCard Locations.dreamGatePointlessReality
+      run placement
+      self `moveTo` dreamGate
+      enemy <- testEnemy & hunter
+      enemy `spawnAt` location1
+      run HuntersMove
+      enemy.location `shouldReturn` Just (toId location1)
+      roland <- addInvestigator rolandBanks & prop @"resources" 2
+      self `moveTo` location2
+      roland `moveTo` location2
+      duringTurn roland do
+        run HuntersMove
+        run $ RevealLocation Nothing (toId location1)
+        roland `playEvent` Events.elusive
+        -- can only move to location 1
+        roland.location `shouldReturn` Just (toId location1)
+
+  context "At the end of the investigation phase" do
+    it
+      "Set Dream-Gate aside, out of play. (If Luke Robinson is here, move him to any revealed location.)"
+      . gameTestWith lukeRobinson
+      $ \self -> do
+        (location1, location2) <- testConnectedLocations id id
+        (dreamGate, placement) <- placeLocationCard Locations.dreamGatePointlessReality
+        run placement
+        self `moveTo` location1
+        duringPhase #investigation $ do
+          self `moveTo` dreamGate
+        useForcedAbility
+        assertNotTarget location2
+        chooseTarget location1
+        self.location `shouldReturn` Just (toId location1)
+
+    it
+      "if there are no revealed locations, he is defeated"
+      . gameTestWith lukeRobinson
+      $ \self -> do
+        (dreamGate, placement) <- placeLocationCard Locations.dreamGatePointlessReality
+        run placement
+        duringPhase #investigation $ do
+          self `moveTo` dreamGate
+        useForcedAbility
+        assert self.defeated

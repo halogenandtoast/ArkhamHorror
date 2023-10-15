e228b8db44e1e7a33568695a14ee2bf25dcbf211
Goose <halogenandtoast@gmail.com>
2023-10-15 19:53:15 +0900

    Fix double double

--- backend/arkham-core/library/Arkham/Asset/Cards/DoubleDouble4.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/DoubleDouble4.hs
@@ -8,11 +8,7 @@ import Arkham.Prelude
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner hiding (PlayCard)
-import Arkham.Card
 import Arkham.Matcher
-import Arkham.Timing qualified as Timing
-import Arkham.Window (Window (..))
-import Arkham.Window qualified as Window
 
 newtype DoubleDouble4 = DoubleDouble4 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -25,22 +21,13 @@ instance HasAbilities DoubleDouble4 where
   getAbilities (DoubleDouble4 a) =
     [ restrictedAbility a 1 ControlsThis
         $ ReactionAbility
-          ( PlayCard
-              Timing.After
-              You
-              (BasicCardMatch (CardWithType EventType))
-          )
-          (ExhaustCost $ toTarget a)
+          (PlayCard #after You $ #event <> CardWithoutModifier RemoveFromGameInsteadOfDiscard)
+          (exhaust a)
     ]
-
-getCardPlayed :: [Window] -> Card
-getCardPlayed [] = error "missing play card window"
-getCardPlayed ((windowType -> Window.PlayCard _ c) : _) = c
-getCardPlayed (_ : xs) = getCardPlayed xs
 
 instance RunMessage DoubleDouble4 where
   runMessage msg a@(DoubleDouble4 attrs) = case msg of
-    UseCardAbility iid (isSource attrs -> True) 1 ws@(getCardPlayed -> card) _ -> do
+    UseCardAbility iid (isSource attrs -> True) 1 ws@(cardPlayed -> card) _ -> do
       pushAll [AddToHand iid [card], PutCardIntoPlay iid card Nothing ws]
       pure a
     _ -> DoubleDouble4 <$> runMessage msg attrs

e8ac9e5fb1f3adb3fe2c4b2fc5fdb326283ae538
Goose <halogenandtoast@gmail.com>
2023-10-21 18:29:26 -0600

    Bloodstained Dagger

--- backend/arkham-core/library/Arkham/Asset/Cards/BloodstainedDagger.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/BloodstainedDagger.hs
@@ -1,21 +1,49 @@
-module Arkham.Asset.Cards.BloodstainedDagger
-  ( bloodstainedDagger
-  , BloodstainedDagger(..)
-  )
+module Arkham.Asset.Cards.BloodstainedDagger (
+  bloodstainedDagger,
+  BloodstainedDagger (..),
+)
 where
 
 import Arkham.Prelude
 
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
 
 newtype BloodstainedDagger = BloodstainedDagger AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 bloodstainedDagger :: AssetCard BloodstainedDagger
 bloodstainedDagger =
   asset BloodstainedDagger Cards.bloodstainedDagger
 
+instance HasAbilities BloodstainedDagger where
+  getAbilities (BloodstainedDagger a) =
+    [ withTooltip "{action}: _Fight_. You get +2 {combat} for this attack."
+        $ restrictedAbility a 1 ControlsThis fightAction_
+    , withTooltip
+        "{action}: Exhaust Bloodstained Dagger and take 1 horror: _Fight_. You get +2 {combat} and deal +1 damage for this attack. If this attack defeats an enemy, draw 1 card."
+        $ restrictedAbility a 2 ControlsThis
+        $ fightAction (exhaust a <> HorrorCost (toSource a) YouTarget 1)
+    ]
+
 instance RunMessage BloodstainedDagger where
-  runMessage msg (BloodstainedDagger attrs) = BloodstainedDagger <$> runMessage msg attrs
+  runMessage msg a@(BloodstainedDagger attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      pushAll
+        [ skillTestModifier attrs iid (SkillModifier #combat 2)
+        , chooseFightEnemy iid (toAbilitySource attrs 1) #combat
+        ]
+      pure a
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      pushAll
+        [ skillTestModifiers attrs iid [SkillModifier #combat 2, DamageDealt 1]
+        , chooseFightEnemy iid (toAbilitySource attrs 2) #combat
+        ]
+      pure a
+    EnemyDefeated _ _ (isAbilitySource attrs 2 -> True) _ -> do
+      for_ (assetController attrs) $ \iid -> do
+        pushM $ drawCards iid (toAbilitySource attrs 2) 1
+      pure a
+    _ -> BloodstainedDagger <$> runMessage msg attrs

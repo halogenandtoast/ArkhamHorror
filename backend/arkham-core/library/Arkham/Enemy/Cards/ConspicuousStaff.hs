8be7e0ab8f2743872b8f316278883993ce428e60
Goose <halogenandtoast@gmail.com>
2023-10-24 10:25:54 -0400

    Hotel Base Enemies

--- backend/arkham-core/library/Arkham/Enemy/Cards/ConspicuousStaff.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/ConspicuousStaff.hs
@@ -1,7 +1,7 @@
-module Arkham.Enemy.Cards.ConspicuousStaff
-  ( conspicuousStaff
-  , ConspicuousStaff(..)
-  )
+module Arkham.Enemy.Cards.ConspicuousStaff (
+  conspicuousStaff,
+  ConspicuousStaff (..),
+)
 where
 
 import Arkham.Prelude
@@ -9,13 +9,23 @@ import Arkham.Prelude
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Runner
+import Arkham.Matcher
+import Arkham.Trait (Trait (CrimeScene))
 
 newtype ConspicuousStaff = ConspicuousStaff EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 conspicuousStaff :: EnemyCard ConspicuousStaff
-conspicuousStaff = enemy ConspicuousStaff Cards.conspicuousStaff (3, Static 2, 3) (1, 1)
+conspicuousStaff =
+  enemyWith
+    ConspicuousStaff
+    Cards.conspicuousStaff
+    (3, Static 2, 3)
+    (1, 1)
+    ( (preyL .~ Prey MostClues)
+        . (spawnAtL ?~ SpawnAt (NearestLocationToYou $ LocationWithTrait CrimeScene))
+    )
 
 instance RunMessage ConspicuousStaff where
   runMessage msg (ConspicuousStaff attrs) =

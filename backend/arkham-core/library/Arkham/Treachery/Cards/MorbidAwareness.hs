7991c67ce76e6400826bc3dd47e0e454de742e16
Goose <halogenandtoast@gmail.com>
2023-10-25 16:07:09 -0400

    Sinister Solution Cards

--- backend/arkham-core/library/Arkham/Treachery/Cards/MorbidAwareness.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/MorbidAwareness.hs
@@ -1,13 +1,18 @@
-module Arkham.Treachery.Cards.MorbidAwareness
-  ( morbidAwareness
-  , MorbidAwareness(..)
-  )
+module Arkham.Treachery.Cards.MorbidAwareness (
+  morbidAwareness,
+  MorbidAwareness (..),
+)
 where
 
 import Arkham.Prelude
 
 import Arkham.Classes
+import Arkham.Distance
+import {-# SOURCE #-} Arkham.GameEnv
+import Arkham.Helpers.Investigator
+import Arkham.Investigator.Types (Field (..))
 import Arkham.Message
+import Arkham.Projection
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Runner
 
@@ -20,5 +25,24 @@ morbidAwareness = treachery MorbidAwareness Cards.morbidAwareness
 
 instance RunMessage MorbidAwareness where
   runMessage msg t@(MorbidAwareness attrs) = case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      l1 <- getJustLocation iid
+      l2 <- getJustLocationByName "Room 212"
+      mdistance <- getDistance l1 l2
+      let n = 6 - maybe 0 unDistance mdistance
+      push $ beginSkillTest iid (toSource attrs) iid #willpower n
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      hasClues <- fieldMap InvestigatorClues (> 0) iid
+      player <- getPlayer iid
+      push
+        $ chooseOrRunOne player
+        $ [ Label
+            "Place 1 of your clues on your location"
+            [InvestigatorPlaceCluesOnLocation iid (toSource attrs) 1]
+          | hasClues
+          ]
+        <> [Label "Take 2 horror" [assignHorror iid (toSource attrs) 2]]
+
+      pure t
     _ -> MorbidAwareness <$> runMessage msg attrs

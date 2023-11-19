a54805b30d774cf904623fb2ccb1692b899130d1
Goose <halogenandtoast@gmail.com>
2023-11-19 14:23:30 +0900

    Fix abilities for multiplayer

--- backend/arkham-core/library/Arkham/Asset/Cards/StickToThePlan3.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/StickToThePlan3.hs
@@ -10,7 +10,6 @@ import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
 import Arkham.Card
 import Arkham.Matcher hiding (PlaceUnderneath)
-import Arkham.Timing qualified as Timing
 import Arkham.Trait qualified as Trait
 
 newtype StickToThePlan3 = StickToThePlan3 AssetAttrs
@@ -25,57 +24,49 @@ instance HasModifiersFor StickToThePlan3 where
     pure $ toModifiers attrs (map AsIfInHand $ assetCardsUnderneath attrs)
   getModifiersFor (CardIdTarget cardId) (StickToThePlan3 attrs)
     | cardId `elem` map toCardId (assetCardsUnderneath attrs) = do
-        pure $ toModifiers attrs [AdditionalCost $ ExhaustCost $ AssetTarget $ toId attrs]
+        pure $ toModifiers attrs [AdditionalCost $ exhaust attrs]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities StickToThePlan3 where
   getAbilities (StickToThePlan3 attrs) =
     [ restrictedAbility attrs 1 ControlsThis
-        $ ReactionAbility (DrawingStartingHand Timing.When You) Free
+        $ freeReaction
+        $ DrawingStartingHand #when You
     ]
 
 instance RunMessage StickToThePlan3 where
   runMessage msg a@(StickToThePlan3 attrs) = case msg of
     UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
-      push
-        $ search
-          iid
-          (toSource attrs)
-          (toTarget iid)
-          [fromDeck]
-          AnyCard
-          (DeferSearchedToTarget $ toTarget attrs)
+      push $ search iid attrs iid [fromDeck] AnyCard (DeferSearchedToTarget $ toTarget attrs)
       pure a
     SearchFound iid (isTarget attrs -> True) _ cards -> do
       let
         tacticsAndSupplies =
           filter
-            ( `cardMatch`
-                ( CardWithType EventType
-                    <> CardWithOneOf
-                      (map CardWithTrait [Trait.Tactic, Trait.Supply])
-                )
-            )
+            (`cardMatch` (#event <> oneOf (map CardWithTrait [Trait.Tactic, Trait.Supply])))
             cards
       additionalTargets <- getAdditionalSearchTargets iid
       player <- getPlayer iid
-      push
-        $ chooseUpToN
-          player
-          (3 + additionalTargets)
-          "Choose no more events"
-          [ targetLabel
-            (toCardId card)
-            [ RemoveCardFromSearch iid (toCardId card)
-            , PlaceUnderneath (toTarget attrs) [card]
-            ]
-          | card <- tacticsAndSupplies
-          ]
+      if null tacticsAndSupplies
+        then pushAll [FocusCards cards, chooseOne player [Label "No cards found" [UnfocusCards]]]
+        else
+          push
+            $ chooseUpToN
+              player
+              (3 + additionalTargets)
+              "Choose no more events"
+              [ targetLabel
+                (toCardId card)
+                [ RemoveCardFromSearch iid (toCardId card)
+                , PlaceUnderneath (toTarget attrs) [card]
+                ]
+              | card <- tacticsAndSupplies
+              ]
       pure a
     InitiatePlayCard iid card _ _ _ | controlledBy attrs iid && card `elem` assetCardsUnderneath attrs -> do
       let remaining = deleteFirstMatch (== card) $ assetCardsUnderneath attrs
       pushAll
-        [ costModifier attrs (toCardId card) (AdditionalCost $ ExhaustCost $ AssetTarget $ toId attrs)
+        [ costModifier attrs (toCardId card) (AdditionalCost $ ExhaustCost $ toTarget attrs)
         , addToHand iid card
         , msg
         ]

fc33d1229b7fc536851dce942d2d69ada883476c
Matthew Mongeau <halogenandtoast@gmail.com>
2023-11-22 19:10:35 +0900

    Finish Beyond the Gates of Sleep Encounter Set

--- backend/arkham-core/library/Arkham/Asset/Cards/RandolphCarterExpertDreamer.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/RandolphCarterExpertDreamer.hs
@@ -1,20 +1,40 @@
 module Arkham.Asset.Cards.RandolphCarterExpertDreamer (
   randolphCarterExpertDreamer,
   RandolphCarterExpertDreamer (..),
-)
-where
+) where
 
-import Arkham.Prelude
-
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.Capability
+import Arkham.ChaosToken
+import Arkham.Matcher
+import Arkham.Prelude
+import Arkham.Window qualified as Window
 
 newtype RandolphCarterExpertDreamer = RandolphCarterExpertDreamer AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 randolphCarterExpertDreamer :: AssetCard RandolphCarterExpertDreamer
 randolphCarterExpertDreamer = ally RandolphCarterExpertDreamer Cards.randolphCarterExpertDreamer (3, 2)
 
+instance HasModifiersFor RandolphCarterExpertDreamer where
+  getModifiersFor (InvestigatorTarget iid) (RandolphCarterExpertDreamer a) | a `controlledBy` iid = do
+    pure $ toModifiers a [SkillModifier #combat 1, SkillModifier #agility 1]
+  getModifiersFor _ _ = pure []
+
+instance HasAbilities RandolphCarterExpertDreamer where
+  getAbilities (RandolphCarterExpertDreamer x) =
+    [ controlledAbility x 1 (exists $ You <> can.draw.cards)
+        $ ReactionAbility (RevealChaosToken #when (InvestigatorAt YourLocation) $ ChaosTokenFaceIs Tablet)
+        $ exhaust x
+    ]
+
 instance RunMessage RandolphCarterExpertDreamer where
-  runMessage msg (RandolphCarterExpertDreamer attrs) = RandolphCarterExpertDreamer <$> runMessage msg attrs
+  runMessage msg a@(RandolphCarterExpertDreamer attrs) = case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 (Window.revealedChaosTokens -> tokens) _ -> do
+      drawing <- drawCards iid (toAbilitySource attrs 1) 2
+      push $ If (Window.RevealChaosTokenAssetAbilityEffect iid tokens (toId attrs)) [drawing]
+      pure a
+    _ -> RandolphCarterExpertDreamer <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Enemy/Cards/AncientZoog.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/AncientZoog.hs
@@ -1,7 +1,7 @@
-module Arkham.Enemy.Cards.AncientZoog
-  ( ancientZoog
-  , AncientZoog(..)
-  )
+module Arkham.Enemy.Cards.AncientZoog (
+  ancientZoog,
+  AncientZoog (..),
+)
 where
 
 import Arkham.Prelude
@@ -9,14 +9,46 @@ import Arkham.Prelude
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Runner
+import Arkham.Matcher
+import Arkham.Trait (Trait (Zoog))
 
 newtype AncientZoog = AncientZoog EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 ancientZoog :: EnemyCard AncientZoog
-ancientZoog = enemy AncientZoog Cards.ancientZoog (3, Static 3, 3) (1, 1)
+ancientZoog =
+  enemyWith
+    AncientZoog
+    Cards.ancientZoog
+    (3, Static 3, 3)
+    (1, 1)
+    ( spawnAtL
+        ?~ SpawnAtFirst
+          [ SpawnAt $ LocationWithUnrevealedTitle "Enchanted Woods" <> UnrevealedLocation
+          , SpawnAt $ LocationWithUnrevealedTitle "Enchanted Woods"
+          ]
+    )
+
+instance HasAbilities AncientZoog where
+  getAbilities (AncientZoog x) =
+    withBaseAbilities x
+      $ [ restrictedAbility
+            x
+            1
+            ( exists (ReadyEnemy <> EnemyWithId (toId x))
+                <> exists (EnemyWithTrait Zoog <> SwarmingEnemy <> NotEnemy IsSwarm)
+            )
+            $ ForcedAbility
+            $ PhaseBegins #when #enemy
+        ]
 
 instance RunMessage AncientZoog where
-  runMessage msg (AncientZoog attrs) =
-    AncientZoog <$> runMessage msg attrs
+  runMessage msg e@(AncientZoog attrs) = case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      zoogs <- selectList $ EnemyWithTrait Zoog <> SwarmingEnemy <> NotEnemy IsSwarm
+      lead <- getLead
+      for_ zoogs $ \zoog ->
+        push $ PlaceSwarmCards lead zoog 1
+      pure e
+    _ -> AncientZoog <$> runMessage msg attrs

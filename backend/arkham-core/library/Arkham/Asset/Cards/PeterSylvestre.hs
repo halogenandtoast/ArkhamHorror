672962fd35b4b6e1336b4b9373ef1cdd87636df5
Matthew Mongeau <halogenandtoast@gmail.com>
2023-11-22 19:10:35 +0900

    Finish Beyond the Gates of Sleep Encounter Set

--- backend/arkham-core/library/Arkham/Asset/Cards/PeterSylvestre.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/PeterSylvestre.hs
@@ -1,17 +1,11 @@
-module Arkham.Asset.Cards.PeterSylvestre (
-  PeterSylvestre (..),
-  peterSylvestre,
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.PeterSylvestre (PeterSylvestre (..), peterSylvestre) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
 import Arkham.Damage
 import Arkham.Matcher
-import Arkham.SkillType
-import Arkham.Timing qualified as Timing
+import Arkham.Prelude
 
 newtype PeterSylvestre = PeterSylvestre AssetAttrs
   deriving anyclass (IsAsset)
@@ -21,25 +15,22 @@ peterSylvestre :: AssetCard PeterSylvestre
 peterSylvestre = ally PeterSylvestre Cards.peterSylvestre (1, 2)
 
 instance HasModifiersFor PeterSylvestre where
-  getModifiersFor (InvestigatorTarget iid) (PeterSylvestre a) =
-    pure [toModifier a (SkillModifier SkillAgility 1) | controlledBy a iid]
+  getModifiersFor (InvestigatorTarget iid) (PeterSylvestre a) | a `controlledBy` iid = do
+    pure $ toModifiers a [SkillModifier #agility 1]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities PeterSylvestre where
   getAbilities (PeterSylvestre x) =
-    [ restrictedAbility
+    [ controlledAbility
         x
         1
-        ( ControlsThis
-            <> AssetExists
-              (HealableAsset (toSource x) HorrorType $ AssetWithId (toId x))
-        )
-        (ReactionAbility (TurnEnds Timing.After You) Free)
+        (exists $ HealableAsset (toSource x) HorrorType $ AssetWithId (toId x))
+        (freeReaction $ TurnEnds #after You)
     ]
 
 instance RunMessage PeterSylvestre where
   runMessage msg a@(PeterSylvestre attrs) = case msg of
-    UseCardAbility _ source 1 _ _ | isSource attrs source -> do
-      push $ HealHorror (toTarget attrs) (toSource attrs) 1
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      push $ HealHorror (toTarget attrs) (toAbilitySource attrs 1) 1
       pure a
     _ -> PeterSylvestre <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Enemy/Cards/LaboringGug.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/LaboringGug.hs
@@ -9,13 +9,21 @@ import Arkham.Prelude
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Runner
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Matcher
 
 newtype LaboringGug = LaboringGug EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
+  deriving anyclass (IsEnemy)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 laboringGug :: EnemyCard LaboringGug
 laboringGug = enemy LaboringGug Cards.laboringGug (5, Static 5, 5) (3, 1)
+
+instance HasModifiersFor LaboringGug where
+  getModifiersFor (LocationTarget lid) (LaboringGug attrs) = do
+    isTheEnchantedPath <- lid <=~> locationIs Locations.theEnchantedPath
+    pure $ toModifiers attrs [CannotBeEnteredBy $ EnemyWithId $ toId attrs | isTheEnchantedPath]
+  getModifiersFor _ _ = pure []
 
 instance RunMessage LaboringGug where
   runMessage msg (LaboringGug attrs) =

db68efd8761e6f92ca348bacc55c45cf68cc360d
Matthew Mongeau <halogenandtoast@gmail.com>
2023-12-04 09:38:57 +0900

    Cleanup Fieldwork

--- backend/arkham-core/library/Arkham/Asset/Cards/Fieldwork.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/Fieldwork.hs
@@ -1,16 +1,11 @@
-module Arkham.Asset.Cards.Fieldwork (
-  fieldwork,
-  Fieldwork (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.Fieldwork (fieldwork, fieldworkEffect, Fieldwork (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
-import Arkham.Card.CardCode
+import Arkham.Effect.Runner
 import Arkham.Matcher
-import Arkham.Timing qualified as Timing
+import Arkham.Prelude
 
 newtype Fieldwork = Fieldwork AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -21,26 +16,34 @@ fieldwork = asset Fieldwork Cards.fieldwork
 
 instance HasAbilities Fieldwork where
   getAbilities (Fieldwork attrs) =
-    [ restrictedAbility
-        attrs
-        1
-        ControlsThis
-        ( ReactionAbility
-            ( Enters Timing.After You
-                $ LocationWithClues (GreaterThan $ Static 0)
-            )
-            $ ExhaustCost
-            $ toTarget attrs
-        )
-    ]
+    [reaction attrs 1 ControlsThis (exhaust attrs) (Enters #after You LocationWithAnyClues)]
 
 instance RunMessage Fieldwork where
   runMessage msg a@(Fieldwork attrs) = case msg of
-    UseCardAbility iid source 1 _ _
-      | isSource attrs source ->
-          a
-            <$ push
-              ( CreateEffect (toCardCode attrs) Nothing source
-                  $ InvestigatorTarget iid
-              )
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ createCardEffect Cards.fieldwork Nothing (attrs.ability 1) iid
+      pure a
     _ -> Fieldwork <$> runMessage msg attrs
+
+newtype FieldworkEffect = FieldworkEffect EffectAttrs
+  deriving anyclass (HasAbilities, IsEffect)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+fieldworkEffect :: EffectArgs -> FieldworkEffect
+fieldworkEffect = cardEffect FieldworkEffect Cards.fieldwork
+
+instance HasModifiersFor FieldworkEffect where
+  getModifiersFor target (FieldworkEffect a) | a `is` target = do
+    mSkillTestSource <- getSkillTestSource
+    pure $ toModifiers a [AnySkillValue 2 | isJust mSkillTestSource]
+  getModifiersFor _ _ = pure []
+
+instance RunMessage FieldworkEffect where
+  runMessage msg e@(FieldworkEffect attrs) = case msg of
+    EndPhase -> do
+      push $ disable attrs
+      pure e
+    SkillTestEnds {} -> do
+      push $ disable attrs
+      pure e
+    _ -> FieldworkEffect <$> runMessage msg attrs

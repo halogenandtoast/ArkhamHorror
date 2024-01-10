05a176f1c02f6dfe605a7ad768d97c29d5d5a4e3
Matthew Mongeau <halogenandtoast@gmail.com>
2024-01-10 09:39:50 +0900

    [SFK] Mt. Ngranek

--- backend/arkham-core/library/Arkham/Location/Cards/MtNgranek.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/MtNgranek.hs
@@ -1,14 +1,13 @@
-module Arkham.Location.Cards.MtNgranek
-  ( mtNgranek
-  , MtNgranek(..)
-  )
-where
-
-import Arkham.Prelude
+module Arkham.Location.Cards.MtNgranek (mtNgranek, MtNgranek (..)) where
 
 import Arkham.GameValue
+import Arkham.Helpers.Story
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Prelude
+import Arkham.ScenarioLogKey
+import Arkham.Story.Cards qualified as Story
 
 newtype MtNgranek = MtNgranek LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -19,9 +18,26 @@ mtNgranek = location MtNgranek Cards.mtNgranek 3 (PerPlayer 1)
 
 instance HasAbilities MtNgranek where
   getAbilities (MtNgranek attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    veiled
+      attrs
+      [ restrictedAbility attrs 1 (not_ $ Remembered ObtainedSuppliesFromBaharna)
+          $ forced
+          $ oneOf [Enters #after You (be attrs), DiscoverClues #after You (be attrs) AnyValue]
+      ]
 
 instance RunMessage MtNgranek where
-  runMessage msg (MtNgranek attrs) =
-    MtNgranek <$> runMessage msg attrs
+  runMessage msg l@(MtNgranek attrs) = case msg of
+    Flip iid _ (isTarget attrs -> True) -> do
+      readStory iid (toId attrs) Story.theLikenessOfOld
+      pure . MtNgranek $ attrs & canBeFlippedL .~ False
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      player <- getPlayer iid
+      push
+        $ chooseOne
+          player
+          [SkillLabel sType [beginSkillTest iid (attrs.ability 1) iid sType 3] | sType <- [#combat, #agility]]
+      pure l
+    FailedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
+      pushAll [SetActions iid (attrs.ability 1) 0, ChooseEndTurn iid]
+      pure l
+    _ -> MtNgranek <$> runMessage msg attrs

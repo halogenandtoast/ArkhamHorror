484309108453e75003fdccd836d3a34f6673896f
Matthew Mongeau <halogenandtoast@gmail.com>
2024-01-10 09:51:35 +0900

    [SFK] Nameless Ruins

--- backend/arkham-core/library/Arkham/Location/Cards/NamelessRuins.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/NamelessRuins.hs
@@ -1,14 +1,12 @@
-module Arkham.Location.Cards.NamelessRuins
-  ( namelessRuins
-  , NamelessRuins(..)
-  )
-where
-
-import Arkham.Prelude
+module Arkham.Location.Cards.NamelessRuins (namelessRuins, NamelessRuins (..)) where
 
 import Arkham.GameValue
+import Arkham.Helpers.Story
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Prelude
+import Arkham.Story.Cards qualified as Story
 
 newtype NamelessRuins = NamelessRuins LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -19,9 +17,19 @@ namelessRuins = location NamelessRuins Cards.namelessRuins 5 (PerPlayer 1)
 
 instance HasAbilities NamelessRuins where
   getAbilities (NamelessRuins attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    veiled
+      attrs
+      [mkAbility attrs 1 $ forced $ TurnEnds #when (You <> at_ (be attrs) <> HasMatchingAsset #ally)]
 
 instance RunMessage NamelessRuins where
-  runMessage msg (NamelessRuins attrs) =
-    NamelessRuins <$> runMessage msg attrs
+  runMessage msg l@(NamelessRuins attrs) = case msg of
+    Flip iid _ (isTarget attrs -> True) -> do
+      readStory iid (toId attrs) Story.whatRemainsOfTyrrhia
+      pure . NamelessRuins $ attrs & canBeFlippedL .~ False
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      allies <- selectList $ assetControlledBy iid <> #ally
+      player <- getPlayer iid
+      push
+        $ chooseOne player [targetLabel ally [AssetDamage ally (attrs.ability 1) 1 0] | ally <- allies]
+      pure l
+    _ -> NamelessRuins <$> runMessage msg attrs

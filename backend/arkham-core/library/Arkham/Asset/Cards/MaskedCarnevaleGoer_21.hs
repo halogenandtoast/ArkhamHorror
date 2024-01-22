f6f250b32b16df707b19123019764186bbf4043c
Matthew Mongeau <halogenandtoast@gmail.com>
2024-01-22 10:30:14 +0900

    Updated Masked Carnevale Goers

--- backend/arkham-core/library/Arkham/Asset/Cards/MaskedCarnevaleGoer_21.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/MaskedCarnevaleGoer_21.hs
@@ -1,9 +1,4 @@
-module Arkham.Asset.Cards.MaskedCarnevaleGoer_21 (
-  maskedCarnevaleGoer_21,
-  MaskedCarnevaleGoer_21 (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.MaskedCarnevaleGoer_21 (maskedCarnevaleGoer_21, MaskedCarnevaleGoer_21 (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
@@ -11,6 +6,8 @@ import Arkham.Asset.Runner
 import Arkham.Card
 import Arkham.Card.PlayerCard
 import Arkham.Placement
+import Arkham.Prelude
+import Arkham.Projection
 
 newtype MaskedCarnevaleGoer_21 = MaskedCarnevaleGoer_21 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -27,28 +24,20 @@ instance HasAbilities MaskedCarnevaleGoer_21 where
 
 instance RunMessage MaskedCarnevaleGoer_21 where
   runMessage msg a@(MaskedCarnevaleGoer_21 attrs) = case msg of
-    UseCardAbility iid source 1 _ _ | isSource attrs source -> do
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
       push $ Flip iid (toSource iid) (toTarget attrs)
       pure a
-    Flip _ _ target | isTarget attrs target -> do
-      case assetPlacement attrs of
-        AtLocation lid -> do
-          let
-            innocentReveler = lookupCard Cards.innocentReveler (toCardId attrs)
-          pushAll
-            [ CreateAssetAt (toId attrs) innocentReveler (AtLocation lid)
-            , Flipped (toSource attrs) innocentReveler
-            ]
-        _ -> error "not possible"
+    Flip _ _ (isTarget attrs -> True) -> do
+      location <- fieldJust AssetLocation (toId attrs)
+      let innocentReveler = lookupCard Cards.innocentReveler (toCardId attrs)
+      pushAll
+        [ CreateAssetAt (toId attrs) innocentReveler (AtLocation location)
+        , Flipped (toSource attrs) innocentReveler
+        ]
       pure a
     LookAtRevealed _ _ target | isTarget a target -> do
-      let
-        innocentReveler =
-          PlayerCard $ lookupPlayerCard Cards.innocentReveler (toCardId attrs)
+      let innocentReveler = PlayerCard $ lookupPlayerCard Cards.innocentReveler (toCardId attrs)
       lead <- getLeadPlayer
-      pushAll
-        [ FocusCards [innocentReveler]
-        , chooseOne lead [Label "Continue" [UnfocusCards]]
-        ]
+      pushAll [FocusCards [innocentReveler], chooseOne lead [Label "Continue" [UnfocusCards]]]
       pure a
     _ -> MaskedCarnevaleGoer_21 <$> runMessage msg attrs

78a2c81a5899c13ea8a190cc847e853a6466e443
Matthew Mongeau <halogenandtoast@gmail.com>
2024-01-30 08:29:11 +0900

    Fix Higher Education (3)

--- backend/arkham-core/library/Arkham/Asset/Cards/HigherEducation.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/HigherEducation.hs
@@ -1,15 +1,10 @@
-module Arkham.Asset.Cards.HigherEducation (
-  higherEducation,
-  HigherEducation (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.HigherEducation (higherEducation, HigherEducation (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
 import Arkham.Matcher
-import Arkham.SkillType
+import Arkham.Prelude
 
 newtype HigherEducation = HigherEducation AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -22,40 +17,20 @@ instance HasAbilities HigherEducation where
   getAbilities (HigherEducation x) =
     [ withTooltip
         "{fast} Spend 1 resource: You get +1 {willpower} for this skill test."
-        $ restrictedAbility x 1 restriction
-        $ FastAbility
-        $ ResourceCost 1
+        $ controlledAbility x 1 restriction (FastAbility $ ResourceCost 1)
     , withTooltip
         "{fast} Spend 1 resource: You get +1 {intellect} for this skill test."
-        $ restrictedAbility x 2 restriction
-        $ FastAbility
-        $ ResourceCost 1
+        $ controlledAbility x 2 restriction (FastAbility $ ResourceCost 1)
     ]
    where
-    restriction =
-      ControlsThis
-        <> DuringSkillTest AnySkillTest
-        <> InvestigatorExists
-          (You <> HandWith (LengthIs $ AtLeast $ Static 5))
+    restriction = DuringSkillTest AnySkillTest <> youExist (HandWith $ LengthIs $ atLeast 5)
 
 instance RunMessage HigherEducation where
   runMessage msg a@(HigherEducation attrs) = case msg of
-    UseCardAbility iid source 1 _ _
-      | isSource attrs source ->
-          a
-            <$ push
-              ( skillTestModifier
-                  attrs
-                  (InvestigatorTarget iid)
-                  (SkillModifier SkillWillpower 1)
-              )
-    UseCardAbility iid source 2 _ _
-      | isSource attrs source ->
-          a
-            <$ push
-              ( skillTestModifier
-                  attrs
-                  (InvestigatorTarget iid)
-                  (SkillModifier SkillIntellect 1)
-              )
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ skillTestModifier attrs iid (SkillModifier #willpower 1)
+      pure a
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      push $ skillTestModifier attrs iid (SkillModifier #intellect 1)
+      pure a
     _ -> HigherEducation <$> runMessage msg attrs

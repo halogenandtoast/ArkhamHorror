d2108961787df3028e1f079c9f63a8c1d9903bee
Matthew Mongeau <halogenandtoast@gmail.com>
2024-01-31 10:08:00 +0900

    Speed test

--- backend/arkham-core/tests/Arkham/GameSpec.hs
+++ backend/arkham-core/tests/Arkham/GameSpec.hs
@@ -3,9 +3,17 @@ module Arkham.GameSpec (spec) where
 import TestImport
 
 import Arkham.Classes.HasGame
+import Test.HUnit.Base (assertFailure)
 
 spec :: Spec
 spec = describe "ToJSON" $ do
   it "is reversable" $ gameTest $ \_ -> do
     game <- getGame
     liftIO $ decode (encode game) == Just game `shouldBe` True
+
+  -- passes at 1500000
+  -- fails at 1000000
+  -- ideally it would be 100000 or lower
+  xit "should be fast enough" $ gameTestFromFile "example.json" $ \_ -> do
+    mResult <- timeout 100000 $ chooseOnlyOption "Continue"
+    liftIO $ maybe (assertFailure "timeout") (const $ pure ()) mResult

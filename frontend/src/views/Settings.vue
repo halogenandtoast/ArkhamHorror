f856ad13b0130318d6de89121edbc00f8035be84
Matthew Mongeau <halogenandtoast@gmail.com>
2024-02-17 08:22:36 +0900

    Italian Cards

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useUserStore } from '@/stores/user';
 import type { User } from '@/types';
 import api from '@/api';
@@ -8,14 +8,26 @@ import SettingsForm from '@/components/SettingsForm.vue';
 const store = useUserStore()
 const currentUser = computed<User | null>(() => store.getCurrentUser)
 
+const currentLanguage = localStorage.getItem('language') ?? 'en'
+
+const language = ref(currentLanguage)
+
 const updateBeta = async (beta: boolean) => {
   await api.put<User>('settings', { beta })
   store.setCurrentUser()
+}
+
+const updateLanguage = () => {
+  localStorage.setItem('language', language.value)
 }
 </script>
 
 <template>
   <div>
     <SettingsForm v-if="currentUser" :user="currentUser" :updateBeta="updateBeta" />
+    <select v-model="language" @change="updateLanguage">
+      <option value="en">English</option>
+      <option value="it">Italian</option>
+    </select>
   </div>
 </template>

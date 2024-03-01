631498cf272585e06e1858ffb9ad9efbf327bb68
AlvaroRF5 <alvarorf94@gmail.com>
2024-03-01 13:58:09 +0100

    Bangle of Jinxes (1)

---
+++ backend/arkham-core/library/Arkham/Asset/Cards/BangleOfJinxes1.hs
@@ -0,0 +1,35 @@
+module Arkham.Asset.Cards.BangleOfJinxes1 (bangleOfJinxes1, BangleOfJinxes1 (..)) where
+
+import Arkham.Ability
+import Arkham.Asset.Cards qualified as Cards
+import Arkham.Asset.Runner hiding (EnemyAttacks)
+import Arkham.Matcher
+import Arkham.Prelude
+
+newtype BangleOfJinxes1 = BangleOfJinxes1 AssetAttrs
+  deriving anyclass (IsAsset, HasModifiersFor)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+bangleOfJinxes1 :: AssetCard BangleOfJinxes1
+bangleOfJinxes1 = asset BangleOfJinxes1 Cards.bangleOfJinxes1
+
+instance HasAbilities BangleOfJinxes1 where
+  getAbilities (BangleOfJinxes1 a) =
+    [ limitedAbility (PlayerLimit PerTestOrAbility 1)
+        $ controlledAbility a 1 (DuringSkillTest AnySkillTest)
+        $ FastAbility 
+        $ assetUseCost a Charge 1
+    ,
+      restrictedAbility a 2 ControlsThis
+        $ freeReaction (EnemyAttacks #after You AnyEnemyAttack AnyEnemy)
+    ]
+
+instance RunMessage BangleOfJinxes1 where
+  runMessage msg a@(BangleOfJinxes1 attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ skillTestModifier attrs iid (AnySkillValue 2)
+      pure a
+    UseThisAbility _ (isSource attrs -> True) 2 -> do
+      push $ AddUses (toId a) Charge 1
+      pure a
+    _ -> BangleOfJinxes1 <$> runMessage msg attrs

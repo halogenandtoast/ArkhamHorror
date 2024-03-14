82443d8bb5291030f554769b424fbf9b0cbf20b7
Matthew Mongeau <halogenandtoast@gmail.com>
2024-03-14 17:11:11 +0900

    [DSM] Locations finished

--- backend/arkham-core/library/Arkham/Location/Cards/TheWhiteShip.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/TheWhiteShip.hs
@@ -1,27 +1,37 @@
-module Arkham.Location.Cards.TheWhiteShip
-  ( theWhiteShip
-  , TheWhiteShip(..)
-  )
-where
+module Arkham.Location.Cards.TheWhiteShip (theWhiteShip, TheWhiteShip (..)) where
 
-import Arkham.Prelude
-
+import Arkham.Asset.Cards qualified as Assets
 import Arkham.GameValue
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
+import Arkham.Matcher
+import Arkham.Placement
+import Arkham.Prelude
+import Arkham.Scenarios.DarkSideOfTheMoon.Helpers
 
 newtype TheWhiteShip = TheWhiteShip LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass (IsLocation)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theWhiteShip :: LocationCard TheWhiteShip
-theWhiteShip = location TheWhiteShip Cards.theWhiteShip 0 (Static 0)
+theWhiteShip = location TheWhiteShip Cards.theWhiteShip 1 (Static 0)
+
+instance HasModifiersFor TheWhiteShip where
+  getModifiersFor (InvestigatorTarget iid) (TheWhiteShip a) = do
+    alarmLevel <- getAlarmLevel iid
+    pure $ toModifiers a [CannotEnter a.id | alarmLevel >= 5]
+  getModifiersFor _ _ = pure []
 
 instance HasAbilities TheWhiteShip where
   getAbilities (TheWhiteShip attrs) =
-    getAbilities attrs
-    -- withRevealedAbilities attrs []
+    extendRevealed attrs [mkAbility attrs 1 $ forced $ RevealLocation #after Anyone $ be attrs]
 
 instance RunMessage TheWhiteShip where
-  runMessage msg (TheWhiteShip attrs) =
-    TheWhiteShip <$> runMessage msg attrs
+  runMessage msg l@(TheWhiteShip attrs) = case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      theCaptain <- getSetAsideCard Assets.theCaptain
+      assetId <- getRandom
+      push $ CreateAssetAt assetId theCaptain (AtLocation attrs.id)
+      pure l
+    _ -> TheWhiteShip <$> runMessage msg attrs

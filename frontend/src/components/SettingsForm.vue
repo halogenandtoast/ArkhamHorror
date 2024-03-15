867b4ad51a96046f0200ee6954ec78067aaa97c5
Matthew Mongeau <halogenandtoast@gmail.com>
2024-03-15 18:41:00 +0900

    Fix digest, and language setting

--- frontend/src/components/SettingsForm.vue
+++ frontend/src/components/SettingsForm.vue
@@ -12,12 +12,9 @@ const beta = ref(props.user.beta ? "On" : "Off")
 
 const betaUpdate = async () => props.updateBeta(beta.value == "On")
 
-const currentLanguage = localStorage.getItem('language') ?? 'en'
-
-const language = ref(currentLanguage)
-
-const updateLanguage = () => {
-  localStorage.setItem('language', language.value)
+const updateLanguage = (a: Event) => {
+  const target = a.target as HTMLInputElement;
+  localStorage.setItem('language', target.value)
 }
 </script>
 

4b15bf2d9b7077c6b059b75f82ccd1dbe62f797b
Goose <halogenandtoast@gmail.com>
2024-03-23 19:35:49 +0700

    [WOC] Atlach-Natcha and Legs

--- backend/arkham-core/library/Arkham/Enemy/Cards/AtlachNacha.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/AtlachNacha.hs
@@ -4,12 +4,17 @@ module Arkham.Enemy.Cards.AtlachNacha (
 )
 where
 
+import Arkham.Ability
+import Arkham.Attack
 import Arkham.Card
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
 import Arkham.Helpers.Modifiers
+import Arkham.Helpers.Modifiers qualified as Msg
 import Arkham.Location.Types (Field (..))
 import Arkham.Matcher
+import Arkham.Matcher qualified as Match
+import Arkham.Message qualified as Msg
 import Arkham.Placement
 import Data.Aeson (Result (..))
 
@@ -19,21 +24,33 @@ newtype Meta = Meta {rotation :: Int}
 
 newtype AtlachNacha = AtlachNacha EnemyAttrs
   deriving anyclass (IsEnemy)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 atlachNacha :: EnemyCard AtlachNacha
 atlachNacha =
   enemyWith
     AtlachNacha
     Cards.atlachNacha
-    (0, Static 1, 0)
-    (0, 0)
+    (4, PerPlayer 4, 4)
+    (2, 2)
     ((asSelfLocationL ?~ "atlachNacha") . setMeta @Meta (Meta 0))
 
 instance HasModifiersFor AtlachNacha where
   getModifiersFor target (AtlachNacha attrs) | attrs `is` target = do
-    pure $ toModifiers attrs [Omnipotent | attrs.placement == Global]
+    pure
+      $ toModifiers attrs
+      $ if attrs.placement == Global
+        then [Omnipotent]
+        else [DoNotExhaustEvaded]
   getModifiersFor _ _ = pure []
+
+instance HasAbilities AtlachNacha where
+  getAbilities (AtlachNacha attrs) =
+    extend
+      attrs
+      [ mkAbility attrs 1 $ forced $ EnemyLeaves #when Anywhere (be attrs)
+      , mkAbility attrs 2 $ forced $ Match.EnemyEvaded #when You (be attrs)
+      ]
 
 instance RunMessage AtlachNacha where
   runMessage msg e@(AtlachNacha attrs) = runQueueT $ case msg of
@@ -51,4 +68,23 @@ instance RunMessage AtlachNacha where
             Success a -> a
             Error _ -> Meta 0
       pure $ AtlachNacha $ setMeta @Meta (Meta ((m + (45 * n)) `mod` 360)) attrs
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      lid <- selectJust $ locationWithEnemy attrs.id
+      iids <- select $ investigatorAt lid
+      if null iids
+        then placeDoom (attrs.ability 1) lid 1
+        else eachInvestigator (\iid' -> push $ EnemyWillAttack $ enemyAttack attrs (attrs.ability 1) iid')
+      pure e
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      iids <- select $ InvestigatorAt $ locationWithInvestigator iid
+      chooseOrRunOne
+        iid
+        [ targetLabel
+          iid'
+          [Msg.roundModifier (attrs.ability 1) iid' $ CannotBeAttackedBy (EnemyWithId attrs.id)]
+        | iid' <- iids
+        ]
+      pure e
+    Msg.EnemyEvaded _ eid | eid == attrs.id -> do
+      pure e
     _ -> AtlachNacha <$> lift (runMessage msg attrs)
--- backend/arkham-core/library/Arkham/Enemy/Cards/LegsOfAtlachNacha_348.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/LegsOfAtlachNacha_348.hs
@@ -4,25 +4,52 @@ module Arkham.Enemy.Cards.LegsOfAtlachNacha_348 (
 )
 where
 
-import Arkham.Prelude
-
-import Arkham.Classes
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Runner
+import Arkham.Enemy.Import.Lifted
+import Arkham.Helpers.Modifiers qualified as Msg
+import Arkham.Location.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Matcher qualified as Match
+import Arkham.Message qualified as Msg
+import Arkham.Modifier (ModifierType (..))
+import Arkham.Modifier qualified as Mod
+import Arkham.Projection
 
 newtype LegsOfAtlachNacha_348 = LegsOfAtlachNacha_348 EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass (IsEnemy)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+instance HasModifiersFor LegsOfAtlachNacha_348 where
+  getModifiersFor target (LegsOfAtlachNacha_348 attrs) | attrs `is` target = do
+    x <- field LocationShroud =<< selectJust (locationWithEnemy attrs)
+    pure $ toModifiers attrs [CannotMakeAttacksOfOpportunity, DoNotExhaustEvaded, Mod.EnemyFight x]
+  getModifiersFor _ _ = pure []
 
 legsOfAtlachNacha_348 :: EnemyCard LegsOfAtlachNacha_348
 legsOfAtlachNacha_348 =
   enemyWith
     LegsOfAtlachNacha_348
     Cards.legsOfAtlachNacha_348
-    (0, Static 1, 0)
-    (0, 0)
+    (0, PerPlayer 3, 3)
+    (1, 1)
     (asSelfLocationL ?~ "legs2")
 
+instance HasAbilities LegsOfAtlachNacha_348 where
+  getAbilities (LegsOfAtlachNacha_348 attrs) = extend attrs [mkAbility attrs 1 $ forced $ Match.EnemyEvaded #when You (be attrs)]
+
 instance RunMessage LegsOfAtlachNacha_348 where
-  runMessage msg (LegsOfAtlachNacha_348 attrs) =
-    LegsOfAtlachNacha_348 <$> runMessage msg attrs
+  runMessage msg e@(LegsOfAtlachNacha_348 attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      iids <- select $ InvestigatorAt $ locationWithInvestigator iid
+      chooseOrRunOne
+        iid
+        [ targetLabel
+          iid'
+          [Msg.roundModifier (attrs.ability 1) iid' $ CannotBeAttackedBy (EnemyWithId attrs.id)]
+        | iid' <- iids
+        ]
+      pure e
+    Msg.EnemyEvaded _ eid | eid == attrs.id -> do
+      pure e
+    _ -> LegsOfAtlachNacha_348 <$> lift (runMessage msg attrs)

838dc5dd37b08157edf321422a05e94e107454b2
Goose <halogenandtoast@gmail.com>
2024-03-23 10:41:01 +0700

    [WOC] Agendas

--- backend/arkham-core/library/Arkham/Agenda/Cards/ATrailOfTwists.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/ATrailOfTwists.hs
@@ -5,17 +5,30 @@ module Arkham.Agenda.Cards.ATrailOfTwists (
 
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Import.Lifted
+import Arkham.Game.Helpers (perPlayer)
+import Arkham.Helpers.Modifiers
+import Arkham.Matcher
 
 newtype ATrailOfTwists = ATrailOfTwists AgendaAttrs
-  deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAgenda, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 aTrailOfTwists :: AgendaCard ATrailOfTwists
-aTrailOfTwists = agenda (2, A) ATrailOfTwists Cards.aTrailOfTwists (Static 9)
+aTrailOfTwists =
+  agendaWith (2, A) ATrailOfTwists Cards.aTrailOfTwists (Static 9)
+    $ removeDoomMatchersL
+    %~ (\m -> m {removeDoomLocations = Nowhere})
+
+instance HasModifiersFor ATrailOfTwists where
+  getModifiersFor target (ATrailOfTwists attrs) | attrs `is` target = do
+    n <- perPlayer 2
+    pure $ toModifiers attrs [DoomThresholdModifier n]
+  getModifiersFor _ _ = pure []
 
 instance RunMessage ATrailOfTwists where
   runMessage msg a@(ATrailOfTwists attrs) = runQueueT $ case msg of
     AdvanceAgenda (isSide B attrs -> True) -> do
+      shuffleEncounterDiscardBackIn
       advanceAgendaDeck attrs
       pure a
     _ -> ATrailOfTwists <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Agenda/Cards/RealitiesInterwoven.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/RealitiesInterwoven.hs
@@ -5,17 +5,25 @@ module Arkham.Agenda.Cards.RealitiesInterwoven (
 
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Import.Lifted
+import Arkham.Game.Helpers (perPlayer)
+import Arkham.Helpers.Modifiers
 
 newtype RealitiesInterwoven = RealitiesInterwoven AgendaAttrs
-  deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAgenda, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 realitiesInterwoven :: AgendaCard RealitiesInterwoven
 realitiesInterwoven = agenda (3, A) RealitiesInterwoven Cards.realitiesInterwoven (Static 11)
 
+instance HasModifiersFor RealitiesInterwoven where
+  getModifiersFor target (RealitiesInterwoven attrs) | attrs `is` target = do
+    n <- perPlayer 2
+    pure $ toModifiers attrs [DoomThresholdModifier n]
+  getModifiersFor _ _ = pure []
+
 instance RunMessage RealitiesInterwoven where
   runMessage msg a@(RealitiesInterwoven attrs) = runQueueT $ case msg of
     AdvanceAgenda (isSide B attrs -> True) -> do
-      advanceAgendaDeck attrs
+      push R2
       pure a
     _ -> RealitiesInterwoven <$> runMessage msg attrs

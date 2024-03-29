f8bea3a02f98232bb272cdd1b280a2b9a76bd107
Goose <halogenandtoast@gmail.com>
2024-03-29 11:44:12 +0700

    Sea Change Harpoom, Silas's Net

--- backend/arkham-core/library/Arkham/Asset/Cards/SeaChangeHarpoon.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/SeaChangeHarpoon.hs
@@ -1,21 +1,65 @@
-module Arkham.Asset.Cards.SeaChangeHarpoon
-  ( seaChangeHarpoon
-  , SeaChangeHarpoon(..)
-  )
+module Arkham.Asset.Cards.SeaChangeHarpoon (
+  seaChangeHarpoon,
+  SeaChangeHarpoon (..),
+)
 where
 
-import Arkham.Prelude
-
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.Card
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Prelude
+import Arkham.Projection
 
 newtype SeaChangeHarpoon = SeaChangeHarpoon AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 seaChangeHarpoon :: AssetCard SeaChangeHarpoon
 seaChangeHarpoon =
   asset SeaChangeHarpoon Cards.seaChangeHarpoon
 
+instance HasAbilities SeaChangeHarpoon where
+  getAbilities (SeaChangeHarpoon attrs) = [restrictedAbility attrs 1 ControlsThis fightAction_]
+
+instance HasModifiersFor SeaChangeHarpoon where
+  getModifiersFor (InvestigatorTarget iid) (SeaChangeHarpoon attrs) = do
+    maybeMods <- runMaybeT $ do
+      source <- MaybeT getSkillTestSource
+      guard $ isAbilitySource attrs 1 source
+      iid' <- MaybeT getSkillTestInvestigator
+      guard $ iid == iid'
+      skillCount <-
+        lift $ fieldMap InvestigatorCommittedCards (count (`cardMatch` CardWithType SkillType)) iid
+      guard $ skillCount > 0
+      pure $ DamageDealt 1
+
+    pure $ toModifiers attrs $ maybeToList maybeMods
+  getModifiersFor _ _ = pure []
+
 instance RunMessage SeaChangeHarpoon where
-  runMessage msg (SeaChangeHarpoon attrs) = SeaChangeHarpoon <$> runMessage msg attrs
+  runMessage msg a@(SeaChangeHarpoon attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      pushAll
+        [ skillTestModifier (attrs.ability 1) iid (SkillModifier #combat 1)
+        , chooseFightEnemy iid (attrs.ability 1) #combat
+        ]
+      pure a
+    SkillTestEnds iid (isAbilitySource attrs 1 -> True) -> do
+      miid <- getSkillTestInvestigator
+      when (Just iid == miid) do
+        player <- getPlayer iid
+        skills <- select $ skillControlledBy iid
+        push
+          $ chooseOne
+            player
+            [ Label
+                "Return Sea Change Harpoon to your hand to return all of your committed skill cards to your hand instead of discarding them"
+                $ ReturnToHand iid (toTarget attrs)
+                : [ReturnToHand iid (toTarget skill) | skill <- skills]
+            , Label "Do nothing" []
+            ]
+      pure a
+    _ -> SeaChangeHarpoon <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Asset/Cards/SilassNet.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/SilassNet.hs
@@ -1,21 +1,61 @@
-module Arkham.Asset.Cards.SilassNet
-  ( silassNet
-  , SilassNet(..)
-  )
+module Arkham.Asset.Cards.SilassNet (
+  silassNet,
+  SilassNet (..),
+)
 where
 
-import Arkham.Prelude
-
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.Matcher
+import Arkham.Prelude
 
 newtype SilassNet = SilassNet AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 silassNet :: AssetCard SilassNet
 silassNet =
   asset SilassNet Cards.silassNet
 
+instance HasAbilities SilassNet where
+  getAbilities (SilassNet attrs) = [restrictedAbility attrs 1 ControlsThis evadeAction_]
+
 instance RunMessage SilassNet where
-  runMessage msg (SilassNet attrs) = SilassNet <$> runMessage msg attrs
+  runMessage msg a@(SilassNet attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ chooseEvadeEnemy iid (attrs.ability 1) #agility
+      pure a
+    SkillTestEnds iid (isAbilitySource attrs 1 -> True) -> do
+      miid <- getSkillTestInvestigator
+      when (Just iid == miid) do
+        player <- getPlayer iid
+        skills <- select $ skillControlledBy iid
+        push
+          $ chooseOne
+            player
+            [ Label
+                "Return Silas's Net to your hand to return all of your committed skill cards to your hand instead of discarding them"
+                $ ReturnToHand iid (toTarget attrs)
+                : [ReturnToHand iid (toTarget skill) | skill <- skills]
+            , Label "Do nothing" []
+            ]
+      pure a
+    PassedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
+      mtarget <- getSkillTestTarget
+      case mtarget of
+        Just eid -> do
+          otherEnemies <- select $ enemyEngagedWith iid <> not_ (EnemyWithId eid)
+          when (notNull otherEnemies) do
+            player <- getPlayer iid
+            push
+              $ chooseOne
+                player
+                [ Label
+                    "Automatically evade another enemy"
+                    [chooseOrRunOne player [targetLabel enemy [EnemyEvaded iid enemy] | enemy <- otherEnemies]]
+                , Label "Do not evade another enemy" []
+                ]
+        Nothing -> error "No target"
+      pure a
+    _ -> SilassNet <$> runMessage msg attrs

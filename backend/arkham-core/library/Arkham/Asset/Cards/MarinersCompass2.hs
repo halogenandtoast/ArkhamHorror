7762faee6c243c1d81070b9a9e5a8271a178cf8e
Matthew Mongeau <halogenandtoast@gmail.com>
2024-04-03 10:47:20 +0900

    Mariner's Compass, Mariner's Compass (2)

---
+++ backend/arkham-core/library/Arkham/Asset/Cards/MarinersCompass2.hs
@@ -0,0 +1,42 @@
+module Arkham.Asset.Cards.MarinersCompass2 (marinersCompass2, MarinersCompass2 (..)) where
+
+import Arkham.Ability
+import Arkham.Asset.Cards qualified as Cards
+import Arkham.Asset.Runner
+import Arkham.Investigate
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Prelude
+import Arkham.Projection
+
+newtype MarinersCompass2 = MarinersCompass2 AssetAttrs
+  deriving anyclass (IsAsset, HasModifiersFor)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+marinersCompass2 :: AssetCard MarinersCompass2
+marinersCompass2 = asset MarinersCompass2 Cards.marinersCompass2
+
+instance HasAbilities MarinersCompass2 where
+  getAbilities (MarinersCompass2 x) =
+    [ restrictedAbility x 1 ControlsThis $ investigateAction (exhaust x)
+    , limitedAbility (PlayerLimit PerTestOrAbility 3)
+        $ controlledAbility x 2 (DuringSkillTest UsingThis)
+        $ FastAbility (ResourceCost 1)
+    ]
+
+instance RunMessage MarinersCompass2 where
+  runMessage msg a@(MarinersCompass2 attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      pushM $ mkInvestigate iid (attrs.ability 1)
+      pure a
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      push $ skillTestModifier attrs iid (SkillModifier #intellect 1)
+      pure a
+    FailedThisSkillTest _iid (isAbilitySource attrs 1 -> True) -> do
+      pushWhen attrs.exhausted $ ready attrs
+      pure a
+    PassedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
+      noResources <- fieldNone InvestigatorResources iid
+      pushWhen noResources $ skillTestModifier attrs iid (DiscoveredClues 1)
+      pure a
+    _ -> MarinersCompass2 <$> runMessage msg attrs

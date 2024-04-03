f102c597c7b7a20bdc68f5bd053631644f119db6
Matthew Mongeau <halogenandtoast@gmail.com>
2024-04-03 15:59:48 +0900

    Blood Pact

---
+++ backend/arkham-core/library/Arkham/Asset/Cards/BloodPact.hs
@@ -0,0 +1,35 @@
+module Arkham.Asset.Cards.BloodPact (bloodPact, BloodPact (..)) where
+
+import Arkham.Ability
+import Arkham.Asset.Cards qualified as Cards
+import Arkham.Asset.Runner
+import Arkham.Prelude
+
+newtype BloodPact = BloodPact AssetAttrs
+  deriving anyclass (IsAsset, HasModifiersFor)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+bloodPact :: AssetCard BloodPact
+bloodPact = asset BloodPact Cards.bloodPact
+
+instance HasAbilities BloodPact where
+  getAbilities (BloodPact x) =
+    [ withTooltip
+        "{fast} Add 1 doom to Blood Pact: You get +2 {willpower} for this skill test. (Limit once per test.)"
+        $ playerLimit PerTestOrAbility
+        $ restrictedAbility x 1 ControlsThis (FastAbility $ ResourceCost 2)
+    , withTooltip
+        "{fast} Add 1 doom to Blood Pact: You get +2 {combat} for this skill test. (Limit once per test.)"
+        $ playerLimit PerTestOrAbility
+        $ restrictedAbility x 2 ControlsThis (FastAbility $ ResourceCost 2)
+    ]
+
+instance RunMessage BloodPact where
+  runMessage msg a@(BloodPact attrs) = case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ skillTestModifier (attrs.ability 1) iid (SkillModifier #intellect 2)
+      pure a
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      push $ skillTestModifier (attrs.ability 2) iid (SkillModifier #combat 2)
+      pure a
+    _ -> BloodPact <$> runMessage msg attrs
--- backend/arkham-core/library/Arkham/Asset/Cards/BloodPact3.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/BloodPact3.hs
@@ -1,14 +1,9 @@
-module Arkham.Asset.Cards.BloodPact3 (
-  bloodPact3,
-  BloodPact3 (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.BloodPact3 (bloodPact3, BloodPact3 (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
-import Arkham.SkillType
+import Arkham.Prelude
 
 newtype BloodPact3 = BloodPact3 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -21,32 +16,20 @@ instance HasAbilities BloodPact3 where
   getAbilities (BloodPact3 x) =
     [ withTooltip
         "{fast} Add 1 doom to Blood Pact: You get +3 {willpower} for this skill test. (Limit once per test.)"
-        $ limitedAbility (PlayerLimit PerTestOrAbility 1)
+        $ playerLimit PerTestOrAbility
         $ restrictedAbility x 1 ControlsThis (FastAbility $ ResourceCost 2)
     , withTooltip
         "{fast} Add 1 doom to Blood Pact: You get +3 {combat} for this skill test. (Limit once per test.)"
-        $ limitedAbility (PlayerLimit PerTestOrAbility 1)
+        $ playerLimit PerTestOrAbility
         $ restrictedAbility x 2 ControlsThis (FastAbility $ ResourceCost 2)
     ]
 
 instance RunMessage BloodPact3 where
   runMessage msg a@(BloodPact3 attrs) = case msg of
-    UseCardAbility iid source 1 _ _
-      | isSource attrs source ->
-          a
-            <$ push
-              ( skillTestModifier
-                  source
-                  (InvestigatorTarget iid)
-                  (SkillModifier SkillIntellect 3)
-              )
-    UseCardAbility iid source 2 _ _
-      | isSource attrs source ->
-          a
-            <$ push
-              ( skillTestModifier
-                  source
-                  (InvestigatorTarget iid)
-                  (SkillModifier SkillIntellect 3)
-              )
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ skillTestModifier (attrs.ability 1) iid (SkillModifier #intellect 3)
+      pure a
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      push $ skillTestModifier (attrs.ability 2) iid (SkillModifier #combat 3)
+      pure a
     _ -> BloodPact3 <$> runMessage msg attrs

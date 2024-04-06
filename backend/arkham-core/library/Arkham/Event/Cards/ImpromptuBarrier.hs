a85a8d628b1793d9e743f1c5b13f2752b92483eb
Matthew Mongeau <halogenandtoast@gmail.com>
2024-04-07 08:03:00 +0900

    Fix Impromptu Barrier

--- backend/arkham-core/library/Arkham/Event/Cards/ImpromptuBarrier.hs
+++ backend/arkham-core/library/Arkham/Event/Cards/ImpromptuBarrier.hs
@@ -1,7 +1,6 @@
 module Arkham.Event.Cards.ImpromptuBarrier (impromptuBarrier, ImpromptuBarrier (..)) where
 
 import Arkham.Action qualified as Action
-import Arkham.Aspect
 import Arkham.Classes
 import Arkham.Deck qualified as Deck
 import Arkham.Enemy.Types qualified as Enemy (Field (..))
@@ -11,10 +10,9 @@ import Arkham.Event.Runner
 import Arkham.Helpers.Modifiers
 import Arkham.Matcher hiding (EnemyEvaded)
 import Arkham.Prelude
-import Arkham.SkillType
 import Arkham.Zone
 
-data Metadata = Metadata {fromDiscard :: Bool, chosenAspect :: Maybe SkillType}
+newtype Metadata = Metadata {fromDiscard :: Bool}
   deriving stock (Show, Eq, Generic)
   deriving anyclass (ToJSON, FromJSON)
 
@@ -23,45 +21,36 @@ newtype ImpromptuBarrier = ImpromptuBarrier (EventAttrs `With` Metadata)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 impromptuBarrier :: EventCard ImpromptuBarrier
-impromptuBarrier = event (ImpromptuBarrier . (`with` Metadata False Nothing)) Cards.impromptuBarrier
+impromptuBarrier = event (ImpromptuBarrier . (`with` Metadata False)) Cards.impromptuBarrier
 
 instance RunMessage ImpromptuBarrier where
   runMessage msg e@(ImpromptuBarrier (attrs `With` meta)) = case msg of
     InvestigatorPlayEvent iid eid _ _ zone | eid == toId attrs -> do
-      chooseEvade <-
-        leftOr
-          <$> aspect iid attrs (#combat `InsteadOf` #agility) (setTarget attrs <$> mkChooseEvade iid attrs)
+      chooseEvade <- toMessage . setTarget attrs <$> mkChooseEvade iid attrs
       pushAll
         $ chooseEvade
-        <> [ShuffleIntoDeck (Deck.InvestigatorDeck iid) (toTarget attrs) | zone == FromDiscard]
-      pure . ImpromptuBarrier $ attrs `with` Metadata (zone == FromDiscard) Nothing
-    ChooseEvadeEnemy choose | isSource attrs choose.source -> do
-      pure . ImpromptuBarrier $ attrs `with` meta {chosenAspect = Just choose.skillType}
+        : [ShuffleIntoDeck (Deck.InvestigatorDeck iid) (toTarget attrs) | zone == FromDiscard]
+      pure . ImpromptuBarrier $ attrs `with` Metadata (zone == FromDiscard)
     ChosenEvadeEnemy (isSource attrs -> True) eid -> do
-      push $ skillTestModifier attrs (EnemyTarget eid) (EnemyEvade (-1))
+      push $ skillTestModifier attrs eid (EnemyEvade (-1))
       pure e
-    Successful (Action.Evade, EnemyTarget enemyId) iid _ (isTarget attrs -> True) n ->
-      do
-        enemies <-
-          selectWithField Enemy.EnemyEvade
-            $ EnemyAt (locationWithEnemy enemyId)
-            <> NotEnemy (EnemyWithId enemyId)
-        let enemies' = map fst $ filter (\(_, x) -> maybe False (<= n) x) enemies
-        player <- getPlayer iid
-        chooseEvade <-
-          toMessage
-            . withSkillType (fromMaybe #combat (chosenAspect meta))
-            . setTarget attrs
-            <$> mkChooseEvadeMatch iid attrs (EnemyOneOf $ map EnemyWithId enemies')
-        pushAll
-          $ EnemyEvaded iid enemyId
-          : [ chooseOne
-              player
-              [ Label "Do not evade another enemy" []
-              , Label "Evade Another Enemy" [chooseEvade]
-              ]
-            | notNull enemies'
+    Successful (Action.Evade, EnemyTarget enemyId) iid _ (isTarget attrs -> True) n -> do
+      enemies <-
+        map fst
+          . filter (\(_, x) -> maybe False (<= n) x)
+          <$> selectWithField Enemy.EnemyEvade (at_ (locationWithEnemy enemyId) <> not_ (EnemyWithId enemyId))
+      player <- getPlayer iid
+      chooseEvade <-
+        toMessage . setTarget attrs <$> mkChooseEvadeMatch iid attrs (oneOf $ map EnemyWithId enemies)
+      pushAll
+        $ EnemyEvaded iid enemyId
+        : [ chooseOne
+            player
+            [ Label "Do not evade another enemy" []
+            , Label "Evade Another Enemy" [chooseEvade]
             ]
+          | notNull enemies
+          ]
 
-        pure e
+      pure e
     _ -> ImpromptuBarrier . (`with` meta) <$> runMessage msg attrs

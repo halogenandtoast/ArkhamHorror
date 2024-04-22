60c13104de31653cd2fc27ca2dd71be0cffce610
Matthew Mongeau <halogenandtoast@gmail.com>
2024-04-22 17:35:07 +0900

    Fix alchemical Concoction

--- backend/arkham-core/library/Arkham/Asset/Cards/AlchemicalConcoction.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/AlchemicalConcoction.hs
@@ -1,16 +1,16 @@
 module Arkham.Asset.Cards.AlchemicalConcoction (alchemicalConcoction, AlchemicalConcoction (..)) where
 
 import Arkham.Ability
-import Arkham.Action qualified as Action
 import Arkham.Aspect
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import Arkham.Enemy.Cards qualified as Enemies
 import Arkham.Fight
 import Arkham.Matcher
 import Arkham.Prelude
 
 newtype AlchemicalConcoction = AlchemicalConcoction AssetAttrs
-  deriving anyclass (IsAsset)
+  deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 alchemicalConcoction :: AssetCard AlchemicalConcoction
@@ -19,23 +19,17 @@ alchemicalConcoction = asset AlchemicalConcoction Cards.alchemicalConcoction
 instance HasAbilities AlchemicalConcoction where
   getAbilities (AlchemicalConcoction a) = [fightAbility a 1 mempty ControlsThis]
 
-instance HasModifiersFor AlchemicalConcoction where
-  getModifiersFor (InvestigatorTarget _) (AlchemicalConcoction a) = do
-    toModifiers a . maybeToList <$> runMaybeT do
-      Action.Fight <- MaybeT getSkillTestAction
-      guardM $ isAbilitySource a 1 <$> MaybeT getSkillTestSource
-      EnemyTarget eid <- MaybeT getSkillTestTarget
-      isTheExperiment <- lift $ enemyMatches eid $ EnemyWithTitle "The Experiment"
-      guard isTheExperiment
-      pure $ DamageDealt 6
-  getModifiersFor _ _ = pure []
-
 instance RunMessage AlchemicalConcoction where
   runMessage msg a@(AlchemicalConcoction attrs) = case msg of
     UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
       let source = attrs.ability 1
-      chooseFight <- aspect iid source (#willpower `InsteadOf` #combat) (mkChooseFight iid source)
-      pushAll $ skillTestModifier source iid (DamageDealt 1) : leftOr chooseFight
+      chooseFight <- aspect iid source (#intellect `InsteadOf` #combat) (mkChooseFight iid source)
+      pushAll $ leftOr chooseFight
+      pure a
+    ChoseEnemy iid (isAbilitySource attrs 1 -> True) eid -> do
+      isTheExperiment <- eid <=~> enemyIs Enemies.theExperiment
+      when isTheExperiment do
+        push $ skillTestModifier (attrs.ability 1) iid (DamageDealt 6)
       pure a
     PassedThisSkillTest _ (isAbilitySource attrs 1 -> True) -> do
       push $ RemoveFromGame (toTarget attrs)

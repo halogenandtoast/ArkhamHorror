d0542c4c9917964308e10a9660e5b0e2155c053b
Matthew Mongeau <halogenandtoast@gmail.com>
2024-04-23 09:28:10 +0900

    Remove traces

--- backend/arkham-core/library/Arkham/Asset/Cards/PowderOfIbnGhazi.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/PowderOfIbnGhazi.hs
@@ -41,15 +41,14 @@ instance RunMessage PowderOfIbnGhazi where
   runMessage msg (PowderOfIbnGhazi attrs) = case msg of
     InvestigatorPlayAsset _ aid | aid == toId attrs -> do
       survivedCount <-
-        traceShowId
-          <$> countM
-            getHasRecord
-            [ DrHenryArmitageSurvivedTheDunwichLegacy
-            , ProfessorWarrenRiceSurvivedTheDunwichLegacy
-            , DrFrancisMorganSurvivedTheDunwichLegacy
-            , ZebulonWhateleySurvivedTheDunwichLegacy
-            , EarlSawyerSurvivedTheDunwichLegacy
-            ]
+        countM
+          getHasRecord
+          [ DrHenryArmitageSurvivedTheDunwichLegacy
+          , ProfessorWarrenRiceSurvivedTheDunwichLegacy
+          , DrFrancisMorganSurvivedTheDunwichLegacy
+          , ZebulonWhateleySurvivedTheDunwichLegacy
+          , EarlSawyerSurvivedTheDunwichLegacy
+          ]
       attrs' <- runMessage msg attrs
       pure . PowderOfIbnGhazi $ attrs' & tokensL %~ setTokens Clue survivedCount
     UseCardAbility iid source 1 _ _ | isSource attrs source -> do

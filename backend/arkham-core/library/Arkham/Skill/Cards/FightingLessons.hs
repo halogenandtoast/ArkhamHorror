d2499c39a6ab8cf73cea001df7c8fcc28eae5811
w4k4w4k4 <seenev@gmail.com>
2024-05-01 12:51:29 -0500

    Added Fighting Lessons

---
+++ backend/arkham-core/library/Arkham/Skill/Cards/FightingLessons.hs
@@ -0,0 +1,25 @@
+module Arkham.Skill.Cards.FightingLessons where
+
+import Arkham.Prelude
+--import Arkham.Ability
+import Arkham.Modifier
+import Arkham.Helpers.Modifiers
+import Arkham.Classes
+import Arkham.Matcher.Types
+import Arkham.Skill.Cards qualified as Cards
+import Arkham.Skill.Runner
+
+newtype FightingLessons = FightingLessons SkillAttrs
+  deriving anyclass (IsSkill, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+fightingLessons :: SkillCard FightingLessons
+fightingLessons = skill FightingLessons Cards.fightingLessons
+
+instance HasModifiersFor FightingLessons where
+  getModifiersFor (InvestigatorTarget iid) (FightingLessons attrs) | controlledBy attrs iid = do
+    pure $ toModifiers attrs [CanCommitToSkillTestPerformedByAnInvestigatorAt Anywhere]
+  getModifiersFor _ _ = pure []
+
+instance RunMessage FightingLessons where
+  runMessage msg (FightingLessons attrs) = FightingLessons <$> runMessage msg attrs

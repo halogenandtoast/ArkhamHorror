d51e0311fc915d9570963fc600a0ed54d69baf16
Matthew Mongeau <halogenandtoast@gmail.com>
2024-07-21 10:43:53 +0900

    Fix The Tower XVI
    Modifiers were only being applied when Unplaced, but we change the in hand placement to be StillInHand

--- backend/arkham-core/library/Arkham/Asset/Cards/TheTowerXVI.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/TheTowerXVI.hs
@@ -1,27 +1,21 @@
-module Arkham.Asset.Cards.TheTowerXVI (
-  theTowerXVI,
-  TheTowerXVI (..),
-)
-where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.TheTowerXVI (theTowerXVI, TheTowerXVI (..)) where
 
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
 import Arkham.Matcher
 import Arkham.Placement
+import Arkham.Prelude
 
 newtype TheTowerXVI = TheTowerXVI AssetAttrs
   deriving anyclass (IsAsset, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theTowerXVI :: AssetCard TheTowerXVI
-theTowerXVI =
-  asset TheTowerXVI Cards.theTowerXVI
+theTowerXVI = asset TheTowerXVI Cards.theTowerXVI
 
 instance HasModifiersFor TheTowerXVI where
-  getModifiersFor (InvestigatorTarget _) (TheTowerXVI attrs)
-    | assetPlacement attrs == Unplaced =
+  getModifiersFor (InvestigatorTarget iid) (TheTowerXVI attrs)
+    | attrs.placement == StillInHand iid =
         pure $ toModifiers attrs [CannotCommitCards AnyCard]
   getModifiersFor _ _ = pure []
 

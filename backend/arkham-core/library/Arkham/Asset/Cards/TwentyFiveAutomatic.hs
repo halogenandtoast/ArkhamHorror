e63c77ba9976f36b3b832303cfaa12198564c353
Matthew Mongeau <halogenandtoast@gmail.com>
2024-07-21 10:40:14 +0900

    Fix .25 Automatic (2)
    Because the skill test can get moved, the modifiers weren't going to carry over, the solution is just to use the modifiers system to manage it.

--- backend/arkham-core/library/Arkham/Asset/Cards/TwentyFiveAutomatic.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/TwentyFiveAutomatic.hs
@@ -2,13 +2,14 @@ module Arkham.Asset.Cards.TwentyFiveAutomatic (twentyFiveAutomatic, TwentyFiveAu
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
-import Arkham.Fight
+import Arkham.Asset.Import.Lifted
+import Arkham.Asset.Uses
+import Arkham.Helpers.Modifiers (ModifierType (..), maybeModified)
+import Arkham.Helpers.SkillTest (getSkillTestSource, getSkillTestTarget)
 import Arkham.Matcher
-import Arkham.Prelude
 
 newtype TwentyFiveAutomatic = TwentyFiveAutomatic AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor)
+  deriving anyclass IsAsset
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 twentyFiveAutomatic :: AssetCard TwentyFiveAutomatic
@@ -17,14 +18,20 @@ twentyFiveAutomatic = asset TwentyFiveAutomatic Cards.twentyFiveAutomatic
 instance HasAbilities TwentyFiveAutomatic where
   getAbilities (TwentyFiveAutomatic attrs) = [restrictedAbility attrs 1 ControlsThis $ fightAction $ assetUseCost attrs Ammo 1]
 
+instance HasModifiersFor TwentyFiveAutomatic where
+  getModifiersFor (InvestigatorTarget iid) (TwentyFiveAutomatic attrs) = do
+    maybeModified attrs do
+      guard $ attrs `controlledBy` iid
+      EnemyTarget eid' <- MaybeT getSkillTestTarget
+      liftGuardM $ eid' <=~> ExhaustedEnemy
+      source <- MaybeT getSkillTestSource
+      guard $ isAbilitySource attrs 1 source
+      pure [SkillModifier #combat 2, DamageDealt 1]
+  getModifiersFor _ _ = pure []
+
 instance RunMessage TwentyFiveAutomatic where
-  runMessage msg a@(TwentyFiveAutomatic attrs) = case msg of
+  runMessage msg a@(TwentyFiveAutomatic attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      pushM $ mkChooseFight iid (attrs.ability 1)
+      chooseFightEnemy iid (attrs.ability 1)
       pure a
-    ChoseEnemy iid (isAbilitySource attrs 1 -> True) eid -> do
-      exhausted <- eid <=~> ExhaustedEnemy
-      when exhausted do
-        push $ skillTestModifiers (attrs.ability 1) iid [SkillModifier #combat 2, DamageDealt 1]
-      pure a
-    _ -> TwentyFiveAutomatic <$> runMessage msg attrs
+    _ -> TwentyFiveAutomatic <$> liftRunMessage msg attrs

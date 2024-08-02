efd132aa2b56cf6a2833421ac02eb5ecd4a0545b
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-02 17:10:38 +0900

    Cleanup Olives

--- backend/arkham-core/library/Arkham/Asset/Cards/OliveMcBride.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/OliveMcBride.hs
@@ -1,17 +1,11 @@
-module Arkham.Asset.Cards.OliveMcBride (
-  oliveMcBride,
-  OliveMcBride (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.OliveMcBride (oliveMcBride, OliveMcBride (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
 import Arkham.ChaosBagStepState
 import Arkham.Matcher
-import Arkham.Timing qualified as Timing
-import Arkham.Window (Window (..), mkWindow)
+import Arkham.Window (Window (..))
 import Arkham.Window qualified as Window
 
 newtype OliveMcBride = OliveMcBride AssetAttrs
@@ -24,31 +18,26 @@ oliveMcBride = ally OliveMcBride Cards.oliveMcBride (1, 3)
 instance HasAbilities OliveMcBride where
   getAbilities (OliveMcBride a) =
     [ restrictedAbility a 1 ControlsThis
-        $ ReactionAbility (WouldRevealChaosToken Timing.When You)
-        $ ExhaustCost (toTarget a)
+        $ ReactionAbility (WouldRevealChaosToken #when You) (exhaust a)
     ]
 
+toDrawSource :: [Window] -> Source
+toDrawSource = \case
+  ((windowType -> (Window.WouldRevealChaosToken source _)) : _) -> source
+  (_ : rest) -> toDrawSource rest
+  _ -> error "Expected WouldRevealChaosToken window"
+
 instance RunMessage OliveMcBride where
-  runMessage msg a@(OliveMcBride attrs) = case msg of
-    UseCardAbility
-      iid
-      (isSource attrs -> True)
-      1
-      [Window Timing.When (Window.WouldRevealChaosToken drawSource _) _]
-      _ ->
-        do
-          ignoreWindow <-
-            checkWindows
-              [mkWindow Timing.After (Window.CancelledOrIgnoredCardOrGameEffect $ toAbilitySource attrs 1)]
-          pushAll
-            [ ReplaceCurrentDraw drawSource iid
-                $ Choose
-                  (toSource attrs)
-                  2
-                  ResolveChoice
-                  [Undecided Draw, Undecided Draw, Undecided Draw]
-                  []
-            , ignoreWindow
-            ]
-          pure a
-    _ -> OliveMcBride <$> runMessage msg attrs
+  runMessage msg a@(OliveMcBride attrs) = runQueueT $ case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 (toDrawSource -> drawSource) _ -> do
+      push
+        $ ReplaceCurrentDraw drawSource iid
+        $ Choose
+          (toSource attrs)
+          2
+          ResolveChoice
+          [Undecided Draw, Undecided Draw, Undecided Draw]
+          []
+      cancelledOrIgnoredCardOrGameEffect (attrs.ability 1)
+      pure a
+    _ -> OliveMcBride <$> liftRunMessage msg attrs
--- backend/arkham-core/library/Arkham/Asset/Cards/OliveMcBride2.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/OliveMcBride2.hs
@@ -1,17 +1,11 @@
-module Arkham.Asset.Cards.OliveMcBride2 (
-  oliveMcBride2,
-  OliveMcBride2 (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Cards.OliveMcBride2 (oliveMcBride2, OliveMcBride2 (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
 import Arkham.ChaosBagStepState
 import Arkham.Matcher
-import Arkham.Timing qualified as Timing
-import Arkham.Window (Window (..), mkWindow)
+import Arkham.Window (Window (..))
 import Arkham.Window qualified as Window
 
 newtype OliveMcBride2 = OliveMcBride2 AssetAttrs
@@ -24,31 +18,26 @@ oliveMcBride2 = ally OliveMcBride2 Cards.oliveMcBride2 (1, 3)
 instance HasAbilities OliveMcBride2 where
   getAbilities (OliveMcBride2 a) =
     [ restrictedAbility a 1 ControlsThis
-        $ ReactionAbility (WouldRevealChaosToken Timing.When You)
-        $ ExhaustCost (toTarget a)
+        $ ReactionAbility (WouldRevealChaosToken #when You) (exhaust a)
     ]
 
+toDrawSource :: [Window] -> Source
+toDrawSource = \case
+  ((windowType -> (Window.WouldRevealChaosToken source _)) : _) -> source
+  (_ : rest) -> toDrawSource rest
+  _ -> error "Expected WouldRevealChaosToken window"
+
 instance RunMessage OliveMcBride2 where
-  runMessage msg a@(OliveMcBride2 attrs) = case msg of
-    UseCardAbility
-      iid
-      (isSource attrs -> True)
-      1
-      [Window Timing.When (Window.WouldRevealChaosToken drawSource _) _]
-      _ ->
-        do
-          ignoreWindow <-
-            checkWindows
-              [mkWindow Timing.After (Window.CancelledOrIgnoredCardOrGameEffect $ toAbilitySource attrs 1)]
-          pushAll
-            [ ReplaceCurrentDraw drawSource iid
-                $ Choose
-                  (toSource attrs)
-                  2
-                  ResolveChoice
-                  [Undecided Draw, Undecided Draw, Undecided Draw, Undecided Draw]
-                  []
-            , ignoreWindow
-            ]
-          pure a
-    _ -> OliveMcBride2 <$> runMessage msg attrs
+  runMessage msg a@(OliveMcBride2 attrs) = runQueueT $ case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 (toDrawSource -> drawSource) _ -> do
+      push
+        $ ReplaceCurrentDraw drawSource iid
+        $ Choose
+          (toSource attrs)
+          2
+          ResolveChoice
+          [Undecided Draw, Undecided Draw, Undecided Draw, Undecided Draw]
+          []
+      cancelledOrIgnoredCardOrGameEffect (attrs.ability 1)
+      pure a
+    _ -> OliveMcBride2 <$> liftRunMessage msg attrs

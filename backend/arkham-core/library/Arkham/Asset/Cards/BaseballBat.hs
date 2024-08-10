06048174e0b64af0d6c75ae06ba1422da320dc7f
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-11 02:12:21 +0900

    Fix Baseball Bat
    Baseball bat should not be discarded if the token was cancelled.

--- backend/arkham-core/library/Arkham/Asset/Cards/BaseballBat.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/BaseballBat.hs
@@ -2,11 +2,11 @@ module Arkham.Asset.Cards.BaseballBat (BaseballBat (..), baseballBat, baseballBa
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
 import Arkham.ChaosToken
-import Arkham.Effect.Runner
-import Arkham.Fight
-import Arkham.Prelude
+import Arkham.Effect.Import
+import Arkham.Helpers.SkillTest (getSkillTestRevealedChaosTokens, withSkillTest)
+import Arkham.Modifier
 
 newtype BaseballBat = BaseballBat AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -19,18 +19,15 @@ instance HasAbilities BaseballBat where
   getAbilities (BaseballBat a) = [fightAbility a 1 mempty ControlsThis]
 
 instance RunMessage BaseballBat where
-  runMessage msg a@(BaseballBat attrs) = case msg of
+  runMessage msg a@(BaseballBat attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       let source = attrs.ability 1
       sid <- getRandom
-      chooseFight <- toMessage <$> mkChooseFight sid iid (attrs.ability 1)
-      pushAll
-        [ skillTestModifiers sid source iid [SkillModifier #combat 2, DamageDealt 1]
-        , createCardEffect Cards.baseballBat (effectMetaTarget sid) source iid
-        , chooseFight
-        ]
+      skillTestModifiers sid source iid [SkillModifier #combat 2, DamageDealt 1]
+      createCardEffect Cards.baseballBat (effectMetaTarget sid) source iid
+      chooseFightEnemy sid iid (attrs.ability 1)
       pure a
-    _ -> BaseballBat <$> runMessage msg attrs
+    _ -> BaseballBat <$> liftRunMessage msg attrs
 
 newtype BaseballBatEffect = BaseballBatEffect EffectAttrs
   deriving anyclass (HasAbilities, IsEffect, HasModifiersFor)
@@ -40,20 +37,17 @@ baseballBatEffect :: EffectArgs -> BaseballBatEffect
 baseballBatEffect = cardEffect BaseballBatEffect Cards.baseballBat
 
 instance RunMessage BaseballBatEffect where
-  runMessage msg e@(BaseballBatEffect attrs) = case msg of
-    RevealChaosToken _ iid token | InvestigatorTarget iid == attrs.target -> do
-      withSkillTest \sid -> do
-        when (maybe False (isTarget sid) attrs.metaTarget) $ do
-          case attrs.source of
-            AbilitySource (AssetSource assetId) 1 ->
-              when (token.face `elem` [Skull, AutoFail])
-                $ pushAll [toDiscardBy iid attrs.source assetId, disable attrs]
-            AbilitySource (ProxySource (CardIdSource _) (AssetSource assetId)) 1 ->
-              when (token.face `elem` [Skull, AutoFail])
-                $ pushAll [toDiscardBy iid attrs.source assetId, disable attrs]
-            _ -> error "wrong source"
-      pure e
-    SkillTestEnds sid _ _ | maybe False (isTarget sid) attrs.metaTarget -> do
-      push (disable attrs)
-      pure e
-    _ -> BaseballBatEffect <$> runMessage msg attrs
+  runMessage msg e@(BaseballBatEffect attrs) = runQueueT $ case msg of
+    SkillTestEnds sid iid _ | maybe False (isTarget sid) attrs.metaTarget -> do
+      when (maybe False (isTarget sid) attrs.metaTarget) $ do
+        tokens <- map (.face) <$> getSkillTestRevealedChaosTokens
+        case attrs.source of
+          AbilitySource (AssetSource assetId) 1 ->
+            when (any (`elem` [Skull, AutoFail]) tokens) do
+              afterSkillTest $ toDiscardBy iid attrs.source assetId
+          AbilitySource (ProxySource (CardIdSource _) (AssetSource assetId)) 1 ->
+            when (any (`elem` [Skull, AutoFail]) tokens) do
+              afterSkillTest $ toDiscardBy iid attrs.source assetId
+          _ -> error "wrong source"
+      disableReturn e
+    _ -> BaseballBatEffect <$> liftRunMessage msg attrs

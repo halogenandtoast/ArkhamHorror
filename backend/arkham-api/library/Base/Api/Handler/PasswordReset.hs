3b6af1937e6213fba624a23fc1d08e1d6bdd6096
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-15 10:48:47 +0900

    Mailtrap setup

--- backend/arkham-api/library/Base/Api/Handler/PasswordReset.hs
+++ backend/arkham-api/library/Base/Api/Handler/PasswordReset.hs
@@ -2,9 +2,13 @@ module Base.Api.Handler.PasswordReset (postApiV1PasswordResetsR, putApiV1Passwor
 
 import Crypto.BCrypt
 import Data.Aeson (withObject)
+import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
 import Data.Time.Clock
+import Data.UUID qualified as UUID (toText)
 import Import
+import Network.Mail.Mailtrap
+import Text.Email.Parser (unsafeEmailAddress)
 
 newtype PasswordResetRequest = PasswordResetRequest {resetEmail :: Text}
 
@@ -22,10 +26,47 @@ postApiV1PasswordResetsR :: Handler ()
 postApiV1PasswordResetsR = do
   payload <- requireCheckJsonBody
   resetExpiresAt <- liftIO $ addUTCTime nominalDay <$> getCurrentTime
-  runDB $ do
-    mUserId <- getBy (UniqueEmail $ resetEmail payload)
-    for_ mUserId $ \(Entity userId _) ->
-      insert_ $ PasswordReset userId resetExpiresAt
+  Entity userId _ <- runDB $ getBy404 (UniqueEmail $ resetEmail payload)
+  token <- runDB $ insert $ PasswordReset userId resetExpiresAt
+  sendPasswordResetEmail (resetEmail payload) (UUID.toText $ coerce token)
+ where
+  sendPasswordResetEmail :: Text -> Text -> Handler ()
+  sendPasswordResetEmail email token = do
+    case parseEmailAddress (TE.encodeUtf8 email) of
+      Left _ -> pure ()
+      Right emailParsed -> do
+        let url = "https://arkhamhorror.app/#/password-reset/" <> token
+        apiToken <- getsYesod $ appMailtrapApiToken . appSettings
+        liftIO $ print apiToken
+        void
+          $ liftIO
+          $ sendEmail apiToken
+          $ Email
+            { email_from =
+                NamedEmailAddress (unsafeEmailAddress "noreply" "arkhamhorror.app") "No Reply"
+            , email_to = [NamedEmailAddress emailParsed ""]
+            , email_cc = []
+            , email_bcc = []
+            , email_attachments = []
+            , email_custom = mempty
+            , email_message =
+                Right
+                  $ Message
+                    { message_subject = "Password Reset for Arkham Horror Online"
+                    , message_body =
+                        PlainTextBody
+                          $ T.unlines
+                            [ "You have requested a password reset."
+                            , ""
+                            , "To reset your password, click the link below:"
+                            , ""
+                            , url
+                            , ""
+                            , "If you did not request a password reset, you can safely ignore this email."
+                            ]
+                    , message_category = "Password Reset"
+                    }
+            }
 
 putApiV1PasswordResetR :: PasswordResetId -> Handler ()
 putApiV1PasswordResetR resetId = do

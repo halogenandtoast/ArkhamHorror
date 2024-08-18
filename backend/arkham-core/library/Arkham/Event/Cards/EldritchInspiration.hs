2fcc97171dbee66bd0311fdca23a054092dc01f1
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-18 17:46:11 +0900

    Fix cancel windows for delay the inevitable and eldritch inspiration

--- backend/arkham-core/library/Arkham/Event/Cards/EldritchInspiration.hs
+++ backend/arkham-core/library/Arkham/Event/Cards/EldritchInspiration.hs
@@ -3,13 +3,13 @@ module Arkham.Event.Cards.EldritchInspiration (
   EldritchInspiration (..),
 ) where
 
-import Arkham.Prelude
-
 import Arkham.Asset.Types (Field (..))
 import Arkham.Card
-import Arkham.Classes
+import Arkham.Classes.HasQueue (fromQueue, popMessageMatching_)
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
+import Arkham.Event.Types qualified as Field
+import Arkham.Helpers.Effect (lookupEffectCard)
 import Arkham.Name
 import Arkham.Projection
 import Arkham.Timing
@@ -24,9 +24,9 @@ eldritchInspiration :: EventCard EldritchInspiration
 eldritchInspiration = event EldritchInspiration Cards.eldritchInspiration
 
 instance RunMessage EldritchInspiration where
-  runMessage msg e@(EldritchInspiration attrs) = case msg of
+  runMessage msg e@(EldritchInspiration attrs) = runQueueT $ case msg of
     PlayThisEvent iid eid | eid == toId attrs -> do
-      mmsg <- fromQueue $ find \case
+      mmsg <- lift $ fromQueue $ find \case
         Do (If wType _) -> case wType of
           Window.RevealChaosTokenEffect {} -> True
           Window.RevealChaosTokenEventEffect {} -> True
@@ -34,29 +34,25 @@ instance RunMessage EldritchInspiration where
           _ -> False
         _ -> False
 
-      player <- getPlayer iid
       for_ mmsg $ \effectMsg -> case effectMsg of
         Do (If (Window.RevealChaosTokenEffect _ _ effectId) _) -> do
           mCardDef <- lookupEffectCard effectId
           for_ mCardDef $ \cardDef ->
-            push
-              $ questionLabel (display $ cdName cardDef) player
+            questionLabel (display $ cdName cardDef) iid
               $ ChooseOne
                 [ Label "Cancel effect" [ResolveEvent iid eid Nothing []]
                 , Label "Resolve an additional time" [effectMsg]
                 ]
         Do (If (Window.RevealChaosTokenEventEffect _ _ eventId) _) -> do
-          cardName <- cdName . toCardDef <$> field EventCard eventId
-          push
-            $ questionLabel (display cardName) player
+          cardName <- cdName . toCardDef <$> field Field.EventCard eventId
+          questionLabel (display cardName) iid
             $ ChooseOne
               [ Label "Cancel effect" [ResolveEvent iid eid Nothing []]
               , Label "Resolve an additional time" [effectMsg]
               ]
         Do (If (Window.RevealChaosTokenAssetAbilityEffect _ _ assetId) _) -> do
           cardName <- cdName . toCardDef <$> field AssetCard assetId
-          push
-            $ questionLabel (display cardName) player
+          questionLabel (display cardName) iid
             $ ChooseOne
               [ Label "Cancel effect" [ResolveEvent iid eid Nothing []]
               , Label "Resolve an additional time" [effectMsg]
@@ -65,19 +61,21 @@ instance RunMessage EldritchInspiration where
 
       pure e
     ResolveEvent _ eid _ _ | eid == toId attrs -> do
-      popMessageMatching_ \case
-        Do (If wType _) -> case wType of
-          Window.RevealChaosTokenEffect {} -> True
-          Window.RevealChaosTokenEventEffect {} -> True
-          Window.RevealChaosTokenAssetAbilityEffect {} -> True
+      lift do
+        popMessageMatching_ \case
+          Do (If wType _) -> case wType of
+            Window.RevealChaosTokenEffect {} -> True
+            Window.RevealChaosTokenEventEffect {} -> True
+            Window.RevealChaosTokenAssetAbilityEffect {} -> True
+            _ -> False
           _ -> False
-        _ -> False
-      popMessageMatching_ \case
-        RunWindow _ [Window AtIf wType _] -> case wType of
-          Window.RevealChaosTokenEffect {} -> True
-          Window.RevealChaosTokenEventEffect {} -> True
-          Window.RevealChaosTokenAssetAbilityEffect {} -> True
+        popMessageMatching_ \case
+          RunWindow _ [Window AtIf wType _] -> case wType of
+            Window.RevealChaosTokenEffect {} -> True
+            Window.RevealChaosTokenEventEffect {} -> True
+            Window.RevealChaosTokenAssetAbilityEffect {} -> True
+            _ -> False
           _ -> False
-        _ -> False
+      cancelledOrIgnoredCardOrGameEffect attrs
       pure e
-    _ -> EldritchInspiration <$> runMessage msg attrs
+    _ -> EldritchInspiration <$> liftRunMessage msg attrs

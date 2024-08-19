9752cb16d6fed371b3549c0456522031bf800902
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-19 13:16:26 +0900

    Beguile Fixes

--- backend/arkham-core/library/Arkham/Asset/Cards/FineClothes.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/FineClothes.hs
@@ -1,8 +1,10 @@
 module Arkham.Asset.Cards.FineClothes (fineClothes, FineClothes (..)) where
 
+import Arkham.Ability
 import Arkham.Action qualified as Action
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Runner
+import {-# SOURCE #-} Arkham.GameEnv
 import Arkham.Prelude
 
 newtype FineClothes = FineClothes AssetAttrs
@@ -13,11 +15,14 @@ fineClothes :: AssetCard FineClothes
 fineClothes = assetWith FineClothes Cards.fineClothes $ (healthL ?~ 1) . (sanityL ?~ 1)
 
 instance HasModifiersFor FineClothes where
-  getModifiersFor (SkillTestTarget _) (FineClothes a) = maybeModified a do
-    Action.Parley <- MaybeT getSkillTestAction
-    iid <- MaybeT getSkillTestInvestigator
-    guard $ a `controlledBy` iid
-    pure [Difficulty (-2)]
+  getModifiersFor (SkillTestTarget _) (FineClothes a) = do
+    -- because this is during a parley we can check if any active abilities are parleys
+    inParley <- any (`abilityIs` #parley) <$> getActiveAbilities
+    maybeModified a do
+      Action.Parley <- MaybeT $ if inParley then pure (Just Action.Parley) else getSkillTestAction
+      iid <- MaybeT getSkillTestInvestigator
+      guard $ a `controlledBy` iid
+      pure [Difficulty (-2)]
   getModifiersFor _ _ = pure []
 
 instance RunMessage FineClothes where

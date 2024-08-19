c3a90daad5fc92cd9d9cab03609d3fdbedcc2c16
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-19 13:23:29 +0900

    Defeating The Experiment should go directly to 3B

--- backend/arkham-core/library/Arkham/Enemy/Cards/TheExperiment.hs
+++ backend/arkham-core/library/Arkham/Enemy/Cards/TheExperiment.hs
@@ -6,13 +6,15 @@ module Arkham.Enemy.Cards.TheExperiment (
 import Arkham.Prelude
 
 import Arkham.Ability
+import Arkham.Act.Cards qualified as Acts
+import Arkham.Act.Sequence
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Runner
 import Arkham.Matcher
 
 newtype TheExperiment = TheExperiment EnemyAttrs
-  deriving anyclass (IsEnemy)
+  deriving anyclass IsEnemy
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theExperiment :: EnemyCard TheExperiment
@@ -22,7 +24,7 @@ instance HasAbilities TheExperiment where
   getAbilities (TheExperiment x) =
     withBaseAbilities x
       $ [ mkAbility x 1 $ ForcedAbility $ PhaseBegins #when #enemy
-        , mkAbility x 2 $ Objective $ ForcedAbility $ EnemyDefeated #when You ByAny $ EnemyWithId (toId x)
+        , mkAbility x 2 $ Objective $ forced $ EnemyDefeated #when You ByAny (be x)
         ]
 
 instance HasModifiersFor TheExperiment where
@@ -37,7 +39,6 @@ instance RunMessage TheExperiment where
       push $ Ready $ toTarget attrs
       pure e
     UseThisAbility _ (isSource attrs -> True) 2 -> do
-      actId <- selectJust AnyAct
-      push $ AdvanceAct actId (toSource attrs) AdvancedWithOther
+      push $ AdvanceToAct 1 Acts.campusSafety B (toSource attrs)
       pure e
     _ -> TheExperiment <$> runMessage msg attrs

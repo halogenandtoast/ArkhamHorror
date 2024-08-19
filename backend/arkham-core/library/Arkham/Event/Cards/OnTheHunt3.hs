32f654e9c4493143b71e0934f7ca5c041dd2c8db
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-19 23:47:22 +0900

    Fix On the Hunt not finding anything

--- backend/arkham-core/library/Arkham/Event/Cards/OnTheHunt3.hs
+++ backend/arkham-core/library/Arkham/Event/Cards/OnTheHunt3.hs
@@ -39,6 +39,9 @@ instance RunMessage OnTheHunt3 where
     SearchNoneFound iid (isTarget attrs -> True) -> do
       drawEncounterCard iid attrs
       pure e
+    SearchFound iid (isTarget attrs -> True) _ [] -> do
+      drawEncounterCard iid attrs
+      pure e
     SearchFound iid (isTarget attrs -> True) _ cards -> do
       additionalTargets <- getAdditionalSearchTargets iid
       let enemyCards = filter (`cardMatch` EnemyType) $ onlyEncounterCards cards

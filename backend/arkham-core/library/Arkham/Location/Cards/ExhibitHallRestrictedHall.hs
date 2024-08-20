8744d349ea9a5f88a9af91988ff319a41ce6e964
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-21 01:33:55 +0900

    The final void fixes

--- backend/arkham-core/library/Arkham/Location/Cards/ExhibitHallRestrictedHall.hs
+++ backend/arkham-core/library/Arkham/Location/Cards/ExhibitHallRestrictedHall.hs
@@ -3,8 +3,6 @@ module Arkham.Location.Cards.ExhibitHallRestrictedHall (
   ExhibitHallRestrictedHall (..),
 ) where
 
-import Arkham.Prelude
-
 import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.GameValue
@@ -12,28 +10,19 @@ import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Helpers
 import Arkham.Location.Runner
 import Arkham.Matcher
+import Arkham.Prelude
 
 newtype ExhibitHallRestrictedHall = ExhibitHallRestrictedHall LocationAttrs
-  deriving anyclass (IsLocation)
+  deriving anyclass IsLocation
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 exhibitHallRestrictedHall :: LocationCard ExhibitHallRestrictedHall
-exhibitHallRestrictedHall =
-  location
-    ExhibitHallRestrictedHall
-    Cards.exhibitHallRestrictedHall
-    3
-    (PerPlayer 2)
+exhibitHallRestrictedHall = location ExhibitHallRestrictedHall Cards.exhibitHallRestrictedHall 3 (PerPlayer 2)
 
 instance HasModifiersFor ExhibitHallRestrictedHall where
-  getModifiersFor target (ExhibitHallRestrictedHall attrs)
-    | isTarget attrs target = do
-        mHuntingHorror <-
-          selectOne
-            $ enemyIs Cards.huntingHorror
-            <> EnemyAt
-              (LocationWithId $ toId attrs)
-        pure $ toModifiers attrs [CannotInvestigate | isJust mHuntingHorror]
+  getModifiersFor target (ExhibitHallRestrictedHall attrs) | isTarget attrs target = do
+    mHuntingHorror <- selectOne $ enemyIs Cards.huntingHorror <> at_ (LocationWithId $ toId attrs)
+    pure $ toModifiers attrs [CannotInvestigate | isJust mHuntingHorror]
   getModifiersFor _ _ = pure []
 
 instance RunMessage ExhibitHallRestrictedHall where

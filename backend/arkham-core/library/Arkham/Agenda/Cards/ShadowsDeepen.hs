681d1851a3df5ec4c004357dccd3dcd5f08f596b
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-21 01:27:30 +0900

    Some other void handling

--- backend/arkham-core/library/Arkham/Agenda/Cards/ShadowsDeepen.hs
+++ backend/arkham-core/library/Arkham/Agenda/Cards/ShadowsDeepen.hs
@@ -1,9 +1,4 @@
-module Arkham.Agenda.Cards.ShadowsDeepen (
-  ShadowsDeepen (..),
-  shadowsDeepen,
-) where
-
-import Arkham.Prelude
+module Arkham.Agenda.Cards.ShadowsDeepen (ShadowsDeepen (..), shadowsDeepen) where
 
 import Arkham.Ability
 import Arkham.Agenda.Cards qualified as Cards
@@ -13,6 +8,7 @@ import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Enemies
 import Arkham.GameValue
 import Arkham.Matcher
+import Arkham.Prelude
 import Arkham.Scenarios.TheMiskatonicMuseum.Helpers
 import Arkham.Timing qualified as Timing
 import Arkham.Treachery.Cards qualified as Treacheries
@@ -28,28 +24,22 @@ shadowsDeepen = agenda (2, A) ShadowsDeepen Cards.shadowsDeepen (Static 7)
 
 instance HasAbilities ShadowsDeepen where
   getAbilities (ShadowsDeepen x) =
-    [ mkAbility x 1
-        $ ForcedAbility
-        $ EnemySpawns Timing.When Anywhere
-        $ enemyIs
-          Enemies.huntingHorror
-    ]
+    [mkAbility x 1 $ forced $ EnemySpawns #when Anywhere $ enemyIs Enemies.huntingHorror]
 
 instance RunMessage ShadowsDeepen where
   runMessage msg a@(ShadowsDeepen attrs) = case msg of
-    UseCardAbility _ source 1 [(windowType -> Window.EnemySpawns eid _)] _
-      | isSource attrs source -> do
-          mShadowSpawnedId <- selectOne $ treacheryIs Treacheries.shadowSpawned
-          shadowSpawned <- genCard Treacheries.shadowSpawned
-          case mShadowSpawnedId of
-            Just tid -> push $ PlaceResources (toAbilitySource attrs 1) (toTarget tid) 1
-            Nothing -> do
-              tid <- getRandom
-              push $ AttachStoryTreacheryTo tid shadowSpawned (EnemyTarget eid)
-          pure a
+    UseCardAbility _ source 1 [(windowType -> Window.EnemySpawns eid _)] _ | isSource attrs source -> do
+      mShadowSpawnedId <- selectOne $ treacheryIs Treacheries.shadowSpawned
+      shadowSpawned <- genCard Treacheries.shadowSpawned
+      case mShadowSpawnedId of
+        Just tid -> push $ PlaceResources (toAbilitySource attrs 1) (toTarget tid) 1
+        Nothing -> do
+          tid <- getRandom
+          push $ AttachStoryTreacheryTo tid shadowSpawned (EnemyTarget eid)
+      pure a
     AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
       leadInvestigatorId <- getLeadInvestigatorId
-      mHuntingHorrorId <- getHuntingHorror
+      mHuntingHorrorId <- getInPlayHuntingHorror
       case mHuntingHorrorId of
         Just eid ->
           pushAll

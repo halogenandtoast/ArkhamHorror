58ed949f629b1db55dad88eeaa898306fdafdc44
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-21 17:39:41 +0900

    Handle campaign log entires "better"

--- frontend/src/locales/en.ts
+++ frontend/src/locales/en.ts
@@ -1,5 +1,6 @@
 import base from '@/locales/en/base.json'
+import campaignLog from '@/locales/en/campaignLog.json'
 import nightOfTheZealot from '@/locales/en/nightOfTheZealot.json'
 import dreamEaters from '@/locales/en/dreamEaters.json'
 
-export default {...base, nightOfTheZealot, dreamEaters}
+export default {...base, ...campaignLog, nightOfTheZealot, dreamEaters}

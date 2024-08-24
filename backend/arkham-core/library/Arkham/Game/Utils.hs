585dd8bf74e4739180a98adbfa12347808d9d47f
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-24 23:02:03 +0900

    Fix In Pursuit

--- backend/arkham-core/library/Arkham/Game/Utils.hs
+++ backend/arkham-core/library/Arkham/Game/Utils.hs
@@ -222,6 +222,7 @@ maybeEnemy eid = do
   g <- getGame
   pure
     $ preview (entitiesL . enemiesL . ix eid) g
+    <|> preview (outOfPlayEntitiesL . each . enemiesL . ix eid) g
     <|> getInDiscardEntity enemiesL eid g
     <|> getInEncounterDiscardEntity enemiesL eid g
     <|> getRemovedEntity enemiesL eid g

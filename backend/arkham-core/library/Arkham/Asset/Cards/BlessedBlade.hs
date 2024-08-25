e0608acbe75db0a99306eb3f30f48d10a29373e3
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-26 08:25:45 +0900

    Fix and simplify Blessed Blade (0, 4)

--- backend/arkham-core/library/Arkham/Asset/Cards/BlessedBlade.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/BlessedBlade.hs
@@ -2,66 +2,46 @@ module Arkham.Asset.Cards.BlessedBlade (blessedBlade, BlessedBlade (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
-import Arkham.ChaosToken
-import Arkham.Fight
+import Arkham.Asset.Import.Lifted
 import Arkham.Helpers.ChaosBag
+import Arkham.Helpers.Cost
+import Arkham.Helpers.SkillTest
 import Arkham.Matcher hiding (RevealChaosToken)
-import Arkham.Message
-import Arkham.Prelude
+import Arkham.Modifier
 
 newtype BlessedBlade = BlessedBlade AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 blessedBlade :: AssetCard BlessedBlade
-blessedBlade = assetWith BlessedBlade Cards.blessedBlade (setMeta @Bool True)
+blessedBlade = asset BlessedBlade Cards.blessedBlade
 
 instance HasAbilities BlessedBlade where
-  getAbilities (BlessedBlade attrs) = [controlledAbility attrs 1 (exists $ be attrs <> AssetReady) fightAction_]
+  getAbilities (BlessedBlade attrs) = [controlledAbility attrs 1 (exists $ be attrs <> #ready) fightAction_]
 
 instance RunMessage BlessedBlade where
-  runMessage msg a@(BlessedBlade attrs) = case msg of
+  runMessage msg a@(BlessedBlade attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
+      let source = attrs.ability 1
       sid <- getRandom
-      chooseFight <- toMessage <$> mkChooseFight sid iid (attrs.ability 1)
-      pushAll
-        [ skillTestModifier sid (attrs.ability 1) iid (SkillModifier #combat 1)
-        , chooseFight
-        ]
+      skillTestModifier sid source iid $ SkillModifier #combat 1
+      onRevealChaosTokenEffect sid (oneOf [#bless, #eldersign]) attrs attrs do
+        skillTestModifier sid source iid $ DamageDealt 1
+      chooseFightEnemy sid iid source
       pure a
-    RevealChaosToken (SkillTestSource sid) iid token -> do
-      s <- fromJustNote "Must be in skillTest" <$> getSkillTestSource
-      if isAbilitySource attrs 1 s
-        then do
-          case maybeResult attrs.meta of
-            Just meta | meta == sid && token.face `elem` [#bless, #eldersign] -> do
-              push $ skillTestModifier sid (attrs.ability 1) iid (DamageDealt 1)
-              pure . BlessedBlade $ attrs & setMeta sid
-            _ -> pure a
-        else pure a
-    SkillTestEnds sid _ _ | maybe False (== sid) (getAssetMeta attrs) -> do
-      pure . BlessedBlade $ attrs & unsetMeta
     BeforeRevealChaosTokens -> do
       void $ runMaybeT $ do
-        source <- MaybeT getSkillTestSource
-        guard $ isAbilitySource attrs 1 source
-        iid <- MaybeT getSkillTestInvestigator
-        player <- lift $ getPlayer iid
-        canAfford <- lift $ getCanAffordCost iid source [] [] (exhaust attrs)
-        hasBlessTokens <- lift $ (> 0) <$> getRemainingBlessTokens
-        guard canAfford
-        guard hasBlessTokens
-
+        st <- MaybeT getSkillTest
+        guard $ isAbilitySource attrs 1 st.source
+        liftGuardM $ getCanAffordCost st.investigator st.source [] [] (exhaust attrs)
+        liftGuardM $ (> 0) <$> getRemainingBlessTokens
         lift
-          $ push
           $ chooseOne
-            player
+            st.investigator
             [ Label
                 "Exhaust Blessed Blade to add 1 {bless} token"
-                [Exhaust (toTarget attrs), AddChaosToken BlessToken]
+                [Exhaust (toTarget attrs), AddChaosToken #bless]
             , Label "Do not exhaust" []
             ]
-
       pure a
-    _ -> BlessedBlade <$> runMessage msg attrs
+    _ -> BlessedBlade <$> liftRunMessage msg attrs

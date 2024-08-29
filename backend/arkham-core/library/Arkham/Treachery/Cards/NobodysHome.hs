8b20855c7c240a8b973040d3311c627ab9f608f7
Matthew Mongeau <halogenandtoast@gmail.com>
2024-08-29 14:04:15 +0900

    Cleanup Nobody's Home

--- backend/arkham-core/library/Arkham/Treachery/Cards/NobodysHome.hs
+++ backend/arkham-core/library/Arkham/Treachery/Cards/NobodysHome.hs
@@ -1,61 +1,43 @@
-module Arkham.Treachery.Cards.NobodysHome (
-  nobodysHome,
-  NobodysHome (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Treachery.Cards.NobodysHome (nobodysHome, NobodysHome (..)) where
 
 import Arkham.Ability
-import Arkham.Action qualified as Action
-import Arkham.Classes
+import Arkham.Helpers.Investigator
 import Arkham.Helpers.Modifiers
-import Arkham.Investigator.Types (Field (InvestigatorLocation))
 import Arkham.Location.Types (Field (LocationClues))
 import Arkham.Matcher
 import Arkham.Projection
-import Arkham.Source
 import Arkham.Treachery.Cards qualified as Cards
-import Arkham.Treachery.Runner
+import Arkham.Treachery.Import.Lifted
 
 newtype NobodysHome = NobodysHome TreacheryAttrs
-  deriving anyclass (IsTreachery)
+  deriving anyclass IsTreachery
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 nobodysHome :: TreacheryCard NobodysHome
 nobodysHome = treachery NobodysHome Cards.nobodysHome
 
 instance HasModifiersFor NobodysHome where
-  getModifiersFor (AbilityTarget _ ab) (NobodysHome attrs) = do
-    case abilitySource ab of
-      LocationSource lid | treacheryOnLocation lid attrs -> do
-        if Action.Investigate `elem` abilityActions ab
-          then pure $ toModifiers attrs [AdditionalCost (ActionCost 1)]
-          else pure []
-      _ -> pure []
+  getModifiersFor (AbilityTarget _ ab) (NobodysHome attrs) = maybeModified attrs do
+    lid <- hoistMaybe ab.source.location
+    guard $ toTarget lid `elem` attrs.attached
+    guard $ #investigate `elem` ab.actions
+    pure [AdditionalCost (ActionCost 1)]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities NobodysHome where
-  getAbilities (NobodysHome a) = case treacheryAttachedTarget a of
+  getAbilities (NobodysHome a) = case a.attached of
     Just (LocationTarget lid) ->
-      [ restrictedAbility
-          a
-          1
-          (exists $ LocationWithId lid <> LocationWithoutClues)
-          $ ForcedAbility AnyWindow
-      ]
+      [restrictedAbility a 1 (exists $ be lid <> LocationWithoutClues) $ ForcedAbility AnyWindow]
     _ -> []
 
 instance RunMessage NobodysHome where
-  runMessage msg t@(NobodysHome attrs) = case msg of
-    Revelation iid source | isSource attrs source -> do
-      mLocation <- field InvestigatorLocation iid
-      for_ mLocation \lid -> do
-        clueless <- fieldP LocationClues (== 0) lid
-        pushAll
-          $ attachTreachery attrs lid
-          : [gainSurge attrs | clueless]
+  runMessage msg t@(NobodysHome attrs) = runQueueT $ case msg of
+    Revelation iid (isSource attrs -> True) -> do
+      withLocationOf iid \lid -> do
+        whenM (fieldNone LocationClues lid) $ gainSurge attrs
+        attachTreachery attrs lid
       pure t
-    UseCardAbility _iid source 1 _ _ | isSource attrs source -> do
-      push $ toDiscard (toAbilitySource attrs 1) attrs
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
+      toDiscard (attrs.ability 1) attrs
       pure t
-    _ -> NobodysHome <$> runMessage msg attrs
+    _ -> NobodysHome <$> liftRunMessage msg attrs

66daab88907752adf0bd04cf9bd804859e7e6163
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-02 12:52:56 +0900

    Fix Leo De Luca (1)

--- backend/arkham-core/library/Arkham/Asset/Cards/LeoDeLuca1.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/LeoDeLuca1.hs
@@ -1,9 +1,8 @@
 module Arkham.Asset.Cards.LeoDeLuca1 where
 
-import Arkham.Prelude
-
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
+import Arkham.Modifier
 
 newtype LeoDeLuca1 = LeoDeLuca1 AssetAttrs
   deriving anyclass (IsAsset, HasAbilities)
@@ -14,12 +13,8 @@ leoDeLuca1 = ally LeoDeLuca1 Cards.leoDeLuca1 (2, 2)
 
 instance HasModifiersFor LeoDeLuca1 where
   getModifiersFor (InvestigatorTarget iid) (LeoDeLuca1 a) =
-    pure $ toModifiers a [AdditionalActions "Leo de Luca" (toSource a) 1 | controlledBy a iid]
+    modified a [AdditionalActions "Leo de Luca" (toSource a) 1 | controlledBy a iid]
   getModifiersFor _ _ = pure []
 
 instance RunMessage LeoDeLuca1 where
-  runMessage msg (LeoDeLuca1 attrs) = case msg of
-    InvestigatorPlayAsset iid aid | aid == toId attrs -> do
-      push $ GainActions iid (toSource aid) 1
-      LeoDeLuca1 <$> runMessage msg attrs
-    _ -> LeoDeLuca1 <$> runMessage msg attrs
+  runMessage msg (LeoDeLuca1 attrs) = LeoDeLuca1 <$> runMessage msg attrs

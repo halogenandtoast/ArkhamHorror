eef26172eb65a014b408adb9e4cf5fcec4449172
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-03 05:47:41 +0900

    Meat Cleaver healing is optional

--- backend/arkham-core/library/Arkham/Asset/Cards/MeatCleaver.hs
+++ backend/arkham-core/library/Arkham/Asset/Cards/MeatCleaver.hs
@@ -2,12 +2,12 @@ module Arkham.Asset.Cards.MeatCleaver (meatCleaver, meatCleaverEffect, MeatCleav
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
-import Arkham.Effect.Runner
-import Arkham.Fight
+import Arkham.Asset.Import.Lifted
+import Arkham.Effect.Import
 import Arkham.Helpers.Investigator
 import Arkham.Investigator.Types (Field (..))
-import Arkham.Prelude
+import Arkham.Message.Lifted.Choose
+import Arkham.Modifier
 import Arkham.Projection
 
 newtype MeatCleaver = MeatCleaver AssetAttrs
@@ -29,20 +29,17 @@ paidHorror (Payments ps) = any paidHorror ps
 paidHorror _ = False
 
 instance RunMessage MeatCleaver where
-  runMessage msg a@(MeatCleaver attrs) = case msg of
+  runMessage msg a@(MeatCleaver attrs) = runQueueT $ case msg of
     UseCardAbility iid (isSource attrs -> True) 1 _ payments -> do
       let source = attrs.ability 1
       sid <- getRandom
       remainingSanity <- field InvestigatorRemainingSanity iid
       let n = if remainingSanity <= 3 then 2 else 1
-      chooseFight <- toMessage <$> mkChooseFight sid iid source
-      pushAll
-        [ skillTestModifiers sid attrs iid $ SkillModifier #combat n : [DamageDealt 1 | paidHorror payments]
-        , createCardEffect Cards.meatCleaver (effectMetaTarget sid) source iid
-        , chooseFight
-        ]
+      skillTestModifiers sid attrs iid $ SkillModifier #combat n : [DamageDealt 1 | paidHorror payments]
+      createCardEffect Cards.meatCleaver (effectMetaTarget sid) source iid
+      chooseFightEnemy sid iid source
       pure a
-    _ -> MeatCleaver <$> runMessage msg attrs
+    _ -> MeatCleaver <$> liftRunMessage msg attrs
 
 newtype MeatCleaverEffect = MeatCleaverEffect EffectAttrs
   deriving anyclass (HasAbilities, IsEffect, HasModifiersFor)
@@ -52,14 +49,18 @@ meatCleaverEffect :: EffectArgs -> MeatCleaverEffect
 meatCleaverEffect = cardEffect MeatCleaverEffect Cards.meatCleaver
 
 instance RunMessage MeatCleaverEffect where
-  runMessage msg e@(MeatCleaverEffect attrs) = case msg of
+  runMessage msg e@(MeatCleaverEffect attrs) = runQueueT $ case msg of
     EnemyDefeated _ _ source _ | attrs.source == source -> do
       case attrs.target of
         InvestigatorTarget iid -> do
           canBeHealed <- canHaveHorrorHealed attrs.source iid
-          pushAll $ [HealHorror (toTarget iid) attrs.source 1 | canBeHealed] <> [disable attrs]
+          when canBeHealed do
+            chooseOneM iid do
+              labeled "Do not heal" nothing
+              horrorLabeled iid $ push $ HealHorror (toTarget iid) attrs.source 1
+          disable attrs
         _ -> error "Invalid target"
       pure e
     SkillTestEnds sid _ _ | maybe False (isTarget sid) attrs.metaTarget -> do
-      e <$ push (disable attrs)
-    _ -> MeatCleaverEffect <$> runMessage msg attrs
+      disableReturn e
+    _ -> MeatCleaverEffect <$> liftRunMessage msg attrs

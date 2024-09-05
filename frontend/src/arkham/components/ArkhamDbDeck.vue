0403ffaaf443d9a8ca26b6b26494ba7c132348a1
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-06 08:49:20 +0900

    Better arkham.build support

--- frontend/src/arkham/components/ArkhamDbDeck.vue
+++ frontend/src/arkham/components/ArkhamDbDeck.vue
@@ -6,27 +6,24 @@ const deck = ref<string | null>(null)
 const deckUrl = ref<string | null>(null)
 
 function loadDeck() {
+  if (!deck.value) return
   model.value = null
+
   const arkhamDbRegex = /https:\/\/arkhamdb\.com\/(deck(list)?)(\/view)?\/([^/]+)/
-  const arkhamBuildRegex = /https:\/\/arkham\.build\/deck\/view\/([^/]+)/
+  const arkhamBuildRegex = /https:\/\/arkham\.build\/(?:deck\/view|share)\/([^/]+)/
   
-  if (deck.value?.match(arkhamDbRegex)) {
-    const matches = deck.value.match(arkhamDbRegex)
-    if (matches) {
-      deckUrl.value = `https://arkhamdb.com/api/public/${matches[1]}/${matches[4]}`
-      fetch(deckUrl.value)
-        .then((response) => response.json(), () => model.value = null)
-        .then((data) => model.value = {...data, url: deckUrl.value}, () => model.value = null)
-    }
-  } else if (deck.value?.match(arkhamBuildRegex)) {
-    const matches = deck.value.match(arkhamBuildRegex)
-    if (matches) {
-      deckUrl.value = `https://api.arkham.build/v1/public/share/${matches[1]}`
-      fetch(deckUrl.value)
-        .then((response) => response.json(), () => model.value = null)
-        .then((data) => model.value = {...data, url: deckUrl.value}, () => model.value = null)
-    }
+  let matches
+  if ((matches = deck.value.match(arkhamDbRegex))) {
+    deckUrl.value = `https://arkhamdb.com/api/public/${matches[1]}/${matches[4]}`
+  } else if ((matches = deck.value.match(arkhamBuildRegex))) {
+    deckUrl.value = `https://api.arkham.build/v1/public/share/${matches[1]}`
+  } else {
+    return
   }
+
+  fetch(deckUrl.value)
+    .then((response) => response.json(), () => model.value = null)
+    .then((data) => model.value = {...data, url: deckUrl.value}, () => model.value = null)
 }
 
 function pasteDeck(evt: ClipboardEvent) {

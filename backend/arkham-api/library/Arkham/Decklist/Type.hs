47f02e7018e8c4bbe6ff332073f2741a946075a3
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-05 13:15:50 +0900

    Add fallback for investigator_name
    arkham.build doesn't provide this so we need to look it up

--- backend/arkham-api/library/Arkham/Decklist/Type.hs
+++ backend/arkham-api/library/Arkham/Decklist/Type.hs
@@ -2,6 +2,8 @@ module Arkham.Decklist.Type where
 
 import Arkham.Card.CardCode
 import Arkham.Id
+import Arkham.Investigator.Cards
+import Arkham.Name
 import Arkham.Prelude
 
 data ArkhamDBDecklist = ArkhamDBDecklist
@@ -18,9 +20,15 @@ data ArkhamDBDecklist = ArkhamDBDecklist
 instance FromJSON ArkhamDBDecklist where
   parseJSON = withObject "ArkhamDBDecklist" $ \o -> do
     slots <- o .: "slots"
-    sideSlots <- (o .: "sideSlots" <|> pure mempty)
+    sideSlots <- o .: "sideSlots" <|> pure mempty
     investigator_code <- o .: "investigator_code"
-    investigator_name <- o .: "investigator_name"
+    investigator_name <-
+      o
+        .:? "investigator_name"
+        .!= maybe
+          (error "missing investigator")
+          toTitle
+          (lookup (coerce investigator_code) allInvestigatorCards)
     meta <- o .:? "meta"
     taboo_id <- o .:? "taboo_id"
     pure $ ArkhamDBDecklist {..}

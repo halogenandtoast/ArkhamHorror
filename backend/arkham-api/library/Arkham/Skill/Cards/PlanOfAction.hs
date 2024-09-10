265ed910bd82d5e20414e95a50d095385a1d3ad4
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-10 15:53:41 +0900

    Fix action count for Plan of Action
    Was counting all actions for each investigator rather than the specific
    turn investigator. Fixes #496

--- backend/arkham-api/library/Arkham/Skill/Cards/PlanOfAction.hs
+++ backend/arkham-api/library/Arkham/Skill/Cards/PlanOfAction.hs
@@ -1,12 +1,10 @@
 module Arkham.Skill.Cards.PlanOfAction (planOfAction, PlanOfAction (..)) where
 
-import Arkham.Classes
 import Arkham.Helpers.Modifiers
 import Arkham.Investigator.Types (Field (..))
 import Arkham.Matcher
-import Arkham.Prelude
 import Arkham.Skill.Cards qualified as Cards
-import Arkham.Skill.Runner
+import Arkham.Skill.Import.Lifted
 
 newtype PlanOfAction = PlanOfAction SkillAttrs
   deriving anyclass (IsSkill, HasAbilities)
@@ -17,17 +15,15 @@ planOfAction = skill PlanOfAction Cards.planOfAction
 
 instance HasModifiersFor PlanOfAction where
   getModifiersFor target (PlanOfAction attrs) | attrs `is` target = do
-    n <- length <$> selectAgg id InvestigatorActionsTaken Anyone
-    pure
-      $ toModifiers attrs
+    n <- length <$> selectAgg id InvestigatorActionsTaken TurnInvestigator
+    modified attrs
       $ [AddSkillIcons [#willpower, #agility] | n == 0]
       <> [AddSkillIcons [#combat, #intellect] | n >= 2]
   getModifiersFor _ _ = pure []
 
 instance RunMessage PlanOfAction where
-  runMessage msg s@(PlanOfAction attrs) = case msg of
+  runMessage msg s@(PlanOfAction attrs) = runQueueT $ case msg of
     PassedSkillTest iid _ _ (SkillTarget sid) _ _ | sid == toId attrs -> do
-      mdrawing <- drawCardsIfCan iid attrs 1
-      for_ mdrawing push
+      drawCardsIfCan iid attrs 1
       pure s
-    _ -> PlanOfAction <$> runMessage msg attrs
+    _ -> PlanOfAction <$> liftRunMessage msg attrs

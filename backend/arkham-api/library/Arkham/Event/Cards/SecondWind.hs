96c68319a19f20ac29f9669212a9b4bd160da229
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-10 12:35:16 +0900

    Correctly move phase to round history
    Fixes #495

--- backend/arkham-api/library/Arkham/Event/Cards/SecondWind.hs
+++ backend/arkham-api/library/Arkham/Event/Cards/SecondWind.hs
@@ -1,13 +1,7 @@
-module Arkham.Event.Cards.SecondWind (
-  secondWind,
-  SecondWind (..),
-) where
+module Arkham.Event.Cards.SecondWind (secondWind, SecondWind (..)) where
 
-import Arkham.Prelude
-
-import Arkham.Classes
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
 import {-# SOURCE #-} Arkham.GameEnv
 import Arkham.History
 
@@ -19,13 +13,11 @@ secondWind :: EventCard SecondWind
 secondWind = event SecondWind Cards.secondWind
 
 instance RunMessage SecondWind where
-  runMessage msg e@(SecondWind attrs@EventAttrs {..}) = case msg of
-    InvestigatorPlayEvent iid eid _ _ _ | eid == eventId -> do
-      roundHistory <- getHistory RoundHistory iid
-      let damageToHeal = if null (historyTreacheriesDrawn roundHistory) then 1 else 2
-      pushAll
-        [ HealDamage (InvestigatorTarget iid) (toSource attrs) damageToHeal
-        , drawCards iid attrs 1
-        ]
+  runMessage msg e@(SecondWind attrs) = runQueueT case msg of
+    PlayThisEvent iid (is attrs -> True) -> do
+      drawn <- traceShowId <$> getHistoryField RoundHistory iid HistoryTreacheriesDrawn
+      let damageToHeal = if null drawn then 1 else 2
+      healDamage iid attrs damageToHeal
+      drawCardsIfCan iid attrs 1
       pure e
-    _ -> SecondWind <$> runMessage msg attrs
+    _ -> SecondWind <$> liftRunMessage msg attrs

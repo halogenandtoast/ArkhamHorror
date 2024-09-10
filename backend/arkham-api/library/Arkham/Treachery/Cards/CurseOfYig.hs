d19940f050f8635811a72340b681bc6b952324fd
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-10 16:24:20 +0900

    Fix curse of yig
    Should affect you when in your threat area. Fixes #497

--- backend/arkham-api/library/Arkham/Treachery/Cards/CurseOfYig.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/CurseOfYig.hs
@@ -2,12 +2,10 @@ module Arkham.Treachery.Cards.CurseOfYig (curseOfYig, CurseOfYig (..)) where
 
 import Arkham.Ability
 import Arkham.Campaigns.TheForgottenAge.Helpers
-import Arkham.Classes
 import Arkham.Helpers.Modifiers
-import Arkham.Prelude
 import Arkham.Trait
 import Arkham.Treachery.Cards qualified as Cards
-import Arkham.Treachery.Runner
+import Arkham.Treachery.Import.Lifted
 
 newtype CurseOfYig = CurseOfYig TreacheryAttrs
   deriving anyclass IsTreachery
@@ -17,24 +15,24 @@ curseOfYig :: TreacheryCard CurseOfYig
 curseOfYig = treachery CurseOfYig Cards.curseOfYig
 
 instance HasModifiersFor CurseOfYig where
-  getModifiersFor (InvestigatorTarget iid) (CurseOfYig attrs) | attrs `on` iid = do
-    pure $ toModifiers attrs [SkillModifier #combat (-1), HealthModifier (-1), AddTrait Serpent]
+  getModifiersFor (InvestigatorTarget iid) (CurseOfYig attrs) | treacheryInThreatArea iid attrs = do
+    modified attrs [SkillModifier #combat (-1), HealthModifier (-1), AddTrait Serpent]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities CurseOfYig where
   getAbilities (CurseOfYig a) = [skillTestAbility $ restrictedAbility a 1 OnSameLocation actionAbility]
 
 instance RunMessage CurseOfYig where
-  runMessage msg t@(CurseOfYig attrs) = case msg of
+  runMessage msg t@(CurseOfYig attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
-      push $ placeInThreatArea attrs iid
+      placeInThreatArea attrs iid
       pure t
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       n <- getVengeanceInVictoryDisplay
       sid <- getRandom
-      push $ beginSkillTest sid iid (attrs.ability 1) iid #willpower (Fixed $ 2 + n)
+      beginSkillTest sid iid (attrs.ability 1) iid #willpower (Fixed $ 2 + n)
       pure t
     PassedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
-      push $ toDiscardBy iid (attrs.ability 1) attrs
+      toDiscardBy iid (attrs.ability 1) attrs
       pure t
-    _ -> CurseOfYig <$> runMessage msg attrs
+    _ -> CurseOfYig <$> liftRunMessage msg attrs

34faa301bbb77babae8789273e73d5590a12bc4f
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-11 20:13:07 +0900

    Quiet Glade is an or not an and

--- backend/arkham-api/library/Arkham/Location/Cards/ArkhamWoodsQuietGlade.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/ArkhamWoodsQuietGlade.hs
@@ -1,53 +1,40 @@
-module Arkham.Location.Cards.ArkhamWoodsQuietGlade (
-  ArkhamWoodsQuietGlade (..),
-  arkhamWoodsQuietGlade,
-) where
-
-import Arkham.Prelude
+module Arkham.Location.Cards.ArkhamWoodsQuietGlade (ArkhamWoodsQuietGlade (..), arkhamWoodsQuietGlade) where
 
 import Arkham.Ability
-import Arkham.Classes
-import Arkham.Damage
 import Arkham.GameValue
 import Arkham.Helpers.Investigator
 import Arkham.Location.Cards qualified as Cards (arkhamWoodsQuietGlade)
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
 import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 
 newtype ArkhamWoodsQuietGlade = ArkhamWoodsQuietGlade LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 arkhamWoodsQuietGlade :: LocationCard ArkhamWoodsQuietGlade
-arkhamWoodsQuietGlade =
-  location ArkhamWoodsQuietGlade Cards.arkhamWoodsQuietGlade 1 (Static 0)
+arkhamWoodsQuietGlade = location ArkhamWoodsQuietGlade Cards.arkhamWoodsQuietGlade 1 (Static 0)
 
 instance HasAbilities ArkhamWoodsQuietGlade where
   getAbilities (ArkhamWoodsQuietGlade attrs) =
-    withRevealedAbilities attrs
-      $ [ limitedAbility (PlayerLimit PerTurn 1)
-            $ withCriteria (mkAbility attrs 1 (ActionAbility [] $ ActionCost 1))
-            $ Here
-            <> InvestigatorExists
-              ( AnyInvestigator
-                  [ HealableInvestigator (toSource attrs) HorrorType You
-                  , HealableInvestigator (toSource attrs) DamageType You
-                  ]
-              )
-        ]
+    extendRevealed
+      attrs
+      [ playerLimit PerTurn
+          $ withCriteria
+            (mkAbility attrs 1 actionAbility)
+            (Here <> any_ [HealableInvestigator (toSource attrs) kind You | kind <- [#horror, #damage]])
+      ]
 
 instance RunMessage ArkhamWoodsQuietGlade where
-  runMessage msg l@(ArkhamWoodsQuietGlade attrs) = case msg of
-    UseCardAbility iid (isSource attrs -> True) 1 _ _ -> do
+  runMessage msg l@(ArkhamWoodsQuietGlade attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
       let source = attrs.ability 1
-      canHealDamage <- canHaveDamageHealed source iid
-      canHealHorror <- canHaveHorrorHealed source iid
-      pushAll
-        $ [ HealDamage (InvestigatorTarget iid) source 1
-          | canHealDamage
-          ]
-        <> [ HealHorror (InvestigatorTarget iid) source 1
-           | canHealHorror
-           ]
+      chooseOrRunOneM iid do
+        whenM (canHaveDamageHealed source iid) do
+          damageLabeled iid do
+            healDamage iid source 1
+        whenM (canHaveHorrorHealed source iid) do
+          horrorLabeled iid do
+            healHorror iid source 1
       pure l
-    _ -> ArkhamWoodsQuietGlade <$> runMessage msg attrs
+    _ -> ArkhamWoodsQuietGlade <$> liftRunMessage msg attrs

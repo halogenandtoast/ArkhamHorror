3d07a9b27ad6825ebfda66b1f73fd7f96ee5f623
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-12 16:07:58 +0900

    Fix Anette Mason and Smuggled Goods

--- backend/arkham-api/library/Arkham/Enemy/Cards/AnetteMason.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/AnetteMason.hs
@@ -1,17 +1,13 @@
-module Arkham.Enemy.Cards.AnetteMason (
-  anetteMason,
-  AnetteMason (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Enemy.Cards.AnetteMason (anetteMason, AnetteMason (..)) where
 
 import Arkham.Ability
 import Arkham.Card
-import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Runner
+import Arkham.Enemy.Import.Lifted
+import Arkham.Enemy.Types (Field (..))
 import Arkham.Matcher
 import Arkham.Projection
+import Arkham.Trait (Trait (Witch))
 
 newtype AnetteMason = AnetteMason EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
@@ -24,15 +20,17 @@ instance HasAbilities AnetteMason where
   getAbilities (AnetteMason a) = withBaseAbilities a [mkAbility a 1 $ ForcedAbility $ PhaseBegins #after #enemy]
 
 instance RunMessage AnetteMason where
-  runMessage msg e@(AnetteMason attrs) = case msg of
+  runMessage msg e@(AnetteMason attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       push $ DiscardTopOfEncounterDeck iid 3 (toSource attrs) (Just $ toTarget attrs)
       pure e
     DiscardedTopOfEncounterDeck _ cards _ (isTarget attrs -> True) -> do
-      mLocation <- field EnemyLocation (toId attrs)
-      for_ mLocation $ \location -> do
-        for cards $ \card -> do
-          pushM $ createEnemyAt_ (EncounterCard card) location Nothing
-      when (null cards) $ push $ Ready (toTarget attrs)
+      let witches = filterCards (CardWithTrait Witch) $ map toCard cards
+      if null witches
+        then push $ Ready (toTarget attrs)
+        else do
+          mLocation <- field EnemyLocation (toId attrs)
+          for_ mLocation \location -> do
+            for_ witches \card -> createEnemyAt_ card location
       pure e
-    _ -> AnetteMason <$> runMessage msg attrs
+    _ -> AnetteMason <$> liftRunMessage msg attrs

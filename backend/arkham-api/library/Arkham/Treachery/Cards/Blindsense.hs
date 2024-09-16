e13f156b74968ebc88c90ba7d8bc31424a287bd7
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-16 21:01:53 +0900

    The Amalgam, Blindsense, From the Depths

--- backend/arkham-api/library/Arkham/Treachery/Cards/Blindsense.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Blindsense.hs
@@ -1,11 +1,11 @@
-module Arkham.Treachery.Cards.Blindsense
-  ( blindsense
-  , Blindsense(..)
-  )
-where
+module Arkham.Treachery.Cards.Blindsense (blindsense, Blindsense (..)) where
 
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.Investigator
+import Arkham.Matcher
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
+import Arkham.Zone
 
 newtype Blindsense = Blindsense TreacheryAttrs
   deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
@@ -16,5 +16,20 @@ blindsense = treachery Blindsense Cards.blindsense
 
 instance RunMessage Blindsense where
   runMessage msg t@(Blindsense attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #agility (Fixed 3)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      selectOne (OutOfPlayEnemy TheDepths $ enemyIs Enemies.theAmalgam) >>= \case
+        Just theAmalgam -> do
+          withLocationOf iid \lid -> do
+            push $ EnemySpawnFromOutOfPlay TheDepths (Just iid) lid theAmalgam
+            initiateEnemyAttack theAmalgam attrs iid
+        Nothing ->
+          selectOne (enemyIs Enemies.theAmalgam) >>= traverse_ \theAmalgam -> do
+            ready theAmalgam
+            enemyEngageInvestigator theAmalgam iid
+            initiateEnemyAttack theAmalgam attrs iid
+      pure t
     _ -> Blindsense <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Treachery/Cards/FromTheDepths.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/FromTheDepths.hs
@@ -1,11 +1,11 @@
-module Arkham.Treachery.Cards.FromTheDepths
-  ( fromTheDepths
-  , FromTheDepths(..)
-  )
-where
+module Arkham.Treachery.Cards.FromTheDepths (fromTheDepths, FromTheDepths (..)) where
 
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.Investigator
+import Arkham.Matcher
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
+import Arkham.Zone
 
 newtype FromTheDepths = FromTheDepths TreacheryAttrs
   deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
@@ -16,5 +16,13 @@ fromTheDepths = treachery FromTheDepths Cards.fromTheDepths
 
 instance RunMessage FromTheDepths where
   runMessage msg t@(FromTheDepths attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      selectOne (OutOfPlayEnemy TheDepths $ enemyIs Enemies.theAmalgam) >>= \case
+        Just theAmalgam -> do
+          withLocationOf iid \lid -> push $ EnemySpawnFromOutOfPlay TheDepths (Just iid) lid theAmalgam
+        Nothing ->
+          selectOne (enemyIs Enemies.theAmalgam) >>= traverse_ \theAmalgam -> do
+            push $ PlaceEnemyOutOfPlay TheDepths theAmalgam
+            gainSurge attrs
+      pure t
     _ -> FromTheDepths <$> liftRunMessage msg attrs

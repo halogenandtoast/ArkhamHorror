806067556f38257869fe55ab7eb8fc1b6ca476b0
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-20 09:08:24 +0900

    Check AVIF support

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,49 @@
 <template>
-  <NavBar/>
-  <Suspense>
-    <router-view v-slot="{ Component }" class="router-container">
-      <transition name="fade">
-        <component :is="Component" />
-      </transition>
-    </router-view>
-    <template #fallback>
-      Loading...
-    </template>
-  </Suspense>
-  <ModalsContainer />
+  <div v-if="!avifSupported" class="error-message container">
+    <header><h2 class="title">Please update your browser</h2></header>
+    <section class="box">
+      <p>Your browser does not support AVIF images. Please update your browser or switch to a different one.</p>
+    </section>
+  </div>
+  <template v-else>
+    <NavBar/>
+    <Suspense>
+      <router-view v-slot="{ Component }" class="router-container">
+        <transition name="fade">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
+    <ModalsContainer />
+  </template>
   <footer><a href="https://www.fantasyflightgames.com/en/products/arkham-horror-the-card-game/" rel="noreferrer" target="_blank" tabindex="-1">Arkham Horror: The Card Game™</a> and all related content © <a href="https://www.fantasyflightgames.com" rel="noreferrer" target="_blank" tabindex="-1">Fantasy Flight Games (FFG)</a>. This site is not produced, endorsed by or affiliated with FFG. <router-link to="/about">{{$t('About')}}.</router-link></footer>
 </template>
 
 <script lang="ts" setup>
 import { ModalsContainer } from 'vue-final-modal'
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useUserStore } from '@/stores/user'
 import NavBar from '@/components/NavBar.vue'
 import 'floating-vue/dist/style.css'
 
 const store = useUserStore()
-onMounted(async () => await store.loadUserFromStorage())
-
+onMounted(async () => {
+  avifSupported.value = await checkAvifSupport();
+  await store.loadUserFromStorage()
+})
+const avifSupported = ref(true);
+const checkAvifSupport = (): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const image = new Image();
+    image.onerror = () => resolve(false)
+    image.onload = () => resolve(true)
+    image.src =
+      "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=";
+  })
+};
 </script>
 
 <style lang="scss">
@@ -433,5 +453,13 @@ footer {
   display: flex;
   flex-direction: row;
   gap: 10px;
+}
+
+.error-message {
+  margin: 50px;
+  header {
+    text-align: center;
+    margin-bottom: 10px;
+  }
 }
 </style>

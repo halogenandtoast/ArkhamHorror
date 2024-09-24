faa8734c864d70cf5addbbda8f364fe88b2bfd59
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-25 06:45:16 +0900

    Fix ELina

--- backend/arkham-api/library/Arkham/Asset/Cards/ElinaHarperKnowsTooMuch.hs
+++ backend/arkham-api/library/Arkham/Asset/Cards/ElinaHarperKnowsTooMuch.hs
@@ -6,7 +6,9 @@ where
 
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
-import Arkham.Modifier
+import Arkham.Helpers.Modifiers (ModifierType (..), maybeModified)
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Projection
 
 newtype ElinaHarperKnowsTooMuch = ElinaHarperKnowsTooMuch AssetAttrs
   deriving anyclass (IsAsset, HasAbilities)
@@ -16,13 +18,13 @@ elinaHarperKnowsTooMuch :: AssetCard ElinaHarperKnowsTooMuch
 elinaHarperKnowsTooMuch = asset ElinaHarperKnowsTooMuch Cards.elinaHarperKnowsTooMuch
 
 instance HasModifiersFor ElinaHarperKnowsTooMuch where
-  getModifiersFor target (ElinaHarperKnowsTooMuch a) = modified a $ case target of
-    InvestigatorTarget iid | iid `controls` a -> do
-      actions <- fieldMap InvestigatorActionsPerformed concat iid
-      pure
-        $ [SkillModifier #intellect 1, SkillModifier #agility 1]
-        <> (if null actions then ActionDoesNotCauseAttacksOfOpportunity <$> [minBound ..] else [])
-    _ -> []
+  getModifiersFor target (ElinaHarperKnowsTooMuch a) = maybeModified a do
+    InvestigatorTarget iid <- pure target
+    guard $ iid `controls` a
+    actions <- lift $ fieldMap InvestigatorActionsPerformed concat iid
+    pure
+      $ [SkillModifier #intellect 1, SkillModifier #agility 1]
+      <> (if null actions then ActionDoesNotCauseAttacksOfOpportunity <$> [minBound ..] else [])
 
 instance RunMessage ElinaHarperKnowsTooMuch where
   runMessage msg (ElinaHarperKnowsTooMuch attrs) = runQueueT $ case msg of

1882dd7327780430ecc077f9be7bc11ccff009e2
Isaac <nemesisq0z@gmail.com>
2024-09-25 09:05:12 +0200

    feat: add spanish as a possible language

---
+++ frontend/src/locales/es.ts
@@ -0,0 +1,4 @@
+import base from '@/locales/es/base.json'
+import nightOfTheZealot from '@/locales/es/nightOfTheZealot.json'
+
+export default {...base, nightOfTheZealot}
--- frontend/src/locales/messages.ts
+++ frontend/src/locales/messages.ts
@@ -1,4 +1,5 @@
 import en from '@/locales/en'
 import it from '@/locales/it'
+import es from '@/locales/es'
 
-export default { en, it }
+export default { en, it, es }

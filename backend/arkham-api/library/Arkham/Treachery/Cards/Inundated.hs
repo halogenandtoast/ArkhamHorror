b59e801e938983cb34ad97d171709468b45c13e8
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-29 12:20:17 +0900

    Inundated

--- backend/arkham-api/library/Arkham/Treachery/Cards/Inundated.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Inundated.hs
@@ -1,9 +1,11 @@
-module Arkham.Treachery.Cards.Inundated
-  ( inundated
-  , Inundated(..)
-  )
-where
+module Arkham.Treachery.Cards.Inundated (inundated, Inundated (..)) where
 
+import Arkham.Helpers.Location
+import Arkham.Helpers.Modifiers (getModifiers)
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Matcher
+import Arkham.Modifier
+import Arkham.Scenarios.InTooDeep.Helpers
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -16,5 +18,18 @@ inundated = treachery Inundated Cards.inundated
 
 instance RunMessage Inundated where
   runMessage msg t@(Inundated attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      ref <- newIORef (0 :: Int)
+      getLocationOf iid >>= traverse_ \lid -> do
+        unlessM (lid <=~> locationIs Locations.desolateCoastline) do
+          getConnectedLocations lid >>= traverse_ \lid' -> do
+            whenM (not <$> (lid' <=~> locationIs Locations.desolateCoastline)) do
+              mods <- getModifiers lid'
+              let ls = concat [xs | Barricades xs <- mods]
+              when (lid `notElem` ls) do
+                modifyIORef ref (+ 1)
+                placeBarrier lid lid'
+      placed <- readIORef ref
+      when (placed == 0) $ gainSurge attrs
+      pure t
     _ -> Inundated <$> liftRunMessage msg attrs

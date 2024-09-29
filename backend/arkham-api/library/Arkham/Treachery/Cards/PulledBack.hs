ff921c4c98e904bcf534bfd7d49a91b655593ba8
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-29 10:44:52 +0900

    Pulled Back

--- backend/arkham-api/library/Arkham/Treachery/Cards/PulledBack.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/PulledBack.hs
@@ -1,9 +1,10 @@
-module Arkham.Treachery.Cards.PulledBack
-  ( pulledBack
-  , PulledBack(..)
-  )
-where
+module Arkham.Treachery.Cards.PulledBack (pulledBack, PulledBack (..)) where
 
+import Arkham.Direction
+import Arkham.Helpers.Location
+import Arkham.Helpers.Scenario
+import Arkham.Investigator.Projection ()
+import Arkham.Location.Grid
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -16,5 +17,19 @@ pulledBack = treachery PulledBack Cards.pulledBack
 
 instance RunMessage PulledBack where
   runMessage msg t@(PulledBack attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #willpower (Fixed 3)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      grid <- getGrid
+      void $ runMaybeT do
+        loc <- MaybeT $ getLocationOf iid
+        pos <- hoistMaybe $ findInGrid loc grid
+        GridLocation _ east <- hoistMaybe $ viewGrid (updatePosition pos East) grid
+        lift do
+          moveTo attrs iid east
+          ks <- iid.keys
+          for_ ks $ placeKey east
+      pure t
     _ -> PulledBack <$> liftRunMessage msg attrs

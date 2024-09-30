e69fbec9433633ddb9cc0fe27eefea5d5daad479
Matthew Mongeau <halogenandtoast@gmail.com>
2024-09-30 22:21:44 +0900

    Some Devil Reef Treacheries

--- backend/arkham-api/library/Arkham/Treachery/Cards/ShapesInTheWater.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/ShapesInTheWater.hs
@@ -1,9 +1,12 @@
-module Arkham.Treachery.Cards.ShapesInTheWater
-  ( shapesInTheWater
-  , ShapesInTheWater(..)
-  )
+module Arkham.Treachery.Cards.ShapesInTheWater (
+  shapesInTheWater,
+  ShapesInTheWater (..),
+)
 where
 
+import Arkham.Campaigns.TheInnsmouthConspiracy.Helpers
+import Arkham.Helpers.Location
+import Arkham.Location.FloodLevel
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -16,5 +19,19 @@ shapesInTheWater = treachery ShapesInTheWater Cards.shapesInTheWater
 
 instance RunMessage ShapesInTheWater where
   runMessage msg t@(ShapesInTheWater attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      extra <-
+        getLocationOf iid >>= \case
+          Nothing -> pure 0
+          Just loc -> do
+            getFloodLevel loc <&> \case
+              Unflooded -> 0
+              PartiallyFlooded -> 1
+              FullyFlooded -> 2
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #willpower (Fixed $ 2 + extra)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      assignHorror iid attrs 2
+      pure t
     _ -> ShapesInTheWater <$> liftRunMessage msg attrs

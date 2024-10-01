d28b5e8d144e457907c7bd1852dfdd668a3fe718
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-01 14:46:21 +0900

    Akachi fixes

--- backend/arkham-api/library/Arkham/Investigator/Cards/AkachiOnyele.hs
+++ backend/arkham-api/library/Arkham/Investigator/Cards/AkachiOnyele.hs
@@ -1,26 +1,25 @@
 module Arkham.Investigator.Cards.AkachiOnyele where
 
-import Arkham.Prelude
-
 import Arkham.Asset.Types (Field (..))
 import Arkham.Asset.Uses
 import Arkham.Game.Helpers
 import Arkham.Helpers.Use
 import Arkham.Investigator.Cards qualified as Cards
-import Arkham.Investigator.Runner
+import Arkham.Investigator.Import.Lifted
 import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Projection
 
 newtype AkachiOnyele = AkachiOnyele InvestigatorAttrs
   deriving anyclass (IsInvestigator, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
-  deriving stock (Data)
+  deriving stock Data
 
 instance HasModifiersFor AkachiOnyele where
-  getModifiersFor (AssetTarget aid) (AkachiOnyele attrs) = do
-    akachiAsset <- fieldP AssetController (== Just (toId attrs)) aid
-    startingChargesCount <- fieldMapM AssetStartingUses (startingUseCountFor Charge) aid
-    pure $ toModifiers attrs [AdditionalStartingUses 1 | akachiAsset, startingChargesCount > 0]
+  getModifiersFor (AssetTarget aid) (AkachiOnyele attrs) = maybeModified attrs do
+    liftGuardM $ fieldMap AssetController (== Just attrs.id) aid
+    liftGuardM $ fieldMap AssetStartingUses (hasUsesFor Charge) aid
+    pure [AdditionalStartingUses 1]
   getModifiersFor _ _ = pure []
 
 akachiOnyele :: InvestigatorCard AkachiOnyele
@@ -34,12 +33,9 @@ instance HasChaosTokenValue AkachiOnyele where
   getChaosTokenValue _ token _ = pure $ ChaosTokenValue token mempty
 
 instance RunMessage AkachiOnyele where
-  runMessage msg i@(AkachiOnyele attrs) = case msg of
-    ResolveChaosToken _ ElderSign iid | attrs `is` iid -> do
+  runMessage msg i@(AkachiOnyele attrs) = runQueueT $ case msg of
+    ElderSignEffect iid | attrs `is` iid -> do
       assets <- filterByField AssetStartingUses (hasUsesFor Charge) =<< select (assetControlledBy iid)
-      player <- getPlayer iid
-      pushIfAny assets
-        $ chooseOne player
-        $ targetLabels assets (\asset -> only $ AddUses #elderSign asset Charge 1)
+      chooseTargetM iid assets \asset -> push $ AddUses #elderSign asset Charge 1
       pure i
-    _ -> AkachiOnyele <$> runMessage msg attrs
+    _ -> AkachiOnyele <$> liftRunMessage msg attrs

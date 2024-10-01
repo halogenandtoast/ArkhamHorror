7b8e2b9fbd65b127ed6e5d01716fce8beeefeace
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-01 14:34:53 +0900

    Cleanup Trish

--- backend/arkham-api/library/Arkham/Investigator/Cards/TrishScarborough.hs
+++ backend/arkham-api/library/Arkham/Investigator/Cards/TrishScarborough.hs
@@ -1,18 +1,16 @@
 module Arkham.Investigator.Cards.TrishScarborough (trishScarborough, TrishScarborough (..)) where
 
-import Arkham.Action qualified as Action
+import Arkham.Ability
 import Arkham.Discover
-import Arkham.Helpers.Modifiers
+import Arkham.Helpers.Location (getLocationOf)
+import Arkham.Helpers.Modifiers (ModifierType (..))
 import Arkham.Helpers.SkillTest
-import Arkham.Id
+import Arkham.Helpers.Window (discoveredLocation)
 import Arkham.Investigator.Cards qualified as Cards
-import Arkham.Investigator.Runner hiding (DiscoverClues)
+import Arkham.Investigator.Import.Lifted hiding (DiscoverClues)
 import Arkham.Matcher hiding (EnemyEvaded)
-import Arkham.Message qualified as Msg
-import Arkham.Prelude
+import Arkham.Message.Lifted.Choose
 import Arkham.Taboo
-import Arkham.Window (Window (..))
-import Arkham.Window qualified as Window
 
 newtype TrishScarborough = TrishScarborough InvestigatorAttrs
   deriving anyclass (IsInvestigator, HasModifiersFor)
@@ -21,76 +19,43 @@ newtype TrishScarborough = TrishScarborough InvestigatorAttrs
 
 trishScarborough :: InvestigatorCard TrishScarborough
 trishScarborough =
-  investigator
-    TrishScarborough
-    Cards.trishScarborough
-    Stats
-      { health = 8
-      , sanity = 6
-      , willpower = 2
-      , intellect = 4
-      , combat = 2
-      , agility = 4
-      }
+  investigator TrishScarborough Cards.trishScarborough
+    $ Stats {health = 8, sanity = 6, willpower = 2, intellect = 4, combat = 2, agility = 4}
 
 instance HasAbilities TrishScarborough where
   getAbilities (TrishScarborough a) =
     [ playerLimit PerRound
-        $ restricted
-          a
-          1
-          ( Self
-              <> oneOf
-                [ exists (LocationBeingDiscovered <> LocationWithAnyClues)
-                , exists $ tabooModifier $ CanEvadeEnemy (toSource a)
-                ]
-          )
+        $ restricted a 1 (Self <> oneOf [exists locationWithAdditionalClues, exists evadableEnemy])
         $ freeReaction
         $ DiscoverClues #after You (LocationWithEnemy AnyEnemy) (atLeast 1)
     ]
    where
     tabooModifier = if tabooed TabooList21 a then (NonEliteEnemy <>) else id
+    locationWithAdditionalClues = LocationBeingDiscovered <> LocationWithAnyClues
+    evadableEnemy = tabooModifier $ CanEvadeEnemy (toSource a)
 
 instance HasChaosTokenValue TrishScarborough where
   getChaosTokenValue iid ElderSign (TrishScarborough attrs) | iid == toId attrs = do
     pure $ ChaosTokenValue ElderSign (PositiveModifier 2)
   getChaosTokenValue _ token _ = pure $ ChaosTokenValue token mempty
 
-toLocation :: [Window] -> LocationId
-toLocation [] = error "missing window"
-toLocation ((windowType -> Window.DiscoverClues _ lid _ _) : _) = lid
-toLocation (_ : xs) = toLocation xs
-
 instance RunMessage TrishScarborough where
-  runMessage msg i@(TrishScarborough attrs) = case msg of
-    UseCardAbility iid (isSource attrs -> True) 1 (toLocation -> lid) _ -> do
-      player <- getPlayer iid
-      enemies <- select $ enemyAt lid <> EnemyCanBeEvadedBy (attrs.ability 1)
-      push
-        $ chooseOrRunOne player
-        $ [Label "Discover 1 additional clue at that location" [Msg.DiscoverClues iid $ discover lid attrs 1]]
-        <> [ Label
-            "Automatically evade that enemy"
-            [chooseOrRunOne player [targetLabel enemy [EnemyEvaded iid enemy] | enemy <- enemies]]
-           | notNull enemies
-           ]
+  runMessage msg i@(TrishScarborough attrs) = runQueueT $ case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 (discoveredLocation -> lid) _ -> do
+      let source = attrs.ability 1
+      chooseOrRunOneM iid do
+        labeled "Discover 1 additional clue at that location" $ discoverAt NotInvestigate iid source lid 1
+        labeled "Automatically evade that enemy" do
+          chooseSelectM iid (enemyAt lid <> EnemyCanBeEvadedBy source) $ automaticallyEvadeEnemy iid
       pure i
-    ResolveChaosToken _ ElderSign iid | attrs `is` iid -> do
-      getSkillTestAction >>= \case
-        Just Action.Investigate -> do
-          mLocation <- getMaybeLocation iid
-          locations <- select $ RevealedLocation <> maybe Anywhere (not_ . LocationWithId) mLocation
-          when (notNull locations) do
-            player <- getPlayer iid
-            withSkillTest \sid -> do
-              push $ chooseOne player $ Label "Do not choose a different location" []
-                : [ targetLabel
-                    location
-                    [ SetSkillTestTarget (toTarget location)
-                    , skillTestModifier sid attrs iid (AsIfAt location)
-                    ]
-                  | location <- locations
-                  ]
-        _ -> pure ()
+    ElderSignEffect (is attrs -> True) -> do
+      whenM isInvestigation do
+        locations <- select . (RevealedLocation <>) . maybe Anywhere (not_ . be) =<< getLocationOf attrs.id
+        when (notNull locations) do
+          withSkillTest \sid -> chooseOneM attrs.id do
+            labeled "Do not choose a different location" nothing
+            targets locations \location -> do
+              push $ SetSkillTestTarget (toTarget location)
+              skillTestModifier sid ElderSign attrs (AsIfAt location)
       pure i
-    _ -> TrishScarborough <$> runMessage msg attrs
+    _ -> TrishScarborough <$> liftRunMessage msg attrs

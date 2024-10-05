0df7c56506bd39f554ce84c54f1bf8eef0e00b4c
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-05 18:23:24 +0900

    Fix Decorated Skull (3) windows

--- backend/arkham-api/library/Arkham/Asset/Assets/DecoratedSkull3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/DecoratedSkull3.hs
@@ -1,9 +1,4 @@
-module Arkham.Asset.Assets.DecoratedSkull3 (
-  decoratedSkull3,
-  DecoratedSkull3 (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Assets.DecoratedSkull3 (decoratedSkull3, DecoratedSkull3 (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
@@ -13,6 +8,7 @@ import Arkham.Asset.Runner hiding (
   InvestigatorDefeated,
  )
 import Arkham.Matcher
+import Arkham.Prelude
 
 newtype DecoratedSkull3 = DecoratedSkull3 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -23,34 +19,24 @@ decoratedSkull3 = asset DecoratedSkull3 Cards.decoratedSkull3
 
 instance HasAbilities DecoratedSkull3 where
   getAbilities (DecoratedSkull3 a) =
-    [ restrictedAbility a 1 ControlsThis
-        $ ReactionAbility
-          ( OrWindowMatcher
-              [ EnemyDefeated #after Anyone ByAny AnyEnemy
-              , InvestigatorDefeated #after ByAny Anyone
-              , AssetDefeated #after ByAny AllyAsset
-              ]
-          )
-          Free
-    , restrictedAbility
-        a
-        2
-        ControlsThis
-        (actionAbilityWithCost $ UpTo (Fixed 3) $ assetUseCost a Charge 1)
+    [ restricted a 1 ControlsThis
+        $ freeReaction
+        $ oneOf
+          [ EnemyDefeated #after Anyone ByAny (EnemyAt YourLocation)
+          , InvestigatorDefeated #after ByAny (InvestigatorAt YourLocation)
+          , AssetDefeated #after ByAny (AllyAsset <> AssetAt YourLocation)
+          ]
+    , restricted a 2 ControlsThis
+        $ actionAbilityWithCost (UpTo (Fixed 3) $ assetUseCost a Charge 1)
     ]
-
-getUsesPaid :: Payment -> Int
-getUsesPaid (UsesPayment n) = n
-getUsesPaid (Payments ps) = sum $ map getUsesPaid ps
-getUsesPaid _ = 0
 
 instance RunMessage DecoratedSkull3 where
   runMessage msg a@(DecoratedSkull3 attrs) = case msg of
     UseThisAbility _ (isSource attrs -> True) 1 -> do
       push $ AddUses (attrs.ability 1) (toId attrs) Charge 1
       pure a
-    UseCardAbility iid (isSource attrs -> True) 2 _ (getUsesPaid -> n) -> do
-      let drawing = drawCards iid (toAbilitySource attrs 2) n
-      pushAll [drawing, TakeResources iid n (toAbilitySource attrs 2) False]
+    UseCardAbility iid (isSource attrs -> True) 2 _ (totalUsesPayment -> n) -> do
+      let drawing = drawCards iid (attrs.ability 2) n
+      pushAll [drawing, TakeResources iid n (attrs.ability 2) False]
       pure a
     _ -> DecoratedSkull3 <$> runMessage msg attrs

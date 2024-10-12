f6ad02d960cb4e0ea0b1cdefcb5b5a866eee7b1b
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-12 20:00:05 +0900

    Fix Keen Eye (3)
    Should be a phase modifier, not a skill test

--- backend/arkham-api/library/Arkham/Asset/Assets/KeenEye3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/KeenEye3.hs
@@ -2,8 +2,8 @@ module Arkham.Asset.Assets.KeenEye3 (keenEye3, KeenEye3 (..)) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
-import Arkham.Prelude
+import Arkham.Asset.Import.Lifted
+import Arkham.Modifier
 
 newtype KeenEye3 = KeenEye3 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -15,19 +15,17 @@ keenEye3 = asset KeenEye3 Cards.keenEye3
 instance HasAbilities KeenEye3 where
   getAbilities (KeenEye3 a) =
     [ withTooltip "{fast} Spend 2 resources: You get +1 {intellect} until the end of the phase."
-        $ restrictedAbility a 1 ControlsThis (FastAbility $ ResourceCost 2)
+        $ restricted a 1 ControlsThis (FastAbility $ ResourceCost 2)
     , withTooltip "{fast} Spend 2 resources: You get +1 {combat} until the end of the phase."
-        $ restrictedAbility a 2 ControlsThis (FastAbility $ ResourceCost 2)
+        $ restricted a 2 ControlsThis (FastAbility $ ResourceCost 2)
     ]
 
 instance RunMessage KeenEye3 where
-  runMessage msg a@(KeenEye3 attrs) = case msg of
+  runMessage msg a@(KeenEye3 attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      withSkillTest \sid ->
-        pushM $ skillTestModifier sid attrs iid (SkillModifier #intellect 1)
+      phaseModifier attrs iid (SkillModifier #intellect 1)
       pure a
     UseThisAbility iid (isSource attrs -> True) 2 -> do
-      withSkillTest \sid ->
-        pushM $ skillTestModifier sid attrs iid (SkillModifier #combat 1)
+      phaseModifier attrs iid (SkillModifier #combat 1)
       pure a
-    _ -> KeenEye3 <$> runMessage msg attrs
+    _ -> KeenEye3 <$> liftRunMessage msg attrs

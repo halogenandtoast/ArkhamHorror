eed0392cafb5b66d1821ece4895c56b1b40d03bc
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-12 16:14:02 +0900

    Fix Mysterious Stairs (187)
    The logic for determining the damage on the Unnamable was inversed.

--- backend/arkham-api/library/Arkham/Location/Cards/MysteriousStairs_187.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/MysteriousStairs_187.hs
@@ -17,37 +17,30 @@ newtype MysteriousStairs_187 = MysteriousStairs_187 LocationAttrs
 
 mysteriousStairs_187 :: LocationCard MysteriousStairs_187
 mysteriousStairs_187 =
-  locationWith
-    MysteriousStairs_187
-    Cards.mysteriousStairs_187
-    3
-    (Static 0)
-    (connectsToL .~ setFromList [Above, Below])
+  locationWith MysteriousStairs_187 Cards.mysteriousStairs_187 3 (Static 0)
+    $ connectsToL
+    .~ setFromList [Above, Below]
 
 instance HasModifiersFor MysteriousStairs_187 where
-  getModifiersFor (InvestigatorTarget iid) (MysteriousStairs_187 attrs) = do
-    here <- iid `isAt` attrs
-    n <- perPlayer 1
-    mTheUnnamable <- selectOne $ enemyIs Enemies.theUnnamable <> enemyAt attrs
-    hasEnoughDamage <- maybe (pure False) (fieldMap EnemyDamage (>= n)) mTheUnnamable
-    toModifiers attrs
-      $ guard (here && hasEnoughDamage)
-      *> [CannotTakeAction #move, CannotTakeAction #resign]
+  getModifiersFor (InvestigatorTarget iid) (MysteriousStairs_187 attrs) = maybeModified attrs do
+    liftGuardM $ iid `isAt` attrs
+    theUnnamable <- MaybeT $ selectOne $ enemyIs Enemies.theUnnamable <> enemyAt attrs
+    n <- lift $ perPlayer 1
+    liftGuardM $ fieldMap EnemyDamage (< n) theUnnamable
+    pure [CannotTakeAction #move, CannotTakeAction #resign]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities MysteriousStairs_187 where
   getAbilities (MysteriousStairs_187 attrs) =
-    extendRevealed
-      attrs
-      [ restrictedAbility attrs 1 (exists $ enemyIs Enemies.theUnnamable <> not_ (enemyAt attrs))
-          $ forced
-          $ RevealLocation #when Anyone (be attrs)
-      ]
+    extendRevealed1 attrs
+      $ restricted attrs 1 (exists $ enemyIs Enemies.theUnnamable <> not_ (enemyAt attrs))
+      $ forced
+      $ RevealLocation #when Anyone (be attrs)
 
 instance RunMessage MysteriousStairs_187 where
   runMessage msg l@(MysteriousStairs_187 attrs) = case msg of
     UseThisAbility _ (isSource attrs -> True) 1 -> do
-      mTheUnnamable <- selectOne $ enemyIs Enemies.theUnnamable
-      for_ mTheUnnamable $ \theUnnamable -> push $ EnemyMove theUnnamable attrs.id
+      selectForMaybeM (enemyIs Enemies.theUnnamable) \theUnnamable -> do
+        push $ EnemyMove theUnnamable attrs.id
       pure l
     _ -> MysteriousStairs_187 <$> runMessage msg attrs

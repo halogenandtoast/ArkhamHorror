281118b0abee19dcef50ea1d142e83deb0358427
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-13 15:50:42 +0900

    Fix Grand Rue

--- backend/arkham-api/library/Arkham/Location/Cards/GrandRue.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/GrandRue.hs
@@ -1,18 +1,12 @@
-module Arkham.Location.Cards.GrandRue (
-  grandRue,
-  GrandRue (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Location.Cards.GrandRue (grandRue, GrandRue (..)) where
 
 import Arkham.Ability
 import Arkham.Agenda.Types (Field (AgendaDoom))
-import Arkham.Classes
 import Arkham.GameValue
 import Arkham.Location.Cards qualified as Cards
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
 import Arkham.Matcher
-import Arkham.Timing qualified as Timing
+import Arkham.Message.Lifted.Choose
 
 newtype GrandRue = GrandRue LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -23,30 +17,18 @@ grandRue = location GrandRue Cards.grandRue 1 (PerPlayer 1)
 
 instance HasAbilities GrandRue where
   getAbilities (GrandRue a) =
-    withRevealedAbilities
-      a
-      [ restrictedAbility a 1 Here
-          $ ForcedAbility
-          $ SkillTestResult Timing.After You AnySkillTest
-          $ SuccessResult
-          $ AtMost
-          $ Static 1
-      ]
+    extendRevealed1 a
+      $ restricted a 1 Here
+      $ forced
+      $ SkillTestResult #after You (WhileInvestigating $ be a)
+      $ SuccessResult (atMost 1)
 
 instance RunMessage GrandRue where
-  runMessage msg l@(GrandRue attrs) = case msg of
-    UseCardAbility iid source 1 _ _ | isSource attrs source -> do
+  runMessage msg l@(GrandRue attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
       maxDoom <- fieldMax AgendaDoom AnyAgenda
-      agendas <-
-        selectMap AgendaTarget
-          $ AgendaWithDoom
-          $ EqualTo
-          $ Static
-            maxDoom
-      player <- getPlayer iid
-      push
-        $ chooseOrRunOne
-          player
-          [targetLabel target [PlaceDoom (toAbilitySource attrs 1) target 1] | target <- agendas]
+      agendas <- select $ AgendaWithDoom $ EqualTo $ Static maxDoom
+      chooseOrRunOneM iid do
+        targets agendas \agenda -> placeDoom (attrs.ability 1) agenda 1
       pure l
-    _ -> GrandRue <$> runMessage msg attrs
+    _ -> GrandRue <$> liftRunMessage msg attrs

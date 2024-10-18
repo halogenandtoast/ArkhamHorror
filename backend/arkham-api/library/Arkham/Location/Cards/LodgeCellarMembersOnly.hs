c164cb91fd7cf31590a3d2f9ab06e6558f119702
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-18 16:23:46 +0900

    Some cleanup

--- backend/arkham-api/library/Arkham/Location/Cards/LodgeCellarMembersOnly.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/LodgeCellarMembersOnly.hs
@@ -4,8 +4,6 @@ module Arkham.Location.Cards.LodgeCellarMembersOnly (
 )
 where
 
-import Arkham.Prelude
-
 import Arkham.GameValue
 import Arkham.Helpers.Ability
 import Arkham.Helpers.Modifiers
@@ -13,18 +11,18 @@ import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Runner
 import Arkham.Matcher hiding (RevealLocation)
 import Arkham.Message
+import Arkham.Prelude
 
 newtype LodgeCellarMembersOnly = LodgeCellarMembersOnly LocationAttrs
-  deriving anyclass (IsLocation)
+  deriving anyclass IsLocation
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 lodgeCellarMembersOnly :: LocationCard LodgeCellarMembersOnly
 lodgeCellarMembersOnly = location LodgeCellarMembersOnly Cards.lodgeCellarMembersOnly 3 (Static 0)
 
 instance HasModifiersFor LodgeCellarMembersOnly where
-  getModifiersFor target (LodgeCellarMembersOnly attrs)
-    | isTarget attrs target =
-        toModifiers attrs [Blocked | not (locationRevealed attrs)]
+  getModifiersFor target (LodgeCellarMembersOnly attrs) | isTarget attrs target = do
+    toModifiers attrs [Blocked | not attrs.revealed]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities LodgeCellarMembersOnly where
@@ -33,21 +31,14 @@ instance HasAbilities LodgeCellarMembersOnly where
       attrs
       [ withTooltip
         "{action}: Investigators at the Lodge Gates spend 1 {perPlayer} clues, as a group: Reveal the Lodge Cellar."
-        $ restrictedAbility
-          (proxied (LocationMatcherSource $ LocationWithTitle "Lodge Gates") attrs)
-          1
-          (OnLocation $ LocationWithTitle "Lodge Gates")
-          ( ActionAbility []
-              $ ActionCost 1
-              <> GroupClueCost (PerPlayer 1) (LocationWithTitle "Lodge Gates")
-          )
+        $ restricted (proxied (LocationMatcherSource "Lodge Gates") attrs) 1 Here
+        $ ActionAbility [] (ActionCost 1 <> GroupClueCost (PerPlayer 1) (LocationWithTitle "Lodge Gates"))
       | unrevealed attrs
       ]
 
 instance RunMessage LodgeCellarMembersOnly where
   runMessage msg l@(LodgeCellarMembersOnly attrs) = case msg of
-    UseCardAbility iid (ProxySource _ source) 1 _ _
-      | isSource attrs source && unrevealed attrs -> do
-          push $ RevealLocation (Just iid) (toId attrs)
-          pure l
+    UseThisAbility iid (ProxySource _ source) 1 | isSource attrs source && unrevealed attrs -> do
+      push $ RevealLocation (Just iid) (toId attrs)
+      pure l
     _ -> LodgeCellarMembersOnly <$> runMessage msg attrs

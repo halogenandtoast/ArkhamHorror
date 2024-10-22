f5face3039a5b350e63c03d542a26bafe8da296e
Matthew Mongeau <halogenandtoast@gmail.com>
2024-10-22 14:13:06 +0900

    Fix Custom Ammunition (3)

--- backend/arkham-api/library/Arkham/Event/Events/CustomAmmunition3.hs
+++ backend/arkham-api/library/Arkham/Event/Events/CustomAmmunition3.hs
@@ -1,16 +1,10 @@
-module Arkham.Event.Events.CustomAmmunition3 (
-  customAmmunition3,
-  CustomAmmunition3 (..),
-) where
+module Arkham.Event.Events.CustomAmmunition3 (customAmmunition3, CustomAmmunition3 (..)) where
 
-import Arkham.Prelude
-
-import Arkham.Action qualified as Action
 import Arkham.Asset.Uses
-import Arkham.Classes
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
 import Arkham.Game.Helpers
+import Arkham.Helpers.SkillTest
 import Arkham.Matcher
 import Arkham.Placement
 import Arkham.Trait
@@ -23,38 +17,25 @@ customAmmunition3 :: EventCard CustomAmmunition3
 customAmmunition3 = event CustomAmmunition3 Cards.customAmmunition3
 
 instance HasModifiersFor CustomAmmunition3 where
-  getModifiersFor (InvestigatorTarget iid) (CustomAmmunition3 a) = do
-    mSource <- getSkillTestSource
-    mTarget <- getSkillTestTarget
-    mAction <- getSkillTestAction
-    case (mAction, mSource, mTarget) of
-      (Just Action.Fight, Just (AssetSource aid), Just (EnemyTarget eid))
-        | Just (AssetTarget aid) == eventAttachedTarget a ->
-            do
-              isMonster <- eid <=~> EnemyWithTrait Monster
-              isController <- iid <=~> HasMatchingAsset (AssetWithId aid)
-              toModifiers a [DamageDealt 1 | isMonster && isController]
-      _ -> pure []
+  getModifiersFor (InvestigatorTarget iid) (CustomAmmunition3 a) = maybeModified a do
+    aid <- MaybeT $ join . fmap (.asset) <$> getSkillTestSource
+    liftGuardM $ iid <=~> HasMatchingAsset (AssetWithId aid)
+    guard $ maybe False (isTarget aid) a.attachedTo
+    liftGuardM $ isFightWith $ EnemyWithTrait Monster
+    pure [DamageDealt 1]
   getModifiersFor _ _ = pure []
 
 instance RunMessage CustomAmmunition3 where
-  runMessage msg e@(CustomAmmunition3 attrs) = case msg of
-    InvestigatorPlayEvent iid eid _ _ _ | eid == toId attrs -> do
+  runMessage msg e@(CustomAmmunition3 attrs) = runQueueT $ case msg of
+    PlayThisEvent iid (is attrs -> True) -> do
       assets <-
         select
           $ AssetControlledBy (affectsOthers $ colocatedWith iid)
-          <> AssetWithTrait Firearm
-          <> NotAsset (AssetWithAttachedEvent $ EventCardMatch $ cardIs Cards.customAmmunition3)
-      player <- getPlayer iid
-      push
-        $ chooseOne
-          player
-          [ targetLabel
-            asset
-            [ PlaceEvent eid (AttachedToAsset asset Nothing)
-            , AddUses (toSource attrs) asset Ammo 2
-            ]
-          | asset <- assets
-          ]
+          <> #firearm
+          <> not_ (AssetWithAttachedEvent $ EventCardMatch $ cardIs Cards.customAmmunition3)
+
+      chooseTargetM iid assets \asset -> do
+        push $ PlaceEvent attrs.id (AttachedToAsset asset Nothing)
+        push $ AddUses (toSource attrs) asset Ammo 2
       pure e
-    _ -> CustomAmmunition3 <$> runMessage msg attrs
+    _ -> CustomAmmunition3 <$> liftRunMessage msg attrs

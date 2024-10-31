c748a73e9cfbf600aa293a6705a9a4093897c9e2
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-01 07:31:53 +0900

    Fix The Geist-Trap

--- backend/arkham-api/library/Arkham/Location/Cards/TheGeistTrap.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/TheGeistTrap.hs
@@ -1,18 +1,12 @@
-module Arkham.Location.Cards.TheGeistTrap (
-  theGeistTrap,
-  TheGeistTrap (..),
-)
-where
+module Arkham.Location.Cards.TheGeistTrap (theGeistTrap, TheGeistTrap (..)) where
 
-import Arkham.Prelude
-
-import Arkham.Action qualified as Action
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Enemies
 import Arkham.GameValue
 import Arkham.Helpers.Modifiers
 import Arkham.Keyword qualified as Keyword
 import Arkham.Location.Cards qualified as Cards
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
 import Arkham.Matcher
 import Arkham.Scenarios.UnionAndDisillusion.Helpers
 
@@ -26,32 +20,31 @@ theGeistTrap = location TheGeistTrap Cards.theGeistTrap 4 (PerPlayer 1)
 instance HasModifiersFor TheGeistTrap where
   getModifiersFor target (TheGeistTrap attrs)
     | attrs `isTarget` target
-    , not (locationRevealed attrs) = do
+    , not attrs.revealed = do
         toModifiers attrs [Blocked]
-  getModifiersFor (EnemyTarget eid) (TheGeistTrap attrs) | locationRevealed attrs = do
-    gainsRetaliate <-
-      selectAny (EnemyWithId eid <> enemyIs Enemies.theSpectralWatcher <> enemyAt (toId attrs))
+  getModifiersFor (EnemyTarget eid) (TheGeistTrap attrs) | attrs.revealed = do
+    gainsRetaliate <- selectAny $ EnemyWithId eid <> enemyIs Enemies.theSpectralWatcher <> at_ attrs
     toModifiers attrs [AddKeyword Keyword.Retaliate | gainsRetaliate]
   getModifiersFor _ _ = pure []
 
 instance HasAbilities TheGeistTrap where
   getAbilities (TheGeistTrap attrs) =
-    withRevealedAbilities
+    extendRevealed
       attrs
-      [ restrictedAbility attrs 1 Here $ ActionAbility ([Action.Circle]) $ ActionCost 1
+      [ restricted attrs 1 Here $ ActionAbility [#circle] $ ActionCost 1
       , haunted "Take 1 damage and 1 horror" attrs 2
       ]
 
 instance RunMessage TheGeistTrap where
-  runMessage msg l@(TheGeistTrap attrs) = case msg of
+  runMessage msg l@(TheGeistTrap attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       sid <- getRandom
       circleTest sid iid (attrs.ability 1) attrs [#willpower, #intellect, #combat, #agility] (Fixed 20)
       pure l
     UseThisAbility iid (isSource attrs -> True) 2 -> do
-      push $ InvestigatorAssignDamage iid (toSource attrs) DamageAny 1 1
+      assignDamageAndHorror iid (attrs.ability 2) 1 1
       pure l
-    PassedThisSkillTest iid (isSource attrs -> True) -> do
+    PassedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
       passedCircleTest iid attrs
       pure l
-    _ -> TheGeistTrap <$> runMessage msg attrs
+    _ -> TheGeistTrap <$> liftRunMessage msg attrs

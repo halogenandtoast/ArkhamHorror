0326fab8d59414b19a4f0c7d1ec1522b37c9814e
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-01 21:48:17 +0900

    Resourceful needs to ensure card can leave discard

--- backend/arkham-api/library/Arkham/Skill/Cards/Resourceful.hs
+++ backend/arkham-api/library/Arkham/Skill/Cards/Resourceful.hs
@@ -1,5 +1,6 @@
 module Arkham.Skill.Cards.Resourceful (resourceful, Resourceful (..)) where
 
+import Arkham.Capability
 import Arkham.Card
 import Arkham.Investigator.Projection ()
 import Arkham.Matcher
@@ -17,14 +18,15 @@ resourceful = skill Resourceful Cards.resourceful
 instance RunMessage Resourceful where
   runMessage msg s@(Resourceful attrs) = runQueueT $ case msg of
     PassedSkillTest _ _ _ target _ _ | isTarget attrs target -> do
-      cards <- select $ inDiscardOf attrs.owner <> basic (#survivor <> not_ (CardWithTitle "Resourceful"))
-      unless (null cards) do
-        discards <- map toCard <$> attrs.owner.discard
-        focusCards discards \unfocus -> do
-          chooseOneM attrs.owner do
-            targets cards \card -> do
-              push unfocus
-              obtainCard card
-              addToHand attrs.owner (only card)
+      whenM (can.have.cards.leaveDiscard attrs.owner) do
+        cards <- select $ inDiscardOf attrs.owner <> basic (#survivor <> not_ (CardWithTitle "Resourceful"))
+        unless (null cards) do
+          discards <- map toCard <$> attrs.owner.discard
+          focusCards discards \unfocus -> do
+            chooseOneM attrs.owner do
+              targets cards \card -> do
+                push unfocus
+                obtainCard card
+                addToHand attrs.owner (only card)
       pure s
     _ -> Resourceful <$> liftRunMessage msg attrs

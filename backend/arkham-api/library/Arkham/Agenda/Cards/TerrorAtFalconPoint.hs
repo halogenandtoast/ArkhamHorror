c9000a8432204772acd9cc1b83d1c2bddbb5c445
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-03 21:54:38 +0900

    Terror at falcon point

--- backend/arkham-api/library/Arkham/Agenda/Cards/TerrorAtFalconPoint.hs
+++ backend/arkham-api/library/Arkham/Agenda/Cards/TerrorAtFalconPoint.hs
@@ -1,17 +1,28 @@
-module Arkham.Agenda.Cards.TerrorAtFalconPoint
-  ( TerrorAtFalconPoint(..)
-  , terrorAtFalconPoint
-  ) where
+module Arkham.Agenda.Cards.TerrorAtFalconPoint (
+  TerrorAtFalconPoint (..),
+  terrorAtFalconPoint,
+) where
 
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Import.Lifted
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.GameValue
+import Arkham.Helpers.Modifiers
+import Arkham.Matcher
 
 newtype TerrorAtFalconPoint = TerrorAtFalconPoint AgendaAttrs
-  deriving anyclass (IsAgenda, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAgenda, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 terrorAtFalconPoint :: AgendaCard TerrorAtFalconPoint
 terrorAtFalconPoint = agenda (4, A) TerrorAtFalconPoint Cards.terrorAtFalconPoint (Static 12)
+
+instance HasModifiersFor TerrorAtFalconPoint where
+  getModifiersFor (EnemyTarget eid) (TerrorAtFalconPoint a) = do
+    isOcieros <- eid <=~> enemyIs Enemies.oceirosMarsh
+    healthModifier <- perPlayer 2
+    toModifiers a [HealthModifier healthModifier | isOcieros]
+  getModifiersFor _ _ = pure []
 
 instance RunMessage TerrorAtFalconPoint where
   runMessage msg a@(TerrorAtFalconPoint attrs) = runQueueT $ case msg of

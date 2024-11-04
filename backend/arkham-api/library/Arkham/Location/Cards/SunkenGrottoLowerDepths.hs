617683c703e70d7988b0621fdcc5deb4e45167f7
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-05 05:39:17 +0900

    More Light in the Fog Locations

--- backend/arkham-api/library/Arkham/Location/Cards/SunkenGrottoLowerDepths.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/SunkenGrottoLowerDepths.hs
@@ -4,9 +4,12 @@ module Arkham.Location.Cards.SunkenGrottoLowerDepths (
 )
 where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Grid
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Scenarios.ALightInTheFog.Helpers.Location
 
 newtype SunkenGrottoLowerDepths = SunkenGrottoLowerDepths LocationAttrs
@@ -17,10 +20,17 @@ sunkenGrottoLowerDepths :: LocationCard SunkenGrottoLowerDepths
 sunkenGrottoLowerDepths = location SunkenGrottoLowerDepths Cards.sunkenGrottoLowerDepths 0 (Static 0)
 
 instance HasAbilities SunkenGrottoLowerDepths where
-  getAbilities (SunkenGrottoLowerDepths attrs) =
-    extendRevealed attrs []
+  getAbilities (SunkenGrottoLowerDepths a) =
+    extendRevealed1 a
+      $ restricted a 1 (exists $ LocationInRow (-2) <> UnrevealedLocation)
+      $ FastAbility (GroupClueCost (PerPlayer 1) (be a))
 
 instance RunMessage SunkenGrottoLowerDepths where
   runMessage msg l@(SunkenGrottoLowerDepths attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      locations <- select $ LocationInRow (-2) <> UnrevealedLocation
+      chooseOrRunOneM iid do
+        targets locations $ lookAtRevealed iid (attrs.ability 1)
+      pure l
     PlaceGrid (GridLocation pos lid) | lid == attrs.id -> setConnectedInRow pos l
     _ -> SunkenGrottoLowerDepths <$> liftRunMessage msg attrs

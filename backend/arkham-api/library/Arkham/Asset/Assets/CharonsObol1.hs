93507ef64aee9bb0729f3c543d4d3bd67c578164
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-10 10:15:08 +0900

    Fix Charon's Obol (1)

--- backend/arkham-api/library/Arkham/Asset/Assets/CharonsObol1.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/CharonsObol1.hs
@@ -1,29 +1,20 @@
-module Arkham.Asset.Assets.CharonsObol1 (
-  charonsObol1,
-  CharonsObol1 (..),
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Assets.CharonsObol1 (charonsObol1, CharonsObol1 (..)) where
 
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
-import Arkham.Matcher
+import Arkham.Asset.Import.Lifted
+import Arkham.Card
+import Arkham.Modifier
 
 newtype CharonsObol1 = CharonsObol1 AssetAttrs
-  deriving anyclass (IsAsset, HasAbilities)
+  deriving anyclass (IsAsset, HasAbilities, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
-
-instance HasModifiersFor CharonsObol1 where
-  getModifiersFor (InvestigatorTarget iid) (CharonsObol1 attrs)
-    | controlledBy attrs iid = do
-        isDefeated <- elem iid <$> select DefeatedInvestigator
-        toModifiers attrs
-          $ KilledIfDefeated
-          : [XPModifier "Charon's Obol" 2 | not isDefeated]
-  getModifiersFor _ _ = pure []
 
 charonsObol1 :: AssetCard CharonsObol1
 charonsObol1 = asset CharonsObol1 Cards.charonsObol1
 
 instance RunMessage CharonsObol1 where
-  runMessage msg (CharonsObol1 attrs) = CharonsObol1 <$> runMessage msg attrs
+  runMessage msg (CharonsObol1 attrs) = runQueueT $ case msg of
+    CardIsEnteringPlay iid card | toCardId card == toCardId attrs -> do
+      gameModifiers attrs iid [KilledIfDefeated, XPModifier "Charon's Obol" 2]
+      CharonsObol1 <$> liftRunMessage msg attrs
+    _ -> CharonsObol1 <$> liftRunMessage msg attrs

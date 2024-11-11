49ce87be980ff6eec59156c16a447bebe6d0b4dd
Goose <halogenandtoast@gmail.com>
2024-11-12 03:59:06 +0900

    Dagon/Hydra

--- backend/arkham-api/library/Arkham/Enemy/Cards/HydraAwakenedAndEnraged.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/HydraAwakenedAndEnraged.hs
@@ -4,22 +4,52 @@ module Arkham.Enemy.Cards.HydraAwakenedAndEnraged (
 )
 where
 
+import Arkham.Ability
+import Arkham.DamageEffect
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Import.Lifted
+import Arkham.Enemy.Import.Lifted hiding (EnemyEvaded)
+import Arkham.Helpers.Modifiers (ModifierType (..), maybeModified)
+import Arkham.Matcher
+import Arkham.Message qualified as Msg
+import Arkham.Trait (Trait (Sanctum))
+import Arkham.Window qualified as Window
 
 newtype HydraAwakenedAndEnraged = HydraAwakenedAndEnraged EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass IsEnemy
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 hydraAwakenedAndEnraged :: EnemyCard HydraAwakenedAndEnraged
 hydraAwakenedAndEnraged =
-  enemyWith
+  enemy
     HydraAwakenedAndEnraged
     Cards.hydraAwakenedAndEnraged
     (7, Static 1, 4)
     (2, 1)
-    (healthL .~ Nothing)
+
+instance HasModifiersFor HydraAwakenedAndEnraged where
+  getModifiersFor target (HydraAwakenedAndEnraged a) = maybeModified a do
+    guard $ isTarget a target
+    n <- selectCount $ LocationWithAnyKeys <> withTrait Sanctum
+    guard $ n > 0
+    pure [EnemyFight (-n)]
+
+instance HasAbilities HydraAwakenedAndEnraged where
+  getAbilities (HydraAwakenedAndEnraged a) =
+    extend
+      a
+      [ restricted a 1 (exists $ enemyIs Cards.hydrasBrood)
+          $ forced
+          $ oneOf [EnemyDealtDamage #after AnyDamageEffect (be a) AnySource, EnemyEvaded #after Anyone (be a)]
+      ]
 
 instance RunMessage HydraAwakenedAndEnraged where
-  runMessage msg (HydraAwakenedAndEnraged attrs) = runQueueT $ case msg of
+  runMessage msg e@(HydraAwakenedAndEnraged attrs) = runQueueT $ case msg of
+    UseCardAbility _iid (isSource attrs -> True) 1 (map Window.windowType -> ws) _ -> do
+      brood <- select $ enemyIs Cards.hydrasBrood
+      for_ ws \case
+        Window.EnemyEvaded iid _ -> for_ brood $ push . Msg.EnemyEvaded iid
+        Window.DealtDamage source damageEffect _ n -> for_ brood \target -> push $ EnemyDamage target $ DamageAssignment source n damageEffect False False
+        _ -> pure ()
+
+      pure e
     _ -> HydraAwakenedAndEnraged <$> liftRunMessage msg attrs

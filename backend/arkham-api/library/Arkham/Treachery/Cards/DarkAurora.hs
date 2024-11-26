db9b02e6cd16aab376b0a6170c688aa1de78b887
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-26 22:17:58 +0900

    Silence and Mystery

--- backend/arkham-api/library/Arkham/Treachery/Cards/DarkAurora.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/DarkAurora.hs
@@ -1,8 +1,4 @@
-module Arkham.Treachery.Cards.DarkAurora
-  ( darkAurora
-  , DarkAurora(..)
-  )
-where
+module Arkham.Treachery.Cards.DarkAurora (darkAurora, DarkAurora (..)) where
 
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
@@ -16,5 +12,13 @@ darkAurora = treachery DarkAurora Cards.darkAurora
 
 instance RunMessage DarkAurora where
   runMessage msg t@(DarkAurora attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      sid <- getRandom
+      onRevealChaosTokenEffect sid #frost attrs iid do
+        assignHorror iid attrs 1
+      revelationSkillTest sid iid attrs #agility (Fixed 3)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      assignHorror iid attrs 2
+      pure t
     _ -> DarkAurora <$> liftRunMessage msg attrs

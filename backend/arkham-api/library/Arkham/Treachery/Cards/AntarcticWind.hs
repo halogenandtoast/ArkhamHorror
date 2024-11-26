d13ff9d97d48678821ee7b32f7aed2727e2a9d71
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-26 21:27:42 +0900

    Deadly Weather

--- backend/arkham-api/library/Arkham/Treachery/Cards/AntarcticWind.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/AntarcticWind.hs
@@ -1,16 +1,38 @@
 module Arkham.Treachery.Cards.AntarcticWind (antarcticWind, AntarcticWind (..)) where
 
+import Arkham.Ability
+import Arkham.Helpers.Location (onSameLocation)
+import Arkham.Helpers.Modifiers (ModifierType (..), maybeModified)
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Placement
+import Arkham.Placement
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
 newtype AntarcticWind = AntarcticWind TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass IsTreachery
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 antarcticWind :: TreacheryCard AntarcticWind
 antarcticWind = treachery AntarcticWind Cards.antarcticWind
 
+instance HasModifiersFor AntarcticWind where
+  getModifiersFor (InvestigatorTarget iid) (AntarcticWind a) = maybeModified a do
+    liftGuardM $ onSameLocation iid a.placement
+    pure [CannotDrawCards, CannotPlay AnyCard]
+  getModifiersFor _ _ = pure []
+
+instance HasAbilities AntarcticWind where
+  getAbilities (AntarcticWind a) = [mkAbility a 1 $ forced $ RoundEnds #when]
+
 instance RunMessage AntarcticWind where
   runMessage msg t@(AntarcticWind attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      ls <- select $ NearestLocationTo iid $ LocationWithoutTreachery (treacheryIs Cards.antarcticWind)
+      chooseTargetM iid ls $ place attrs . AttachedToLocation
+      pure t
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
+      toDiscard (attrs.ability 1) attrs
+      pure t
     _ -> AntarcticWind <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Treachery/Cards/PolarVortex.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/PolarVortex.hs
@@ -1,16 +1,43 @@
-module Arkham.Treachery.Cards.PolarVortex ( polarVortex , PolarVortex(..)) where
+module Arkham.Treachery.Cards.PolarVortex (polarVortex, PolarVortex (..)) where
 
+import Arkham.Ability
+import Arkham.Helpers.Investigator (withLocationOf)
+import Arkham.Matcher
+import Arkham.Message (pattern DealAssetDamage)
+import Arkham.Message.Lifted.Choose
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
 newtype PolarVortex = PolarVortex TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsTreachery, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 polarVortex :: TreacheryCard PolarVortex
 polarVortex = treachery PolarVortex Cards.polarVortex
 
+instance HasAbilities PolarVortex where
+  getAbilities (PolarVortex a) =
+    [ mkAbility a 1 $ forced $ TurnEnds #when (You <> at_ (locationWithTreachery a))
+    , mkAbility a 2 $ forced $ RoundEnds #when
+    ]
+
 instance RunMessage PolarVortex where
   runMessage msg t@(PolarVortex attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      withLocationOf iid $ attachTreachery attrs
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      assets <-
+        select
+          $ assetControlledBy iid
+          <> AssetWithAnyRemainingHealth
+          <> AssetCanBeDamagedBySource (attrs.ability 1)
+      chooseOneAtATimeM iid do
+        targeting iid $ directDamage iid (attrs.ability 1) 1
+        for_ assets \asset -> do
+          targeting asset $ push $ DealAssetDamage asset (attrs.ability 1) 1 0
+      pure t
+    UseThisAbility _iid (isSource attrs -> True) 2 -> do
+      toDiscard (attrs.ability 1) attrs
+      pure t
     _ -> PolarVortex <$> liftRunMessage msg attrs

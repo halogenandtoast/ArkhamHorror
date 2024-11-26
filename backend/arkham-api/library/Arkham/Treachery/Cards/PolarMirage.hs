e8c8744efa62df223688d55e30474aed234e6066
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-26 22:17:58 +0900

    Silence and Mystery

--- backend/arkham-api/library/Arkham/Treachery/Cards/PolarMirage.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/PolarMirage.hs
@@ -1,16 +1,37 @@
 module Arkham.Treachery.Cards.PolarMirage (polarMirage, PolarMirage (..)) where
 
+import Arkham.Ability
+import Arkham.Helpers.Message.Discard.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Placement
+import Arkham.Placement
 import Arkham.Treachery.Cards qualified as Cards
-import Arkham.Treachery.Import.Lifted
+import Arkham.Treachery.Import.Lifted hiding (DiscoverClues)
 
 newtype PolarMirage = PolarMirage TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsTreachery, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 polarMirage :: TreacheryCard PolarMirage
 polarMirage = treachery PolarMirage Cards.polarMirage
 
+-- TODO: Need to update for Frenzied Explorer and Lost Researcher
+instance HasAbilities PolarMirage where
+  getAbilities (PolarMirage a) = [mkAbility a 1 $ forced $ DiscoverClues #after You (locationWithTreachery a) AnyValue]
+
 instance RunMessage PolarMirage where
   runMessage msg t@(PolarMirage attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      ls <-
+        select
+          $ NearestLocationTo iid
+          $ LocationWithoutTreachery (treacheryIs Cards.kindredMist)
+          <> LocationWithAnyClues
+      chooseTargetM iid ls $ place attrs . AttachedToLocation
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      discardAll iid (attrs.ability 1) $ NonWeakness <> DiscardableCard
+      toDiscardBy iid (attrs.ability 1) attrs
+      pure t
     _ -> PolarMirage <$> liftRunMessage msg attrs

7b4822b8b758d65fc90717fb36f091ec4a987ca2
kaixinqi <48914549+kaixinqi@users.noreply.github.com>
2024-11-30 22:20:18 +0800

    zh trans: campaign log for basic game

--- frontend/src/locales/zh.ts
+++ frontend/src/locales/zh.ts
@@ -1,4 +1,13 @@
 import base from '@/locales/zh/base.json'
-import nightOfTheZealot from '@/locales/zh/nightOfTheZealot.json'
+import campaignLog from '@/locales/zh/campaignLog.json'
+import nightOfTheZealot from '@/locales/zh/nightOfTheZealot'
+import theDunwichLegacy from '@/locales/zh/theDunwichLegacy'
+import thePathToCarcosa from '@/locales/zh/thePathToCarcosa'
+import theForgottenAge from '@/locales/zh/theForgottenAge'
+import theCircleUndone from '@/locales/zh/theCircleUndone'
+import theDreamEaters from '@/locales/zh/theDreamEaters'
+import theInnsmouthConspiracy from '@/locales/zh/theInnsmouthConspiracy'
+import edgeOfTheEarth from '@/locales/zh/edgeOfTheEarth'
+import standalone from '@/locales/zh/standalone'
 
-export default {...base, nightOfTheZealot}
+export default {...base, ...campaignLog, nightOfTheZealot, theDunwichLegacy, thePathToCarcosa, theForgottenAge, theCircleUndone, theDreamEaters, theInnsmouthConspiracy, edgeOfTheEarth, standalone}

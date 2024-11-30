06a065765d0b4b1fb0a37dc7c71ae1fa7c601793
Matthew Mongeau <halogenandtoast@gmail.com>
2024-11-30 10:41:07 +0900

    Some fixes

--- backend/arkham-api/library/Arkham/Act/Cards/PursuingShadows.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/PursuingShadows.hs
@@ -1,20 +1,11 @@
-module Arkham.Act.Cards.PursuingShadows (
-  PursuingShadows (..),
-  pursuingShadows,
-) where
-
-import Arkham.Prelude
+module Arkham.Act.Cards.PursuingShadows (PursuingShadows (..), pursuingShadows) where
 
 import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
-import Arkham.Act.Helpers
-import Arkham.Act.Runner
+import Arkham.Act.Import.Lifted
 import Arkham.CampaignLogKey
-import Arkham.Classes
 import Arkham.Matcher
-import Arkham.Resolution
 import Arkham.Scenarios.APhantomOfTruth.Helpers
-import Arkham.Timing qualified as Timing
 
 newtype PursuingShadows = PursuingShadows ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
@@ -24,32 +15,26 @@ pursuingShadows :: ActCard PursuingShadows
 pursuingShadows = act (2, A) PursuingShadows Cards.pursuingShadows Nothing
 
 instance HasAbilities PursuingShadows where
-  getAbilities (PursuingShadows a)
-    | onSide A a =
-        [ reaction a 1 NoRestriction (GroupClueCost (Static 1) YourLocation)
-            $ EnemyAttacked Timing.After You AnySource (EnemyWithTitle "The Organist")
-        , restrictedAbility
-            a
-            2
-            ( EnemyCriteria
-                $ EnemyExists
-                $ EnemyWithTitle "The Organist"
-                <> EnemyWithClues (AtLeast $ PerPlayer 3)
-            )
-            $ Objective
-            $ ForcedAbility AnyWindow
-        ]
+  getAbilities (PursuingShadows a) | onSide A a = do
+    [ reaction a 1 NoRestriction (GroupClueCost (Static 1) YourLocation)
+        $ EnemyAttackedSuccessfully #after You AnySource (EnemyWithTitle "The Organist")
+      , restricted a 2 (exists $ EnemyWithTitle "The Organist" <> EnemyWithClues (AtLeast $ PerPlayer 3))
+          $ Objective
+          $ forced AnyWindow
+      ]
   getAbilities _ = []
 
 instance RunMessage PursuingShadows where
-  runMessage msg a@(PursuingShadows attrs) = case msg of
-    UseCardAbility _ source 1 _ _ | isSource attrs source -> do
-      theOrganist <- EnemyTarget <$> getTheOrganist
-      a <$ push (PlaceClues (toAbilitySource attrs 1) theOrganist 1)
-    UseCardAbility _ source 2 _ _
-      | isSource attrs source ->
-          a <$ push (AdvanceAct (toId attrs) source AdvancedWithOther)
-    AdvanceAct aid _ _ | aid == toId a && onSide B attrs -> do
+  runMessage msg a@(PursuingShadows attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      theOrganist <- getTheOrganist
+      placeClues (attrs.ability 1) theOrganist 1
+      pure a
+    UseThisAbility _ (isSource attrs -> True) 2 -> do
+      advancedWithOther attrs
+      pure a
+    AdvanceAct (isSide B attrs -> True) _ _ -> do
       intrudedOnASecretMeeting <- getHasRecord YouIntrudedOnASecretMeeting
-      a <$ push (ScenarioResolution $ Resolution $ if intrudedOnASecretMeeting then 2 else 1)
-    _ -> PursuingShadows <$> runMessage msg attrs
+      push $ if intrudedOnASecretMeeting then R2 else R1
+      pure a
+    _ -> PursuingShadows <$> liftRunMessage msg attrs

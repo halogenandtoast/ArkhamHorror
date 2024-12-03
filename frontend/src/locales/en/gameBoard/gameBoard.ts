55965c65ac8ea84be0fdac6eb2676eff52c104d7
kaixinqi <48914549+kaixinqi@users.noreply.github.com>
2024-12-04 03:57:29 +0800

    I18n supports: Phases and Skill Test-Steps

---
+++ frontend/src/locales/en/gameBoard/gameBoard.ts
@@ -0,0 +1,5 @@
+import base from '@/locales/en/gameBoard/base.json'
+import phase from '@/locales/en/gameBoard/phases.json'
+import skillTest from '@/locales/en/gameBoard/skill_test.json'
+
+export default {...base, phase, skillTest}

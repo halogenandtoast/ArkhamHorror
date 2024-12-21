8d45e34bc77858f0dbc7fc8b4b53dd7caf7e5015
Goose <halogenandtoast@gmail.com>
2024-12-21 06:53:36 -0600

    Airfield

--- backend/arkham-api/library/Arkham/Location/Cards/Airfield.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/Airfield.hs
@@ -1,19 +1,38 @@
 module Arkham.Location.Cards.Airfield (airfield) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.Card.CardDef
+import Arkham.Cost
+import Arkham.Enemy.Cards qualified as Cards
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Scenarios.FatalMirage.Helpers
+import Arkham.Story.Cards qualified as Stories
 
 newtype Airfield = Airfield LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass IsLocation
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 airfield :: LocationCard Airfield
 airfield = location Airfield Cards.airfield 4 (PerPlayer 2)
 
+mirageCards :: [CardDef]
+mirageCards = [Cards.memoryOfAMissingFather]
+
+instance HasModifiersFor Airfield where
+  getModifiersFor (Airfield a) = do
+    modifySelfWhenM
+      a
+      (selectAny $ assetIs Assets.takadaHirokoAeroplaneMechanic <> at_ (be a))
+      [ShroudModifier (-2)]
+    modifySelf a [AdditionalCostToInvestigate (ResourceCost 1)]
+
 instance HasAbilities Airfield where
-  getAbilities (Airfield attrs) =
-    extendRevealed attrs []
+  getAbilities (Airfield a) =
+    extendRevealed a [mirage a 2 mirageCards]
 
 instance RunMessage Airfield where
   runMessage msg (Airfield attrs) = runQueueT $ case msg of
-    _ -> Airfield <$> liftRunMessage msg attrs
+    _ -> Airfield <$> mirageRunner Stories.airfield mirageCards 2 msg attrs
--- backend/arkham-api/library/Arkham/Story/Cards/MoaiStatues.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/MoaiStatues.hs
@@ -1,13 +1,8 @@
 module Arkham.Story.Cards.MoaiStatues (moaiStatues) where
 
 import Arkham.Asset.Cards qualified as Assets
-import Arkham.CampaignLog
-import Arkham.Campaigns.EdgeOfTheEarth.Helpers
 import Arkham.Enemy.Cards qualified as Enemies
-import Arkham.Helpers.Query
 import Arkham.Location.Cards qualified as Locations
-import Arkham.Matcher
-import Arkham.Message.Lifted.Move
 import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
@@ -22,17 +17,10 @@ moaiStatues = story MoaiStatues Cards.moaiStatues
 instance RunMessage MoaiStatues where
   runMessage msg s@(MoaiStatues attrs) = runQueueT $ case msg of
     ResolveStory _iid ResolveIt story' | story' == toId attrs -> do
-      getPartnerStatus Assets.roaldEllsworthIntrepidExplorer >>= \case
-        Eliminated -> do
-          loc <- selectJust (locationIs Locations.moaiStatues)
-          selectEach (investigatorAt loc) \investigator ->
-            moveTo_ attrs investigator (locationIs Locations.prisonOfMemories)
-          selectEach (oneOf [UnengagedEnemy, MassiveEnemy] <> enemyAt loc) \enemy ->
-            moveTo_ attrs enemy (locationIs Locations.prisonOfMemories)
-          addToVictory loc
-          mayAdvance attrs
-        _ ->
-          getSetAsideCard Enemies.memoryOfAnAlienTranslation
-            >>= (`createEnemy_` Locations.moaiStatues)
+      handleMemory
+        attrs
+        Assets.roaldEllsworthIntrepidExplorer
+        Locations.moaiStatues
+        Enemies.memoryOfAnAlienTranslation
       pure s
     _ -> MoaiStatues <$> liftRunMessage msg attrs

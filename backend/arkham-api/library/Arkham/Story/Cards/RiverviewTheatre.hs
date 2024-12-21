82ed1b3e8e31dd49b3b6fe820dd889f7efafeabd
Goose <halogenandtoast@gmail.com>
2024-12-21 06:31:46 -0600

    Riverview Theatre

--- backend/arkham-api/library/Arkham/Story/Cards/RiverviewTheatre.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/RiverviewTheatre.hs
@@ -1,5 +1,8 @@
 module Arkham.Story.Cards.RiverviewTheatre (riverviewTheatre) where
 
+import Arkham.Helpers.Query
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Message.Lifted.Choose
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -12,6 +15,17 @@ riverviewTheatre = story RiverviewTheatre Cards.riverviewTheatre
 
 instance RunMessage RiverviewTheatre where
   runMessage msg s@(RiverviewTheatre attrs) = runQueueT $ case msg of
-    ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+    ResolveStory iid ResolveIt story' | story' == toId attrs -> do
+      mDrKenslersOffice <- getSetAsideCardMaybe Locations.drKenslersOffice
+      mInfirmary <- getSetAsideCardMaybe Locations.infirmaryFatalMirage
+
+      chooseOneM iid do
+        for_ mDrKenslersOffice
+          $ labeled "Put the set-aside Dr. Kensler's Office location into play."
+          . placeLocation_
+        for_ mInfirmary
+          $ labeled "Put the set-aside Infirmary location into play."
+          . placeLocation_
+
       pure s
     _ -> RiverviewTheatre <$> liftRunMessage msg attrs

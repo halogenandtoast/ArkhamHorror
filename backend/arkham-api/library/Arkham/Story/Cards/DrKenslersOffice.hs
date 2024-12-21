7edb61ed40ef8ec35fafaf70a3bef9eccc6dab6b
Goose <halogenandtoast@gmail.com>
2024-12-21 15:35:01 -0600

    More Fatal Mirage Locations

--- backend/arkham-api/library/Arkham/Story/Cards/DrKenslersOffice.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/DrKenslersOffice.hs
@@ -1,5 +1,9 @@
 module Arkham.Story.Cards.DrKenslersOffice (drKenslersOffice) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -13,5 +17,10 @@ drKenslersOffice = story DrKenslersOffice Cards.drKenslersOffice
 instance RunMessage DrKenslersOffice where
   runMessage msg s@(DrKenslersOffice attrs) = runQueueT $ case msg of
     ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+      handleMemory
+        attrs
+        Assets.drAmyKenslerProfessorOfBiology
+        Locations.drKenslersOffice
+        Enemies.memoryOfAnUnrequitedLove
       pure s
     _ -> DrKenslersOffice <$> liftRunMessage msg attrs

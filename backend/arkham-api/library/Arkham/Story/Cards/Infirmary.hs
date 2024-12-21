efdf3b417b0d70db6f3894c44d30e33ece41d767
Goose <halogenandtoast@gmail.com>
2024-12-21 15:35:01 -0600

    More Fatal Mirage Locations

--- backend/arkham-api/library/Arkham/Story/Cards/Infirmary.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/Infirmary.hs
@@ -1,5 +1,9 @@
 module Arkham.Story.Cards.Infirmary (infirmary) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -13,5 +17,10 @@ infirmary = story Infirmary Cards.infirmary
 instance RunMessage Infirmary where
   runMessage msg s@(Infirmary attrs) = runQueueT $ case msg of
     ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+      handleMemory
+        attrs
+        Assets.drMalaSinhaDaringPhysician
+        Locations.infirmaryFatalMirage
+        Enemies.memoryOfALostPatient
       pure s
     _ -> Infirmary <$> liftRunMessage msg attrs

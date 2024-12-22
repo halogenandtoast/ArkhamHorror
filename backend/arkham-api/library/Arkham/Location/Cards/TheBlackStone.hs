63ed90f98ee9d2e955ab6a7c97d85c83fee6e8bf
Goose <halogenandtoast@gmail.com>
2024-12-21 23:19:57 -0600

    Fatal Mirage locations

--- backend/arkham-api/library/Arkham/Location/Cards/TheBlackStone.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/TheBlackStone.hs
@@ -1,19 +1,51 @@
 module Arkham.Location.Cards.TheBlackStone (theBlackStone) where
 
+import Arkham.Ability
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.Card.CardDef
+import Arkham.Enemy.Cards qualified as Cards
+import Arkham.Helpers.Modifiers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Scenarios.FatalMirage.Helpers
+import Arkham.Story.Cards qualified as Stories
 
 newtype TheBlackStone = TheBlackStone LocationAttrs
-  deriving anyclass (IsLocation, HasModifiersFor)
+  deriving anyclass IsLocation
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theBlackStone :: LocationCard TheBlackStone
 theBlackStone = location TheBlackStone Cards.theBlackStone 4 (PerPlayer 2)
 
+mirageCards :: [CardDef]
+mirageCards = [Cards.memoryOfATerribleDiscovery]
+
+instance HasModifiersFor TheBlackStone where
+  getModifiersFor (TheBlackStone a) = do
+    modifySelfWhenM
+      a
+      ( selectAny
+          $ mapOneOf
+            assetIs
+            [ Assets.averyClaypoolAntarcticGuide
+            , Assets.averyClaypoolAntarcticGuideResolute
+            ]
+          <> at_ (be a)
+      )
+      [ShroudModifier (-2)]
+
 instance HasAbilities TheBlackStone where
-  getAbilities (TheBlackStone attrs) =
-    extendRevealed attrs []
+  getAbilities (TheBlackStone a) =
+    extendRevealed
+      a
+      [ mirage a 2 mirageCards
+      , restricted a 1 (Here <> thisExists a LocationWithAnyClues) $ FastAbility (AddFrostTokenCost 1)
+      ]
 
 instance RunMessage TheBlackStone where
-  runMessage msg (TheBlackStone attrs) = runQueueT $ case msg of
-    _ -> TheBlackStone <$> liftRunMessage msg attrs
+  runMessage msg l@(TheBlackStone attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      discoverAt NotInvestigate iid (attrs.ability 1) attrs 2
+      pure l
+    _ -> TheBlackStone <$> mirageRunner Stories.theBlackStone mirageCards 2 msg attrs
--- backend/arkham-api/library/Arkham/Story/Cards/OttomanFront.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/OttomanFront.hs
@@ -1,5 +1,9 @@
 module Arkham.Story.Cards.OttomanFront (ottomanFront) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Location.Cards qualified as Locations
+import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -13,5 +17,10 @@ ottomanFront = story OttomanFront Cards.ottomanFront
 instance RunMessage OttomanFront where
   runMessage msg s@(OttomanFront attrs) = runQueueT $ case msg of
     ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+      handleMemory
+        attrs
+        Assets.jamesCookieFredericksDubiousChoice
+        Locations.ottomanFront
+        Enemies.memoryOfARavagedCountry
       pure s
     _ -> OttomanFront <$> liftRunMessage msg attrs

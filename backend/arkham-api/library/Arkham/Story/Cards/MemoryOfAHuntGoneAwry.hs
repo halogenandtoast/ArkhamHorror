4d74f259ebaa1e683cb9b5384f75520401acda6b
Goose <halogenandtoast@gmail.com>
2024-12-21 23:27:17 -0600

    Memory of a Hunt Gone Awry

--- backend/arkham-api/library/Arkham/Story/Cards/MemoryOfAHuntGoneAwry.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/MemoryOfAHuntGoneAwry.hs
@@ -1,5 +1,11 @@
 module Arkham.Story.Cards.MemoryOfAHuntGoneAwry (memoryOfAHuntGoneAwry) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.CampaignLog (PartnerStatus (Resolute))
+import Arkham.CampaignLogKey
+import Arkham.Campaigns.EdgeOfTheEarth.Helpers
+import Arkham.Matcher
+import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -13,5 +19,11 @@ memoryOfAHuntGoneAwry = story MemoryOfAHuntGoneAwry Cards.memoryOfAHuntGoneAwry
 instance RunMessage MemoryOfAHuntGoneAwry where
   runMessage msg s@(MemoryOfAHuntGoneAwry attrs) = runQueueT $ case msg of
     ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+      record EliyahHasConfrontedHisDemons
+      setPartnerStatus Assets.eliyahAshevakDogHandler Resolute
+      selectForMaybeM (assetIs Assets.eliyahAshevakDogHandler) \eliyah ->
+        push $ ReplaceAsset eliyah Assets.eliyahAshevakDogHandlerResolute
+      addToVictory attrs
+      mayAdvance attrs
       pure s
     _ -> MemoryOfAHuntGoneAwry <$> liftRunMessage msg attrs

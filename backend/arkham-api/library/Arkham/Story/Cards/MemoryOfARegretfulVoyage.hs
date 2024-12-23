50c56f18e4357f707076414ae7f3b1f4aeaab79b
Goose <halogenandtoast@gmail.com>
2024-12-23 11:08:29 -0600

    Memory of A Regretful Voyage and Unspeakable Evil

--- backend/arkham-api/library/Arkham/Story/Cards/MemoryOfARegretfulVoyage.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/MemoryOfARegretfulVoyage.hs
@@ -1,5 +1,11 @@
 module Arkham.Story.Cards.MemoryOfARegretfulVoyage (memoryOfARegretfulVoyage) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.CampaignLog (PartnerStatus (Resolute))
+import Arkham.CampaignLogKey
+import Arkham.Campaigns.EdgeOfTheEarth.Helpers
+import Arkham.Matcher
+import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -13,5 +19,11 @@ memoryOfARegretfulVoyage = story MemoryOfARegretfulVoyage Cards.memoryOfARegretf
 instance RunMessage MemoryOfARegretfulVoyage where
   runMessage msg s@(MemoryOfARegretfulVoyage attrs) = runQueueT $ case msg of
     ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+      record DyerHasConfrontedHisDemons
+      setPartnerStatus Assets.professorWilliamDyerProfessorOfGeology Resolute
+      selectForMaybeM (assetIs Assets.professorWilliamDyerProfessorOfGeology) \dyer ->
+        push $ ReplaceAsset dyer Assets.professorWilliamDyerProfessorOfGeologyResolute
+      addToVictory attrs
+      mayAdvance attrs
       pure s
     _ -> MemoryOfARegretfulVoyage <$> liftRunMessage msg attrs

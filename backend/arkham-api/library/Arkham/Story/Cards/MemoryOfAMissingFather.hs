3aff39e43c9f2dc0cffce4ff162558edd8d59028
Goose <halogenandtoast@gmail.com>
2024-12-22 19:55:12 -0600

    Memory of a missing father

--- backend/arkham-api/library/Arkham/Story/Cards/MemoryOfAMissingFather.hs
+++ backend/arkham-api/library/Arkham/Story/Cards/MemoryOfAMissingFather.hs
@@ -1,5 +1,11 @@
 module Arkham.Story.Cards.MemoryOfAMissingFather (memoryOfAMissingFather) where
 
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.CampaignLog (PartnerStatus (Resolute))
+import Arkham.CampaignLogKey
+import Arkham.Campaigns.EdgeOfTheEarth.Helpers
+import Arkham.Matcher
+import Arkham.Scenarios.FatalMirage.Helpers
 import Arkham.Story.Cards qualified as Cards
 import Arkham.Story.Import.Lifted
 
@@ -13,5 +19,11 @@ memoryOfAMissingFather = story MemoryOfAMissingFather Cards.memoryOfAMissingFath
 instance RunMessage MemoryOfAMissingFather where
   runMessage msg s@(MemoryOfAMissingFather attrs) = runQueueT $ case msg of
     ResolveStory _ ResolveIt story' | story' == toId attrs -> do
+      record TakadaHasConfrontedHerDemons
+      setPartnerStatus Assets.takadaHirokoAeroplaneMechanic Resolute
+      selectForMaybeM (assetIs Assets.takadaHirokoAeroplaneMechanic) \takada ->
+        push $ ReplaceAsset takada Assets.takadaHirokoAeroplaneMechanicResolute
+      addToVictory attrs
+      mayAdvance attrs
       pure s
     _ -> MemoryOfAMissingFather <$> liftRunMessage msg attrs

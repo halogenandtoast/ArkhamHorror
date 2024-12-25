c3a23ac9ad764beea6ff244acf84c565cd9dfd8b
Goose <halogenandtoast@gmail.com>
2024-12-25 00:40:39 -0600

    Finish resolute versions

--- backend/arkham-api/library/Arkham/Asset/Assets/RoaldEllsworthIntrepidExplorerResolute.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/RoaldEllsworthIntrepidExplorerResolute.hs
@@ -16,18 +16,26 @@ newtype RoaldEllsworthIntrepidExplorerResolute = RoaldEllsworthIntrepidExplorerR
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 roaldEllsworthIntrepidExplorerResolute :: AssetCard RoaldEllsworthIntrepidExplorerResolute
-roaldEllsworthIntrepidExplorerResolute = allyWith RoaldEllsworthIntrepidExplorerResolute Cards.roaldEllsworthIntrepidExplorerResolute (5, 2) noSlots
+roaldEllsworthIntrepidExplorerResolute =
+  allyWith
+    RoaldEllsworthIntrepidExplorerResolute
+    Cards.roaldEllsworthIntrepidExplorerResolute
+    (5, 2)
+    noSlots
 
 instance HasAbilities RoaldEllsworthIntrepidExplorerResolute where
   getAbilities (RoaldEllsworthIntrepidExplorerResolute a) =
-    [ controlled a 1 (DuringTurn You <> exists (TreacheryAttachedToLocation Anywhere))
-        $ FastAbility (exhaust a <> assetUseCost a Supply 1)
+    [ controlled
+        a
+        1
+        (DuringTurn You <> exists (TreacheryIsNonWeakness <> TreacheryAttachedToLocation Anywhere))
+        $ FastAbility (OrCost [exhaust a, assetUseCost a Supply 1])
     ]
 
 instance RunMessage RoaldEllsworthIntrepidExplorerResolute where
   runMessage msg a@(RoaldEllsworthIntrepidExplorerResolute attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      chooseSelectM iid (TreacheryAttachedToLocation Anywhere) \x -> do
+      chooseSelectM iid (TreacheryIsNonWeakness <> TreacheryAttachedToLocation Anywhere) \x -> do
         turnModifier iid (attrs.ability 1) x Blank
       pure a
     _ -> RoaldEllsworthIntrepidExplorerResolute <$> liftRunMessage msg attrs

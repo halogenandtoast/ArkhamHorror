7c6ca40674dd67dd45971544725733700554b214
Matthew Mongeau <halogenandtoast@gmail.com>
2025-01-24 11:33:25 +0900

    Fix Open Gate

--- backend/arkham-api/library/Arkham/Event/Events/OpenGate.hs
+++ backend/arkham-api/library/Arkham/Event/Events/OpenGate.hs
@@ -1,13 +1,11 @@
-module Arkham.Event.Events.OpenGate (openGate, OpenGate (..)) where
+module Arkham.Event.Events.OpenGate (openGate) where
 
-import Arkham.Classes
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
 import Arkham.Helpers.Investigator
 import Arkham.Helpers.Modifiers
 import Arkham.Matcher
 import Arkham.Placement
-import Arkham.Prelude
 
 newtype OpenGate = OpenGate EventAttrs
   deriving anyclass (IsEvent, HasAbilities)
@@ -17,17 +15,15 @@ openGate :: EventCard OpenGate
 openGate = event OpenGate Cards.openGate
 
 instance HasModifiersFor OpenGate where
-  getModifiersFor (OpenGate a) =
-    case a.placement of
-      AttachedToLocation lid -> do
-        modifySelectMap a (not_ (LocationWithId lid) <> LocationWithAttachedEvent (eventIs Cards.openGate)) \lid' ->
-          [ConnectedToWhen (LocationWithId lid) (LocationWithId lid')]
-      _ -> pure mempty
+  getModifiersFor (OpenGate a) = case a.placement of
+    AttachedToLocation lid -> do
+      modifySelectMap a (not_ (be lid) <> LocationWithAttachedEvent (eventIs Cards.openGate)) \lid' ->
+        [ConnectedToWhen (LocationWithId lid') (LocationWithId lid)]
+    _ -> pure ()
 
 instance RunMessage OpenGate where
-  runMessage msg e@(OpenGate attrs) = case msg of
-    PlayThisEvent iid eid | eid == toId attrs -> do
-      lid <- getJustLocation iid
-      push $ PlaceEvent eid (AttachedToLocation lid)
+  runMessage msg e@(OpenGate attrs) = runQueueT $ case msg of
+    PlayThisEvent iid (is attrs -> True) -> do
+      withLocationOf iid $ place attrs
       pure e
-    _ -> OpenGate <$> runMessage msg attrs
+    _ -> OpenGate <$> liftRunMessage msg attrs

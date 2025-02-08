134c0febca81c1672b606608f7e2566aed29a9a7
Matthew Mongeau <halogenandtoast@gmail.com>
2025-02-08 12:46:19 +0900

    Update other grotesque statue

--- backend/arkham-api/library/Arkham/Asset/Assets/GrotesqueStatue2.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/GrotesqueStatue2.hs
@@ -1,16 +1,11 @@
-module Arkham.Asset.Assets.GrotesqueStatue2 (
-  GrotesqueStatue2 (..),
-  grotesqueStatue2,
-) where
-
-import Arkham.Prelude
+module Arkham.Asset.Assets.GrotesqueStatue2 ( grotesqueStatue2,) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
 import Arkham.ChaosBagStepState
 import Arkham.Matcher
-import Arkham.Window (Window (..), mkWindow)
+import Arkham.Window (Window (..))
 import Arkham.Window qualified as Window
 
 newtype GrotesqueStatue2 = GrotesqueStatue2 AssetAttrs
@@ -23,7 +18,7 @@ grotesqueStatue2 =
 
 instance HasAbilities GrotesqueStatue2 where
   getAbilities (GrotesqueStatue2 x) =
-    [ restrictedAbility x 1 ControlsThis
+    [ restricted x 1 ControlsThis
         $ ReactionAbility (WouldRevealChaosToken #when You)
         $ assetUseCost x Charge 1
     ]
@@ -34,14 +29,12 @@ toDrawSource ((windowType -> Window.WouldRevealChaosToken drawSource _) : _) = d
 toDrawSource (_ : rest) = toDrawSource rest
 
 instance RunMessage GrotesqueStatue2 where
-  runMessage msg a@(GrotesqueStatue2 attrs) = case msg of
+  runMessage msg a@(GrotesqueStatue2 attrs) = runQueueT $ case msg of
     UseCardAbility iid (isSource attrs -> True) 1 (toDrawSource -> drawSource) _ -> do
-      ignoreWindow <-
-        checkWindows [mkWindow #after (Window.CancelledOrIgnoredCardOrGameEffect (toSource attrs))]
-      pushAll
-        [ ReplaceCurrentDraw drawSource iid
-            $ Choose (toSource attrs) 1 ResolveChoice [Undecided Draw, Undecided Draw] [] Nothing
-        , ignoreWindow
-        ]
+      checkWhen $ Window.WouldRevealChaosTokens drawSource iid
+      push
+        $ ReplaceCurrentDraw drawSource iid
+        $ Choose (toSource attrs) 1 ResolveChoice [Undecided Draw, Undecided Draw] [] Nothing
+      cancelledOrIgnoredCardOrGameEffect (attrs.ability 1)
       pure a
-    _ -> GrotesqueStatue2 <$> runMessage msg attrs
+    _ -> GrotesqueStatue2 <$> liftRunMessage msg attrs

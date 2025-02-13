a72aa7e4980b715c0c24a839f9db645b32aea1e3
Matthew Mongeau <halogenandtoast@gmail.com>
2025-02-13 18:43:58 +0900

    Dial of Ancients: Unidentified

--- backend/arkham-api/library/Arkham/Asset/Assets/DialOfAncientsUnidentified.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/DialOfAncientsUnidentified.hs
@@ -2,13 +2,14 @@ module Arkham.Asset.Assets.DialOfAncientsUnidentified (dialOfAncientsUnidentifie
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Import.Lifted
+import Arkham.Asset.Import.Lifted hiding (EnemyEvaded)
 import Arkham.Asset.Uses
 import Arkham.CampaignLogKey
 import Arkham.Matcher
+import Arkham.Message.Lifted.Log
 
 newtype DialOfAncientsUnidentified = DialOfAncientsUnidentified AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 dialOfAncientsUnidentified :: AssetCard DialOfAncientsUnidentified
@@ -20,11 +21,11 @@ instance HasAbilities DialOfAncientsUnidentified where
 
 instance RunMessage DialOfAncientsUnidentified where
   runMessage msg a@(DialOfAncientsUnidentified attrs) = runQueueT $ case msg of
-    UseThisAbility iid (isSource attrs -> True) 1 -> do
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
       addUses (attrs.ability 1) attrs Charge 1
       doStep 1 msg
       pure a
-    DoStep 1 (UseThisAbility iid (isSource attrs -> True) 1) -> do
+    DoStep 1 (UseThisAbility _iid (isSource attrs -> True) 1) -> do
       recordWhen (attrs.use Charge >= 4) YouHaveCalculatedTheDayOfReckoning
       pure a
     _ -> DialOfAncientsUnidentified <$> liftRunMessage msg attrs

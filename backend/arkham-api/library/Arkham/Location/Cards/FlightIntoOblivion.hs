b520e116cb18db83105e68108a8f6d9ca5ee263d
Matthew Mongeau <halogenandtoast@gmail.com>
2025-03-20 09:08:34 +0900

    Adjust Cosmos locations

--- backend/arkham-api/library/Arkham/Location/Cards/FlightIntoOblivion.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/FlightIntoOblivion.hs
@@ -1,11 +1,6 @@
-module Arkham.Location.Cards.FlightIntoOblivion (
-  flightIntoOblivion,
-  FlightIntoOblivion (..),
-)
-where
+module Arkham.Location.Cards.FlightIntoOblivion (flightIntoOblivion) where
 
-import Arkham.Prelude
-
+import Arkham.Ability
 import Arkham.Card
 import Arkham.Direction
 import Arkham.GameValue
@@ -14,7 +9,8 @@ import Arkham.Investigator.Types (Field (..))
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Cards qualified as Locations
 import Arkham.Location.Helpers
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Choose
 import Arkham.Projection
 import Arkham.Scenarios.BeforeTheBlackThrone.Cosmos
 import Arkham.Scenarios.BeforeTheBlackThrone.Helpers
@@ -34,77 +30,63 @@ flightIntoOblivion =
 
 instance HasAbilities FlightIntoOblivion where
   getAbilities (FlightIntoOblivion attrs) =
-    withRevealedAbilities
+    extendRevealed
       attrs
       [ cosmos attrs 1
-      , limitedAbility (GroupLimit PerGame 1)
-          $ restrictedAbility attrs 2 (Here <> CanMoveThis GridUp)
-          $ ActionAbility [] (ActionCost 1 <> ScenarioResourceCost 1)
+      , groupLimit PerGame
+          $ restricted attrs 2 (Here <> CanMoveThis GridUp)
+          $ actionAbilityWithCost (ScenarioResourceCost 1)
       ]
 
 instance RunMessage FlightIntoOblivion where
-  runMessage msg l@(FlightIntoOblivion attrs) = case msg of
+  runMessage msg l@(FlightIntoOblivion attrs) = runQueueT $ case msg of
     RunCosmos iid lid msgs | lid == toId attrs -> do
-      mpos <- findCosmosPosition iid
-      case mpos of
+      findCosmosPosition iid >>= \case
         Nothing -> cosmosFail attrs
         Just pos -> do
           aboveChoice <- getEmptyPositionsInDirections pos [GridUp]
           topmostPositions <- topmostRevealedLocationPositions
 
-          let
-            positionPairs = flip map topmostPositions $ \pos' ->
-              ( pos'
-              , [GridUp, GridDown, GridLeft, GridRight]
-              )
-          emptyPositions <- flip concatMapM positionPairs $ \(pos', dirs) ->
-            getEmptyPositionsInDirections pos' dirs
+          let positionPairs = map (,[GridUp ..]) topmostPositions
+          emptyPositions <- concatMapM (uncurry getEmptyPositionsInDirections) positionPairs
 
           if null aboveChoice && null topmostPositions
             then cosmosFail attrs
             else do
-              player <- getPlayer iid
-              push
-                $ chooseOne player
-                $ [ Label
-                    "Connect Above"
-                    [ chooseOrRunOne
-                        player
-                        [ GridLabel (cosmicLabel pos') (PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid) : msgs)
-                        | pos'@(Pos x y) <- aboveChoice
-                        ]
-                    ]
-                  | notNull aboveChoice
-                  ]
-                <> [ Label
-                    "Take 2 horror and connect to the topmost revealed location in a direction of your choice"
-                    [ assignHorror iid (toAbilitySource attrs 1) 2
-                    , chooseOrRunOne
-                        player
-                        [ GridLabel (cosmicLabel pos') (PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid) : msgs)
-                        | pos'@(Pos x y) <- emptyPositions
-                        ]
-                    ]
-                   | notNull emptyPositions
-                   ]
+              chooseOneM iid do
+                questionLabeled "Choose where to connect"
+                questionLabeledCard attrs
+                when (notNull aboveChoice) do
+                  labeled "Connect Above" do
+                    chooseOrRunOneM iid do
+                      for_ aboveChoice \pos'@(Pos x y) -> do
+                        gridLabeled (cosmicLabel pos')
+                          $ pushAll
+                          $ PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid)
+                          : msgs
+                when (notNull emptyPositions) do
+                  labeled "Take 2 horror and connect to the topmost revealed location in a direction of your choice" do
+                    assignHorror iid (toAbilitySource attrs 1) 2
+                    chooseOrRunOneM iid do
+                      for_ emptyPositions \pos'@(Pos x y) -> do
+                        gridLabeled (cosmicLabel pos')
+                          $ pushAll
+                          $ PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid)
+                          : msgs
       pure l
-    UseCardAbility iid (isSource attrs -> True) 2 _ _ -> do
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
       (map toCard -> cards, _) <- fieldMap InvestigatorDeck (draw 1) iid
       case cards of
         [card] -> do
           cosmos' <- getCosmos
-          let mpos = findInCosmos (toId attrs) cosmos'
-          case mpos of
+          case findInCosmos (toId attrs) cosmos' of
             Nothing -> error "location not found in cosmos, we shouldn't be here"
             Just pos -> do
-              (emptySpace', placeEmptySpace) <- placeLocationCard Locations.emptySpace
-              pushAll
-                [ ObtainCard card.id
-                , placeEmptySpace
-                , PlaceCosmos iid emptySpace' (EmptySpace pos card)
-                , PlaceCosmos iid (toId attrs) (CosmosLocation (updatePosition pos GridUp) (toId attrs))
-                ]
+              obtainCard card
+              emptySpace' <- placeLocationCard Locations.emptySpace
+              push $ PlaceCosmos iid emptySpace' (EmptySpace pos card)
+              push $ PlaceCosmos iid (toId attrs) (CosmosLocation (updatePosition pos GridUp) (toId attrs))
         [] -> error "empty deck, what should we do?, maybe don't let this be called?"
         _ -> error "too many cards, why did this happen?"
       pure l
-    _ -> FlightIntoOblivion <$> runMessage msg attrs
+    _ -> FlightIntoOblivion <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Location/Cards/InfinityOfDarkness.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/InfinityOfDarkness.hs
@@ -1,11 +1,6 @@
-module Arkham.Location.Cards.InfinityOfDarkness (
-  infinityOfDarkness,
-  InfinityOfDarkness (..),
-)
-where
+module Arkham.Location.Cards.InfinityOfDarkness (infinityOfDarkness) where
 
-import Arkham.Prelude
-
+import Arkham.Ability
 import Arkham.Card
 import Arkham.Direction
 import Arkham.GameValue
@@ -14,7 +9,8 @@ import Arkham.Investigator.Types (Field (..))
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Cards qualified as Locations
 import Arkham.Location.Helpers
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Choose
 import Arkham.Projection
 import Arkham.Scenarios.BeforeTheBlackThrone.Cosmos
 import Arkham.Scenarios.BeforeTheBlackThrone.Helpers
@@ -34,79 +30,65 @@ infinityOfDarkness =
 
 instance HasAbilities InfinityOfDarkness where
   getAbilities (InfinityOfDarkness attrs) =
-    withRevealedAbilities
+    extendRevealed
       attrs
       [ cosmos attrs 1
       , groupLimit PerGame
-          $ restrictedAbility attrs 2 (Here <> CanMoveThis GridDown)
-          $ ActionAbility [] (ActionCost 1 <> ScenarioResourceCost 1)
+          $ restricted attrs 2 (Here <> CanMoveThis GridDown)
+          $ actionAbilityWithCost (ScenarioResourceCost 1)
       ]
 
--- withRevealedAbilities attrs []
-
 instance RunMessage InfinityOfDarkness where
-  runMessage msg l@(InfinityOfDarkness attrs) = case msg of
+  runMessage msg l@(InfinityOfDarkness attrs) = runQueueT $ case msg of
     RunCosmos iid lid msgs | lid == toId attrs -> do
-      mpos <- findCosmosPosition iid
-      case mpos of
+      findCosmosPosition iid >>= \case
         Nothing -> cosmosFail attrs
         Just pos -> do
           belowChoice <- getEmptyPositionsInDirections pos [GridDown]
           bottommostPositions <- bottommostRevealedLocationPositions
 
-          let
-            positionPairs = flip map bottommostPositions $ \pos' ->
-              ( pos'
-              , [GridUp, GridDown, GridLeft, GridRight]
-              )
-          emptyPositions <- flip concatMapM positionPairs $ \(pos', dirs) ->
-            getEmptyPositionsInDirections pos' dirs
+          let positionPairs = map (,[GridUp ..]) bottommostPositions
+          emptyPositions <- concatMapM (uncurry getEmptyPositionsInDirections) positionPairs
 
           if null belowChoice && null bottommostPositions
             then cosmosFail attrs
             else do
-              player <- getPlayer iid
-              push
-                $ chooseOne player
-                $ [ Label
-                    "Connect Below"
-                    [ chooseOrRunOne
-                        player
-                        [ GridLabel (cosmicLabel pos') (PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid) : msgs)
-                        | pos'@(Pos x y) <- belowChoice
-                        ]
-                    ]
-                  | notNull belowChoice
-                  ]
-                <> [ Label
-                    "Take 2 horror and connect to the bottommost revealed location in a direction of your choice"
-                    [ assignDamage iid (toAbilitySource attrs 1) 2
-                    , chooseOrRunOne
-                        player
-                        [ GridLabel (cosmicLabel pos') (PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid) : msgs)
-                        | pos'@(Pos x y) <- emptyPositions
-                        ]
-                    ]
-                   | notNull emptyPositions
-                   ]
+              chooseOneM iid do
+                questionLabeled "Choose where to connect"
+                questionLabeledCard attrs
+                when (notNull belowChoice) do
+                  labeled "Connect Below" do
+                    chooseOrRunOneM iid do
+                      for_ belowChoice \pos'@(Pos x y) -> do
+                        gridLabeled (cosmicLabel pos')
+                          $ pushAll
+                          $ PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid)
+                          : msgs
+                when (notNull emptyPositions) do
+                  labeled
+                    "Take 2 damage and connect to the bottommost revealed location in a direction of your choice"
+                    do
+                      assignDamage iid (attrs.ability 1) 2
+                      chooseOrRunOneM iid do
+                        for_ emptyPositions \pos'@(Pos x y) -> do
+                          gridLabeled (cosmicLabel pos')
+                            $ pushAll
+                            $ PlaceCosmos iid (toId attrs) (CosmosLocation (Pos x y) lid)
+                            : msgs
       pure l
     UseCardAbility iid (isSource attrs -> True) 2 _ _ -> do
       (map toCard -> cards, _) <- fieldMap InvestigatorDeck (draw 1) iid
       case cards of
         [card] -> do
           cosmos' <- getCosmos
-          let mpos = findInCosmos (toId attrs) cosmos'
-          case mpos of
+          case findInCosmos (toId attrs) cosmos' of
             Nothing -> error "location not found in cosmos, we shouldn't be here"
             Just pos -> do
-              (emptySpace', placeEmptySpace) <- placeLocationCard Locations.emptySpace
-              pushAll
-                [ ObtainCard card.id
-                , placeEmptySpace
-                , PlaceCosmos iid emptySpace' (EmptySpace pos card)
-                , PlaceCosmos iid (toId attrs) (CosmosLocation (updatePosition pos GridDown) (toId attrs))
-                ]
+              obtainCard card
+              emptySpace' <- placeLocationCard Locations.emptySpace
+              push $ PlaceCosmos iid emptySpace' (EmptySpace pos card)
+              push $ PlaceCosmos iid (toId attrs) (CosmosLocation (updatePosition pos GridDown) (toId attrs))
         [] -> error "empty deck, what should we do?, maybe don't let this be called?"
         _ -> error "too many cards, why did this happen?"
       pure l
-    _ -> InfinityOfDarkness <$> runMessage msg attrs
+    _ -> InfinityOfDarkness <$> liftRunMessage msg attrs

29ef7b385b97ef377d1002fe222ecc3214a0fe6a
Goose <halogenandtoast@gmail.com>
2025-03-29 20:23:39 -1000

    Pursuit of the Unknown

--- backend/arkham-api/library/Arkham/Act/Cards/PursuitOfTheUnknownV2.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/PursuitOfTheUnknownV2.hs
@@ -1,18 +1,55 @@
 module Arkham.Act.Cards.PursuitOfTheUnknownV2 (pursuitOfTheUnknownV2) where
 
+import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Scenario.Deck
+import Arkham.Strategy
 
 newtype PursuitOfTheUnknownV2 = PursuitOfTheUnknownV2 ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 pursuitOfTheUnknownV2 :: ActCard PursuitOfTheUnknownV2
 pursuitOfTheUnknownV2 = act (2, A) PursuitOfTheUnknownV2 Cards.pursuitOfTheUnknownV2 Nothing
 
+instance HasAbilities PursuitOfTheUnknownV2 where
+  getAbilities (PursuitOfTheUnknownV2 x) =
+    [ restricted x 1 (exists $ DeckWith $ HasCard $ CardWithTitle "Tekeli-li")
+        $ actionAbilityWithCost (SpendTokenKeyCost 2 #tablet)
+    , restricted
+        x
+        2
+        (EachUndefeatedInvestigator $ at_ $ "Hidden Tunnel" <> LocationWithoutClues)
+        $ Objective
+        $ forced AnyWindow
+    ]
+
 instance RunMessage PursuitOfTheUnknownV2 where
   runMessage msg a@(PursuitOfTheUnknownV2 attrs) = runQueueT $ case msg of
     AdvanceAct (isSide B attrs -> True) _ _ -> do
-      advanceActDeck attrs
+      push R1
+      pure a
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
+      eachInvestigator \iid -> do
+        search
+          iid
+          (attrs.ability 1)
+          attrs
+          [fromDeck]
+          (basic $ CardWithTitle "Tekeli-li")
+          (defer attrs IsNotDraw)
+
+      pure a
+    SearchFound iid (isTarget attrs -> True) _ [] -> do
+      chooseOneM iid $ labeled "No Cards Found" nothing
+      pure a
+    SearchFound iid (isTarget attrs -> True) _ cards -> do
+      chooseTargetM iid cards $ shuffleCardsIntoDeck TekeliliDeck . only
+      pure a
+    UseThisAbility _ (isSource attrs -> True) 2 -> do
+      advancedWithOther attrs
       pure a
     _ -> PursuitOfTheUnknownV2 <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Act/Cards/PursuitOfTheUnknownV3.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/PursuitOfTheUnknownV3.hs
@@ -1,18 +1,42 @@
 module Arkham.Act.Cards.PursuitOfTheUnknownV3 (pursuitOfTheUnknownV3) where
 
+import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted
+import Arkham.Matcher
+import Arkham.Trait (Trait (ElderThing, Elite, Shoggoth))
 
 newtype PursuitOfTheUnknownV3 = PursuitOfTheUnknownV3 ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 pursuitOfTheUnknownV3 :: ActCard PursuitOfTheUnknownV3
 pursuitOfTheUnknownV3 = act (2, A) PursuitOfTheUnknownV3 Cards.pursuitOfTheUnknownV3 Nothing
 
+instance HasAbilities PursuitOfTheUnknownV3 where
+  getAbilities (PursuitOfTheUnknownV3 x) =
+    [ mkAbility x 1
+        $ forced
+        $ EnemyEnters #when (LocationWithEnemy $ EnemyWithTrait ElderThing) (hasAnyTrait [Shoggoth, Elite])
+    , restricted x 2 (exists $ OnlyInBag #elderthing)
+        $ actionAbilityWithCost (SpendTokenKeyCost 2 #elderthing)
+    , restricted
+        x
+        3
+        (EachUndefeatedInvestigator $ at_ $ "Hidden Tunnel" <> LocationWithoutClues)
+        $ Objective
+        $ forced AnyWindow
+    ]
+
 instance RunMessage PursuitOfTheUnknownV3 where
   runMessage msg a@(PursuitOfTheUnknownV3 attrs) = runQueueT $ case msg of
     AdvanceAct (isSide B attrs -> True) _ _ -> do
-      advanceActDeck attrs
+      push R1
+      pure a
+    UseThisAbility _iid (isSource attrs -> True) 2 -> do
+      push $ RemoveAllChaosTokens #elderthing
+      pure a
+    UseThisAbility _ (isSource attrs -> True) 3 -> do
+      advancedWithOther attrs
       pure a
     _ -> PursuitOfTheUnknownV3 <$> liftRunMessage msg attrs

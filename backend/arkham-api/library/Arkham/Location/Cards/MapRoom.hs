bab2871e7e46c789dbb358f6074b115282d7fd0a
Goose <halogenandtoast@gmail.com>
2025-03-31 09:35:10 -1000

    More locations, and some adjustments

--- backend/arkham-api/library/Arkham/Location/Cards/MapRoom.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/MapRoom.hs
@@ -1,7 +1,12 @@
 module Arkham.Location.Cards.MapRoom (mapRoom) where
 
+import Arkham.Ability
+import Arkham.Campaigns.EdgeOfTheEarth.Key
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Log
 
 newtype MapRoom = MapRoom LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +17,22 @@ mapRoom = locationWith MapRoom Cards.mapRoom 2 (PerPlayer 1) connectsToAdjacent
 
 instance HasAbilities MapRoom where
   getAbilities (MapRoom attrs) =
-    extendRevealed attrs []
+    extendRevealed1 attrs
+      $ restricted attrs 1 Here
+      $ actionAbilityWithCost (SpendTokenKeyCost 2 #"-3")
 
 instance RunMessage MapRoom where
-  runMessage msg (MapRoom attrs) = runQueueT $ case msg of
+  runMessage msg l@(MapRoom attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      ls <- select $ UnrevealedLocation <> not_ "Hidden Tunnel"
+      chooseNM iid 3 $ targets ls \lid -> do
+        reveal lid
+        -- Need to delay discovering until after the location has been revealed
+        -- otherwise it will be rejected
+        forTarget lid msg
+      record TheTeamReadTheMap
+      pure l
+    ForTarget (LocationTarget lid) (UseThisAbility iid (isSource attrs -> True) 1) -> do
+      discoverAt NotInvestigate iid (attrs.ability 1) lid 1
+      pure l
     _ -> MapRoom <$> liftRunMessage msg attrs

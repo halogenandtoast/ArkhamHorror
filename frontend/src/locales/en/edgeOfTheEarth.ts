143a69080d051ba95c8ecaab6a71f18ce01c8c15
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-02 19:09:59 +0900

    Copy

--- frontend/src/locales/en/edgeOfTheEarth.ts
+++ frontend/src/locales/en/edgeOfTheEarth.ts
@@ -3,5 +3,6 @@ import iceAndDeath from '@/locales/en/edgeOfTheEarth/scenarios/iceAndDeath.json'
 import fatalMirage from '@/locales/en/edgeOfTheEarth/scenarios/fatalMirage.json'
 import toTheForbiddenPeaks from '@/locales/en/edgeOfTheEarth/scenarios/toTheForbiddenPeaks.json'
 import cityOfTheElderThings from '@/locales/en/edgeOfTheEarth/scenarios/cityOfTheElderThings.json'
+import theHeartOfMadness from '@/locales/en/edgeOfTheEarth/scenarios/theHeartOfMadness.json'
 
-export default {...base, iceAndDeath, fatalMirage, toTheForbiddenPeaks, cityOfTheElderThings}
+export default {...base, iceAndDeath, fatalMirage, toTheForbiddenPeaks, cityOfTheElderThings, theHeartOfMadness}

ffddfaa4d9434a3844174e9bd656d2ad14a66329
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-04 22:19:00 +0900

    Collapse the pylons

--- backend/arkham-api/library/Arkham/Act/Cards/CollapseThePylons.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/CollapseThePylons.hs
@@ -3,16 +3,22 @@ module Arkham.Act.Cards.CollapseThePylons (collapseThePylons) where
 import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted hiding (DiscoverClues)
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.Query (getSetAsideCardsMatching)
 import Arkham.Helpers.Window (discoveredCluesAt)
+import Arkham.Location.Cards qualified as Locations
 import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Placement
 import Arkham.Modifier
+import Arkham.Placement
 
 newtype CollapseThePylons = CollapseThePylons ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 collapseThePylons :: ActCard CollapseThePylons
-collapseThePylons = act (1, A) CollapseThePylons Cards.collapseThePylons Nothing
+collapseThePylons = act (3, A) CollapseThePylons Cards.collapseThePylons Nothing
 
 instance HasAbilities CollapseThePylons where
   getAbilities (CollapseThePylons x) =
@@ -34,6 +40,24 @@ instance RunMessage CollapseThePylons where
       advancedWithOther attrs
       pure a
     AdvanceAct (isSide B attrs -> True) _ _ -> do
-      advanceActDeck attrs
+      nameless <- select $ enemyIs Enemies.theNamelessMadness
+      lead <- getLead
+      let x = max 0 (length nameless - 3)
+      when (x > 3) do
+        chooseNM lead x do
+          questionLabeled $ "Set " <> tshow x <> " the nameless madness aside"
+          targets nameless (`place` SetAsideZone)
+      doStep 1 msg
+      eachInvestigator (discardAllClues attrs)
+      eachInvestigator (`place` Unplaced)
+      selectEach (not_ $ locationIs Locations.theGateOfYquaa) removeLocation
+      push $ SetLayout ["theGateOfYquaa titanicRamp1 titanicRamp2 titanicRamp3 titanicRamp4 hiddenTunnel"]
+      placeRandomLocationGroup "titanicRamp" =<< getSetAsideCardsMatching "Titanic Ramp"
+      placeSetAsideLocation_ Locations.hiddenTunnelAWayOut
+      doStep 2 msg
+      theFinalMirage <- getSetAsideCard Cards.theFinalMirage
+      push $ SetCurrentActDeck 1 [theFinalMirage]
+      push $ SetCurrentAgendaDeck 1 []
+      toDiscard GameSource attrs
       pure a
     _ -> CollapseThePylons <$> liftRunMessage msg attrs

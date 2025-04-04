1d215e43d4014dd562948d56e62a0b78c0485326
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-05 07:24:43 +0900

    Deduction (2) fixes

--- backend/arkham-api/library/Arkham/Skill/Cards/Deduction2.hs
+++ backend/arkham-api/library/Arkham/Skill/Cards/Deduction2.hs
@@ -1,14 +1,9 @@
-module Arkham.Skill.Cards.Deduction2 where
+module Arkham.Skill.Cards.Deduction2 (deduction2) where
 
-import Arkham.Action qualified as Action
-import Arkham.Classes
-import Arkham.Discover
-import Arkham.Effect.Runner
-import Arkham.Investigator.Types (Field (..))
-import Arkham.Prelude
-import Arkham.Projection
+import Arkham.Helpers.SkillTest (withSkillTest, isInvestigation)
+import Arkham.Modifier
 import Arkham.Skill.Cards qualified as Cards
-import Arkham.Skill.Runner
+import Arkham.Skill.Import.Lifted
 
 newtype Deduction2 = Deduction2 SkillAttrs
   deriving anyclass (IsSkill, HasModifiersFor, HasAbilities)
@@ -18,29 +13,9 @@ deduction2 :: SkillCard Deduction2
 deduction2 = skill Deduction2 Cards.deduction2
 
 instance RunMessage Deduction2 where
-  runMessage msg s@(Deduction2 attrs@SkillAttrs {..}) = case msg of
-    PassedSkillTest iid (Just Action.Investigate) _ (SkillTarget sid) _ n | sid == skillId -> do
-      mlid <- field InvestigatorLocation iid
-      for_ mlid $ \lid -> do
-        effectMessage <- createCardEffect Cards.deduction2 (effectMetaTarget lid) attrs iid
-        pushAll $ replicate (if n >= 2 then 2 else 1) effectMessage
+  runMessage msg s@(Deduction2 attrs) = runQueueT $ case msg of
+    PassedSkillTest iid _ _ (isTarget attrs -> True) _ n -> do
+      whenM isInvestigation do
+        withSkillTest \sid -> skillTestModifier sid attrs iid $ DiscoveredClues $ if n >= 2 then 2 else 1
       pure s
-    _ -> Deduction2 <$> runMessage msg attrs
-
-newtype Deduction2Effect = Deduction2Effect EffectAttrs
-  deriving anyclass (HasAbilities, IsEffect, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
-
-deduction2Effect :: EffectArgs -> Deduction2Effect
-deduction2Effect = cardEffect Deduction2Effect Cards.deduction2
-
-instance RunMessage Deduction2Effect where
-  runMessage msg e@(Deduction2Effect attrs@EffectAttrs {..}) = case msg of
-    Successful (Action.Investigate, _) iid _ (LocationTarget lid) _ ->
-      case effectMetadata of
-        Just (EffectMetaTarget (LocationTarget lid')) | lid == lid' -> do
-          push $ DiscoverClues iid $ viaInvestigate $ discover lid (toSource attrs) 1
-          pure e
-        _ -> pure e
-    SkillTestEnds _ _ _ -> e <$ push (DisableEffect effectId)
-    _ -> Deduction2Effect <$> runMessage msg attrs
+    _ -> Deduction2 <$> liftRunMessage msg attrs

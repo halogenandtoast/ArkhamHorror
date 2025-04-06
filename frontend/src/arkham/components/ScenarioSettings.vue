16eb2b3e57e6e4b56f25efa20c7ee8b101bf1bb2
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-07 07:38:43 +0900

    More Standalone Fixes

--- frontend/src/arkham/components/ScenarioSettings.vue
+++ frontend/src/arkham/components/ScenarioSettings.vue
@@ -1,13 +1,11 @@
 <script lang="ts" setup>
-import { formatKey } from '@/arkham/types/Log';
-import { useI18n } from 'vue-i18n';
 import { watch, ref, computed } from 'vue'
 import scenarioJSON from '@/arkham/data/scenarios'
-import {toCapitalizedWords} from '@/arkham/helpers'
 import {updateStandaloneSettings} from '@/arkham/api'
 import { Game } from '../types/Game'
-import { Scenario, scenarioToKeyI18n } from '../types/Scenario'
+import { Scenario } from '../types/Scenario'
 import { StandaloneSetting, RecordedContent, SettingCondition } from '../types/StandaloneSetting'
+import ScenarioSetting from '@/arkham/components/ScenarioSetting.vue'
 
 const props = defineProps<{
   game: Game
@@ -15,7 +13,6 @@ const props = defineProps<{
   playerId: string
 }>()
 const standaloneSettings = ref<StandaloneSetting[]>([])
-const { t } = useI18n()
 
 // computed standaloneSettings is a bit of a hack, because nested values change by value
 // when we change standaloneSettings they are "cached" so to avoid this we deep copy the
@@ -71,13 +68,20 @@ const inactive = (cond: SettingCondition): boolean => {
   }
 
   if (cond.type === 'not') {
-    return inactive(cond.content)
+    return !inactive(cond.content)
+  }
+
+  if (cond.type === 'or') {
+    return !cond.content.some((c) => !inactive(c))
   }
 
   if (cond.type === 'option') {
     const k = standaloneSettings.value.find((s) => s.key === cond.key)
     if (!k) return false
     if (k.type === 'ToggleOption') {
+      if (k.ifRecorded) {
+        return !k.content && !k.ifRecorded.some((cond) => inactive(cond))
+      }
       return !k.content
     }
 
@@ -85,15 +89,6 @@ const inactive = (cond: SettingCondition): boolean => {
   }
 
   throw new Error(`Unknown condition type ${JSON.stringify(cond)}`)
-}
-
-const optionActive = (entry: RecordedContent) => {
-  const {ifRecorded} = entry
-  if (ifRecorded) {
-    return !ifRecorded.some((cond) => inactive(cond))
-  }
-
-  return true
 }
 
 const activeSettings = computed(() => {
@@ -106,11 +101,6 @@ const activeSettings = computed(() => {
     return true
   })
 })
-
-const displayKey = (key: string) => {
-  const prefix = scenarioToKeyI18n(props.scenario)
-  return t(formatKey({tag: prefix, contents: key}))
-}
 </script>
 
 <template>
@@ -120,160 +110,7 @@ const displayKey = (key: string) => {
       <p>There are currently no standalone settings available for this scenario.</p>
     </div>
     <div v-for="setting in activeSettings" :key="setting.key">
-      <div v-if="setting.type === 'ChooseRecord'" class="options">
-        <fieldset>
-          <legend>{{toCapitalizedWords(setting.label)}}</legend>
-          <template v-for="item in setting.content" :key="item.key">
-            <input
-              type="radio"
-              v-model="setting.selected"
-              :id="`${setting.key}${setting.label}${item.key}`"
-              :value="item.key"
-            />
-            <label :for="`${setting.key}${setting.label}${item.key}`"> {{toCapitalizedWords(item.key)}}</label>
-          </template>
-        </fieldset>
-      </div>
-      <div v-if="setting.type === 'ChooseNum'" class="options">
-        <input type="number" v-model="setting.content" :id="setting.key" :max="setting.max" :min="setting.min || 0" />
-        <label :for="setting.key"> {{toCapitalizedWords(setting.key)}}</label>
-      </div>
-      <div v-if="setting.type === 'ToggleKey'" class="options">
-        <input type="checkbox" v-model="setting.content" :id="setting.key"/>
-        <label :for="setting.key"> {{toCapitalizedWords(setting.key)}}</label>
-      </div>
-      <div v-else-if="setting.type === 'ToggleOption'" class="options">
-        <input type="checkbox" v-model="setting.content" :id="setting.key"/>
-        <label :for="setting.key"> {{toCapitalizedWords(setting.key)}}</label>
-      </div>
-      <div v-else-if="setting.type === 'PickKey'">
-        <h3 v-if="setting.content">{{toCapitalizedWords(setting.key)}}</h3>
-        <div class="options">
-          <template v-for="key in setting.keys" :key="`${setting.key}${key}`">
-            <input
-              type="radio"
-              v-model="setting.content"
-              :value="key"
-              :name="setting.key"
-              :id="`${setting.key}${key}`"
-              :checked="key === setting.content"
-            />
-            <label :for="`${setting.key}${key}`">{{displayKey(key)}}</label>
-          </template>
-        </div>
-      </div>
-      <div v-else-if="setting.type === 'ToggleCrossedOut'">
-        {{toCapitalizedWords(setting.key)}}
-        <div class="options">
-          <template v-for="option in setting.content" :key="option.key">
-            <input
-              type="checkbox"
-              v-model="option.content"
-              :id="`${option.key}${option.label}`"
-              class="invert"
-              :checked="option.content"
-            />
-            <label :for="`${option.key}${option.label}`">
-              <s v-if="option.content">{{option.label}}</s>
-              <span v-else>{{option.label}}</span>
-            </label>
-          </template>
-        </div>
-      </div>
-      <div v-else-if="setting.type === 'ToggleRecords'" class="records">
-        {{toCapitalizedWords(setting.key)}}
-        <div class="options">
-          <template v-for="option in setting.content" :key="option.key">
-            <input
-              type="checkbox"
-              v-model="option.content"
-              :id="`${setting.key}${option.key}`"
-              :checked="option.content"
-              v-if="optionActive(option)"
-            />
-            <label :for="`${setting.key}${option.key}`">{{option.label}}</label>
-          </template>
-        </div>
-      </div>
-      <div v-else-if="setting.type === 'SetPartnerKilled'" class="options">
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08720"
-          :id="`${setting.key}-kensler`"
-        />
-        <label :for="`${setting.key}-kensler`">Dr. Amy Kensler</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08714"
-          :id="`${setting.key}-dyer`"
-        />
-        <label :for="`${setting.key}-dyer`">Prof. William Dyer</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08715"
-          :id="`${setting.key}-danforth`"
-        />
-        <label :for="`${setting.key}-danforth`">Danforth</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08721"
-          :id="`${setting.key}-ellsworth`"
-        />
-        <label :for="`${setting.key}-ellsworth`">Roald Ellsworth</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08722"
-          :id="`${setting.key}-takada`"
-        />
-        <label :for="`${setting.key}-takada`">Takada Hiroko</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08718"
-          :id="`${setting.key}-claypool`"
-        />
-        <label :for="`${setting.key}-claypool`">Avery Claypool</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08717"
-          :id="`${setting.key}-sinha`"
-        />
-        <label :for="`${setting.key}-sinha`">Dr. Mala Sinha</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08719"
-          :id="`${setting.key}-cookie`"
-        />
-        <label :for="`${setting.key}-cookie`">James "Cookie" Fredericks</label>
-        <input
-          type="radio"
-          v-model="setting.content"
-          value="08716"
-          :id="`${setting.key}-ashevak`"
-        />
-        <label :for="`${setting.key}-ashevak`">Eliyah Ashevak</label>
-      </div>
-      <div v-else-if="setting.type === 'SetPartnerDetails'" class="partner">
-        <h3 class="partner-name">{{setting.key}}</h3>
-        <div class="options">
-          <label :for="`${setting.key}-damage`">Damage:</label>
-          <input type="number" v-model="setting.content.damage" :id="`${setting.key}-damage`" :min="0" :max="setting.maxDamage" />
-          <label :for="`${setting.key}-horror`">Horror:</label>
-          <input type="number" v-model="setting.content.horror" :id="`${setting.key}-horror`" :min="0" :max="setting.maxHorror" />
-          <select v-model="setting.content.status" :id="`${setting.key}-status`">
-            <option value="Safe">Safe</option>
-            <option value="Resolute">Resolute</option>
-            <option value="Eliminated">Eliminated</option>
-          </select>
-        </div>
-      </div>
+      <ScenarioSetting :setting="setting" :scenario="scenario" :game="game" :playerId="playerId" />
     </div>
     <button @click="submit">Begin</button>
   </div>

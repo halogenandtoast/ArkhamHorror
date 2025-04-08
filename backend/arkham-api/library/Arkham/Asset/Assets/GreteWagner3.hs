1ca6bc51bc31c3da977e35097bc4dbdd55982bbf
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-08 14:42:15 +0900

    Fix Grete Wagner (3)

--- backend/arkham-api/library/Arkham/Asset/Assets/GreteWagner3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/GreteWagner3.hs
@@ -2,12 +2,9 @@ module Arkham.Asset.Assets.GreteWagner3 (greteWagner3) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner hiding (EnemyDefeated)
-import Arkham.Discover
+import Arkham.Asset.Import.Lifted hiding (EnemyDefeated)
 import Arkham.Helpers.Modifiers
 import Arkham.Matcher
-import Arkham.Message qualified as Msg
-import Arkham.Prelude
 
 newtype GreteWagner3 = GreteWagner3 AssetAttrs
   deriving anyclass IsAsset
@@ -21,18 +18,18 @@ instance HasModifiersFor GreteWagner3 where
 
 instance HasAbilities GreteWagner3 where
   getAbilities (GreteWagner3 a) =
-    [ controlled
-        a
-        1
-        (ClueOnLocation <> youExist (InvestigatorCanDiscoverCluesAt YourLocation))
-        $ triggered
-          (EnemyDefeated #after You ByAny AnyEnemy)
-          (exhaust a <> DamageCost (toSource a) (toTarget a) 1)
+    [ controlled a 1 (AbleToDiscoverCluesAt (orConnected YourLocation))
+        $ triggered (EnemyDefeated #after You ByAny AnyEnemy) (exhaust a <> damageCost a 1)
     ]
 
 instance RunMessage GreteWagner3 where
-  runMessage msg a@(GreteWagner3 attrs) = case msg of
+  runMessage msg a@(GreteWagner3 attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      push $ Msg.DiscoverClues iid $ discoverAtYourLocation (toAbilitySource attrs 1) 1
+      discoverAtMatchingLocation
+        NotInvestigate
+        iid
+        (attrs.ability 1)
+        (orConnected $ locationWithInvestigator iid)
+        1
       pure a
-    _ -> GreteWagner3 <$> runMessage msg attrs
+    _ -> GreteWagner3 <$> liftRunMessage msg attrs

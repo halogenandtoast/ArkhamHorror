83922acab2ef8c48b6549ade40bf7e936de902d9
Jiduo Zhang <11661760+kidozh@users.noreply.github.com>
2025-04-13 06:42:50 +0100

    Zh locale for campaign Edge of the Earth Campaign (#1662)
    * add translation for zh campaign log
    
    * add translation for zh campaign log fully resolved
    
    * add zh-locale to base of eoe
    
    * add zh-locale to fatalMirage of eoe
    
    * add zh-locale to Heart of Madness of eoe
    
    * add zh-locale to Forbidden peaks of eoe
    
    * add zh-locale to ice and death of eoe
    
    * add to TS file so it appears in the code

--- frontend/src/locales/zh/edgeOfTheEarth.ts
+++ frontend/src/locales/zh/edgeOfTheEarth.ts
@@ -1,4 +1,8 @@
 import base from '@/locales/zh/edgeOfTheEarth/base.json'
 import iceAndDeath from '@/locales/zh/edgeOfTheEarth/scenarios/iceAndDeath.json'
+import fatalMirage from '@/locales/zh/edgeOfTheEarth/scenarios/fatalMirage.json'
+import toTheForbiddenPeaks from '@/locales/zh/edgeOfTheEarth/scenarios/toTheForbiddenPeaks.json'
+import cityOfTheElderThings from '@/locales/zh/edgeOfTheEarth/scenarios/cityOfTheElderThings.json'
+import theHeartOfMadness from '@/locales/zh/edgeOfTheEarth/scenarios/theHeartOfMadness.json'
 
-export default {...base, iceAndDeath}
+export default {...base, iceAndDeath, fatalMirage, toTheForbiddenPeaks, cityOfTheElderThings, theHeartOfMadness}

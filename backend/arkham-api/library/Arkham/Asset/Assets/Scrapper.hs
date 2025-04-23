245025e0302cf59daa1694340038826a79cb417b
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-23 21:11:04 +0900

    Fix Scrapper

--- backend/arkham-api/library/Arkham/Asset/Assets/Scrapper.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/Scrapper.hs
@@ -1,8 +1,9 @@
-module Arkham.Asset.Assets.Scrapper (scrapper, Scrapper (..)) where
+module Arkham.Asset.Assets.Scrapper (scrapper) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
+import Arkham.Helpers.SkillTest (withSkillTest)
 import Arkham.Matcher
 import Arkham.Modifier
 
@@ -17,18 +18,18 @@ instance HasAbilities Scrapper where
   getAbilities (Scrapper a) =
     [ withTooltip "{fast} Spend 1 resource: You get +1 {combat} for this skill test."
         $ wantsSkillTest (YourSkillTest #combat)
-        $ controlledAbility a 1 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
+        $ controlled a 1 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
     , withTooltip "{fast} Spend 1 resource: You get +1 {agility} for this skill test."
         $ wantsSkillTest (YourSkillTest #agility)
-        $ controlledAbility a 2 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
+        $ controlled a 2 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
     ]
 
 instance RunMessage Scrapper where
   runMessage msg a@(Scrapper attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      phaseModifier (attrs.ability 1) iid $ SkillModifier #combat 1
+      withSkillTest \sid -> skillTestModifier sid (attrs.ability 1) iid $ SkillModifier #combat 1
       pure a
     UseThisAbility iid (isSource attrs -> True) 2 -> do
-      phaseModifier (attrs.ability 2) iid $ SkillModifier #agility 1
+      withSkillTest \sid -> skillTestModifier sid (attrs.ability 2) iid $ SkillModifier #agility 1
       pure a
     _ -> Scrapper <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Asset/Assets/Scrapper3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/Scrapper3.hs
@@ -1,8 +1,9 @@
-module Arkham.Asset.Assets.Scrapper3 (scrapper3, Scrapper3 (..)) where
+module Arkham.Asset.Assets.Scrapper3 (scrapper3) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
+import Arkham.Helpers.SkillTest (withSkillTest)
 import Arkham.Matcher
 import Arkham.Modifier
 
@@ -17,18 +18,18 @@ instance HasAbilities Scrapper3 where
   getAbilities (Scrapper3 a) =
     [ withTooltip "{fast} Spend 1 resource: You get +1 {combat} for this skill test."
         $ wantsSkillTest (YourSkillTest #combat)
-        $ controlledAbility a 1 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
+        $ controlled a 1 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
     , withTooltip "{fast} Spend 1 resource: You get +1 {agility} for this skill test."
         $ wantsSkillTest (YourSkillTest #agility)
-        $ controlledAbility a 2 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
+        $ controlled a 2 (DuringSkillTest AnySkillTest) (FastAbility $ ResourceCost 1)
     ]
 
 instance RunMessage Scrapper3 where
   runMessage msg a@(Scrapper3 attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      phaseModifier (attrs.ability 1) iid $ SkillModifier #combat 1
+      withSkillTest \sid -> skillTestModifier sid (attrs.ability 1) iid $ SkillModifier #combat 1
       pure a
     UseThisAbility iid (isSource attrs -> True) 2 -> do
-      phaseModifier (attrs.ability 2) iid $ SkillModifier #agility 1
+      withSkillTest \sid -> skillTestModifier sid (attrs.ability 2) iid $ SkillModifier #agility 1
       pure a
     _ -> Scrapper3 <$> liftRunMessage msg attrs

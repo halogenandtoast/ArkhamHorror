b22dc52531b974ad6f84022ba5d949c4038d7510
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-25 08:13:18 +0900

    Fix Dr. Henry Armitage

--- backend/arkham-api/library/Arkham/Asset/Assets/DrHenryArmitage.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/DrHenryArmitage.hs
@@ -1,10 +1,10 @@
-module Arkham.Asset.Assets.DrHenryArmitage (DrHenryArmitage (..), drHenryArmitage) where
+module Arkham.Asset.Assets.DrHenryArmitage (drHenryArmitage) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
+import Arkham.Capability
 import Arkham.Matcher
-import Arkham.Prelude
 
 newtype DrHenryArmitage = DrHenryArmitage AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -15,14 +15,15 @@ drHenryArmitage = ally DrHenryArmitage Cards.drHenryArmitage (2, 2)
 
 instance HasAbilities DrHenryArmitage where
   getAbilities (DrHenryArmitage a) =
-    [ restrictedAbility a 1 ControlsThis
-        $ ReactionAbility (DrawCard #after You (basic DiscardableCard) (DeckOf You))
-        $ Costs [DiscardDrawnCardCost, exhaust a]
+    [ controlled a 1 (youExist can.gain.resources)
+        $ triggered
+          (DrawCard #after You (basic DiscardableCard) (DeckOf You))
+          (DiscardDrawnCardCost <> exhaust a)
     ]
 
 instance RunMessage DrHenryArmitage where
-  runMessage msg a@(DrHenryArmitage attrs) = case msg of
+  runMessage msg a@(DrHenryArmitage attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      push $ takeResources iid (attrs.ability 1) 3
+      gainResources iid (attrs.ability 1) 3
       pure a
-    _ -> DrHenryArmitage <$> runMessage msg attrs
+    _ -> DrHenryArmitage <$> liftRunMessage msg attrs

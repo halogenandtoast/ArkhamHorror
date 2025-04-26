6b4951abc06a669e97878cf6e6f8e00f8d3ec9c7
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-26 15:08:17 +0900

    Fix Logical Reasoning (4)

--- backend/arkham-api/library/Arkham/Event/Events/LogicalReasoning4.hs
+++ backend/arkham-api/library/Arkham/Event/Events/LogicalReasoning4.hs
@@ -1,13 +1,7 @@
-module Arkham.Event.Events.LogicalReasoning4 (
-  logicalReasoning4,
-  LogicalReasoning4 (..),
-) where
+module Arkham.Event.Events.LogicalReasoning4 (logicalReasoning4) where
 
-import Arkham.Prelude hiding (terror)
-
-import Arkham.Classes
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
 import Arkham.Helpers.Investigator
 import Arkham.Investigator.Types (Field (..))
 import Arkham.Matcher
@@ -22,37 +16,22 @@ logicalReasoning4 :: EventCard LogicalReasoning4
 logicalReasoning4 = event LogicalReasoning4 Cards.logicalReasoning4
 
 instance RunMessage LogicalReasoning4 where
-  runMessage msg e@(LogicalReasoning4 attrs) = case msg of
-    InvestigatorPlayEvent iid eid _ _ _ | eid == toId attrs -> do
+  runMessage msg e@(LogicalReasoning4 attrs) = runQueueT $ case msg of
+    PlayThisEvent iid (is attrs -> True) -> do
       n <- fieldMap InvestigatorClues (min 3) iid
-      pushAll $ replicate n $ ResolveEvent iid eid Nothing []
+      repeated n $ do_ msg
       pure e
-    ResolveEvent iid eid _ _ | eid == toId attrs -> do
+    Do (PlayThisEvent iid (is attrs -> True)) -> do
       iids <- select $ affectsOthers $ colocatedWith iid
-      options <- for iids $ \iid' -> do
-        canHealHorror <- canHaveHorrorHealed attrs iid'
-        terrors <- select $ TreacheryWithTrait Terror <> treacheryInThreatAreaOf iid'
-        player <- getPlayer iid'
-        pure
-          ( iid'
-          , player
-          , [Label "Heal 2 Horror" [HealHorror (toTarget iid') (toSource attrs) 2] | canHealHorror]
-              <> [ Label
-                  "Discard a Terror"
-                  [ chooseOne player
-                      $ targetLabels terrors
-                      $ only
-                      . toDiscardBy iid attrs
-                  ]
-                 | notNull terrors
-                 ]
-          )
-      let
-        choices = flip mapMaybe options $ \(iid', player, choices') ->
-          case choices' of
-            [] -> Nothing
-            _ -> Just $ targetLabel iid' [chooseOrRunOne player choices']
-      player <- getPlayer iid
-      pushWhen (notNull choices) $ chooseOrRunOne player choices
+      canHealHorror <- filterM (canHaveHorrorHealed attrs) iids
+      terrors <- select $ TreacheryWithTrait Terror <> at_ (locationWithInvestigator iid)
+
+      chooseOrRunOneM iid do
+        unless (null canHealHorror) do
+          labeled "Heal 2 Horror"
+            $ chooseOrRunOneM iid
+            $ targets canHealHorror \iid' -> healHorror iid' attrs 2
+        unless (null terrors) do
+          labeled "Discard a Terror" $ chooseTargetM iid terrors (toDiscardBy iid attrs)
       pure e
-    _ -> LogicalReasoning4 <$> runMessage msg attrs
+    _ -> LogicalReasoning4 <$> liftRunMessage msg attrs

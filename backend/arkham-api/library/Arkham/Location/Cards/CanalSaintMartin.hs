39bd0a6b409e70efb99cc5ab23d58e56ca3d4f4f
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-26 14:01:40 +0900

    Fix Canal Saint Martin

--- backend/arkham-api/library/Arkham/Location/Cards/CanalSaintMartin.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/CanalSaintMartin.hs
@@ -1,54 +1,34 @@
 module Arkham.Location.Cards.CanalSaintMartin (canalSaintMartin) where
 
 import Arkham.Ability
-import Arkham.Classes
 import Arkham.GameValue
+import Arkham.Helpers.Window
 import Arkham.Location.Cards qualified as Cards
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
 import Arkham.Matcher
 import Arkham.Matcher qualified as Matcher
-import Arkham.Prelude
-import Arkham.Timing qualified as Timing
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Move
 
 newtype CanalSaintMartin = CanalSaintMartin LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 canalSaintMartin :: LocationCard CanalSaintMartin
-canalSaintMartin =
-  location CanalSaintMartin Cards.canalSaintMartin 4 (PerPlayer 1)
+canalSaintMartin = location CanalSaintMartin Cards.canalSaintMartin 4 (PerPlayer 1)
 
 instance HasAbilities CanalSaintMartin where
-  getAbilities (CanalSaintMartin attrs) =
-    withBaseAbilities
-      attrs
-      [ limitedAbility (PlayerLimit PerRound 1)
-          $ restrictedAbility attrs 1 Here
-          $ ReactionAbility
-            ( Matcher.EnemyEvaded Timing.After You
-                $ EnemyAt (LocationWithId $ toId attrs)
-            )
-            Free
-      | locationRevealed attrs
-      ]
+  getAbilities (CanalSaintMartin a) =
+    extendRevealed1 a
+      $ playerLimit PerRound
+      $ restricted a 1 Here
+      $ freeReaction
+      $ Matcher.EnemyEvaded #after You (at_ $ be a)
 
 instance RunMessage CanalSaintMartin where
-  runMessage msg a@(CanalSaintMartin attrs) = case msg of
-    UseCardAbility iid source 1 _ _ | isSource attrs source -> do
-      enemies <-
-        selectMap EnemyTarget
-          $ EnemyAt (LocationWithId $ toId attrs)
-      connectingLocations <-
-        select
-          $ AccessibleFrom
-          $ LocationWithId
-          $ toId
-            attrs
-      player <- getPlayer iid
-      push $ chooseOrRunOne player $ do
-        e <- enemies
-        pure $ TargetLabel e $ do
-          l <- connectingLocations
-          pure $ chooseOrRunOne player [targetLabel l [MoveUntil l e]]
+  runMessage msg a@(CanalSaintMartin attrs) = runQueueT $ case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 (evadedEnemy -> eid) _ -> do
+      connectingLocations <- select $ ConnectedFrom (be attrs) <> LocationCanBeEnteredBy eid
+      chooseTargetM iid connectingLocations $ enemyMoveTo eid
       pure a
-    _ -> CanalSaintMartin <$> runMessage msg attrs
+    _ -> CanalSaintMartin <$> liftRunMessage msg attrs

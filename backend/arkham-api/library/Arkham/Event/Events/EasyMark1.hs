9bdba67d1db6f4bb0349766b03f3e9bd026c935f
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-28 15:04:29 +0900

    More fixes

--- backend/arkham-api/library/Arkham/Event/Events/EasyMark1.hs
+++ backend/arkham-api/library/Arkham/Event/Events/EasyMark1.hs
@@ -1,12 +1,9 @@
-module Arkham.Event.Events.EasyMark1 (easyMark1, EasyMark1 (..)) where
+module Arkham.Event.Events.EasyMark1 (easyMark1) where
 
 import Arkham.Ability
-import Arkham.Capability
-import Arkham.Classes
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
 import Arkham.Matcher
-import Arkham.Prelude
 
 newtype EasyMark1 = EasyMark1 EventAttrs
   deriving anyclass (IsEvent, HasModifiersFor)
@@ -17,21 +14,18 @@ easyMark1 = event EasyMark1 Cards.easyMark1
 
 instance HasAbilities EasyMark1 where
   getAbilities (EasyMark1 a) =
-    [ controlledAbility a 1 (youExist $ handWith Cards.easyMark1)
-        $ freeReaction (Arkham.Matcher.PlayEventDiscarding #after You $ EventWithId $ toId a)
+    [ controlled a 1 (youExist $ handWith Cards.easyMark1)
+        $ freeReaction (Arkham.Matcher.PlayEventDiscarding #after You (be a))
     ]
 
 instance RunMessage EasyMark1 where
-  runMessage msg e@(EasyMark1 attrs) = case msg of
-    PlayThisEvent iid eid | eid == toId attrs -> do
-      mDrawCards <- drawCardsIfCan iid attrs 1
-      gainResources <- can.gain.resources iid
-      pushAll
-        $ [takeResources iid attrs 2 | gainResources]
-        <> toList mDrawCards
+  runMessage msg e@(EasyMark1 attrs) = runQueueT $ case msg of
+    PlayThisEvent iid (is attrs -> True) -> do
+      gainResources iid attrs 2
+      drawCards iid attrs 1
       pure e
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       mcard <- selectOne $ inHandOf ForPlay iid <> basic (cardIs Cards.easyMark1)
-      for_ mcard $ push . putCardIntoPlay iid
+      for_ mcard $ putCardIntoPlay iid
       pure e
-    _ -> EasyMark1 <$> runMessage msg attrs
+    _ -> EasyMark1 <$> liftRunMessage msg attrs

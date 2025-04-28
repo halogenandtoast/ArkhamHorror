06124c18200728f6c35ebe3a0abee3855af170c8
Matthew Mongeau <halogenandtoast@gmail.com>
2025-04-28 15:04:29 +0900

    More fixes

--- backend/arkham-api/library/Arkham/Agenda/Cards/RestrictedAccess.hs
+++ backend/arkham-api/library/Arkham/Agenda/Cards/RestrictedAccess.hs
@@ -2,14 +2,11 @@ module Arkham.Agenda.Cards.RestrictedAccess (restrictedAccess) where
 
 import Arkham.Ability
 import Arkham.Agenda.Cards qualified as Cards
-import Arkham.Agenda.Runner
+import Arkham.Agenda.Import.Lifted
 import Arkham.Card
-import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Enemies
-import Arkham.GameValue
 import Arkham.Helpers.Query
 import Arkham.Matcher
-import Arkham.Prelude
 import Arkham.Scenarios.TheMiskatonicMuseum.Helpers
 import Arkham.Treachery.Cards qualified as Treacheries
 
@@ -22,53 +19,38 @@ restrictedAccess = agenda (1, A) RestrictedAccess Cards.restrictedAccess (Static
 
 instance HasAbilities RestrictedAccess where
   getAbilities (RestrictedAccess a) =
-    [ restricted a 1 (oneOf [thisExists a (AgendaWithSide A), IsReturnTo])
+    [ groupLimit PerTestOrAbility
+        $ restricted a 1 (oneOf [thisExists a (AgendaWithSide A), IsReturnTo])
         $ forced
         $ EnemySpawns #when Anywhere
         $ enemyIs Enemies.huntingHorror
     ]
 
 instance RunMessage RestrictedAccess where
-  runMessage msg a@(RestrictedAccess attrs) = case msg of
-    UseCardAbility _ (isSource attrs -> True) 1 _ _ -> do
-      mShadowSpawnedId <- selectOne $ treacheryIs Treacheries.shadowSpawned
-      case mShadowSpawnedId of
-        Just tid -> push $ PlaceResources (toAbilitySource attrs 1) (toTarget tid) 1
+  runMessage msg a@(RestrictedAccess attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      selectOne (treacheryIs Treacheries.shadowSpawned) >>= \case
+        Just tid -> placeTokens (attrs.ability 1) tid #resource 1
         Nothing -> do
           huntingHorror <- selectJust $ enemyIs Enemies.huntingHorror
           shadowSpawned <- getSetAsideCard Treacheries.shadowSpawned
           tid <- getRandom
           push $ AttachStoryTreacheryTo tid shadowSpawned (toTarget huntingHorror)
       pure a
-    AdvanceAgenda aid | aid == toId attrs && onSide B attrs -> do
-      leadInvestigatorId <- getLead
-      mHuntingHorrorId <- getInPlayHuntingHorror
-      case mHuntingHorrorId of
-        Just eid ->
-          pushAll
-            [ PlaceDoom (toSource attrs) (toTarget eid) 1
-            , advanceAgendaDeck attrs
-            ]
-        Nothing ->
-          push
-            $ FindEncounterCard
-              leadInvestigatorId
-              (toTarget attrs)
-              [FromEncounterDeck, FromEncounterDiscard, FromVoid]
-              (cardIs Enemies.huntingHorror)
+    AdvanceAgenda (isSide B attrs -> True) -> do
+      getInPlayHuntingHorror >>= \case
+        Just eid -> placeDoom attrs eid 1
+        Nothing -> do
+          lead <- getLead
+          findEncounterCardIn lead attrs (cardIs Enemies.huntingHorror) [#deck, #discard, #void]
+      advanceAgendaDeck attrs
       pure a
-    FoundEnemyInOutOfPlay VoidZone _ target eid | isTarget attrs target -> do
+    FoundEnemyInOutOfPlay VoidZone _ (isTarget attrs -> True) eid -> do
       lid <- selectJust $ LocationWithTitle "Museum Halls"
-      pushAll
-        [ EnemySpawnFromOutOfPlay VoidZone Nothing lid eid
-        , advanceAgendaDeck attrs
-        ]
+      push $ EnemySpawnFromOutOfPlay VoidZone Nothing lid eid
       pure a
-    FoundEncounterCard _ target ec | isTarget attrs target -> do
+    FoundEncounterCard _ (isTarget attrs -> True) ec -> do
       lid <- selectJust $ LocationWithTitle "Museum Halls"
-      pushAll
-        [ SpawnEnemyAt (EncounterCard ec) lid
-        , advanceAgendaDeck attrs
-        ]
+      push $ SpawnEnemyAt (EncounterCard ec) lid
       pure a
-    _ -> RestrictedAccess <$> runMessage msg attrs
+    _ -> RestrictedAccess <$> liftRunMessage msg attrs

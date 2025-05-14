27a466c5bdc57eba53c1592ead156b55eb9b59f5
Matthew Mongeau <halogenandtoast@gmail.com>
2025-05-15 08:01:01 +0900

    Fix Hot Pursuit

--- backend/arkham-api/library/Arkham/Agenda/Cards/HotPursuit.hs
+++ backend/arkham-api/library/Arkham/Agenda/Cards/HotPursuit.hs
@@ -1,12 +1,14 @@
-module Arkham.Agenda.Cards.HotPursuit (HotPursuit (..), hotPursuit) where
+module Arkham.Agenda.Cards.HotPursuit (hotPursuit) where
 
+import Arkham.Ability
 import Arkham.Agenda.Cards qualified as Cards
 import Arkham.Agenda.Import.Lifted
 import Arkham.Helpers.Modifiers (ModifierType (..), modifySelect)
 import Arkham.Matcher
+import Arkham.Scenarios.HorrorInHighGear.Helpers
 
 newtype HotPursuit = HotPursuit AgendaAttrs
-  deriving anyclass (IsAgenda, HasAbilities)
+  deriving anyclass IsAgenda
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 hotPursuit :: AgendaCard HotPursuit
@@ -16,11 +18,17 @@ instance HasModifiersFor HotPursuit where
   getModifiersFor (HotPursuit a) =
     modifySelect a (not_ $ InVehicleMatching AnyAsset) [AdditionalActionCostOf #move 2]
 
+instance HasAbilities HotPursuit where
+  getAbilities (HotPursuit a) = [mkAbility a 1 $ forced $ RoundEnds #when]
+
 instance RunMessage HotPursuit where
   runMessage msg a@(HotPursuit attrs) = runQueueT $ case msg of
     AdvanceAgenda (isSide B attrs -> True) -> do
       eachInvestigator \iid -> do
         sufferPhysicalTrauma iid 1
         investigatorDefeated attrs iid
+      pure a
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      advanceRoad
       pure a
     _ -> HotPursuit <$> liftRunMessage msg attrs

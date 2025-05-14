35e9db7df61096b207b101f715521ba074a93347
Matthew Mongeau <halogenandtoast@gmail.com>
2025-05-15 08:14:19 +0900

    Fix Blasphemous Visions, Fix resource border

--- backend/arkham-api/library/Arkham/Treachery/Cards/BlasphemousVisions.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/BlasphemousVisions.hs
@@ -1,5 +1,6 @@
 module Arkham.Treachery.Cards.BlasphemousVisions (blasphemousVisions) where
 
+import Arkham.Ability
 import Arkham.Campaigns.EdgeOfTheEarth.Helpers
 import Arkham.Card
 import Arkham.EncounterSet (EncounterSet (Tekelili))
@@ -10,7 +11,7 @@ import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
 newtype BlasphemousVisions = BlasphemousVisions TreacheryAttrs
-  deriving anyclass (IsTreachery, HasAbilities)
+  deriving anyclass IsTreachery
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 blasphemousVisions :: TreacheryCard BlasphemousVisions
@@ -18,8 +19,12 @@ blasphemousVisions = treachery BlasphemousVisions Cards.blasphemousVisions
 
 instance HasModifiersFor BlasphemousVisions where
   getModifiersFor (BlasphemousVisions a) = do
-    cards <- findAllCards (`cardMatch` (CardFromEncounterSet Tekelili))
+    cards <- findAllCards (`cardMatch` CardFromEncounterSet Tekelili)
     for_ cards \card -> modified_ a card [ResolveEffectsAgain]
+
+instance HasAbilities BlasphemousVisions where
+  getAbilities (BlasphemousVisions a) =
+    [skillTestAbility $ restricted a 1 OnSameLocation actionAbility]
 
 instance RunMessage BlasphemousVisions where
   runMessage msg t@(BlasphemousVisions attrs) = runQueueT $ case msg of
@@ -28,5 +33,12 @@ instance RunMessage BlasphemousVisions where
       unless (null cards) do
         addTekelili iid cards
         placeInThreatArea attrs iid
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      sid <- getRandom
+      beginSkillTest sid iid (attrs.ability 1) iid #willpower (Fixed 3)
+      pure t
+    PassedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
+      toDiscardBy iid (attrs.ability 1) attrs
       pure t
     _ -> BlasphemousVisions <$> liftRunMessage msg attrs

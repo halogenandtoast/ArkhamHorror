137e20da5460dc5e53a6d04f21858a3d27015717
푸른이 (derornos) <derornos@gmail.com>
2025-05-24 22:55:54 +0900

    feat: initial work for ko translations

---
+++ frontend/src/locales/ko/theCircleUndone.ts
@@ -0,0 +1,12 @@
+import base from '@/locales/ko/theCircleUndone/base.json'
+import disappearanceAtTheTwilightEstate from '@/locales/ko/theCircleUndone/scenarios/disappearanceAtTheTwilightEstate.json'
+import theWitchingHour from '@/locales/ko/theCircleUndone/scenarios/theWitchingHour.json'
+import atDeathsDoorstep from '@/locales/ko/theCircleUndone/scenarios/atDeathsDoorstep.json'
+import theSecretName from '@/locales/ko/theCircleUndone/scenarios/theSecretName.json'
+import theWagesOfSin from '@/locales/ko/theCircleUndone/scenarios/theWagesOfSin.json'
+import forTheGreaterGood from '@/locales/ko/theCircleUndone/scenarios/forTheGreaterGood.json'
+import unionAndDisillusion from '@/locales/ko/theCircleUndone/scenarios/unionAndDisillusion.json'
+import inTheClutchesOfChaos from '@/locales/ko/theCircleUndone/scenarios/inTheClutchesOfChaos.json'
+import beforeTheBlackThrone from '@/locales/ko/theCircleUndone/scenarios/beforeTheBlackThrone.json'
+
+export default {...base, disappearanceAtTheTwilightEstate, theWitchingHour, atDeathsDoorstep, theSecretName, theWagesOfSin, forTheGreaterGood, unionAndDisillusion, inTheClutchesOfChaos, beforeTheBlackThrone}
---
+++ frontend/src/locales/ko/theDreamEaters.ts
@@ -0,0 +1,11 @@
+import base from '@/locales/ko/theDreamEaters/base.json'
+import beyondTheGatesOfSleep from '@/locales/ko/theDreamEaters/scenarios/beyondTheGatesOfSleep.json'
+import wakingNightmare from '@/locales/ko/theDreamEaters/scenarios/wakingNightmare.json'
+import theSearchForKadath from '@/locales/ko/theDreamEaters/scenarios/theSearchForKadath.json'
+import aThousandShapesOfHorror from '@/locales/ko/theDreamEaters/scenarios/aThousandShapesOfHorror.json'
+import darkSideOfTheMoon from '@/locales/ko/theDreamEaters/scenarios/darkSideOfTheMoon.json'
+import pointOfNoReturn from '@/locales/ko/theDreamEaters/scenarios/pointOfNoReturn.json'
+import whereTheGodsDwell from '@/locales/ko/theDreamEaters/scenarios/whereTheGodsDwell.json'
+import weaverOfTheCosmos from '@/locales/ko/theDreamEaters/scenarios/weaverOfTheCosmos.json'
+
+export default {...base, beyondTheGatesOfSleep, wakingNightmare, theSearchForKadath, aThousandShapesOfHorror, darkSideOfTheMoon, pointOfNoReturn, whereTheGodsDwell, weaverOfTheCosmos}

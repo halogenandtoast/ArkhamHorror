e43980ef486b1f14f66f3146d1ee291b9a68dd73
Matthew Mongeau <halogenandtoast@gmail.com>
2025-05-29 06:39:31 +0900

    Fix the onyx castle

--- backend/arkham-api/library/Arkham/Game.hs
+++ backend/arkham-api/library/Arkham/Game.hs
@@ -1303,6 +1303,7 @@ getTreacheriesMatching matcher = do
     TreacheryIsAttachedTo target -> \treachery -> do
       let treacheryTarget = treacheryAttachedTarget (toAttrs treachery)
       pure $ treacheryTarget == Just target
+    SignatureTreachery -> pure . isSignature . toCardDef
     TreacheryInHandOf investigatorMatcher -> \treachery -> do
       iids <- select investigatorMatcher
       pure $ case treachery.placement of
@@ -3097,6 +3098,7 @@ enemyMatcherFilter es matcher' = case matcher' of
   AnyEnemy -> pure es
   EnemyIs cardCode -> pure $ filter ((== cardCode) . toCardCode) es
   NonWeaknessEnemy -> pure $ filter (isNothing . cdCardSubType . toCardDef) es
+  SignatureEnemy -> pure $ filter (isSignature . toCardDef) es
   EnemyInHandOf investigatorMatcher -> do
     iids <- select investigatorMatcher
     pure $ flip filter es \enemy -> do
--- backend/arkham-api/library/Arkham/Location/Cards/TheOnyxCastle.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/TheOnyxCastle.hs
@@ -1,11 +1,12 @@
-module Arkham.Location.Cards.TheOnyxCastle (theOnyxCastle, TheOnyxCastle (..)) where
+module Arkham.Location.Cards.TheOnyxCastle (theOnyxCastle) where
 
 import Arkham.Ability
 import Arkham.Helpers.Modifiers (ModifierType (..), modifySelf)
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
 import Arkham.Matcher
-import Arkham.Message qualified as Msg
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Placement
 import Arkham.Placement qualified as Placement
 
 newtype TheOnyxCastle = TheOnyxCastle LocationAttrs
@@ -19,34 +20,31 @@ instance HasModifiersFor TheOnyxCastle where
   getModifiersFor (TheOnyxCastle a) = whenUnrevealed a $ modifySelf a [Blocked]
 
 instance HasAbilities TheOnyxCastle where
-  getAbilities (TheOnyxCastle attrs) =
-    extendRevealed
-      attrs
-      [ restrictedAbility
-          attrs
-          1
-          (exists (NotYou <> investigatorAt attrs.id) <> exists (TreacheryInHandOf You))
-          actionAbility
-      ]
+  getAbilities (TheOnyxCastle a) =
+    extendRevealed1 a
+      $ restricted
+        a
+        1
+        ( exists (NotYou <> at_ (be a))
+            <> oneOf
+              [ exists (TreacheryInHandOf You <> not_ SignatureTreachery)
+              , exists (EnemyInHandOf You <> not_ SignatureEnemy)
+              ]
+        )
+        actionAbility
 
 instance RunMessage TheOnyxCastle where
   runMessage msg l@(TheOnyxCastle attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       investigators <- select $ not_ (InvestigatorWithId iid) <> investigatorAt attrs
-      treacheries <- select $ TreacheryInHandOf $ InvestigatorWithId iid
-      player <- getPlayer iid
-      chooseOrRunOne
-        iid
-        [ targetLabel
-          treachery
-          [ Msg.chooseOrRunOne
-              player
-              [ targetLabel investigator [PlaceTreachery treachery (Placement.HiddenInHand investigator)]
-              | investigator <- investigators
-              ]
-          ]
-        | treachery <- treacheries
-        ]
-
+      treacheries <- select $ treacheryInHandOf iid <> not_ SignatureTreachery
+      enemies <- select $ enemyInHandOf iid <> not_ SignatureEnemy
+      chooseOrRunOneM iid do
+        targets treacheries \treachery ->
+          chooseOneM iid do
+            targets investigators $ place treachery . Placement.HiddenInHand
+        targets enemies \enemy ->
+          chooseOneM iid do
+            targets investigators $ place enemy . Placement.HiddenInHand
       pure l
     _ -> TheOnyxCastle <$> liftRunMessage msg attrs

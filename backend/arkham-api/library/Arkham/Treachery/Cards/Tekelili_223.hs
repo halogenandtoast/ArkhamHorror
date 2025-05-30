2ace6cce6c1020828b1abe505a1cedd9a6042c29
Matthew Mongeau <halogenandtoast@gmail.com>
2025-05-30 10:49:08 +0900

    Fix Blasphemous Visions, Fix Molly Maxwell

--- backend/arkham-api/library/Arkham/Treachery/Cards/Tekelili_223.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Tekelili_223.hs
@@ -1,8 +1,6 @@
 module Arkham.Treachery.Cards.Tekelili_223 (tekelili_223) where
 
 import Arkham.Campaigns.EdgeOfTheEarth.Helpers
-import Arkham.Card
-import Arkham.Helpers.Modifiers (ModifierType (..), hasModifier)
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -16,7 +14,7 @@ tekelili_223 = treachery Tekelili_223 Cards.tekelili_223
 instance RunMessage Tekelili_223 where
   runMessage msg t@(Tekelili_223 attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
-      n <- ifM_ (hasModifier (toCard attrs) ResolveEffectsAgain) 2 1
+      n <- ifM_ (getShouldResolveEffectsAgain iid attrs) 2 1
       repeated n $ assignHorror iid attrs 1
       resolveTekelili iid attrs
       pure t
--- backend/arkham-api/library/Arkham/Treachery/Cards/Tekelili_228.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Tekelili_228.hs
@@ -1,8 +1,6 @@
-module Arkham.Treachery.Cards.Tekelili_228 (tekelili_228, Tekelili_228 (..)) where
+module Arkham.Treachery.Cards.Tekelili_228 (tekelili_228) where
 
 import Arkham.Campaigns.EdgeOfTheEarth.Helpers
-import Arkham.Card
-import Arkham.Helpers.Modifiers (ModifierType (..), hasModifier)
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -16,7 +14,7 @@ tekelili_228 = treachery Tekelili_228 Cards.tekelili_228
 instance RunMessage Tekelili_228 where
   runMessage msg t@(Tekelili_228 attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
-      n <- ifM_ (hasModifier (toCard attrs) ResolveEffectsAgain) 2 1
+      n <- ifM_ (getShouldResolveEffectsAgain iid attrs) 2 1
       repeated n $ placeCluesOnLocation iid attrs 1
       resolveTekelili iid attrs
       pure t

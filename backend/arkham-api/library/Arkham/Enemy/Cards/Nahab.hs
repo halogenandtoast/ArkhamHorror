1f99a0aa0d161d002e3e9da6ec39f636be35e5c9
Matthew Mongeau <halogenandtoast@gmail.com>
2025-06-07 09:55:17 +0900

    Fix Nahab engagement

--- backend/arkham-api/library/Arkham/Enemy/Cards/Nahab.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/Nahab.hs
@@ -1,19 +1,11 @@
-module Arkham.Enemy.Cards.Nahab (
-  nahab,
-  Nahab (..),
-) where
+module Arkham.Enemy.Cards.Nahab (nahab) where
 
-import Arkham.Prelude
-
-import Arkham.Classes
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Runner hiding (EnemyFight)
+import Arkham.Enemy.Import.Lifted
 import Arkham.Helpers.Agenda
 import Arkham.Helpers.Modifiers
 import Arkham.Matcher
-import Arkham.Token
-
--- Do not remove doom from Nahab when the agenda advances.
 
 newtype Nahab = Nahab EnemyAttrs
   deriving anyclass IsEnemy
@@ -22,22 +14,22 @@ newtype Nahab = Nahab EnemyAttrs
 nahab :: EnemyCard Nahab
 nahab = enemy Nahab Cards.nahab (1, PerPlayer 1, 3) (1, 2)
 
+-- Do not remove doom from Nahab when the agenda advances.
 instance HasModifiersFor Nahab where
   getModifiersFor (Nahab attrs) = do
     agendaStep <- getCurrentAgendaStep
     modifySelf attrs [DoNotRemoveDoom, EnemyFight agendaStep]
 
 instance HasAbilities Nahab where
-  getAbilities (Nahab attrs) =
-    withBaseAbilities attrs
-      $ [ restrictedAbility attrs 1 (EnemyCriteria $ ThisEnemy ReadyEnemy)
-            $ ForcedAbility
-            $ PhaseBegins #after #enemy
-        ]
+  getAbilities (Nahab a) =
+    extend1 a
+      $ restricted a 1 (EnemyCriteria $ ThisEnemy ReadyEnemy)
+      $ forced
+      $ PhaseBegins #after #enemy
 
 instance RunMessage Nahab where
-  runMessage msg e@(Nahab attrs) = case msg of
-    UseCardAbility _ (isSource attrs -> True) 1 _ _ -> do
-      push $ PlaceTokens (toSource attrs) (toTarget attrs) Doom 1
+  runMessage msg e@(Nahab attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      placeDoom attrs attrs 1
       pure e
-    _ -> Nahab <$> runMessage msg attrs
+    _ -> Nahab <$> liftRunMessage msg attrs

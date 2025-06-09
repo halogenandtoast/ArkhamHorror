12289b1fe089f37c4975a0be754587c9b8fd1e87
SHOrTEVA <37376947+SHOrTEVA@users.noreply.github.com>
2025-06-09 14:21:43 +0800

    Adjust card overlay layout and behavior (#2137)
    - Hand cards (elements with class list containing "in-hand") retain their original overlay behavior.
    - Other cards (e.g., location, act, agenda) now keep the overlay visible until the user clicks anywhere.
    - Sideways cards now support overflow with scroll.

--- frontend/src/arkham/components/HandCard.vue
+++ frontend/src/arkham/components/HandCard.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
-import { computed, inject, ref, Ref } from 'vue'
+import { computed, inject, Ref } from 'vue'
 import { CardContents, type Card } from '@/arkham/types/Card'
 import type { Game } from '@/arkham/types/Game'
-import { useDebug } from '@/arkham/debug';
 import type { AbilityLabel, AbilityMessage, Message } from '@/arkham/types/Message'
 import { MessageType} from '@/arkham/types/Message'
 import { imgsrc } from '@/arkham/helpers'
@@ -17,9 +16,6 @@ export interface Props {
 }
 
 const props = defineProps<Props>()
-
-const debug = useDebug()
-const dragging = ref(false)
 
 const investigator = computed(() => Object.values(props.game.investigators).find((i) => i.playerId === props.playerId))
 const investigatorId = computed(() => investigator.value?.id)
@@ -100,15 +96,6 @@ const image = computed(() => {
   const mutatedSuffix = mutated ? `_${mutated}` : ''
   return imgsrc(`cards/${cardCode.replace('c', '')}${mutatedSuffix}.avif`);
 })
-
-function startDrag(event: DragEvent) {
-  dragging.value = true
-  if (event.dataTransfer) {
-    console.log('dragging', id.value)
-    event.dataTransfer.effectAllowed = 'move'
-    event.dataTransfer.setData('text/plain', JSON.stringify({ "tag": "CardIdTarget", "contents": id.value }))
-  }
-}
 
 /*
 const painted = computed(() => {
@@ -235,12 +222,10 @@ function oilPaintEffect(canvas, radius, intensity) {
   <div class="card-container" :data-index="id" v-if="solo || (investigatorId == ownerId) || revealed">
     <img
       :class="classObject"
-      class="card"
+      class="card in-hand"
       :src="image"
       :data-customizations="JSON.stringify(card.contents.customizations)"
       @click="$emit('choose', cardAction)"
-      @dragstart="startDrag($event)"
-      :draggable="debug.active"
     />
 
     <AbilityButton
@@ -254,7 +239,7 @@ function oilPaintEffect(canvas, radius, intensity) {
 
   </div>
   <div class="card-container" v-else>
-    <img class="card" :src="cardBack" />
+    <img class="card in-hand" :src="cardBack" />
   </div>
 </template>
 

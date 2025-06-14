fc306374dc01f3f369d877a40699ce0007a2e96f
Matthew Mongeau <halogenandtoast@gmail.com>
2025-06-14 11:33:00 +0900

    RTTBB locations

--- backend/arkham-api/library/Arkham/Location/Cards/ReturnToXochimilco.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/ReturnToXochimilco.hs
@@ -1,7 +1,9 @@
 module Arkham.Location.Cards.ReturnToXochimilco (returnToXochimilco) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
 
 newtype ReturnToXochimilco = ReturnToXochimilco LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +13,15 @@ returnToXochimilco :: LocationCard ReturnToXochimilco
 returnToXochimilco = symbolLabel $ location ReturnToXochimilco Cards.returnToXochimilco 3 (Static 0)
 
 instance HasAbilities ReturnToXochimilco where
-  getAbilities (ReturnToXochimilco attrs) =
-    extendRevealed attrs []
+  getAbilities (ReturnToXochimilco a) =
+    extendRevealed1 a
+      $ mkAbility a 1
+      $ freeReaction
+      $ SkillTestResult #after (You <> at_ (be a)) (oneOf [#fighting, #evading]) #success
 
 instance RunMessage ReturnToXochimilco where
-  runMessage msg (ReturnToXochimilco attrs) = runQueueT $ case msg of
+  runMessage msg l@(ReturnToXochimilco attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ Explore iid (attrs.ability 1) $ CardWithPrintedLocationSymbol $ locationSymbol attrs
+      pure l
     _ -> ReturnToXochimilco <$> liftRunMessage msg attrs

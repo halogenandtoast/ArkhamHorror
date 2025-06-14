14efc29092c0314bff9dfb944e4e738b08a0541b
foodiemonster <hyo.exe@gmail.com>
2025-06-14 14:55:42 +0100

    Add difficulty-specific chaos bag tokens

---
+++ backend/arkham-api/library/Arkham/Scenarios/TheMidwinterGala/FlavorText.hs
@@ -0,0 +1,86 @@
+module Arkham.Scenarios.TheMidwinterGala.FlavorText where
+
+import Arkham.Prelude
+
+import Arkham.Text
+
+intro1 :: FlavorText
+intro1 = FlavorText (Just "The Midwinter Gala") [BasicEntry "TODO"]
+
+intro2 :: FlavorText
+intro2 = FlavorText (Just "The Midwinter Gala") [BasicEntry "TODO"]
+
+intro3 :: FlavorText
+intro3 = FlavorText (Just "The Midwinter Gala") [BasicEntry "TODO"]
+
+intro4 :: FlavorText
+intro4 = FlavorText (Just "The Midwinter Gala") [BasicEntry "TODO"]
+
+-- placeholder flavor texts for additional story elements
+
+theTruth1 :: FlavorText
+theTruth1 = FlavorText (Just "The Truth 1") [BasicEntry "TODO"]
+
+theTruth2 :: FlavorText
+theTruth2 = FlavorText (Just "The Truth 2") [BasicEntry "TODO"]
+
+theTruth3 :: FlavorText
+theTruth3 = FlavorText (Just "The Truth 3") [BasicEntry "TODO"]
+
+theTruth4 :: FlavorText
+theTruth4 = FlavorText (Just "The Truth 4") [BasicEntry "TODO"]
+
+theTruth5 :: FlavorText
+theTruth5 = FlavorText (Just "The Truth 5") [BasicEntry "TODO"]
+
+theTruth6 :: FlavorText
+theTruth6 = FlavorText (Just "The Truth 6") [BasicEntry "TODO"]
+
+theTruth7 :: FlavorText
+theTruth7 = FlavorText (Just "The Truth 7") [BasicEntry "TODO"]
+
+alienDeviceAndTimeWornLocket :: FlavorText
+alienDeviceAndTimeWornLocket = FlavorText (Just "If Alien Device and Time-Worn Locket are in play") [BasicEntry "TODO"]
+
+alienDeviceAndSinisterSolution :: FlavorText
+alienDeviceAndSinisterSolution = FlavorText (Just "If Alien Device and Sinister Solution are in play") [BasicEntry "TODO"]
+
+alienDeviceAndManagersKey :: FlavorText
+alienDeviceAndManagersKey = FlavorText (Just "If Alien Device and Manager\'s Key are in play") [BasicEntry "TODO"]
+
+alienDeviceAndTomeOfRituals :: FlavorText
+alienDeviceAndTomeOfRituals = FlavorText (Just "If Alien Device and Tome of Rituals are in play") [BasicEntry "TODO"]
+
+timeWornLocketAndSinisterSolution :: FlavorText
+timeWornLocketAndSinisterSolution = FlavorText (Just "If Time-Worn Locket and Sinister Solution are in play") [BasicEntry "TODO"]
+
+timeWornLocketAndManagersKey :: FlavorText
+timeWornLocketAndManagersKey = FlavorText (Just "If Time-Worn Locket and Manager\'s Key are in play") [BasicEntry "TODO"]
+
+timeWornLocketAndTomeOfRituals :: FlavorText
+timeWornLocketAndTomeOfRituals = FlavorText (Just "If Time-Worn Locket and Tome of Rituals are in play") [BasicEntry "TODO"]
+
+sinisterSolutionAndManagersKey :: FlavorText
+sinisterSolutionAndManagersKey = FlavorText (Just "If Sinister Solution and Manager\'s Key are in play") [BasicEntry "TODO"]
+
+sinisterSolutionAndTomeOfRituals :: FlavorText
+sinisterSolutionAndTomeOfRituals = FlavorText (Just "If Sinister Solution and Tome of Rituals are in play") [BasicEntry "TODO"]
+
+managersKeyAndTomeOfRituals :: FlavorText
+managersKeyAndTomeOfRituals = FlavorText (Just "If Manager\'s Key and Tome of Rituals are in play") [BasicEntry "TODO"]
+
+noResolution :: FlavorText
+noResolution = FlavorText Nothing [BasicEntry "TODO"]
+
+noResolutionResigned :: FlavorText
+noResolutionResigned = FlavorText Nothing [BasicEntry "TODO"]
+
+resolution1 :: FlavorText
+resolution1 = FlavorText (Just "Resolution 1") [BasicEntry "TODO"]
+
+resolution2 :: FlavorText
+resolution2 = FlavorText (Just "Resolution 2") [BasicEntry "TODO"]
+
+resolution3 :: FlavorText
+resolution3 = FlavorText (Just "Resolution 3") [BasicEntry "TODO"]
+

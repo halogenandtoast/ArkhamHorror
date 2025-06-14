69d8f41723d48e0456c352c34e03a786901c674d
Matthew Mongeau <halogenandtoast@gmail.com>
2025-06-14 10:43:43 +0900

    Makefile for languages

--- backend/arkham-api/library/Arkham/Location/Cards/ReturnToTempleRuins.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/ReturnToTempleRuins.hs
@@ -1,7 +1,9 @@
 module Arkham.Location.Cards.ReturnToTempleRuins (returnToTempleRuins) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
 
 newtype ReturnToTempleRuins = ReturnToTempleRuins LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +13,14 @@ returnToTempleRuins :: LocationCard ReturnToTempleRuins
 returnToTempleRuins = symbolLabel $ location ReturnToTempleRuins Cards.returnToTempleRuins 5 (Static 0)
 
 instance HasAbilities ReturnToTempleRuins where
-  getAbilities (ReturnToTempleRuins attrs) =
-    extendRevealed attrs []
+  getAbilities (ReturnToTempleRuins a) =
+    extendRevealed1 a
+      $ playerLimit PerRound
+      $ mkAbility a 1 (freeReaction $ SkillTestResult #after You (whileInvestigating a) #success)
 
 instance RunMessage ReturnToTempleRuins where
-  runMessage msg (ReturnToTempleRuins attrs) = runQueueT $ case msg of
+  runMessage msg l@(ReturnToTempleRuins attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      push $ Explore iid (attrs.ability 1) $ CardWithPrintedLocationSymbol $ locationSymbol attrs
+      pure l
     _ -> ReturnToTempleRuins <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Location/Cards/TempleRuins.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/TempleRuins.hs
@@ -1,32 +1,25 @@
-module Arkham.Location.Cards.TempleRuins (templeRuins, TempleRuins (..)) where
+module Arkham.Location.Cards.TempleRuins (templeRuins) where
 
 import Arkham.Ability
 import Arkham.GameValue
 import Arkham.Location.Cards qualified as Cards
-import Arkham.Location.Runner
+import Arkham.Location.Import.Lifted
 import Arkham.Matcher
-import Arkham.Prelude
 
 newtype TempleRuins = TempleRuins LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 templeRuins :: LocationCard TempleRuins
-templeRuins = locationWith TempleRuins Cards.templeRuins 4 (Static 0) (labelL .~ "circle")
+templeRuins = symbolLabel $ location TempleRuins Cards.templeRuins 4 (Static 0)
 
 instance HasAbilities TempleRuins where
-  getAbilities (TempleRuins attrs) =
-    withRevealedAbilities
-      attrs
-      [ restrictedAbility attrs 1 Here
-          $ ActionAbility [#explore]
-          $ ActionCost 2
-      ]
+  getAbilities (TempleRuins a) =
+    extendRevealed1 a $ restricted a 1 Here $ ActionAbility [#explore] $ ActionCost 2
 
 instance RunMessage TempleRuins where
-  runMessage msg l@(TempleRuins attrs) = case msg of
+  runMessage msg l@(TempleRuins attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      let source = toAbilitySource attrs 1
-      push $ Explore iid source $ CardWithPrintedLocationSymbol $ locationSymbol attrs
+      push $ Explore iid (attrs.ability 1) $ CardWithPrintedLocationSymbol $ locationSymbol attrs
       pure l
-    _ -> TempleRuins <$> runMessage msg attrs
+    _ -> TempleRuins <$> liftRunMessage msg attrs

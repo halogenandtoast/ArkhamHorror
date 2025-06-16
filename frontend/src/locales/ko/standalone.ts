a53d707ea55bcd88f140d014b70674c9b4281448
foodiemonster <hyo.exe@gmail.com>
2025-06-17 00:53:47 +0100

    Add The Midwinter Gala standalone scenario

--- frontend/src/locales/ko/standalone.ts
+++ frontend/src/locales/ko/standalone.ts
@@ -1,5 +1,6 @@
 import curseOfTheRougarou from '@/locales/ko/standalone/curseOfTheRougarou.json'
 import carnevaleOfHorrors from '@/locales/ko/standalone/carnevaleOfHorrors.json'
 import murderAtTheExcelsiorHotel from '@/locales/ko/standalone/murderAtTheExcelsiorHotel.json'
+import theMidwinterGala from '@/locales/ko/standalone/theMidwinterGala.json'
 
-export default {curseOfTheRougarou, carnevaleOfHorrors, murderAtTheExcelsiorHotel}
+export default {curseOfTheRougarou, carnevaleOfHorrors, murderAtTheExcelsiorHotel, theMidwinterGala}

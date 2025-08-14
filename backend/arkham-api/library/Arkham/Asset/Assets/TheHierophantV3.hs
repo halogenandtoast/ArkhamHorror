9e0563cac6887335b6a0d30668cdddf7ba954082
Matthew Mongeau <halogenandtoast@gmail.com>
2025-08-14 16:21:26 -0400

    Some slot fixes

--- backend/arkham-api/library/Arkham/Asset/Assets/TheHierophantV3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/TheHierophantV3.hs
@@ -2,11 +2,10 @@ module Arkham.Asset.Assets.TheHierophantV3 (theHierophantV3) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
+import Arkham.Asset.Import.Lifted
 import Arkham.Card
 import Arkham.Helpers.Modifiers
 import Arkham.Matcher
-import Arkham.Prelude
 import Arkham.Slot
 
 newtype TheHierophantV3 = TheHierophantV3 AssetAttrs
@@ -17,9 +16,7 @@ theHierophantV3 :: AssetCard TheHierophantV3
 theHierophantV3 = asset TheHierophantV3 Cards.theHierophantV3
 
 instance HasAbilities TheHierophantV3 where
-  getAbilities (TheHierophantV3 a) =
-    [ restrictedAbility a 1 InYourHand $ freeReaction (GameBegins #when)
-    ]
+  getAbilities (TheHierophantV3 a) = [restricted a 1 InYourHand $ freeReaction (GameBegins #when)]
 
 instance HasModifiersFor TheHierophantV3 where
   getModifiersFor (TheHierophantV3 attrs) =
@@ -30,11 +27,11 @@ instance HasModifiersFor TheHierophantV3 where
       ]
 
 instance RunMessage TheHierophantV3 where
-  runMessage msg a@(TheHierophantV3 attrs) = case msg of
+  runMessage msg a@(TheHierophantV3 attrs) = runQueueT $ case msg of
     InHand _ (UseThisAbility iid (isSource attrs -> True) 1) -> do
-      push $ putCardIntoPlay iid attrs
+      putCardIntoPlay iid attrs
       pure a
     CardIsEnteringPlay iid card | toCardId card == toCardId attrs -> do
       push $ AddSlot iid ArcaneSlot (Slot (toSource attrs) [])
-      TheHierophantV3 <$> runMessage msg attrs
-    _ -> TheHierophantV3 <$> runMessage msg attrs
+      TheHierophantV3 <$> liftRunMessage msg attrs
+    _ -> TheHierophantV3 <$> liftRunMessage msg attrs

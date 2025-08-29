b03d5797db53592b570f711c629cfb7d969f5ba9
Matthew Mongeau <halogenandtoast@gmail.com>
2025-08-29 09:39:38 +0900

    French

---
+++ frontend/src/locales/fr/theDreamEaters.ts
@@ -0,0 +1,11 @@
+import base from '@/locales/en/theDreamEaters/base.json'
+import beyondTheGatesOfSleep from '@/locales/en/theDreamEaters/scenarios/beyondTheGatesOfSleep.json'
+import wakingNightmare from '@/locales/en/theDreamEaters/scenarios/wakingNightmare.json'
+import theSearchForKadath from '@/locales/en/theDreamEaters/scenarios/theSearchForKadath.json'
+import aThousandShapesOfHorror from '@/locales/en/theDreamEaters/scenarios/aThousandShapesOfHorror.json'
+import darkSideOfTheMoon from '@/locales/en/theDreamEaters/scenarios/darkSideOfTheMoon.json'
+import pointOfNoReturn from '@/locales/en/theDreamEaters/scenarios/pointOfNoReturn.json'
+import whereTheGodsDwell from '@/locales/en/theDreamEaters/scenarios/whereTheGodsDwell.json'
+import weaverOfTheCosmos from '@/locales/en/theDreamEaters/scenarios/weaverOfTheCosmos.json'
+
+export default {...base, beyondTheGatesOfSleep, wakingNightmare, theSearchForKadath, aThousandShapesOfHorror, darkSideOfTheMoon, pointOfNoReturn, whereTheGodsDwell, weaverOfTheCosmos}

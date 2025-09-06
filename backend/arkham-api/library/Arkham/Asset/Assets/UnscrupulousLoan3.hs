3bead8f9896df56b32d95770a3afaf3c502b9f2c
Matthew Mongeau <halogenandtoast@gmail.com>
2025-09-06 17:29:32 +0900

    Fix Evanescent Mist, Escape the Cage, Unscrupulous Loan

--- backend/arkham-api/library/Arkham/Asset/Assets/UnscrupulousLoan3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/UnscrupulousLoan3.hs
@@ -1,8 +1,4 @@
-module Arkham.Asset.Assets.UnscrupulousLoan3 (
-  unscrupulousLoan3,
-  UnscrupulousLoan3 (..),
-)
-where
+module Arkham.Asset.Assets.UnscrupulousLoan3 (unscrupulousLoan3) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
@@ -17,26 +13,21 @@ newtype UnscrupulousLoan3 = UnscrupulousLoan3 AssetAttrs
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 unscrupulousLoan3 :: AssetCard UnscrupulousLoan3
-unscrupulousLoan3 = asset UnscrupulousLoan3 Cards.unscrupulousLoan3
+unscrupulousLoan3 = assetWith UnscrupulousLoan3 Cards.unscrupulousLoan3 cannotLeavePlay
 
 instance HasAbilities UnscrupulousLoan3 where
   getAbilities (UnscrupulousLoan3 a) =
-    controlledAbility
-      a
-      1
-      (youExist can.gain.resources)
-      (freeReaction $ PlayCard #after You $ isThisCard a)
-      : [ controlledAbility a 2 (youExist $ InvestigatorWithResources $ lessThan 10)
-          $ ForcedAbility (eliminationWindow iid)
+    controlled a 1 (youExist can.gain.resources) (freeReaction $ PlayCard #after You $ isThisCard a)
+      : [ controlled a 2 (youExist $ InvestigatorWithResources $ lessThan 10) $ forced (eliminationWindow iid)
         | iid <- toList a.owner
         ]
 
 instance RunMessage UnscrupulousLoan3 where
   runMessage msg a@(UnscrupulousLoan3 attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      gainResourcesIfCan iid (attrs.ability 1) 10
+      gainResources iid (attrs.ability 1) 10
       pure a
     UseThisAbility _iid (isSource attrs -> True) 2 -> do
-      push $ Exile (toTarget attrs)
+      exile attrs
       pure a
     _ -> UnscrupulousLoan3 <$> liftRunMessage msg attrs

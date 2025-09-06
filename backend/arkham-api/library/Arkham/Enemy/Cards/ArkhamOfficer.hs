2fced72177c069b7e6943a5bf183c3a210b209bc
Matthew Mongeau <halogenandtoast@gmail.com>
2025-09-06 19:51:15 +0900

    Fixes

--- backend/arkham-api/library/Arkham/Enemy/Cards/ArkhamOfficer.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/ArkhamOfficer.hs
@@ -1,16 +1,13 @@
-module Arkham.Enemy.Cards.ArkhamOfficer (
-  arkhamOfficer,
-  ArkhamOfficer (..),
-)
-where
+module Arkham.Enemy.Cards.ArkhamOfficer (arkhamOfficer) where
 
-import Arkham.Prelude
-
-import Arkham.Classes
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Runner
+import Arkham.Enemy.Import.Lifted
+import Arkham.Enemy.Types (Field (..))
+import Arkham.Helpers.SkillTest.Lifted (parley)
 import Arkham.Matcher
 import Arkham.Message qualified as Msg
+import Arkham.Message.Lifted.Choose
 import Arkham.Projection
 import Arkham.Token
 
@@ -22,38 +19,35 @@ arkhamOfficer :: EnemyCard ArkhamOfficer
 arkhamOfficer = enemy ArkhamOfficer Cards.arkhamOfficer (3, Static 3, 2) (1, 0)
 
 instance HasAbilities ArkhamOfficer where
-  getAbilities (ArkhamOfficer attrs) =
-    withBaseAbilities
-      attrs
-      [ restrictedAbility attrs 1 (exists $ locationWithEnemy (toId attrs) <> LocationWithAnyClues)
-          $ ForcedAbility
+  getAbilities (ArkhamOfficer a) =
+    extend
+      a
+      [ restricted a 1 (exists $ locationWithEnemy a <> LocationWithAnyClues)
+          $ forced
           $ PhaseEnds #when #enemy
-      , skillTestAbility $ restrictedAbility attrs 2 OnSameLocation parleyAction_
+      , skillTestAbility $ restricted a 2 OnSameLocation parleyAction_
       ]
 
 instance RunMessage ArkhamOfficer where
-  runMessage msg e@(ArkhamOfficer attrs) = case msg of
+  runMessage msg e@(ArkhamOfficer attrs) = runQueueT $ case msg of
     UseThisAbility _ (isSource attrs -> True) 1 -> do
       location <- selectJust $ locationWithEnemy (toId attrs)
-      pushAll
-        [MovedClues (attrs.ability 1) (toSource location) (toTarget attrs) 1, FlipClues (toTarget attrs) 1]
+      push $ Msg.MovedClues (attrs.ability 1) (toSource location) (toTarget attrs) 1
+      flipCluesToDoom attrs 1
       pure e
     UseThisAbility iid (isSource attrs -> True) 2 -> do
       sid <- getRandom
-      push $ parley sid iid (attrs.ability 2) iid #willpower (Fixed 3)
+      parley sid iid (attrs.ability 2) attrs #willpower (Fixed 3)
       pure e
     PassedThisSkillTest iid (isAbilitySource attrs 2 -> True) -> do
-      player <- getPlayer iid
-      let exhausted = enemyExhausted attrs
       doom <- fieldMap EnemyTokens (countTokens #doom) (toId attrs)
 
-      pushWhen (not exhausted || doom > 0)
-        $ chooseOrRunOne player
-        $ [Label "Automatically evade Arkham Officer" [Msg.EnemyEvaded iid (toId attrs)] | not exhausted]
-        <> [ Label
-            "Flip one of its doom to its clue side and take control of it."
-            [FlipDoom (toTarget attrs) 1, MovedClues (attrs.ability 2) (toSource attrs) (toTarget iid) 1]
-           | doom > 0
-           ]
+      chooseOrRunOneM iid do
+        unless attrs.exhausted do
+          labeled "Automatically evade Arkham Officer" $ automaticallyEvadeEnemy iid attrs
+        when (doom > 0) do
+          labeled "Flip one of its doom to its clue side and take control of it." do
+            flipDoomToClues attrs 1
+            push $ Msg.MovedClues (attrs.ability 2) (toSource attrs) (toTarget iid) 1
       pure e
-    _ -> ArkhamOfficer <$> runMessage msg attrs
+    _ -> ArkhamOfficer <$> liftRunMessage msg attrs

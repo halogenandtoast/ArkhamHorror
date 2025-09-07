f918ddcc474f56fe87037686a908a26c10fdec5f
Matthew Mongeau <halogenandtoast@gmail.com>
2025-09-07 20:37:49 +0900

    Fix multiplayer can fight fast situation

--- backend/arkham-api/library/Arkham/Game.hs
+++ backend/arkham-api/library/Arkham/Game.hs
@@ -3363,7 +3363,8 @@ enemyMatcherFilter es matcher' = do
       sources <- select sourceMatcher
       nubOrdOn (.id) . concat <$> traverse (enemyMatcherFilter es . CanFightEnemy) sources
     CanFightEnemy source -> do
-      iid <- view activeInvestigatorIdL <$> getGame
+      miid <- getSourceController source
+      iid <- maybe (view activeInvestigatorIdL <$> getGame) pure miid
       modifiers' <- getModifiers iid
       let cannotAttackEnemy e = CannotAttackEnemy e.id `elem` modifiers'
       let es' = filter (not . cannotAttackEnemy) es

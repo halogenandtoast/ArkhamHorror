461f707c85999ba2255a56d9de2d4bfe3208484e
Matthew Mongeau <halogenandtoast@gmail.com>
2025-09-12 08:34:35 +0900

    Fix Aloof, Hydra Deep in Slumber

--- backend/arkham-api/library/Arkham/Enemy/Cards/Lloigor.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/Lloigor.hs
@@ -3,7 +3,6 @@ module Arkham.Enemy.Cards.Lloigor (lloigor) where
 import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
-import Arkham.ForMovement
 import Arkham.Helpers.Message.Discard.Lifted
 import Arkham.Helpers.Modifiers (ModifierType (..), modifySelfWhen)
 import Arkham.Keyword (Keyword (Aloof))
@@ -14,31 +13,24 @@ newtype Lloigor = Lloigor EnemyAttrs
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 lloigor :: EnemyCard Lloigor
-lloigor =
-  enemyWith Lloigor Cards.lloigor (3, Static 5, 4) (1, 1)
-    $ preyL
-    .~ Prey FewestCardsInHand
+lloigor = enemyWith Lloigor Cards.lloigor (3, Static 5, 4) (1, 1) (preyL .~ Prey FewestCardsInHand)
 
 instance HasModifiersFor Lloigor where
   getModifiersFor (Lloigor a) = do
-    valid <-
-      selectAny $ InvestigatorAt (locationWithEnemy a.id) <> HandWith (LengthIs $ EqualTo $ Static 0)
+    valid <- selectAny $ at_ (locationWithEnemy a.id) <> HandWith (LengthIs $ EqualTo $ Static 0)
     modifySelfWhen a valid [RemoveKeyword Aloof]
 
 instance HasAbilities Lloigor where
   getAbilities (Lloigor a) =
     extend1 a
-      $ restricted
-        a
-        1
-        (exists $ at_ (orConnected NotForMovement $ locationWithEnemy a) <> HandWith AnyCards)
+      $ restricted a 1 (exists $ at_ (orConnected_ $ locationWithEnemy a) <> HandWith AnyCards)
       $ forced
       $ PhaseEnds #when #enemy
 
 instance RunMessage Lloigor where
   runMessage msg e@(Lloigor attrs) = runQueueT $ case msg of
     UseThisAbility _ (isSource attrs -> True) 1 -> do
-      selectEach (at_ (orConnected NotForMovement $ locationWithEnemy attrs) <> HandWith AnyCards) \iid -> do
+      selectEach (at_ (orConnected_ $ locationWithEnemy attrs) <> HandWith AnyCards) \iid -> do
         randomDiscard iid (attrs.ability 1)
       pure e
     _ -> Lloigor <$> liftRunMessage msg attrs

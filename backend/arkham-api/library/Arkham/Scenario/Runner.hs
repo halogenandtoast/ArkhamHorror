6632e718752fd93fceedb68608154c69d2e3584a
Matthew Mongeau <halogenandtoast@gmail.com>
2025-09-22 19:50:19 +0900

    Fix label

--- backend/arkham-api/library/Arkham/Scenario/Runner.hs
+++ backend/arkham-api/library/Arkham/Scenario/Runner.hs
@@ -1098,12 +1098,13 @@ runScenarioAttrs msg a@ScenarioAttrs {..} = runQueueT $ case msg of
                           <> [ShuffleDeck deck | shouldShuffle && length targetCards == length foundCards]
                           <> [after]
             ReturnCards -> do
+              -- When only one card we want to prompt the user before putting back
               unless (all null (toList targetCards)) do
                 pushAll
-                  [ PreSearchFound iid Nothing deck (concat $ toList targetCards)
-                  , After (PreSearchFound iid Nothing deck (concat $ toList targetCards))
-                  , chooseOne player [Label "Done" []]
-                  ]
+                  $ [ PreSearchFound iid Nothing deck (concat $ toList targetCards)
+                    , After (PreSearchFound iid Nothing deck (concat $ toList targetCards))
+                    ]
+                  <> [chooseOne player [Label "Done" []] | length (toList targetCards) > 1]
       _ -> pure ()
     pure a
   After (PreSearchFound iid mSearchTarget Deck.EncounterDeck _) -> do

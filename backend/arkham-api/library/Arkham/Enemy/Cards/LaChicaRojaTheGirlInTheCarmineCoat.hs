c240e8837554d7e6013e290ff987a8487aeff677
Matthew Mongeau <halogenandtoast@gmail.com>
2025-10-17 20:10:48 +0900

    Some enemies

--- backend/arkham-api/library/Arkham/Enemy/Cards/LaChicaRojaTheGirlInTheCarmineCoat.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/LaChicaRojaTheGirlInTheCarmineCoat.hs
@@ -1,15 +1,32 @@
 module Arkham.Enemy.Cards.LaChicaRojaTheGirlInTheCarmineCoat (laChicaRojaTheGirlInTheCarmineCoat) where
 
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
+import Arkham.Matcher
 
 newtype LaChicaRojaTheGirlInTheCarmineCoat = LaChicaRojaTheGirlInTheCarmineCoat EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 laChicaRojaTheGirlInTheCarmineCoat :: EnemyCard LaChicaRojaTheGirlInTheCarmineCoat
-laChicaRojaTheGirlInTheCarmineCoat = enemy LaChicaRojaTheGirlInTheCarmineCoat Cards.laChicaRojaTheGirlInTheCarmineCoat (3, Static 2, 5) (1, 1)
+laChicaRojaTheGirlInTheCarmineCoat =
+  enemy
+    LaChicaRojaTheGirlInTheCarmineCoat
+    Cards.laChicaRojaTheGirlInTheCarmineCoat
+    (3, Static 2, 5)
+    (1, 1)
+
+instance HasAbilities LaChicaRojaTheGirlInTheCarmineCoat where
+  getAbilities (LaChicaRojaTheGirlInTheCarmineCoat a) =
+    extend1 a
+      $ mkAbility a 1
+      $ forced
+      $ Moves #when (You <> investigatorEngagedWith a) AnySource Anywhere Anywhere
 
 instance RunMessage LaChicaRojaTheGirlInTheCarmineCoat where
-  runMessage msg (LaChicaRojaTheGirlInTheCarmineCoat attrs) = runQueueT $ case msg of
+  runMessage msg e@(LaChicaRojaTheGirlInTheCarmineCoat attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      disengageEnemy iid attrs
+      pure e
     _ -> LaChicaRojaTheGirlInTheCarmineCoat <$> liftRunMessage msg attrs

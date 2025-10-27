acd35321c1cd030ac6f0b7fbc58f2ac8048f4f4b
Matthew Mongeau <halogenandtoast@gmail.com>
2025-10-28 06:41:42 +0900

    Fix Undertow

--- backend/arkham-api/library/Arkham/Treachery/Cards/Undertow.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Undertow.hs
@@ -1,6 +1,8 @@
-module Arkham.Treachery.Cards.Undertow (undertow, Undertow (..)) where
+module Arkham.Treachery.Cards.Undertow (undertow) where
 
 import Arkham.Ability
+import Arkham.Campaigns.TheInnsmouthConspiracy.Helpers
+import Arkham.Location.FloodLevel
 import Arkham.Matcher
 import Arkham.Message.Lifted.Choose
 import Arkham.Treachery.Cards qualified as Cards
@@ -15,16 +17,16 @@ undertow = treachery Undertow Cards.undertow
 
 instance HasAbilities Undertow where
   getAbilities (Undertow a) =
-    [ restrictedAbility a 1 (InThreatAreaOf You) $ forced $ Moves #after You AnySource Anywhere Anywhere
-    , skillTestAbility $ restrictedAbility a 2 OnSameLocation $ FastAbility $ HandDiscardCost 1 #any
+    [ restricted a 1 (InThreatAreaOf You) $ forced $ Moves #after You AnySource Anywhere Anywhere
+    , skillTestAbility $ restricted a 2 OnSameLocation $ FastAbility $ HandDiscardCost 1 #any
     ]
 
 instance RunMessage Undertow where
   runMessage msg t@(Undertow attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
-      selectAny (locationWithInvestigator iid <> FloodedLocation) >>= \case
-        True -> placeInThreatArea attrs iid
-        False -> gainSurge attrs
+      getFloodLevelFor iid >>= \case
+        Unflooded -> gainSurge attrs
+        _ -> placeInThreatArea attrs iid
       pure t
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       assignDamageAndHorror iid (attrs.ability 1) 2 2
@@ -32,9 +34,7 @@ instance RunMessage Undertow where
       pure t
     UseThisAbility iid (isSource attrs -> True) 2 -> do
       sid <- getRandom
-      chooseOneM iid do
-        for_ [#combat, #agility] $ \skill ->
-          skillLabeled skill $ beginSkillTest sid iid (attrs.ability 2) iid skill (Fixed 3)
+      chooseBeginSkillTest sid iid (attrs.ability 2) iid [#combat, #agility] (Fixed 3)
       pure t
     PassedThisSkillTest iid (isAbilitySource attrs 2 -> True) -> do
       toDiscardBy iid (attrs.ability 2) attrs

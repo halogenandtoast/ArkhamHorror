6e0b1f2b516e958402f69076f84ebe88dc403a44
Matthew Mongeau <halogenandtoast@gmail.com>
2025-10-30 10:05:13 +0900

    Evade/Spawn shenanigans Fix
    Disc of Itzamna evades, but the enemy MAYBE shouldn't engage you

--- backend/arkham-api/library/Arkham/Asset/Assets/DiscOfItzamna.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/DiscOfItzamna.hs
@@ -3,8 +3,9 @@ module Arkham.Asset.Assets.DiscOfItzamna where
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
-import Arkham.DamageEffect
+import Arkham.I18n
 import Arkham.Matcher hiding (EnemyEvaded, NonAttackDamageEffect)
+import Arkham.Message.Lifted.Choose
 import Arkham.Window (Window (..))
 import Arkham.Window qualified as Window
 
@@ -17,20 +18,18 @@ discOfItzamna = asset DiscOfItzamna Cards.discOfItzamna
 
 instance HasAbilities DiscOfItzamna where
   getAbilities (DiscOfItzamna a) =
-    [ restrictedAbility a 1 ControlsThis
-        $ ReactionAbility (EnemySpawns #when YourLocation NonEliteEnemy) (discardCost a)
-    ]
+    [controlled_ a 1 $ triggered (EnemySpawns #when YourLocation NonEliteEnemy) (discardCost a)]
 
 instance RunMessage DiscOfItzamna where
   runMessage msg a@(DiscOfItzamna attrs) = runQueueT $ case msg of
     UseCardAbility iid (isSource attrs -> True) 1 (map windowType -> [Window.EnemySpawns eid _]) _ -> do
       canDamage <- eid <=~> EnemyCanBeDamagedBySource (attrs.ability 1)
       canEvade <- eid <=~> EnemyCanBeEvadedBy (attrs.ability 1)
-      when (canDamage || canEvade) do
-        chooseOrRunOne
-          iid
-          [ Label "Evade enemy" [EnemyEvaded iid eid]
-          , Label "Deal 2 damage" [EnemyDamage eid $ nonAttack (Just iid) attrs 2]
-          ]
+      card <- fetchCard eid
+      chooseOrRunOneM iid $ withI18n do
+        cardNameVar card $ labeledValidate' canEvade "automaticallyEvade" $ automaticallyEvadeEnemy iid eid
+        countVar 2
+          $ labeledValidate' canDamage "dealDamage"
+          $ nonAttackEnemyDamage (Just iid) (attrs.ability 1) 2 eid
       pure a
     _ -> DiscOfItzamna <$> liftRunMessage msg attrs

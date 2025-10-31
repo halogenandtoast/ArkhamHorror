331553812bbd4f220f7946f8af1c4a598f17b71c
Matthew Mongeau <halogenandtoast@gmail.com>
2025-10-31 13:41:10 +0900

    Fix Astral Travel
    Keep track of correct investigator

--- backend/arkham-api/library/Arkham/Event/Events/AstralTravel.hs
+++ backend/arkham-api/library/Arkham/Event/Events/AstralTravel.hs
@@ -1,6 +1,5 @@
 module Arkham.Event.Events.AstralTravel (astralTravel) where
 
-import Arkham.ChaosBag.RevealStrategy
 import Arkham.ChaosToken
 import Arkham.Cost
 import Arkham.Event.Cards qualified as Cards
@@ -8,7 +7,6 @@ import Arkham.Event.Import.Lifted
 import Arkham.Helpers.Location
 import Arkham.Helpers.Message qualified as Msg
 import Arkham.Matcher hiding (MoveAction)
-import Arkham.RequestedChaosTokenStrategy
 import Arkham.Trait qualified as Trait
 import Arkham.Window qualified as Window
 
@@ -23,19 +21,19 @@ instance RunMessage AstralTravel where
   runMessage msg e@(AstralTravel attrs) = runQueueT $ case msg of
     PlayThisEvent iid eid | eid == toId attrs -> do
       locations <- getCanMoveToMatchingLocations iid attrs RevealedLocation
-      chooseOne iid [targetLabel lid [MoveAction iid lid Free False] | lid <- locations]
-      push $ RequestChaosTokens (toSource attrs) Nothing (Reveal 1) SetAside
+      chooseOneM iid $ targets locations \lid ->  push $ MoveAction iid lid Free False
+      requestChaosTokens iid attrs 1
       pure e
-    RequestedChaosTokens (isSource attrs -> True) _ tokens -> do
+    RequestedChaosTokens (isSource attrs -> True) (Just iid) tokens -> do
       let faces = [Skull, Cultist, Tablet, ElderThing, AutoFail]
       when (any ((`elem` faces) . chaosTokenFace) tokens) do
-        assets <- select $ oneOf $ AssetWithTrait <$> [Trait.Item, Trait.Ally]
-        player <- getPlayer attrs.owner
+        assets <- select $ assetControlledBy iid <> mapOneOf AssetWithTrait [Trait.Item, Trait.Ally]
+        player <- getPlayer iid
         push
           $ If (Window.RevealChaosTokenEventEffect attrs.owner tokens attrs.id)
           $ case assets of
             [] -> [Msg.assignDamage attrs.owner attrs 1]
             xs -> [Msg.chooseOne player $ targetLabels xs $ only . Msg.toDiscardBy attrs.controller attrs]
-      push $ ResetChaosTokens (toSource attrs)
+      resetChaosTokens attrs
       pure e
     _ -> AstralTravel <$> liftRunMessage msg attrs

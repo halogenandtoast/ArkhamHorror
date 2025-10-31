5ddf3cd9b1ef305d536ea7db6db9898a65eb02ab
Matthew Mongeau <halogenandtoast@gmail.com>
2025-10-31 10:57:57 +0900

    Fix Bewitching (3)
    Cards are unique by title, not card code

--- backend/arkham-api/library/Arkham/Asset/Assets/Bewitching3.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/Bewitching3.hs
@@ -8,10 +8,9 @@ import Arkham.Card
 import Arkham.Helpers.Modifiers (getAdditionalSearchTargets)
 import Arkham.Matcher hiding (PlaceUnderneath)
 import Arkham.Message.Lifted.Choose
+import Arkham.Name (toTitle)
 import Arkham.Strategy
 import Arkham.Trait qualified as Trait
-import Data.Function (on)
-import Data.List (nubBy)
 
 newtype Bewitching3 = Bewitching3 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -34,15 +33,21 @@ instance RunMessage Bewitching3 where
       search iid attrs iid [fromDeck] #any (defer attrs IsNotDraw)
       pure a
     SearchFound iid (isTarget attrs -> True) _ cards -> do
-      let tricks = nubBy ((==) `on` toCardCode) $ filterCards (CardWithTrait Trait.Trick) cards
+      let tricks = filterCards (CardWithTrait Trait.Trick) cards
       additionalTargets <- getAdditionalSearchTargets iid
       if null tricks
         then focusCards cards $ chooseOneM iid $ labeled "No cards found" nothing
-        else do
-          chooseUpToNM iid (3 + additionalTargets) "Choose no more Trick cards" do
-            targets tricks \card -> do
-              push $ RemoveCardFromSearch iid (toCardId card)
-              push $ PlaceUnderneath (toTarget attrs) [card]
+        else doStep (3 + additionalTargets) msg
+      pure a
+    DoStep n msg'@(SearchFound iid (isTarget attrs -> True) _ cards) | n > 0 -> do
+      let underTitles = map toTitle attrs.cardsUnderneath
+      let tricks = cards & filterCards (CardWithTrait Trait.Trick) & filter ((`notElem` underTitles) . toTitle)
+      unless (null tricks) do
+        chooseUpToNM iid 1 "Choose no more Trick cards" do
+          targets tricks \card -> do
+            push $ RemoveCardFromSearch iid card.id
+            push $ PlaceUnderneath (toTarget attrs) [card]
+            doStep (n - 1) msg'
       pure a
     UseThisAbility iid (isSource attrs -> True) 2 -> do
       canSearch <- can.search.deck iid
@@ -53,7 +58,7 @@ instance RunMessage Bewitching3 where
           labeled
             "Search the top 9 cards of your deck for a copy of an attached card, draw it, and shuffle your deck"
             do
-              let cardMatcher = mapOneOf (CardWithCardCode . toCardCode) attrs.cardsUnderneath
+              let cardMatcher = mapOneOf (CardWithTitle . toTitle) attrs.cardsUnderneath
               search iid attrs iid [fromTopOfDeck 9] (basic cardMatcher) (DrawFound iid 1)
       pure a
     _ -> Bewitching3 <$> liftRunMessage msg attrs

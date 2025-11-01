dccd05a0ac85eab7694cfe892213d31ee7923408
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-01 20:44:41 +0900

    Some TSK stuff

--- backend/arkham-api/library/Arkham/Act/Cards/SearchForTheManuscript.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/SearchForTheManuscript.hs
@@ -3,8 +3,11 @@ module Arkham.Act.Cards.SearchForTheManuscript (searchForTheManuscript) where
 import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted
+import Arkham.Campaigns.TheScarletKeys.Concealed.Helpers
+import Arkham.Enemy.Cards qualified as Enemies
 import Arkham.Matcher
 import Arkham.Modifier
+import Arkham.Placement
 
 newtype SearchForTheManuscript = SearchForTheManuscript ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
@@ -33,6 +36,12 @@ instance RunMessage SearchForTheManuscript where
       advanceVia #other attrs (attrs.ability 1)
       pure a
     AdvanceAct (isSide B attrs -> True) _ _ -> do
+      cultists <- select $ NonWeaknessEnemy <> #cultist <> not_ (EnemyWithPlacement InTheShadows)
+      lead <- getLead
+      for_ cultists (resolveConcealed lead)
+      shuffleSetAsideIntoEncounterDeck
+        $ mapOneOf cardIs [Enemies.umbralHarbinger, Enemies.emissaryFromYuggoth]
+      shuffleEncounterDiscardBackIn
       advanceActDeck attrs
       pure a
     _ -> SearchForTheManuscript <$> liftRunMessage msg attrs

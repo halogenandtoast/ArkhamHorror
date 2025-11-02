5612ddad7852b79982b48128bccca4a509e78309
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-02 17:14:01 +0900

    Galata, Obelisk of Theodosius

--- backend/arkham-api/library/Arkham/Location/Cards/Galata.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/Galata.hs
@@ -1,7 +1,11 @@
 module Arkham.Location.Cards.Galata (galata) where
 
+import Arkham.Ability
+import Arkham.Campaigns.TheScarletKeys.Concealed.Helpers
+import Arkham.Campaigns.TheScarletKeys.Concealed.Matcher
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Choose
 
 newtype Galata = Galata LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +15,16 @@ galata :: LocationCard Galata
 galata = symbolLabel $ location Galata Cards.galata 3 (PerPlayer 1)
 
 instance HasAbilities Galata where
-  getAbilities (Galata attrs) =
-    extendRevealed attrs []
+  getAbilities (Galata a) =
+    extendRevealed1 a
+      $ groupLimit PerRound
+      $ restricted a 1 (Here <> exists ConcealedCardAny)
+      $ FastAbility' (ResourceCost 3) [#parley]
 
 instance RunMessage Galata where
-  runMessage msg (Galata attrs) = runQueueT $ case msg of
+  runMessage msg l@(Galata attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      concealed <- selectMap (.id) ConcealedCardAny
+      chooseTargetM iid concealed $ exposeConcealed iid (attrs.ability 1)
+      pure l
     _ -> Galata <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Location/Cards/ObeliskOfTheodosius.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/ObeliskOfTheodosius.hs
@@ -1,7 +1,10 @@
 module Arkham.Location.Cards.ObeliskOfTheodosius (obeliskOfTheodosius) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 
 newtype ObeliskOfTheodosius = ObeliskOfTheodosius LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +14,16 @@ obeliskOfTheodosius :: LocationCard ObeliskOfTheodosius
 obeliskOfTheodosius = symbolLabel $ location ObeliskOfTheodosius Cards.obeliskOfTheodosius 3 (PerPlayer 1)
 
 instance HasAbilities ObeliskOfTheodosius where
-  getAbilities (ObeliskOfTheodosius attrs) =
-    extendRevealed attrs []
+  getAbilities (ObeliskOfTheodosius a) =
+    extendRevealed1 a
+      $ restricted a 1 (Here <> youExist InvestigatorWithAnyClues <> exists (enemy_ #cultist))
+      $ forced
+      $ SkillTestResult #after You (WhileInvestigating (be a)) #failure
 
 instance RunMessage ObeliskOfTheodosius where
-  runMessage msg (ObeliskOfTheodosius attrs) = runQueueT $ case msg of
+  runMessage msg l@(ObeliskOfTheodosius attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      cultists <- select $ NearestEnemyToFallback iid #cultist
+      chooseTargetM iid cultists $ moveTokensTo (attrs.ability 1) iid #clue 1
+      pure l
     _ -> ObeliskOfTheodosius <$> liftRunMessage msg attrs

559b8829cf398bb2235b273a9ab9ccc5dd1387f4
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-03 17:30:33 +0900

    Sinister Aspirant (A, B, C)

--- backend/arkham-api/library/Arkham/Enemy/Cards/SinisterAspirantA.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/SinisterAspirantA.hs
@@ -1,15 +1,23 @@
 module Arkham.Enemy.Cards.SinisterAspirantA (sinisterAspirantA) where
 
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
+import Arkham.Matcher
 
 newtype SinisterAspirantA = SinisterAspirantA EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 sinisterAspirantA :: EnemyCard SinisterAspirantA
 sinisterAspirantA = enemy SinisterAspirantA Cards.sinisterAspirantA (2, Static 3, 4) (0, 1)
 
+instance HasAbilities SinisterAspirantA where
+  getAbilities (SinisterAspirantA a) = extend1 a $ restricted a 1 (thisExists a ReadyEnemy) $ forced $ PhaseEnds #when #enemy
+
 instance RunMessage SinisterAspirantA where
-  runMessage msg (SinisterAspirantA attrs) = runQueueT $ case msg of
+  runMessage msg e@(SinisterAspirantA attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      placeDoom (attrs.ability 1) attrs 1
+      pure e
     _ -> SinisterAspirantA <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Enemy/Cards/SinisterAspirantC.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/SinisterAspirantC.hs
@@ -1,15 +1,23 @@
 module Arkham.Enemy.Cards.SinisterAspirantC (sinisterAspirantC) where
 
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
+import Arkham.Matcher
 
 newtype SinisterAspirantC = SinisterAspirantC EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 sinisterAspirantC :: EnemyCard SinisterAspirantC
 sinisterAspirantC = enemy SinisterAspirantC Cards.sinisterAspirantC (2, Static 3, 4) (0, 1)
 
+instance HasAbilities SinisterAspirantC where
+  getAbilities (SinisterAspirantC a) = extend1 a $ restricted a 1 (thisExists a ReadyEnemy) $ forced $ PhaseEnds #when #enemy
+
 instance RunMessage SinisterAspirantC where
-  runMessage msg (SinisterAspirantC attrs) = runQueueT $ case msg of
+  runMessage msg e@(SinisterAspirantC attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      placeDoom (attrs.ability 1) attrs 1
+      pure e
     _ -> SinisterAspirantC <$> liftRunMessage msg attrs

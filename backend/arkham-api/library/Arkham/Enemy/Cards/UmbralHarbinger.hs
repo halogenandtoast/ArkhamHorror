60c06f2c49a972ce07f23b25354199956cdc1ae7
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-03 17:03:51 +0900

    Umbral Harbinger

--- backend/arkham-api/library/Arkham/Enemy/Cards/UmbralHarbinger.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/UmbralHarbinger.hs
@@ -1,15 +1,31 @@
 module Arkham.Enemy.Cards.UmbralHarbinger (umbralHarbinger) where
 
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
+import Arkham.Helpers.Location (withLocationOf)
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 
 newtype UmbralHarbinger = UmbralHarbinger EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 umbralHarbinger :: EnemyCard UmbralHarbinger
 umbralHarbinger = enemy UmbralHarbinger Cards.umbralHarbinger (3, Static 5, 1) (1, 1)
 
+instance HasAbilities UmbralHarbinger where
+  getAbilities (UmbralHarbinger a) =
+    extend1 a
+      $ restricted a 1 (exists $ InPlayEnemy #cultist)
+      $ forced
+      $ EnemyDealtDamage #when AnyDamageEffect (be a) AnySource
+
 instance RunMessage UmbralHarbinger where
-  runMessage msg (UmbralHarbinger attrs) = runQueueT $ case msg of
+  runMessage msg e@(UmbralHarbinger attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      withLocationOf attrs \lid -> do
+        cultists <- select $ NearestEnemyToLocationFallback lid #cultist
+        chooseTargetM iid cultists $ placeDoomOn attrs 1
+      pure e
     _ -> UmbralHarbinger <$> liftRunMessage msg attrs

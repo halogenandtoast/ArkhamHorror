d7388d6621bed78e09f5b74bba5c67221940278f
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-03 17:30:33 +0900

    Sinister Aspirant (A, B, C)

--- backend/arkham-api/library/Arkham/Enemy/Cards/SinisterAspirantB.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/SinisterAspirantB.hs
@@ -1,15 +1,23 @@
 module Arkham.Enemy.Cards.SinisterAspirantB (sinisterAspirantB) where
 
+import Arkham.Ability
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
+import Arkham.Matcher
 
 newtype SinisterAspirantB = SinisterAspirantB EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 sinisterAspirantB :: EnemyCard SinisterAspirantB
 sinisterAspirantB = enemy SinisterAspirantB Cards.sinisterAspirantB (2, Static 3, 4) (0, 1)
 
+instance HasAbilities SinisterAspirantB where
+  getAbilities (SinisterAspirantB a) = extend1 a $ restricted a 1 (thisExists a ReadyEnemy) $ forced $ PhaseEnds #after #enemy
+
 instance RunMessage SinisterAspirantB where
-  runMessage msg (SinisterAspirantB attrs) = runQueueT $ case msg of
+  runMessage msg e@(SinisterAspirantB attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      placeDoom (attrs.ability 1) attrs 1
+      pure e
     _ -> SinisterAspirantB <$> liftRunMessage msg attrs

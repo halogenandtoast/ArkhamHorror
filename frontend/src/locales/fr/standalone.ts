c326fa1b0a52199fb1377a90987998da9c966cb6
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-03 19:17:43 +0900

    Add Film Fatale French translations

--- frontend/src/locales/fr/standalone.ts
+++ frontend/src/locales/fr/standalone.ts
@@ -2,5 +2,6 @@ import curseOfTheRougarou from '@/locales/fr/standalone/curseOfTheRougarou.json'
 import carnevaleOfHorrors from '@/locales/fr/standalone/carnevaleOfHorrors.json'
 import murderAtTheExcelsiorHotel from '@/locales/fr/standalone/murderAtTheExcelsiorHotel.json'
 import theMidwinterGala from '@/locales/fr/standalone/theMidwinterGala.json'
+import filmFatale from '@/locales/fr/standalone/filmFatale.json'
 
-export default {curseOfTheRougarou, carnevaleOfHorrors, murderAtTheExcelsiorHotel, theMidwinterGala}
+export default {curseOfTheRougarou, carnevaleOfHorrors, murderAtTheExcelsiorHotel, theMidwinterGala, filmFatale}

780d67a672ed1cc23cb276f82458c8257133dc32
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-03 17:41:31 +0900

    Accosted

--- backend/arkham-api/library/Arkham/Treachery/Cards/Accosted.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Accosted.hs
@@ -1,5 +1,8 @@
 module Arkham.Treachery.Cards.Accosted (accosted) where
 
+import Arkham.Enemy.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +15,19 @@ accosted = treachery Accosted Cards.accosted
 
 instance RunMessage Accosted where
   runMessage msg t@(Accosted attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      cultists <- select $ NearestEnemyToFallback iid (InPlayEnemy #cultist)
+      if null cultists
+        then do
+          assignDamage iid attrs 1
+          gainSurge attrs
+        else do
+          sid <- getRandom
+          chooseTargetM iid cultists \x -> do
+            placeDoom attrs x 1
+            beginSkillTest sid iid attrs iid #agility (EnemyMaybeFieldCalculation x EnemyEvade)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      assignDamage iid attrs 2
+      pure t
     _ -> Accosted <$> liftRunMessage msg attrs

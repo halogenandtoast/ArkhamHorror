4368aa674cd7bd2e8bb648a92e578a4c0e02c875
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-08 06:53:55 +0900

    Fix Ends and Means (a bit of a hack)

--- backend/arkham-api/library/Arkham/Agenda/Cards/EndsAndMeans.hs
+++ backend/arkham-api/library/Arkham/Agenda/Cards/EndsAndMeans.hs
@@ -28,7 +28,8 @@ instance HasModifiersFor EndsAndMeans where
 
 instance HasAbilities EndsAndMeans where
   getAbilities (EndsAndMeans a) =
-    [mkAbility a 1 $ forced $ EnemyDefeated #when You ByAny $ EnemyWithTrait SilverTwilight]
+    [ mkAbility a 1 $ forced $ EnemyDefeated #when You ByAny $ InPlayEnemy $ EnemyWithTrait SilverTwilight
+    ]
 
 instance RunMessage EndsAndMeans where
   runMessage msg a@(EndsAndMeans attrs) = runQueueT $ case msg of

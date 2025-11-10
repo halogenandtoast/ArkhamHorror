3df41105138df523dd3cb19b1baba61488f2cc82
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-10 17:45:50 +0900

    Secrets Lost

--- backend/arkham-api/library/Arkham/Treachery/Cards/SecretsLost.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/SecretsLost.hs
@@ -1,5 +1,9 @@
 module Arkham.Treachery.Cards.SecretsLost (secretsLost) where
 
+import Arkham.Campaigns.TheScarletKeys.Helpers
+import Arkham.I18n
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +16,21 @@ secretsLost = treachery SecretsLost Cards.secretsLost
 
 instance RunMessage SecretsLost where
   runMessage msg t@(SecretsLost attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #willpower (Fixed 3)
+      pure t
+    FailedThisSkillTestBy iid (isSource attrs -> True) n -> do
+      cards <- select $ inHandOf NotForPlay iid <> basic NonWeakness
+      chooseNM iid n $ targets cards $ hollow iid
+      pure t
+    PassedThisSkillTestBy iid (isSource attrs -> True) n | n >= 2 -> do
+      hollows <- select HollowedCard
+      unless (null hollows) do
+        chooseOneM iid $ campaignI18n do
+          unscoped skip_
+          labeled' "secretsLost.hollowed" do
+            assignHorror iid attrs 1
+            focusCards hollows $ chooseTargetM iid hollows \c -> for_ c.owner \owner -> addToHand owner [c]
+      pure t
     _ -> SecretsLost <$> liftRunMessage msg attrs

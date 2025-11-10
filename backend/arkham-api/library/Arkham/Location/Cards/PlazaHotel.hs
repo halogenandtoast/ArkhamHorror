f25b759797c64875f68a4fcb28ee44f22c78158f
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-10 15:33:25 +0900

    Plaza Hotel

--- backend/arkham-api/library/Arkham/Location/Cards/PlazaHotel.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/PlazaHotel.hs
@@ -1,7 +1,12 @@
 module Arkham.Location.Cards.PlazaHotel (plazaHotel) where
 
+import Arkham.Ability
+import Arkham.Campaigns.TheScarletKeys.Concealed
+import Arkham.Campaigns.TheScarletKeys.Concealed.Helpers
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 
 newtype PlazaHotel = PlazaHotel LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +16,15 @@ plazaHotel :: LocationCard PlazaHotel
 plazaHotel = symbolLabel $ location PlazaHotel Cards.plazaHotel 2 (PerPlayer 1)
 
 instance HasAbilities PlazaHotel where
-  getAbilities (PlazaHotel attrs) =
-    extendRevealed attrs []
+  getAbilities (PlazaHotel a) =
+    extendRevealed1 a $ restricted a 1 Here $ FastAbility (HorrorCost (toSource a) YouTarget 1)
 
 instance RunMessage PlazaHotel where
-  runMessage msg (PlazaHotel attrs) = runQueueT $ case msg of
+  runMessage msg l@(PlazaHotel attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      locations <- select (orConnected_ (LocationWithId attrs.id) <> LocationWithConcealedCard)
+      chooseTargetM iid locations \lid -> do
+        concealed <- map toId <$> getConcealedAt (ForExpose $ toSource attrs) lid
+        chooseOrRunOneM iid $ targets concealed $ revealConcealed iid (attrs.ability 1)
+      pure l
     _ -> PlazaHotel <$> liftRunMessage msg attrs

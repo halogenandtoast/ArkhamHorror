3c04fdac0872b70b2eec40466c95723ec1f23a79
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-10 16:34:40 +0900

    Agents of the Outside

--- backend/arkham-api/library/Arkham/Treachery/Cards/BodySnatched.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/BodySnatched.hs
@@ -1,5 +1,10 @@
 module Arkham.Treachery.Cards.BodySnatched (bodySnatched) where
 
+import Arkham.Asset.Types (Field (AssetCard))
+import Arkham.Campaigns.TheScarletKeys.Helpers
+import Arkham.Matcher hiding (AssetCard)
+import Arkham.Message.Lifted.Choose
+import Arkham.Trait (Trait (Ally, Item))
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +17,18 @@ bodySnatched = treachery BodySnatched Cards.bodySnatched
 
 instance RunMessage BodySnatched where
   runMessage msg t@(BodySnatched attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #willpower (Fixed 4)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      assets <-
+        selectWithField AssetCard
+          $ assetControlledBy iid
+          <> hasAnyTrait [Ally, Item]
+          <> AssetCanLeavePlayByNormalMeans
+      if null assets
+        then assignHorror iid attrs 2
+        else chooseOneM iid $ for assets \(aid, card) -> targeting aid $ hollow iid card
+      pure t
     _ -> BodySnatched <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Treachery/Cards/MatterInversion.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/MatterInversion.hs
@@ -1,16 +1,51 @@
 module Arkham.Treachery.Cards.MatterInversion (matterInversion) where
 
+import Arkham.Helpers.Modifiers (ModifierType (..), modified_, getModifiers)
+import Arkham.Keyword qualified as Keyword
+import Arkham.Matcher
+import Arkham.Ability
+import Arkham.Message.Lifted.Choose
+import Arkham.Placement
+import Arkham.Trait (Trait (Outsider))
 import Arkham.Treachery.Cards qualified as Cards
-import Arkham.Treachery.Import.Lifted
+import Arkham.Treachery.Import.Lifted hiding (EnemyEvaded)
 
 newtype MatterInversion = MatterInversion TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass IsTreachery
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 matterInversion :: TreacheryCard MatterInversion
 matterInversion = treachery MatterInversion Cards.matterInversion
 
+instance HasModifiersFor MatterInversion where
+  getModifiersFor (MatterInversion attrs) = case attrs.placement of
+    AttachedToEnemy eid ->
+      modified_
+        attrs
+        eid
+        [RemoveKeyword Keyword.Aloof, AddKeyword Keyword.Retaliate, EnemyFight 1, EnemyEvade 1]
+    _ -> pure mempty
+
+instance HasAbilities MatterInversion where
+  getAbilities (MatterInversion a) =
+    [mkAbility a 1 $ forced $ EnemyEvaded #after You (EnemyWithAttachedTreachery (be a))]
+
 instance RunMessage MatterInversion where
   runMessage msg t@(MatterInversion attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      enemies <-
+        select
+          $ EnemyWithTrait Outsider
+          <> not_ (EnemyWithAttachedTreachery $ treacheryIs Cards.matterInversion)
+      if null enemies
+        then gainSurge attrs
+        else chooseTargetM iid enemies \e -> attachTreachery attrs e >> enemyCheckEngagement e
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      toDiscardBy iid (attrs.ability 1) attrs
+      mods <- getModifiers iid
+      hollows <- traverse fetchCard [cardId | Hollow cardId <- mods]
+      focusCards hollows do
+        chooseTargetM iid hollows \card -> addToHand iid [card]
+      pure t
     _ -> MatterInversion <$> liftRunMessage msg attrs

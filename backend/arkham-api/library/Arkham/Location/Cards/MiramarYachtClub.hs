512f7d4b35f9ff1babb24930654026ccc065bde1
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-10 15:08:49 +0900

    Miramar Yacht Club

--- backend/arkham-api/library/Arkham/Location/Cards/MiramarYachtClub.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/MiramarYachtClub.hs
@@ -1,7 +1,12 @@
 module Arkham.Location.Cards.MiramarYachtClub (miramarYachtClub) where
 
+import Arkham.Ability
+import Arkham.Action qualified as Action
+import Arkham.Campaigns.TheScarletKeys.Concealed.Helpers
+import Arkham.Helpers.SkillTest.Lifted
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Choose
 
 newtype MiramarYachtClub = MiramarYachtClub LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +16,17 @@ miramarYachtClub :: LocationCard MiramarYachtClub
 miramarYachtClub = symbolLabel $ location MiramarYachtClub Cards.miramarYachtClub 1 (PerPlayer 1)
 
 instance HasAbilities MiramarYachtClub where
-  getAbilities (MiramarYachtClub attrs) =
-    extendRevealed attrs []
+  getAbilities (MiramarYachtClub a) =
+    extendRevealed1 a $ restricted a 1 Here investigateAction_
 
 instance RunMessage MiramarYachtClub where
-  runMessage msg (MiramarYachtClub attrs) = runQueueT $ case msg of
+  runMessage msg l@(MiramarYachtClub attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      sid <- getRandom
+      investigateEdit_ sid iid (attrs.ability 1) (setTarget attrs)
+      pure l
+    Successful (Action.Investigate, _) iid (isAbilitySource attrs 1 -> True) (isTarget attrs -> True) n -> do
+      concealed <- map toId <$> getConcealedAtAll (ForExpose $ toSource attrs) attrs.id
+      chooseNM iid (1 + (n `div` 2)) $ targets concealed $ revealConcealed iid (attrs.ability 1)
+      pure l
     _ -> MiramarYachtClub <$> liftRunMessage msg attrs

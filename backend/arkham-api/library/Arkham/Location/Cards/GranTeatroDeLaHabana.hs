50ad32df54801af677655186bbe2313d25136f77
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-10 15:03:39 +0900

    Gran Teatro De La Habana

--- backend/arkham-api/library/Arkham/Location/Cards/GranTeatroDeLaHabana.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/GranTeatroDeLaHabana.hs
@@ -1,7 +1,10 @@
 module Arkham.Location.Cards.GranTeatroDeLaHabana (granTeatroDeLaHabana) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher hiding (DuringTurn)
+import Arkham.Modifier
 
 newtype GranTeatroDeLaHabana = GranTeatroDeLaHabana LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -11,9 +14,15 @@ granTeatroDeLaHabana :: LocationCard GranTeatroDeLaHabana
 granTeatroDeLaHabana = symbolLabel $ location GranTeatroDeLaHabana Cards.granTeatroDeLaHabana 4 (PerPlayer 1)
 
 instance HasAbilities GranTeatroDeLaHabana where
-  getAbilities (GranTeatroDeLaHabana attrs) =
-    extendRevealed attrs []
+  getAbilities (GranTeatroDeLaHabana a) =
+    extendRevealed1 a
+      $ playerLimit PerTurn
+      $ restricted a 1 (Here <> DuringTurn You)
+      $ FastAbility (ResourceCost 2)
 
 instance RunMessage GranTeatroDeLaHabana where
-  runMessage msg (GranTeatroDeLaHabana attrs) = runQueueT $ case msg of
+  runMessage msg l@(GranTeatroDeLaHabana attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      turnModifier iid (attrs.ability 1) attrs (ShroudModifier (-2))
+      pure l
     _ -> GranTeatroDeLaHabana <$> liftRunMessage msg attrs

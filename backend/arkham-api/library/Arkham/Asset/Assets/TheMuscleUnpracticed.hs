031c03b4538dc340f6a573b8bb9c10bd7baaeb90
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-12 16:02:51 +0900

    The Muscle (Unpracticed), and token translations

--- backend/arkham-api/library/Arkham/Asset/Assets/TheMuscleUnpracticed.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/TheMuscleUnpracticed.hs
@@ -1,15 +1,29 @@
 module Arkham.Asset.Assets.TheMuscleUnpracticed (theMuscleUnpracticed) where
 
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Import.Lifted
+import Arkham.Asset.Import.Lifted hiding (EnemyDefeated)
+import Arkham.Helpers.Window (defeatedEnemy)
+import Arkham.Matcher
+import Arkham.Scenarios.FortuneAndFolly.Helpers
+import Arkham.Trait (Trait (Casino))
 
 newtype TheMuscleUnpracticed = TheMuscleUnpracticed AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theMuscleUnpracticed :: AssetCard TheMuscleUnpracticed
 theMuscleUnpracticed = asset TheMuscleUnpracticed Cards.theMuscleUnpracticed
 
+instance HasAbilities TheMuscleUnpracticed where
+  getAbilities (TheMuscleUnpracticed a) =
+    [controlled_ a 1 $ triggered (EnemyDefeated #after You ByAny (EnemyWithTrait Casino)) (exhaust a)]
+
 instance RunMessage TheMuscleUnpracticed where
-  runMessage msg (TheMuscleUnpracticed attrs) = runQueueT $ case msg of
+  runMessage msg a@(TheMuscleUnpracticed attrs) = runQueueT $ case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 (defeatedEnemy -> eid) _ -> do
+      byTwo <-
+        selectNone $ EnemyAt (orConnected_ (locationWithInvestigator iid)) <> not_ (EnemyWithId eid)
+      reduceAlarmLevelBy (if byTwo then 2 else 1) (attrs.ability 1) iid
+      pure a
     _ -> TheMuscleUnpracticed <$> liftRunMessage msg attrs

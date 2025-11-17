0e5d2cffa6ddf40f4b9c8358c833aa2f89b3b3f4
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-17 16:31:00 +0900

    Cleanup Fortuitous Discovery

--- backend/arkham-api/library/Arkham/Event/Events/FortuitousDiscovery.hs
+++ backend/arkham-api/library/Arkham/Event/Events/FortuitousDiscovery.hs
@@ -1,4 +1,4 @@
-module Arkham.Event.Events.FortuitousDiscovery (fortuitousDiscovery, FortuitousDiscovery (..)) where
+module Arkham.Event.Events.FortuitousDiscovery (fortuitousDiscovery) where
 
 import Arkham.Event.Cards qualified as Cards
 import Arkham.Event.Import.Lifted
@@ -14,10 +14,10 @@ fortuitousDiscovery = event FortuitousDiscovery Cards.fortuitousDiscovery
 
 instance RunMessage FortuitousDiscovery where
   runMessage msg e@(FortuitousDiscovery attrs) = runQueueT $ case msg of
-    PlayThisEvent iid eid | eid == toId attrs -> do
+    PlayThisEvent iid (is attrs -> True) -> do
       sid <- getRandom
       x <- selectCount $ inDiscardOf iid <> basic (cardIs Cards.fortuitousDiscovery)
-      skillTestModifiers sid (toSource attrs) iid [SkillModifier #intellect x, DiscoveredClues x]
+      skillTestModifiers sid attrs iid [SkillModifier #intellect x, DiscoveredClues x]
       investigate sid iid attrs
       pure e
     _ -> FortuitousDiscovery <$> liftRunMessage msg attrs

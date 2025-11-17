29306d844dc5f65e4f1f377431169f9cfbb79525
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-17 10:43:12 +0900

    Fix some b side interactions

--- backend/arkham-api/library/Arkham/Helpers/EncounterSet.hs
+++ backend/arkham-api/library/Arkham/Helpers/EncounterSet.hs
@@ -17,5 +17,12 @@ gatherEncounterSet encounterSet =
   defs =
     filter (and . sequence [not . hasBSide, (== Just encounterSet) . cdEncounterSet]) $ toList allEncounterCards
   hasBSide = and . sequence [isDoubleSided, isSuffixOf "b" . unCardCode . toCardCode]
-  isDoubleSided = or . sequence [cdDoubleSided, isJust . cdOtherSide] . toCardDef
+
+  -- Location cards are weird because they are always double sided, but when we
+  -- gather cards we want to include them even when the suffix is a "b", in these
+  -- cases we just look to ensure it's not the other side of a card.
+  isDoubleSided =
+    or
+      . sequence [and . sequence [cdDoubleSided, (/= LocationType) . cdCardType], isJust . cdOtherSide]
+      . toCardDef
 

02b8b7b85e82482153e973bfbbbb5e062db8f7aa
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-24 21:34:30 +0900

    Add card filter

---
+++ frontend/src/composeable/debouncedRef.ts
@@ -0,0 +1,20 @@
+import { customRef } from 'vue'
+
+export function useDebouncedRef(value, delay = 200) {
+  let timeout
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          value = newValue
+          trigger()
+        }, delay)
+      }
+    }
+  })
+}

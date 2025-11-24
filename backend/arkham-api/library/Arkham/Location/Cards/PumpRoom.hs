469b7213c9c91ccea803d6dd57d6338174a3a2af
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-24 15:24:53 +0900

    Fix Pump Room

--- backend/arkham-api/library/Arkham/Location/Cards/PumpRoom.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/PumpRoom.hs
@@ -1,7 +1,8 @@
-module Arkham.Location.Cards.PumpRoom (pumpRoom, PumpRoom (..)) where
+module Arkham.Location.Cards.PumpRoom (pumpRoom) where
 
 import Arkham.Ability
 import Arkham.Campaigns.TheInnsmouthConspiracy.Helpers
+import Arkham.I18n
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
 import Arkham.Matcher
@@ -22,9 +23,7 @@ instance RunMessage PumpRoom where
   runMessage msg l@(PumpRoom attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       sid <- getRandom
-      chooseOneM iid do
-        for_ [#intellect, #agility] \sType ->
-          skillLabeled sType $ beginSkillTest sid iid (attrs.ability 1) iid sType (Fixed 2)
+      chooseBeginSkillTest sid iid (attrs.ability 1) iid [#intellect, #agility] (Fixed 2)
       pure l
     PassedThisSkillTestBy iid (isAbilitySource attrs 1 -> True) n -> do
       floodedLocations <- select FloodedLocation
@@ -32,11 +31,10 @@ instance RunMessage PumpRoom where
         questionLabeled "Choose location to decrease flood level"
         targets floodedLocations \lid -> do
           decreaseThisFloodLevel lid
-          when (n < 2) do
-            floodable <- select CanHaveFloodLevelIncreased
-            chooseOneM iid do
-              questionLabeled "Choose location to increase flood level"
-              targets (deleteFirst lid floodable) increaseThisFloodLevel
-
+          floodable <- select $ CanHaveFloodLevelIncreased <> not_ (be lid)
+          chooseOneM iid do
+            questionLabeled "Choose location to increase flood level"
+            when (n >= 2) $ withI18n skip_
+            targets (deleteFirst lid floodable) increaseThisFloodLevel
       pure l
     _ -> PumpRoom <$> liftRunMessage msg attrs

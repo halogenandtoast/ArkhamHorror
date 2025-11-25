b121a7e11fddccec029526078d3a3b1c6be36ebf
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-25 12:33:04 +0900

    Paradimensional Understanding

--- backend/arkham-api/library/Arkham/Treachery/Cards/ParadimensionalUnderstanding.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/ParadimensionalUnderstanding.hs
@@ -1,5 +1,9 @@
 module Arkham.Treachery.Cards.ParadimensionalUnderstanding (paradimensionalUnderstanding) where
 
+import Arkham.Campaigns.TheScarletKeys.Helpers
+import Arkham.Campaigns.TheScarletKeys.Key.Matcher
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +16,13 @@ paradimensionalUnderstanding = treachery ParadimensionalUnderstanding Cards.para
 
 instance RunMessage ParadimensionalUnderstanding where
   runMessage msg t@(ParadimensionalUnderstanding attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      unstableKeys <- select $ ScarletKeyWithInvestigator (InvestigatorWithId iid) <> UnstableScarletKey
+      stableKeys <- select $ ScarletKeyWithInvestigator (InvestigatorWithId iid) <> StableScarletKey
+      chooseOneM iid $ campaignI18n do
+        labeled' "paradimensionalUnderstanding.unstable" do
+          chooseTargetM iid unstableKeys shift
+        labeled' "paradimensionalUnderstanding.stable" do
+          for_ stableKeys $ flipOverBy iid attrs
+      pure t
     _ -> ParadimensionalUnderstanding <$> liftRunMessage msg attrs

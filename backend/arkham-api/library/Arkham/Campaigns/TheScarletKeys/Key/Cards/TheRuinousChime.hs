c5833540e4fadad9280a54cd03528bf4934de427
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-26 16:08:35 +0900

    The Ruinous Chime

--- backend/arkham-api/library/Arkham/Campaigns/TheScarletKeys/Key/Cards/TheRuinousChime.hs
+++ backend/arkham-api/library/Arkham/Campaigns/TheScarletKeys/Key/Cards/TheRuinousChime.hs
@@ -1,7 +1,10 @@
 module Arkham.Campaigns.TheScarletKeys.Key.Cards.TheRuinousChime (theRuinousChime) where
 
+import Arkham.Ability
 import Arkham.Campaigns.TheScarletKeys.Key.Cards qualified as Cards
 import Arkham.Campaigns.TheScarletKeys.Key.Import.Lifted
+import Arkham.Matcher hiding (key)
+import Arkham.Message.Lifted.Choose
 
 newtype TheRuinousChime = TheRuinousChime ScarletKeyAttrs
   deriving anyclass (IsScarletKey, HasModifiersFor)
@@ -12,7 +15,16 @@ theRuinousChime = key TheRuinousChime Cards.theRuinousChime
 
 instance HasAbilities TheRuinousChime where
   getAbilities (TheRuinousChime a) = case a.bearer of
-    InvestigatorTarget _iid -> []
+    InvestigatorTarget iid ->
+      case a.stability of
+        Stable ->
+          [ restricted
+              a
+              1
+              (exists $ EnemyAt (locationWithInvestigator iid) <> EnemyCanBeEvadedBy (a.ability 1))
+              $ FastAbility Free
+          ]
+        Unstable -> [restricted a 1 (youExist (InvestigatorWithId iid)) $ FastAbility Free]
     _ -> []
 
 instance RunMessage TheRuinousChime where
@@ -20,7 +32,13 @@ instance RunMessage TheRuinousChime where
     UseThisAbility _ (isSource attrs -> True) 1 -> liftRunMessage (CampaignSpecific "shift[09770]" Null) k
     CampaignSpecific "shift[09770]" _ -> do
       shiftKey attrs do
-        when attrs.unstable $ pure ()
-        when attrs.stable $ pure ()
+        when attrs.unstable do
+          eachInvestigator \iid -> directHorror iid (attrs.ability 1) 1
+          withInvestigatorBearer attrs (`flipOver` attrs)
+        when attrs.stable do
+          withInvestigatorBearer attrs \iid -> do
+            enemies <- select $ EnemyAt (locationWithInvestigator iid) <> EnemyCanBeEvadedBy (attrs.ability 1)
+            chooseOneAtATimeM iid $ targets enemies $ automaticallyEvadeEnemy iid
+            flipOver iid attrs
       pure k
     _ -> TheRuinousChime <$> liftRunMessage msg attrs

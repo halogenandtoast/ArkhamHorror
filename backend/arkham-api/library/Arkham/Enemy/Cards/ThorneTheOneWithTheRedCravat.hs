ae3f218ec1102c68bdfb3fcf37d80357c23affec
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-27 17:06:01 +0900

    On Thin Ice Enemies

--- backend/arkham-api/library/Arkham/Enemy/Cards/ThorneTheOneWithTheRedCravat.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/ThorneTheOneWithTheRedCravat.hs
@@ -1,15 +1,54 @@
 module Arkham.Enemy.Cards.ThorneTheOneWithTheRedCravat (thorneTheOneWithTheRedCravat) where
 
+import Arkham.Ability
+import Arkham.Campaigns.TheScarletKeys.Helpers
+import Arkham.Campaigns.TheScarletKeys.Key.Cards qualified as Keys
+import Arkham.Campaigns.TheScarletKeys.Key.Matcher
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Import.Lifted
+import Arkham.Enemy.Import.Lifted hiding (EnemyDefeated)
+import Arkham.Helpers.GameValue (perPlayer)
+import Arkham.Helpers.Modifiers (ModifierType (..), modifySelf)
+import Arkham.Helpers.Query (getInvestigators, getLead)
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Placement
+import Arkham.Trait (Trait (Hazard))
 
 newtype ThorneTheOneWithTheRedCravat = ThorneTheOneWithTheRedCravat EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass IsEnemy
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 thorneTheOneWithTheRedCravat :: EnemyCard ThorneTheOneWithTheRedCravat
-thorneTheOneWithTheRedCravat = enemy ThorneTheOneWithTheRedCravat Cards.thorneTheOneWithTheRedCravat (0, Static 1, 0) (0, 0)
+thorneTheOneWithTheRedCravat = enemy ThorneTheOneWithTheRedCravat Cards.thorneTheOneWithTheRedCravat (4, Static 6, 4) (2, 1)
+
+instance HasModifiersFor ThorneTheOneWithTheRedCravat where
+  getModifiersFor (ThorneTheOneWithTheRedCravat a) = do
+    n <- perPlayer 2
+    modifySelf a [HealthModifier n]
+
+instance HasAbilities ThorneTheOneWithTheRedCravat where
+  getAbilities (ThorneTheOneWithTheRedCravat a) =
+    extend
+      a
+      [ restricted a 1 (enemyWithScarletKey a)
+          $ forced
+          $ DrawCard #after Anyone (basic $ #treachery <> CardWithTrait Hazard) AnyDeck
+      , restricted a 2 (thisExists a $ EnemyWithScarletKey $ scarletKeyIs Keys.theSableGlass)
+          $ forced
+          $ EnemyDefeated #when Anyone ByAny (be a)
+      ]
 
 instance RunMessage ThorneTheOneWithTheRedCravat where
-  runMessage msg (ThorneTheOneWithTheRedCravat attrs) = runQueueT $ case msg of
+  runMessage msg e@(ThorneTheOneWithTheRedCravat attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      lead <- getLead
+      skeys <- select $ scarletKeyWithEnemy attrs.id
+      chooseOneAtATimeM lead $ targets skeys shift
+      pure e
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      lead <- getLead
+      investigators <- getInvestigators
+      theSableGlass <- selectJust $ scarletKeyIs Keys.theSableGlass
+      chooseTargetM lead investigators $ push . PlaceScarletKey theSableGlass . AttachedToInvestigator
+      pure e
     _ -> ThorneTheOneWithTheRedCravat <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Enemy/Cards/VoidChimeraFellbeak.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/VoidChimeraFellbeak.hs
@@ -2,14 +2,17 @@ module Arkham.Enemy.Cards.VoidChimeraFellbeak (voidChimeraFellbeak) where
 
 import Arkham.Enemy.Cards qualified as Cards
 import Arkham.Enemy.Import.Lifted
+import Arkham.Helpers.GameValue (perPlayer)
+import Arkham.Helpers.Modifiers (ModifierType (..), modifySelf)
 
 newtype VoidChimeraFellbeak = VoidChimeraFellbeak EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
+  deriving anyclass (IsEnemy, RunMessage)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 voidChimeraFellbeak :: EnemyCard VoidChimeraFellbeak
-voidChimeraFellbeak = enemy VoidChimeraFellbeak Cards.voidChimeraFellbeak (0, Static 1, 0) (0, 0)
+voidChimeraFellbeak = enemy VoidChimeraFellbeak Cards.voidChimeraFellbeak (4, Static 2, 2) (3, 0)
 
-instance RunMessage VoidChimeraFellbeak where
-  runMessage msg (VoidChimeraFellbeak attrs) = runQueueT $ case msg of
-    _ -> VoidChimeraFellbeak <$> liftRunMessage msg attrs
+instance HasModifiersFor VoidChimeraFellbeak where
+  getModifiersFor (VoidChimeraFellbeak a) = do
+    n <- perPlayer 1
+    modifySelf a [HealthModifier n]

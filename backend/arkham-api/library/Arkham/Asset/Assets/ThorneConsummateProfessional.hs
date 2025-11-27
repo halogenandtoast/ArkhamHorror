8176aa8b8205022640ea72e4b802575a347e85cf
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-27 17:23:50 +0900

    More On Thin Ice

--- backend/arkham-api/library/Arkham/Asset/Assets/ThorneConsummateProfessional.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/ThorneConsummateProfessional.hs
@@ -1,15 +1,33 @@
 module Arkham.Asset.Assets.ThorneConsummateProfessional (thorneConsummateProfessional) where
 
+import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
+import Arkham.Matcher
+import Arkham.Window qualified as Window
 
 newtype ThorneConsummateProfessional = ThorneConsummateProfessional AssetAttrs
-  deriving anyclass (IsAsset, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsAsset, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 thorneConsummateProfessional :: AssetCard ThorneConsummateProfessional
-thorneConsummateProfessional = asset ThorneConsummateProfessional Cards.thorneConsummateProfessional
+thorneConsummateProfessional = allyWith ThorneConsummateProfessional Cards.thorneConsummateProfessional (2, 2) noSlots
+
+instance HasAbilities ThorneConsummateProfessional where
+  getAbilities (ThorneConsummateProfessional a) =
+    [ controlled_ a 1
+        $ triggered (CampaignEvent #after (Just $ affectsColocatedMatch You) "hollowed") (exhaust a)
+    ]
 
 instance RunMessage ThorneConsummateProfessional where
-  runMessage msg (ThorneConsummateProfessional attrs) = runQueueT $ case msg of
+  runMessage msg a@(ThorneConsummateProfessional attrs) = runQueueT $ case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 ws _ -> do
+      let
+        go = \case
+          [] -> pure ()
+          ((Window.windowType -> Window.CampaignEvent "hollowed" (Just iid) _) : _) -> do
+            takeActionAsIfTurn iid (attrs.ability 1)
+          (_ : xs) -> go xs
+      go ws
+      pure a
     _ -> ThorneConsummateProfessional <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Treachery/Cards/CrackingIce.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/CrackingIce.hs
@@ -1,5 +1,9 @@
 module Arkham.Treachery.Cards.CrackingIce (crackingIce) where
 
+import Arkham.I18n
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Placement
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +16,26 @@ crackingIce = treachery CrackingIce Cards.crackingIce
 
 instance RunMessage CrackingIce where
   runMessage msg t@(CrackingIce attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation _iid (isSource attrs -> True) -> do
+      placeTreachery attrs NextToAgenda
+      doStep 1 msg
+      pure t
+    DoStep 1 (Revelation _iid (isSource attrs -> True)) -> do
+      crackingIces <- select $ treacheryIs Cards.crackingIce
+      when (length crackingIces >= 3) $ do
+        eachInvestigator \iid -> do
+          sid <- getRandom
+          revelationSkillTest sid iid attrs #agility (Fixed 4)
+        for_ crackingIces $ toDiscard attrs
+      pure t
+    FailedThisSkillTestBy _iid (isSource attrs -> True) n -> do
+      doStep n msg
+      pure t
+    DoStep n msg'@(FailedThisSkillTest iid (isSource attrs -> True)) | n > 0 -> do
+      chooseOneM iid $ withI18n $ countVar 1 $ do
+        labeled' "takeDamage" $ assignDamage iid attrs 1
+        labeled' "takeHorror" $ assignHorror iid attrs 1
+        labeled' "loseActions" $ loseActions iid attrs 1
+      doStep (n - 1) msg'
+      pure t
     _ -> CrackingIce <$> liftRunMessage msg attrs

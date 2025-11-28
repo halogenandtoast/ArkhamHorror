86f37cad4267d736586f386c5ca6fedecb8d0d69
Matthew Mongeau <halogenandtoast@gmail.com>
2025-11-29 07:42:03 +0900

    Fix House Dealer

--- backend/arkham-api/library/Arkham/Enemy/Cards/HouseDealerA.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/HouseDealerA.hs
@@ -14,10 +14,11 @@ houseDealerA = enemy HouseDealerA Cards.houseDealerA (2, Static 1, 2) (0, 1)
 
 instance HasModifiersFor HouseDealerA where
   getModifiersFor (HouseDealerA a) = do
-    abilities <- select $ AbilityOnLocation (locationWithEnemy a)
-    eachInvestigator \iid -> do
-      for_ abilities \ab ->
-        modified_ a (AbilityTarget iid ab.ref) [ActionCostModifier 1]
+    when a.ready do
+      abilities <- select $ AbilityOnLocation (locationWithEnemy a)
+      eachInvestigator \iid -> do
+        for_ abilities \ab ->
+          modified_ a (AbilityTarget iid ab.ref) [ActionCostModifier 1]
 
 instance RunMessage HouseDealerA where
   runMessage msg (HouseDealerA attrs) = HouseDealerA <$> runMessage msg attrs
--- backend/arkham-api/library/Arkham/Enemy/Cards/HouseDealerB.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/HouseDealerB.hs
@@ -14,10 +14,11 @@ houseDealerB = enemy HouseDealerB Cards.houseDealerB (2, Static 1, 2) (0, 1)
 
 instance HasModifiersFor HouseDealerB where
   getModifiersFor (HouseDealerB a) = do
-    abilities <- select $ AbilityOnLocation (locationWithEnemy a)
-    eachInvestigator \iid -> do
-      for_ abilities \ab ->
-        modified_ a (AbilityTarget iid ab.ref) [ActionCostModifier 1]
+    when a.ready do
+      abilities <- select $ AbilityOnLocation (locationWithEnemy a)
+      eachInvestigator \iid -> do
+        for_ abilities \ab ->
+          modified_ a (AbilityTarget iid ab.ref) [ActionCostModifier 1]
 
 instance RunMessage HouseDealerB where
   runMessage msg (HouseDealerB attrs) = HouseDealerB <$> runMessage msg attrs

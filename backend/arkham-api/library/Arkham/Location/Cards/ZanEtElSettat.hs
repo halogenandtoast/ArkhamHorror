b6d6bd83f576944bd9de14d292d54bd07c62b25d
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-02 20:46:28 +0900

    Dogs of War progress

--- backend/arkham-api/library/Arkham/Location/Cards/ZanEtElSettat.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/ZanEtElSettat.hs
@@ -1,7 +1,16 @@
 module Arkham.Location.Cards.ZanEtElSettat (zanEtElSettat) where
 
+import Arkham.Ability
+import Arkham.Helpers.SkillTest (getSkillTest)
+import Arkham.I18n
+import Arkham.Investigator.Types (Field (..))
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Modifier
+import Arkham.Projection
+import Arkham.Scenarios.DogsOfWar.Helpers
 
 newtype ZanEtElSettat = ZanEtElSettat LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +21,22 @@ zanEtElSettat = symbolLabel $ location ZanEtElSettat Cards.zanEtElSettat 0 (Stat
 
 instance HasAbilities ZanEtElSettat where
   getAbilities (ZanEtElSettat a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ restricted a 1 Here
+      $ forced
+      $ InitiatedSkillTest #when You #any #any (WhileAttackingAnEnemy AnyEnemy)
 
 instance RunMessage ZanEtElSettat where
-  runMessage msg (ZanEtElSettat attrs) = runQueueT $ case msg of
+  runMessage msg l@(ZanEtElSettat attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      actions <- field InvestigatorRemainingActions iid
+      chooseOneM iid $ scenarioI18n do
+        unscoped
+          $ countVar 1
+          $ labeledValidate' (actions > 0) "spendActions"
+          $ spendActions iid (attrs.ability 1) 1
+        labeled' "zanEtElSettat.increaseFight" do
+          whenJustM getSkillTest \st ->
+            skillTestModifier st.id (attrs.ability 1) st.target (EnemyFight 2)
+      pure l
     _ -> ZanEtElSettat <$> liftRunMessage msg attrs

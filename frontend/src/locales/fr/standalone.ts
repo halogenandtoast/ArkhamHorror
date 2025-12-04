d84d7c82875f474fd4f4d7032b75d4ca9807247b
Ayoross <youri.andropov@gmail.com>
2025-12-04 01:15:25 +0100

    Rougarou and Carnevale FR translation (#3462)

--- frontend/src/locales/fr/standalone.ts
+++ frontend/src/locales/fr/standalone.ts
@@ -1,7 +1,15 @@
 import curseOfTheRougarou from '@/locales/fr/standalone/curseOfTheRougarou.json'
 import carnevaleOfHorrors from '@/locales/fr/standalone/carnevaleOfHorrors.json'
 import murderAtTheExcelsiorHotel from '@/locales/fr/standalone/murderAtTheExcelsiorHotel.json'
+import fortuneAndFolly from '@/locales/fr/standalone/fortuneAndFolly.json'
 import theMidwinterGala from '@/locales/fr/standalone/theMidwinterGala.json'
 import filmFatale from '@/locales/fr/standalone/filmFatale.json'
 
-export default {curseOfTheRougarou, carnevaleOfHorrors, murderAtTheExcelsiorHotel, theMidwinterGala, filmFatale}
+export default {
+  curseOfTheRougarou,
+  carnevaleOfHorrors,
+  murderAtTheExcelsiorHotel,
+  fortuneAndFolly,
+  theMidwinterGala,
+  filmFatale
+}

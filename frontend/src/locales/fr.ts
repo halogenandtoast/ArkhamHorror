d2b7d31e3f77dd4a1836d121e67f870b44237762
Ayoross <youri.andropov@gmail.com>
2025-12-04 22:33:35 +0100

    Some updates on FR locale

--- frontend/src/locales/fr.ts
+++ frontend/src/locales/fr.ts
@@ -1,5 +1,7 @@
-import base from '@/locales/fr/base.json'
+import log from '@/locales/fr/log.json'
+import cards from '@/locales/fr/cards.json'
 import label from '@/locales/fr/label.json'
+import investigators from '@/locales/fr/investigators.json'
 import campaignLog from '@/locales/fr/campaignLog.json'
 import nightOfTheZealot from '@/locales/fr/nightOfTheZealot'
 import theDunwichLegacy from '@/locales/fr/theDunwichLegacy'
@@ -9,8 +11,29 @@ import theCircleUndone from '@/locales/fr/theCircleUndone'
 import theDreamEaters from '@/locales/fr/theDreamEaters'
 import theInnsmouthConspiracy from '@/locales/fr/theInnsmouthConspiracy'
 import edgeOfTheEarth from '@/locales/fr/edgeOfTheEarth'
+import theScarletKeys from '@/locales/fr/theScarletKeys'
 import standalone from '@/locales/fr/standalone'
 import gameBoard from '@/locales/fr/gameBoard/gameBoard'
 import xp from '@/locales/fr/xp.json'
 
-export default {...base, ...campaignLog, ...gameBoard, label, xp, nightOfTheZealot, theDunwichLegacy, thePathToCarcosa, theForgottenAge, theCircleUndone, theDreamEaters, theInnsmouthConspiracy, edgeOfTheEarth, standalone}
+export default
+  { ...base
+  , ...campaignLog
+  , ...gameBoard
+  , cards
+  , investigators
+  , label: { ...label, cards: cards["label"] }
+  , log
+  , xp
+  , nightOfTheZealot
+  , theDunwichLegacy
+  , thePathToCarcosa
+  , theForgottenAge
+  , returnToTheForgottenAge: theForgottenAge
+  , theCircleUndone
+  , theDreamEaters
+  , theInnsmouthConspiracy
+  , edgeOfTheEarth
+  , theScarletKeys
+  , standalone
+  }

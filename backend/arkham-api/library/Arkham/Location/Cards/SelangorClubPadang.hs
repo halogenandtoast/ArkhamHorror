f5cb36566f15c7b7cbca2b41fc9d1d4690238981
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-05 14:49:47 +0900

    Selangor Club Padang

--- backend/arkham-api/library/Arkham/Location/Cards/SelangorClubPadang.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/SelangorClubPadang.hs
@@ -1,19 +1,36 @@
 module Arkham.Location.Cards.SelangorClubPadang (selangorClubPadang) where
 
+import Arkham.Ability
+import Arkham.Helpers.Modifiers (ModifierType (..), semaphore)
+import Arkham.Helpers.SkillTest.Lifted
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Log
+import Arkham.ScenarioLogKey
 
 newtype SelangorClubPadang = SelangorClubPadang LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 selangorClubPadang :: LocationCard SelangorClubPadang
-selangorClubPadang = symbolLabel $ location SelangorClubPadang Cards.selangorClubPadang 0 (Static 0)
+selangorClubPadang = symbolLabel $ location SelangorClubPadang Cards.selangorClubPadang 3 (PerPlayer 2)
 
 instance HasAbilities SelangorClubPadang where
   getAbilities (SelangorClubPadang a) =
-    extendRevealed a []
+    extendRevealed1 a $ skillTestAbility $ restricted a 1 Here parleyAction_
 
 instance RunMessage SelangorClubPadang where
-  runMessage msg (SelangorClubPadang attrs) = runQueueT $ case msg of
+  runMessage msg l@(SelangorClubPadang attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      sid <- getRandom
+      parley sid iid (attrs.ability 1) iid #combat (Fixed 4)
+      pure l
+    FailedThisSkillTest _iid (isAbilitySource attrs 1 -> True) -> do
+      remember EmbarrassedTheConsulate
+      pure l
+    PassedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
+      semaphore attrs do
+        roundModifier (attrs.ability 1) attrs Semaphore
+        gainResources iid (attrs.ability 1) 3
+      pure l
     _ -> SelangorClubPadang <$> liftRunMessage msg attrs

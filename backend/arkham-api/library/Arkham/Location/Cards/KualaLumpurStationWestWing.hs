97080c08bc7442d4e7d3476c220985f4346a09e4
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-05 14:21:58 +0900

    Kuala Lumpur Station (West Wing)

--- backend/arkham-api/library/Arkham/Location/Cards/KualaLumpurStationWestWing.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/KualaLumpurStationWestWing.hs
@@ -1,19 +1,41 @@
 module Arkham.Location.Cards.KualaLumpurStationWestWing (kualaLumpurStationWestWing) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Helpers
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Log
+import Arkham.ScenarioLogKey
+import Arkham.Scenarios.ShadesOfSuffering.Helpers
 
 newtype KualaLumpurStationWestWing = KualaLumpurStationWestWing LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 kualaLumpurStationWestWing :: LocationCard KualaLumpurStationWestWing
-kualaLumpurStationWestWing = symbolLabel $ location KualaLumpurStationWestWing Cards.kualaLumpurStationWestWing 0 (Static 0)
+kualaLumpurStationWestWing =
+  symbolLabel $ location KualaLumpurStationWestWing Cards.kualaLumpurStationWestWing 4 (PerPlayer 1)
 
 instance HasAbilities KualaLumpurStationWestWing where
   getAbilities (KualaLumpurStationWestWing a) =
-    extendRevealed a []
+    extendRevealed
+      a
+      [ scenarioI18n $ withI18nTooltip "kualaLumpurStationWestWing.resign" $ resignAction a
+      , restricted
+          a
+          1
+          ( Here
+              <> oneOf
+                [not_ $ Remembered FoundACheapMemento, youExist $ InvestigatorWithHealableHorror (a.ability 1)]
+          )
+          $ actionAbilityWithCost (GroupClueCost (PerPlayer 1) (be a))
+      ]
 
 instance RunMessage KualaLumpurStationWestWing where
-  runMessage msg (KualaLumpurStationWestWing attrs) = runQueueT $ case msg of
+  runMessage msg l@(KualaLumpurStationWestWing attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      healHorror iid (attrs.ability 1) 2
+      remember FoundACheapMemento
+      pure l
     _ -> KualaLumpurStationWestWing <$> liftRunMessage msg attrs

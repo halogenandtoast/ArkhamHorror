190627296651c88c61db4a58ea0a96a52fa9de07
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-05 14:02:15 +0900

    Kuala Lumpur Station (East Wing)

--- backend/arkham-api/library/Arkham/Location/Cards/KualaLumpurStationEastWing.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/KualaLumpurStationEastWing.hs
@@ -1,19 +1,45 @@
 module Arkham.Location.Cards.KualaLumpurStationEastWing (kualaLumpurStationEastWing) where
 
+import Arkham.Ability
+import Arkham.Card
+import Arkham.ForMovement
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher hiding (DuringTurn)
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Log
+import Arkham.Message.Lifted.Move
+import Arkham.ScenarioLogKey
 
 newtype KualaLumpurStationEastWing = KualaLumpurStationEastWing LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 kualaLumpurStationEastWing :: LocationCard KualaLumpurStationEastWing
-kualaLumpurStationEastWing = symbolLabel $ location KualaLumpurStationEastWing Cards.kualaLumpurStationEastWing 0 (Static 0)
+kualaLumpurStationEastWing =
+  symbolLabel $ location KualaLumpurStationEastWing Cards.kualaLumpurStationEastWing 3 (PerPlayer 1)
 
 instance HasAbilities KualaLumpurStationEastWing where
   getAbilities (KualaLumpurStationEastWing a) =
-    extendRevealed a []
+    extendRevealed
+      a
+      [ playerLimit PerTurn
+          $ restricted a 1 (Here <> DuringTurn You <> CanMoveTo (ConnectedFrom ForMovement (be a)))
+          $ FastAbility (ResourceCost 2)
+      , groupLimit PerGame
+          $ restricted a 2 Here
+          $ actionAbilityWithCost (DiscardAssetCost $ AssetControlledBy You)
+      ]
 
 instance RunMessage KualaLumpurStationEastWing where
-  runMessage msg (KualaLumpurStationEastWing attrs) = runQueueT $ case msg of
+  runMessage msg l@(KualaLumpurStationEastWing attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      connected <- select $ ConnectedTo ForMovement (be attrs)
+      chooseTargetM iid connected $ moveTo (attrs.ability 1) iid
+      pure l
+    UseCardAbility iid (isSource attrs -> True) 2 _ (discardPayments -> ps) -> do
+      for_ (nonEmpty ps) \((_, card) :| _) -> do
+        gainResources iid (attrs.ability 2) $ printedCardCost card `div` 2
+      remember TradedForAKitten
+      pure l
     _ -> KualaLumpurStationEastWing <$> liftRunMessage msg attrs

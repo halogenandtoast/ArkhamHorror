9f4a5ef26df8fffb78942e061fbc27316382cc17
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-05 10:20:27 +0900

    Fix FR translations

---
+++ frontend/src/locales/fr/theScarletKeys.ts
@@ -0,0 +1,14 @@
+import base from '@/locales/fr/theScarletKeys/base.json'
+import congressOfKeys from '@/locales/fr/theScarletKeys/scenarios/congressOfKeys.json'
+import dancingMad from '@/locales/fr/theScarletKeys/scenarios/dancingMad.json'
+import deadHeat from '@/locales/fr/theScarletKeys/scenarios/deadHeat.json'
+import dealingsInTheDark from '@/locales/fr/theScarletKeys/scenarios/dealingsInTheDark.json'
+import dogsOfWar from '@/locales/fr/theScarletKeys/scenarios/dogsOfWar.json'
+import onThinIce from '@/locales/fr/theScarletKeys/scenarios/onThinIce.json'
+import riddlesAndRain from '@/locales/fr/theScarletKeys/scenarios/riddlesAndRain.json'
+import sanguineShadows from '@/locales/fr/theScarletKeys/scenarios/sanguineShadows.json'
+import shadesOfSuffering from '@/locales/fr/theScarletKeys/scenarios/shadesOfSuffering.json'
+import withoutATrace from '@/locales/fr/theScarletKeys/scenarios/withoutATrace.json'
+
+
+export default {...base, congressOfKeys, dancingMad, deadHeat, dealingsInTheDark, dogsOfWar, onThinIce, riddlesAndRain, sanguineShadows, shadesOfSuffering, withoutATrace}

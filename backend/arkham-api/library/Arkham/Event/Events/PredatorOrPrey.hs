c8d6a040f999ed2fb19110213e3233fb7676c4e6
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-09 19:30:04 +0900

    Predator or Prey Errata
    Should only consider Non Elite Enemies

--- backend/arkham-api/library/Arkham/Event/Events/PredatorOrPrey.hs
+++ backend/arkham-api/library/Arkham/Event/Events/PredatorOrPrey.hs
@@ -18,7 +18,7 @@ predatorOrPrey = event PredatorOrPrey Cards.predatorOrPrey
 instance RunMessage PredatorOrPrey where
   runMessage msg e@(PredatorOrPrey attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
-      enemies <- select AnyInPlayEnemy
+      enemies <- select $ InPlayEnemy NonEliteEnemy
       if null enemies
         then drawCardsIfCan iid attrs 1
         else do

9ccb0670072d1fa85a7d572069e760727902fb54
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-11 18:13:24 +0900

    More Progress in Without a Trace

--- backend/arkham-api/library/Arkham/Act/Cards/EscapingTheOtherworld.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/EscapingTheOtherworld.hs
@@ -1,18 +1,55 @@
 module Arkham.Act.Cards.EscapingTheOtherworld (escapingTheOtherworld) where
 
+import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
-import Arkham.Act.Import.Lifted
+import Arkham.Act.Import.Lifted hiding (AssetDefeated)
+import Arkham.Asset.Cards qualified as Assets
+import Arkham.Helpers.Modifiers (ModifierType (..), modifySelect)
+import Arkham.Keyword qualified as Keyword
+import Arkham.Matcher
+import Arkham.Trait (Trait (Outsider))
 
 newtype EscapingTheOtherworld = EscapingTheOtherworld ActAttrs
-  deriving anyclass (IsAct, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass IsAct
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 escapingTheOtherworld :: ActCard EscapingTheOtherworld
 escapingTheOtherworld = act (3, A) EscapingTheOtherworld Cards.escapingTheOtherworld Nothing
 
+instance HasModifiersFor EscapingTheOtherworld where
+  getModifiersFor (EscapingTheOtherworld a) =
+    modifySelect a (EnemyWithTrait Outsider) [RemoveKeyword Keyword.Aloof]
+
+instance HasAbilities EscapingTheOtherworld where
+  getAbilities = actAbilities \a ->
+    [ mkAbility a 1 $ forced $ AssetDefeated #when ByAny (assetIs Assets.theRedGlovedManHeWasAlwaysThere)
+    , restricted a 2 AllUndefeatedInvestigatorsResigned $ Objective $ forced AnyWindow
+    ]
+
 instance RunMessage EscapingTheOtherworld where
   runMessage msg a@(EscapingTheOtherworld attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      -- noop, will automatically occur
+      pure a
+    UseThisAbility _ (isSource attrs -> True) 2 -> do
+      advancedWithOther attrs
+      pure a
     AdvanceAct (isSide B attrs -> True) _ _ -> do
-      advanceActDeck attrs
+      aliki <-
+        selectAny
+          $ VictoryDisplayCardMatch
+          $ basic
+          $ cardIs Assets.alikiZoniUperetriaTheMaidWithTheScarletSash
+      theRedGlovedMan <-
+        selectAny
+          $ VictoryDisplayCardMatch
+          $ basic
+          $ cardIs Assets.alikiZoniUperetriaTheMaidWithTheScarletSash
+
+      push $ case (aliki, theRedGlovedMan) of
+        (True, True) -> R1
+        (False, True) -> R2
+        (True, False) -> R3
+        (False, False) -> R4
       pure a
     _ -> EscapingTheOtherworld <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Enemy/Cards/MimeticNemesisOtherworldlySubjugator.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/MimeticNemesisOtherworldlySubjugator.hs
@@ -8,6 +8,7 @@ import Arkham.Enemy.Import.Lifted
 import Arkham.Helpers.Window (damagedEnemyAmount)
 import Arkham.Matcher
 import Arkham.Message.Lifted.Choose
+import Arkham.Scenarios.WithoutATrace.Helpers
 
 newtype MimeticNemesisOtherworldlySubjugator = MimeticNemesisOtherworldlySubjugator EnemyAttrs
   deriving anyclass (IsEnemy, HasModifiersFor)
@@ -25,12 +26,23 @@ instance HasAbilities MimeticNemesisOtherworldlySubjugator where
   getAbilities (MimeticNemesisOtherworldlySubjugator a) =
     extend
       a
-      [mkAbility a 1 $ forced $ EnemyWouldTakeDamage #when (be a)]
+      [ mkAbility a 1 $ forced $ EnemyWouldTakeDamage #when (be a)
+      , restricted a 2 (exists $ InvestigatorAt $ locationWithEnemy a) $ forced $ RoundEnds #when
+      ]
 
 instance RunMessage MimeticNemesisOtherworldlySubjugator where
   runMessage msg e@(MimeticNemesisOtherworldlySubjugator attrs) = runQueueT $ case msg of
     UseCardAbility iid (isSource attrs -> True) 1 (damagedEnemyAmount -> n) _ -> do
       hollows <- select $ HollowedCard <> basic (not_ $ cardIs Assets.theRedGlovedManHeWasAlwaysThere)
       chooseNM iid n $ targets hollows $ discardCard iid (attrs.ability 1)
+      pure e
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      selectEach (InvestigatorAt $ locationWithEnemy attrs) \iid -> do
+        cards <- select $ inHandOf NotForPlay iid <> basic NonWeakness
+        chooseOneM iid $ scenarioI18n do
+          labeledValidate' (notNull cards) "mimeticNemesis.hollow" do
+            chooseTargetM iid cards $ hollow iid
+          labeled' "mimeticNemesis.attack" do
+            initiateEnemyAttack attrs (attrs.ability 1) iid
       pure e
     _ -> MimeticNemesisOtherworldlySubjugator <$> liftRunMessage msg attrs

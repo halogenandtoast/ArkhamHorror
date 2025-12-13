a421e3c6970cccc161bd321499d95866c4b98fc1
Fernando Tarriño del Pozo <fernando_sanvi3@hotmail.com>
2025-12-13 18:47:28 +0100

    Creadted "need_translation" json

---
+++ frontend/src/locales/es/gameBoard/gameBoard.ts
@@ -0,0 +1,11 @@
+import base from '@/locales/en/gameBoard/base.json'
+import phase from '@/locales/en/gameBoard/phases.json'
+import skillTest from '@/locales/en/gameBoard/skill_test.json'
+import gameBar from '@/locales/en/gameBoard/game_bar.json'
+import investigator from '@/locales/en/gameBoard/investigator.json'
+import scenario from '@/locales/en/gameBoard/scenario.json'
+import card from '@/locales/en/gameBoard/card.json'
+import upgrade from '@/locales/en/gameBoard/upgrade.json'
+import create from '@/locales/en/gameBoard/create.json'
+
+export default {...base, phase, skillTest, gameBar, investigator, scenario, card, upgrade, create}

028ebdb61db553dbb2c7f41043776dc493cc1dc6
Fernando Tarriño del Pozo <fernando_sanvi3@hotmail.com>
2025-12-15 02:53:28 +0100

    Language esp (#3529)
    * Reorder files for esp
    
    * General translation
    
    Translations have been created for the "base" and "label" files for the generic application texts. A file called "base_old.json" is maintained as a backup of what another user had done previously
    
    * Creadted "need_translation" json
    
    * Translate all from "gameBoard"
    
    * Update es locales file
    
    ---------
    
    Co-authored-by: Matthew Mongeau <halogenandtoast@gmail.com>

--- frontend/src/locales/es.ts
+++ frontend/src/locales/es.ts
@@ -1,4 +1,6 @@
 import base from '@/locales/es/base.json'
+import label from '@/locales/es/label.json'
+import gameBoard from '@/locales/es/gameBoard/gameBoard'
 import nightOfTheZealot from '@/locales/es/nightOfTheZealot.json'
 
-export default {...base, nightOfTheZealot}
+export default {...base, ...label, ...gameBoard, nightOfTheZealot}

fc8f06e3d799e596a1968bd617b027a1879d6674
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-17 10:19:17 +0900

    Congress work

--- backend/arkham-api/library/Arkham/Act/Cards/SecretsAndLiesV3.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/SecretsAndLiesV3.hs
@@ -2,13 +2,27 @@ module Arkham.Act.Cards.SecretsAndLiesV3 (secretsAndLiesV3) where
 
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted
+import Arkham.Campaigns.TheScarletKeys.Concealed.Kind
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.Modifiers (ModifierType (..), modifySelectWith)
+import Arkham.Keyword qualified as Keyword
+import Arkham.Matcher
+import Arkham.Modifier (setActiveDuringSetup)
 
 newtype SecretsAndLiesV3 = SecretsAndLiesV3 ActAttrs
-  deriving anyclass (IsAct, HasModifiersFor)
+  deriving anyclass IsAct
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
 
 secretsAndLiesV3 :: ActCard SecretsAndLiesV3
 secretsAndLiesV3 = act (1, A) SecretsAndLiesV3 Cards.secretsAndLiesV3 Nothing
+
+instance HasModifiersFor SecretsAndLiesV3 where
+  getModifiersFor (SecretsAndLiesV3 a) = do
+    modifySelectWith
+      a
+      (enemyIs Enemies.theRedGlovedManPurposeUnknown)
+      setActiveDuringSetup
+      [AddKeyword $ Keyword.Concealed TheRedGlovedMan (PerPlayer 1), CannotBeDamaged]
 
 instance RunMessage SecretsAndLiesV3 where
   runMessage msg a@(SecretsAndLiesV3 attrs) = runQueueT $ case msg of

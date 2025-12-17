fdba0a6aecb0ff32fef5ec9b28a998fc1702db0d
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-18 06:16:10 +0900

    Some congress

--- backend/arkham-api/library/Arkham/Act/Cards/ToTheTower.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/ToTheTower.hs
@@ -1,5 +1,6 @@
 module Arkham.Act.Cards.ToTheTower (toTheTower) where
 
+import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted
 import Arkham.Helpers.Modifiers (ModifierType (..), modifySelect)
@@ -8,7 +9,7 @@ import Arkham.Trait (Trait (Tower))
 
 newtype ToTheTower = ToTheTower ActAttrs
   deriving anyclass IsAct
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 toTheTower :: ActCard ToTheTower
 toTheTower = act (2, A) ToTheTower Cards.toTheTower Nothing
@@ -17,8 +18,15 @@ instance HasModifiersFor ToTheTower where
   getModifiersFor (ToTheTower a) = do
     modifySelect a (LocationWithTrait Tower) [ScenarioModifier "noCityOfRemnants"]
 
+instance HasAbilities ToTheTower where
+  getAbilities = actAbilities1 \a ->
+    mkAbility a 1 $ Objective $ forced $ Enters #after Anyone "Gravity-Defying Climb"
+
 instance RunMessage ToTheTower where
   runMessage msg a@(ToTheTower attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      advancedWithOther attrs
+      pure a
     AdvanceAct (isSide B attrs -> True) _ _ -> do
       advanceActDeck attrs
       pure a

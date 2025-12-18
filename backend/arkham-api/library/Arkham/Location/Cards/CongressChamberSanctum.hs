7b62495e8e1a1bbfd65fa833510b0ca630fc64e7
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-18 19:55:07 +0900

    Some locations

--- backend/arkham-api/library/Arkham/Location/Cards/CongressChamberSanctum.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/CongressChamberSanctum.hs
@@ -1,7 +1,12 @@
 module Arkham.Location.Cards.CongressChamberSanctum (congressChamberSanctum) where
 
+import Arkham.Ability
+import Arkham.Capability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Strategy
+import Arkham.Trait (Trait (Conspirator))
 
 newtype CongressChamberSanctum = CongressChamberSanctum LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +17,13 @@ congressChamberSanctum = location CongressChamberSanctum Cards.congressChamberSa
 
 instance HasAbilities CongressChamberSanctum where
   getAbilities (CongressChamberSanctum a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ restricted a 1 (Here <> youExist can.search.deck)
+      $ actionAbilityWithCost (ExhaustAssetCost $ AssetWithTrait Conspirator <> AssetControlledBy You)
 
 instance RunMessage CongressChamberSanctum where
-  runMessage msg (CongressChamberSanctum attrs) = runQueueT $ case msg of
+  runMessage msg l@(CongressChamberSanctum attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      search iid (attrs.ability 1) iid [fromTopOfDeck 6] #asset (DrawFound iid 1)
+      pure l
     _ -> CongressChamberSanctum <$> liftRunMessage msg attrs

459dfdcf97843927986dd3b3e47e172ef5745b3b
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-18 17:15:53 +0900

    The Knotted Tower

--- backend/arkham-api/library/Arkham/Location/Cards/TheKnottedTower.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/TheKnottedTower.hs
@@ -1,19 +1,35 @@
 module Arkham.Location.Cards.TheKnottedTower (theKnottedTower) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Message.Lifted.Move
+import Arkham.Window (Window (..))
+import Arkham.Window qualified as Window
 
 newtype TheKnottedTower = TheKnottedTower LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theKnottedTower :: LocationCard TheKnottedTower
-theKnottedTower = symbolLabel $ locationWith TheKnottedTower Cards.theKnottedTower 0 (Static 0) connectsToAdjacent
+theKnottedTower = locationWith TheKnottedTower Cards.theKnottedTower 3 (Static 0) connectsToAdjacent
 
 instance HasAbilities TheKnottedTower where
   getAbilities (TheKnottedTower a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ restricted a 1 Here
+      $ freeReaction (ScenarioEvent #after (Just You) "exposedAdjacentLocation")
+
+getExposedLocation :: [Window] -> LocationId
+getExposedLocation = \case
+  [] -> error "missing exposed location"
+  ((windowType -> Window.ScenarioEvent "exposedAdjacentLocation" _ value) : _) -> toResult value
+  (_ : xs) -> getExposedLocation xs
 
 instance RunMessage TheKnottedTower where
-  runMessage msg (TheKnottedTower attrs) = runQueueT $ case msg of
+  runMessage msg l@(TheKnottedTower attrs) = runQueueT $ case msg of
+    UseCardAbility iid (isSource attrs -> True) 1 ws _ -> do
+      moveTo (attrs.ability 1) iid (getExposedLocation ws)
+      pure l
     _ -> TheKnottedTower <$> liftRunMessage msg attrs

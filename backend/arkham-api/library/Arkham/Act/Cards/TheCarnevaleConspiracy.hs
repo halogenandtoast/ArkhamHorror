69c9c893b92daf022d3d18801ba8b71d3c8d914e
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-31 20:14:51 +0900

    Fix San Marco Basilica
    Place cards underneath correctly

--- backend/arkham-api/library/Arkham/Act/Cards/TheCarnevaleConspiracy.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/TheCarnevaleConspiracy.hs
@@ -2,87 +2,51 @@ module Arkham.Act.Cards.TheCarnevaleConspiracy (theCarnevaleConspiracy) where
 
 import Arkham.Ability
 import Arkham.Act.Cards qualified as Cards
-import Arkham.Act.Runner
+import Arkham.Act.Import.Lifted
 import Arkham.Asset.Cards qualified as Assets
-import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Enemies
-import Arkham.Helpers.Query
 import Arkham.Helpers.Modifiers
 import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Placement
-import Arkham.Prelude
 
 newtype TheCarnevaleConspiracy = TheCarnevaleConspiracy ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 theCarnevaleConspiracy :: ActCard TheCarnevaleConspiracy
-theCarnevaleConspiracy =
-  act (1, A) TheCarnevaleConspiracy Cards.theCarnevaleConspiracy Nothing
+theCarnevaleConspiracy = act (1, A) TheCarnevaleConspiracy Cards.theCarnevaleConspiracy Nothing
 
 instance HasAbilities TheCarnevaleConspiracy where
-  getAbilities (TheCarnevaleConspiracy x)
-    | onSide A x =
-        [ restrictedAbility
-            x
-            1
-            ( AssetExists
-                $ AssetWithTitle "Masked Carnevale-Goer"
-                <> AssetWithoutModifier CannotBeRevealed
-            )
-            ( ActionAbility []
-                $ Costs [ActionCost 1, GroupClueCost (PerPlayer 1) Anywhere]
-            )
-        , restrictedAbility
-            x
-            2
-            ( UnderneathCardCount
-                (EqualTo $ Static 3)
-                (UnderActDeck <> UnderAgendaDeck)
-                (oneOf [cardIs Assets.innocentReveler, CardWithTitle "Masked Carnevale-Goer"])
-            )
-            $ ForcedAbility AnyWindow
-        ]
-  getAbilities _ = []
+  getAbilities = actAbilities \x ->
+    [ restricted
+        x
+        1
+        (exists $ "Masked Carnevale-Goer" <> AssetWithoutModifier CannotBeRevealed)
+        (actionAbilityWithCost $ GroupClueCost (PerPlayer 1) Anywhere)
+    , restricted
+        x
+        2
+        ( UnderneathCardCount (EqualTo $ Static 3) (UnderActDeck <> UnderAgendaDeck)
+            $ oneOf [cardIs Assets.innocentReveler, CardWithTitle "Masked Carnevale-Goer"]
+        )
+        $ forced AnyWindow
+    ]
 
 instance RunMessage TheCarnevaleConspiracy where
-  runMessage msg a@(TheCarnevaleConspiracy attrs@ActAttrs {..}) = case msg of
-    UseCardAbility iid source 1 _ _ | isSource attrs source -> do
-      maskedCarnevaleGoers <-
-        select
-          (AssetWithTitle "Masked Carnevale-Goer")
-      filteredMaskedCarnevaleGoers <-
-        flip filterM maskedCarnevaleGoers $ \aid -> do
-          modifiers' <- getModifiers (AssetTarget aid)
-          pure $ CannotBeRevealed `notElem` modifiers'
-      case filteredMaskedCarnevaleGoers of
-        [] -> pure ()
-        xs -> do
-          player <- getPlayer iid
-          push
-            $ chooseOne
-              player
-              [targetLabel x [LookAtRevealed iid (toSource attrs) (AssetTarget x)] | x <- xs]
+  runMessage msg a@(TheCarnevaleConspiracy attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      maskedCarnevaleGoers <- select $ "Masked Carnevale-Goer" <> AssetWithoutModifier CannotBeRevealed
+      chooseTargetM iid maskedCarnevaleGoers (lookAtRevealed iid (attrs.ability 1))
       pure a
-    UseCardAbility _ source 2 _ _ | isSource attrs source -> do
-      a <$ push (AdvanceAct (toId attrs) source AdvancedWithOther)
-    AdvanceAct aid _ _ | aid == actId && onSide B attrs -> do
-      (leadInvestigatorId, lead) <- getLeadInvestigatorPlayer
-      cnidathqua <- getSetAsideCard Enemies.cnidathqua
-      maskedCarnevaleGoers <-
-        select
-          (AssetWithTitle "Masked Carnevale-Goer")
-      let
-        flipMsg = case maskedCarnevaleGoers of
-          [] -> []
-          xs ->
-            [ chooseOne
-                lead
-                [ targetLabel x [Flip leadInvestigatorId (toSource attrs) (AssetTarget x)]
-                | x <- xs
-                ]
-            ]
-      createCnidathqua <- toMessage <$> createEnemy cnidathqua Global
-      pushAll $ [createCnidathqua, advanceActDeck attrs] <> flipMsg
+    UseThisAbility _ (isSource attrs -> True) 2 -> do
+      advancedWithOther attrs
       pure a
-    _ -> TheCarnevaleConspiracy <$> runMessage msg attrs
+    AdvanceAct (isSide B attrs -> True) _ _ -> do
+      createEnemyCard_ Enemies.cnidathqua Global
+      advanceActDeck attrs
+      lead <- getLead
+      maskedCarnevaleGoers <- select (AssetWithTitle "Masked Carnevale-Goer")
+      chooseTargetM lead maskedCarnevaleGoers (flipOverBy lead (attrs.ability 2))
+      pure a
+    _ -> TheCarnevaleConspiracy <$> liftRunMessage msg attrs

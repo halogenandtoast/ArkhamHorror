81c8fdcb64177c54ce727e7c6f3c8ccf5a19e440
Matthew Mongeau <halogenandtoast@gmail.com>
2025-12-31 20:34:34 +0900

    Fix Chaos at the Carnevale
    Cnidathqua might not be in play

--- backend/arkham-api/library/Arkham/Agenda/Cards/ChaosAtTheCarnevale.hs
+++ backend/arkham-api/library/Arkham/Agenda/Cards/ChaosAtTheCarnevale.hs
@@ -2,18 +2,13 @@ module Arkham.Agenda.Cards.ChaosAtTheCarnevale (chaosAtTheCarnevale) where
 
 import Arkham.Ability
 import Arkham.Agenda.Cards qualified as Cards
-import Arkham.Agenda.Runner
+import Arkham.Agenda.Import.Lifted
 import Arkham.Asset.Cards qualified as Assets
 import Arkham.Attack
-import Arkham.Classes
 import Arkham.Enemy.Cards qualified as Enemies
-import Arkham.GameValue
-import Arkham.Helpers.Query
+import Arkham.Helpers.Window (getEnemy)
 import Arkham.Matcher
-import Arkham.Prelude
-import Arkham.Timing qualified as Timing
-import Arkham.Window (Window (..))
-import Arkham.Window qualified as Window
+import Arkham.Strategy
 
 newtype ChaosAtTheCarnevale = ChaosAtTheCarnevale AgendaAttrs
   deriving anyclass (IsAgenda, HasModifiersFor)
@@ -24,27 +19,19 @@ chaosAtTheCarnevale = agenda (3, A) ChaosAtTheCarnevale Cards.chaosAtTheCarneval
 
 instance HasAbilities ChaosAtTheCarnevale where
   getAbilities (ChaosAtTheCarnevale x) =
-    [ mkAbility x 1
-        $ ForcedAbility
-        $ EnemySpawns Timing.After Anywhere
-        $ enemyIs Enemies.writhingAppendage
+    [ mkAbility x 1 $ forced $ EnemySpawns #after Anywhere $ enemyIs Enemies.writhingAppendage
     | onSide A x
     ]
 
 instance RunMessage ChaosAtTheCarnevale where
-  runMessage msg a@(ChaosAtTheCarnevale attrs@AgendaAttrs {..}) = case msg of
-    UseCardAbility _ source 1 [(windowType -> Window.EnemySpawns eid _)] _
-      | isSource attrs source -> a <$ push (PlaceDoom (toAbilitySource attrs 1) (toTarget eid) 2)
-    AdvanceAgenda aid | aid == agendaId && onSide B attrs -> do
-      investigatorIds <- getInvestigators
-      cnidathquaId <- selectJust $ enemyIs Enemies.cnidathqua
-      pushAll
-        $ [ EnemyAttack
-              $ enemyAttack cnidathquaId attrs iid
-              & damageStrategyL
-              .~ DamageFirst Assets.innocentReveler
-          | iid <- investigatorIds
-          ]
-        <> [RevertAgenda (toId attrs)]
+  runMessage msg a@(ChaosAtTheCarnevale attrs) = runQueueT $ case msg of
+    UseCardAbility _ (isSource attrs -> True) 1 (getEnemy -> eid) _ -> do
+      placeDoom (attrs.ability 1) eid 2
       pure a
-    _ -> ChaosAtTheCarnevale <$> runMessage msg attrs
+    AdvanceAgenda (isSide B attrs -> True) -> do
+      whenJustM (selectOne $ enemyIs Enemies.cnidathqua) \cnidathqua ->
+        eachInvestigator \iid -> do
+          initiateEnemyAttackEdit cnidathqua attrs iid (damageStrategyL .~ DamageFirst Assets.innocentReveler)
+      revertAgenda attrs
+      pure a
+    _ -> ChaosAtTheCarnevale <$> liftRunMessage msg attrs

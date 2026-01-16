1cb64e1208cf20a027121d687bc2f3f7d6d98131
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-16 09:52:28 +0900

    Fix Angered Spirits

--- backend/arkham-api/library/Arkham/Treachery/Cards/AngeredSpirits.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/AngeredSpirits.hs
@@ -1,7 +1,9 @@
 module Arkham.Treachery.Cards.AngeredSpirits (angeredSpirits) where
 
-import Arkham.Ability
+import Control.Lens (cosmos, _head)
+import Arkham.Ability hiding (cosmos)
 import Arkham.Asset.Uses
+import Arkham.Helpers.Ref
 import Arkham.Matcher hiding (FastPlayerWindow)
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
@@ -15,19 +17,25 @@ angeredSpirits = treachery AngeredSpirits Cards.angeredSpirits
 
 instance HasAbilities AngeredSpirits where
   getAbilities (AngeredSpirits a) =
-    restricted a 1 OnSameLocation (FastAbility $ ExhaustAssetCost $ #spell <> AssetControlledBy You <> AssetWithUses Charge)
+    restricted
+      a
+      1
+      OnSameLocation
+      (FastAbility $ ExhaustAssetCost $ #spell <> AssetControlledBy You <> AssetWithUses Charge)
       : [ restricted a 2 (TokensOnThis #charge $ lessThan 4) (forcedOnElimination iid)
         | iid <- maybeToList a.owner
         ]
+
+exhaustPayment :: Payment -> Maybe Target
+exhaustPayment = preview (cosmos . _ExhaustPayment . _head)
 
 instance RunMessage AngeredSpirits where
   runMessage msg t@(AngeredSpirits attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
       placeInThreatArea attrs iid
       pure t
-    UseCardAbility _ (isSource attrs -> True) 1 _ (ExhaustPayment [target]) -> do
-      spendUses (attrs.ability 1) target Charge 1
-      placeTokens (attrs.ability 1) attrs Charge 1
+    UseCardAbility _ (isSource attrs -> True) 1 _ (exhaustPayment -> Just target) -> do
+      moveTokens (attrs.ability 1) (targetToSource target) attrs Charge 1
       pure t
     UseThisAbility _ (isSource attrs -> True) 2 -> do
       withTreacheryInvestigator attrs (`sufferPhysicalTrauma` 1)

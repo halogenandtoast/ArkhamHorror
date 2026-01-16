7884811696b6e0ba031e6369c79e72b2b9de7a4e
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-17 08:54:33 +0900

    Cleanup Glimpse the Unthinkable (1)

--- backend/arkham-api/library/Arkham/Event/Events/GlimpseTheUnthinkable1.hs
+++ backend/arkham-api/library/Arkham/Event/Events/GlimpseTheUnthinkable1.hs
@@ -1,15 +1,7 @@
-module Arkham.Event.Events.GlimpseTheUnthinkable1 (
-  glimpseTheUnthinkable1,
-  GlimpseTheUnthinkable1 (..),
-) where
+module Arkham.Event.Events.GlimpseTheUnthinkable1 (glimpseTheUnthinkable1) where
 
-import Arkham.Prelude
-
-import Arkham.Card
-import Arkham.Classes
-import Arkham.Deck
 import Arkham.Event.Cards qualified as Cards
-import Arkham.Event.Runner
+import Arkham.Event.Import.Lifted
 import Arkham.Matcher
 
 newtype GlimpseTheUnthinkable1 = GlimpseTheUnthinkable1 EventAttrs
@@ -17,52 +9,22 @@ newtype GlimpseTheUnthinkable1 = GlimpseTheUnthinkable1 EventAttrs
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 glimpseTheUnthinkable1 :: EventCard GlimpseTheUnthinkable1
-glimpseTheUnthinkable1 =
-  event GlimpseTheUnthinkable1 Cards.glimpseTheUnthinkable1
+glimpseTheUnthinkable1 = event GlimpseTheUnthinkable1 Cards.glimpseTheUnthinkable1
 
 instance RunMessage GlimpseTheUnthinkable1 where
-  runMessage msg e@(GlimpseTheUnthinkable1 attrs) = case msg of
-    InvestigatorPlayEvent iid eid mtarget windows' _ | eid == toId attrs -> do
-      let drawing = drawCards iid attrs 1
-      pushAll
-        [ drawing
-        , ResolveEvent iid eid mtarget windows'
-        ]
+  runMessage msg e@(GlimpseTheUnthinkable1 attrs) = runQueueT $ case msg of
+    PlayThisEvent iid (is attrs -> True) -> do
+      drawCards iid attrs 1
+      doStep 1 msg
       pure e
-    ResolveEvent iid eid _ _ | eid == toId attrs -> do
-      cards <-
-        select
-          $ InHandOf NotForPlay (InvestigatorWithId iid)
-          <> BasicCardMatch NonWeakness
-      player <- getPlayer iid
-      pushM
-        $ chooseAmounts
-          player
-          "Choose number of cards to discard"
-          (MaxAmountTarget $ length cards)
-          [("Number of cards to discard", (0, length cards))]
-          (toTarget attrs)
+    DoStep 1 (PlayThisEvent iid (is attrs -> True)) -> do
+      cards <- select $ inHandOf NotForPlay iid <> basic NonWeakness
+      chooseAmount iid "Choose number of cards to discard" "$cards" 0 (length cards) attrs
       pure e
     ResolveAmounts iid choices (isTarget attrs -> True) -> do
-      let
-        n = getChoiceAmount "Number of cards to discard" choices
-      cards <-
-        select
-          $ InHandOf NotForPlay (InvestigatorWithId iid)
-          <> BasicCardMatch NonWeakness
-      let drawing = drawCards iid attrs n
-      player <- getPlayer iid
-      pushAll
-        [ chooseN
-            player
-            n
-            [ TargetLabel
-              (CardIdTarget $ toCardId c)
-              [ShuffleCardsIntoDeck (InvestigatorDeck iid) [c]]
-            | c <- cards
-            ]
-        , drawing
-        ]
-
+      let n = getChoiceAmount "$cards" choices
+      cards <- select $ inHandOf NotForPlay iid <> basic NonWeakness
+      chooseNM iid n $ targets cards $ shuffleCardsIntoDeck iid . only
+      drawCards iid attrs n
       pure e
-    _ -> GlimpseTheUnthinkable1 <$> runMessage msg attrs
+    _ -> GlimpseTheUnthinkable1 <$> liftRunMessage msg attrs

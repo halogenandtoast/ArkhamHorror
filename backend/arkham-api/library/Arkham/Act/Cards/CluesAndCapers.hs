90683df8a75418340d1eb89dd73cd8ae4e51ce6f
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-21 06:38:29 +0900

    Fix Riddles and Rain agenda doom
    Should carry over when the act advances the agenda

--- backend/arkham-api/library/Arkham/Act/Cards/CluesAndCapers.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/CluesAndCapers.hs
@@ -3,8 +3,11 @@ module Arkham.Act.Cards.CluesAndCapers (cluesAndCapers) where
 import Arkham.Act.Cards qualified as Cards
 import Arkham.Act.Import.Lifted
 import Arkham.Agenda.Cards qualified as Agendas
+import Arkham.Agenda.Types (Field (..))
 import Arkham.Enemy.Cards qualified as Enemies
 import Arkham.Location.Cards qualified as Locations
+import Arkham.Matcher.Agenda
+import Arkham.Projection
 
 newtype CluesAndCapers = CluesAndCapers ActAttrs
   deriving anyclass (IsAct, HasModifiersFor)
@@ -19,8 +22,10 @@ instance RunMessage CluesAndCapers where
       placeSetAsideLocations_ [Locations.kensingtonGardens, Locations.westminsterAbbey, Locations.bigBen]
       lead <- getLead
       theRedGlovedMan <- fetchCard Enemies.theRedGlovedManShroudedInMystery
+      doom <- field AgendaDoom =<< selectJust AnyAgenda
       drawCard lead theRedGlovedMan
       advanceToAgendaA attrs Agendas.figuresInTheFog
       advanceActDeck attrs
+      placeDoomOnAgenda doom
       pure a
     _ -> CluesAndCapers <$> liftRunMessage msg attrs

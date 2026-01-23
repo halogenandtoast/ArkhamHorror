f9ffab8266aa5ce92d8a49cefb6a2f2fec3bc6e8
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 06:55:39 +0900

    Empyrean Brilliance, Captivating Gleam

--- backend/arkham-api/library/Arkham/Treachery/Cards/CaptivatingGleam.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/CaptivatingGleam.hs
@@ -1,16 +1,29 @@
 module Arkham.Treachery.Cards.CaptivatingGleam (captivatingGleam) where
 
+import Arkham.Ability
+import Arkham.Matcher
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
 newtype CaptivatingGleam = CaptivatingGleam TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsTreachery, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 captivatingGleam :: TreacheryCard CaptivatingGleam
 captivatingGleam = treachery CaptivatingGleam Cards.captivatingGleam
 
+instance HasAbilities CaptivatingGleam where
+  getAbilities (CaptivatingGleam a) =
+    [restricted a 1 (InThreatAreaOf You <> youExist (HandWith NoCards)) $ forced AnyWindow]
+
 instance RunMessage CaptivatingGleam where
   runMessage msg t@(CaptivatingGleam attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      whenNone (treacheryInThreatAreaOf iid <> treacheryIs Cards.captivatingGleam) do
+        placeInThreatArea attrs iid
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      assignHorror iid (attrs.ability 1) 5
+      toDiscardBy iid (attrs.ability 1) attrs
+      pure t
     _ -> CaptivatingGleam <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Treachery/Cards/EmpyreanBrilliance.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/EmpyreanBrilliance.hs
@@ -1,5 +1,10 @@
 module Arkham.Treachery.Cards.EmpyreanBrilliance (empyreanBrilliance) where
 
+import Arkham.Campaigns.TheFeastOfHemlockVale.Helpers
+import Arkham.Card.Cost
+import Arkham.Helpers.Message.Discard.Lifted
+import Arkham.Investigator.Types (Field (..))
+import Arkham.Projection
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +17,26 @@ empyreanBrilliance = treachery EmpyreanBrilliance Cards.empyreanBrilliance
 
 instance RunMessage EmpyreanBrilliance where
   runMessage msg t@(EmpyreanBrilliance attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      getCampaignTime >>= \case
+        Day -> do
+          n <- fieldMap InvestigatorHand length iid
+          sid <- getRandom
+          revelationSkillTest sid iid attrs #willpower (Fixed $ 2 + if n >= 5 then 2 else 0)
+        Night -> randomDiscardEdit iid attrs \d -> d {discardTarget = Just (toTarget attrs)}
+      pure t
+    DiscardedCards iid _ (isTarget attrs -> True) cards -> do
+      let
+        totalPrintedCost =
+          case mapMaybe (.cost) cards of
+            [] -> 0
+            costs -> sum (map toPrintedCost costs)
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #willpower (Fixed totalPrintedCost)
+      pure t
+    FailedThisSkillTest iid (isSource attrs -> True) -> do
+      getCampaignTime >>= \case
+        Day -> chooseAndDiscardCards iid attrs 2
+        Night -> assignHorror iid attrs 2
+      pure t
     _ -> EmpyreanBrilliance <$> liftRunMessage msg attrs

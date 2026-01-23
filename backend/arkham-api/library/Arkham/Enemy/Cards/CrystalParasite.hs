620659fb01b3b770652698768041b38723b12bcd
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-23 21:15:06 +0900

    Chroma Blight and Crystal Parasite

--- backend/arkham-api/library/Arkham/Enemy/Cards/CrystalParasite.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/CrystalParasite.hs
@@ -1,15 +1,37 @@
 module Arkham.Enemy.Cards.CrystalParasite (crystalParasite) where
 
+import Arkham.Ability
+import Arkham.Campaigns.TheFeastOfHemlockVale.Helpers
 import Arkham.Enemy.Cards qualified as Cards
-import Arkham.Enemy.Import.Lifted
+import Arkham.Enemy.Import.Lifted hiding (EnemyAttacks)
+import Arkham.Enemy.Types.Attrs (enemyDamage)
+import Arkham.Helpers.Modifiers (ModifierType (..), modifySelf)
+import Arkham.Matcher
 
 newtype CrystalParasite = CrystalParasite EnemyAttrs
-  deriving anyclass (IsEnemy, HasModifiersFor)
-  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity, HasAbilities)
+  deriving anyclass IsEnemy
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 crystalParasite :: EnemyCard CrystalParasite
 crystalParasite = enemy CrystalParasite Cards.crystalParasite (2, Static 6, 2) (1, 1)
 
+instance HasAbilities CrystalParasite where
+  getAbilities (CrystalParasite a) =
+    extend1 a
+      $ restricted a 1 IsDay
+      $ forced
+      $ EnemyAttacks #after Anyone AnyEnemyAttack (be a <> EnemyWithAnyDamage)
+
+instance HasModifiersFor CrystalParasite where
+  getModifiersFor (CrystalParasite a) = do
+    time <- getCampaignTime
+    let n = enemyDamage a `div` 2
+    when (time == Night && n > 0) do
+      modifySelf a [DamageDealt n, EnemyFight (n * 2)]
+
 instance RunMessage CrystalParasite where
-  runMessage msg (CrystalParasite attrs) = runQueueT $ case msg of
+  runMessage msg e@(CrystalParasite attrs) = runQueueT $ case msg of
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
+      healDamage attrs (attrs.ability 1) 2
+      pure e
     _ -> CrystalParasite <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Treachery/Cards/ChromaBlight.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/ChromaBlight.hs
@@ -1,16 +1,39 @@
 module Arkham.Treachery.Cards.ChromaBlight (chromaBlight) where
 
+import Arkham.Ability
+import Arkham.Enemy.Cards qualified as Enemies
+import Arkham.Helpers.Location (withLocationOf)
+import Arkham.Matcher
+import Arkham.Token
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
 newtype ChromaBlight = ChromaBlight TreacheryAttrs
-  deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
+  deriving anyclass (IsTreachery, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 chromaBlight :: TreacheryCard ChromaBlight
 chromaBlight = treachery ChromaBlight Cards.chromaBlight
 
+instance HasAbilities ChromaBlight where
+  getAbilities (ChromaBlight a) =
+    [ restricted a 1 (InThreatAreaOf You) $ forced $ DrawsCards #after You AnyCards (atLeast 1)
+    , restricted a 2 (InThreatAreaOf You <> criteria) $ forced AnyWindow
+    ]
+   where
+    criteria = if a.token Brilliance >= 6 then NoRestriction else Never
+
 instance RunMessage ChromaBlight where
   runMessage msg t@(ChromaBlight attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      whenNone (treacheryInThreatAreaOf iid <> treacheryIs Cards.chromaBlight) do
+        placeInThreatArea attrs iid
+      pure t
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
+      placeTokens (attrs.ability 1) attrs Brilliance 1
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      withLocationOf iid $ createSetAsideEnemy_ Enemies.crystalParasite
+      removeFromGame attrs
+      pure t
     _ -> ChromaBlight <$> liftRunMessage msg attrs

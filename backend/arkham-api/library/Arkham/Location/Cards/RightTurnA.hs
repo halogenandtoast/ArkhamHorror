2607fdf7bd21379097d5bd495e9704cddf718622
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 18:03:07 +0900

    Right Turns

--- backend/arkham-api/library/Arkham/Location/Cards/RightTurnA.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/RightTurnA.hs
@@ -1,7 +1,14 @@
 module Arkham.Location.Cards.RightTurnA (rightTurnA) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Grid
 import Arkham.Location.Import.Lifted
+import Arkham.Location.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Projection
+import Arkham.Scenarios.WrittenInRock.Helpers
 
 newtype RightTurnA = RightTurnA LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +19,20 @@ rightTurnA = location RightTurnA Cards.rightTurnA 2 (PerPlayer 1)
 
 instance HasAbilities RightTurnA where
   getAbilities (RightTurnA a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ groupLimit PerGame
+      $ restricted a 1 (Here <> HasAdjacentLocations LocationCanBeSwapped) doubleActionAbility
 
 instance RunMessage RightTurnA where
-  runMessage msg (RightTurnA attrs) = runQueueT $ case msg of
+  runMessage msg l@(RightTurnA attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      locations <- select LocationCanBeSwapped
+      chooseTargetM iid locations $ handleTarget iid (attrs.ability 1)
+      pure l
+    HandleTargetChoice iid (isAbilitySource attrs 1 -> True) (LocationTarget lid) -> do
+      pos <- fieldJust LocationPosition lid
+      locations <-
+        select $ mapOneOf LocationInPosition (adjacentPositions pos) <> LocationCanBeSwapped
+      chooseTargetM iid locations (swapLocations lid)
+      pure l
     _ -> RightTurnA <$> liftRunMessage msg attrs
--- backend/arkham-api/library/Arkham/Location/Cards/RightTurnB.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/RightTurnB.hs
@@ -1,7 +1,11 @@
 module Arkham.Location.Cards.RightTurnB (rightTurnB) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Grid
 import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Choose
+import Arkham.Scenarios.WrittenInRock.Helpers
 
 newtype RightTurnB = RightTurnB LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +16,24 @@ rightTurnB = location RightTurnB Cards.rightTurnB 2 (PerPlayer 1)
 
 instance HasAbilities RightTurnB where
   getAbilities (RightTurnB a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ groupLimit PerGame
+      $ restricted a 1 (Here <> exists LocationCanBeSlid)
+      $ actionAbilityWithCost (ResourceCost 2)
 
 instance RunMessage RightTurnB where
   runMessage msg (RightTurnB attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      locations <- select LocationCanBeSlid
+      chooseTargetM iid locations $ handleTarget iid (attrs.ability 1)
+      pure l
+    HandleTargetChoice iid (isAbilitySource attrs 1 -> True) (LocationTarget lid) -> do
+      slideLocations <-
+        matches lid LocationCanBeSlid >>= \case
+          False -> pure []
+          True -> getEmptyPositions lid
+      chooseOneM iid do
+        for_ slideLocations \newPos ->
+          gridLabeled (gridLabel newPos) $ push $ PlaceGrid (GridLocation newPos lid)
+      pure l
     _ -> RightTurnB <$> liftRunMessage msg attrs

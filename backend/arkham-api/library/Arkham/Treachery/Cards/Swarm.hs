0ac709d1f7961f0e25b9ff46f552b600832a649a
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 09:34:38 +0900

    Swarm

--- backend/arkham-api/library/Arkham/Treachery/Cards/Swarm.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Swarm.hs
@@ -1,5 +1,8 @@
 module Arkham.Treachery.Cards.Swarm (swarm) where
 
+import Arkham.I18n
+import Arkham.Investigator.Projection ()
+import Arkham.Message.Lifted.Choose
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +15,18 @@ swarm = treachery Swarm Cards.swarm
 
 instance RunMessage Swarm where
   runMessage msg t@(Swarm attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      sid <- getRandom
+      revelationSkillTest sid iid attrs #agility (Fixed 3)
+      pure t
+    FailedThisSkillTestBy _iid (isSource attrs -> True) n -> do
+      doStep n msg
+      pure t
+    DoStep n msg'@(FailedThisSkillTest iid (isSource attrs -> True)) | n > 0 -> do
+      resourceOk <- (> 0) <$> iid.resources
+      chooseOrRunOneM iid $ withI18n do
+        countVar 1 $ labeledValidate' resourceOk "loseResources" $ loseResources iid attrs 1
+        countVar 1 $ labeled' "takeDamage" $ assignDamage iid attrs 1
+      doStep (n - 1) msg'
+      pure t
     _ -> Swarm <$> liftRunMessage msg attrs

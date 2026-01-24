6b9abe47c6a312b59760e82e6e617a3d6eaabfcf
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 21:56:12 +0900

    Fix Spider of Leng

--- backend/arkham-api/library/Arkham/Enemy/Cards/SpiderOfLeng.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/SpiderOfLeng.hs
@@ -23,7 +23,7 @@ instance RunMessage SpiderOfLeng where
   runMessage msg e@(SpiderOfLeng attrs) = runQueueT $ case msg of
     UseThisAbility _iid (isSource attrs -> True) 1 -> do
       lead <- getLead
-      swarmsOfSpiders <- select $ enemyIs Cards.swarmOfSpiders <> not_ IsSwarm
+      swarmsOfSpiders <- select $ InPlayEnemy $ enemyIs Cards.swarmOfSpiders <> not_ IsSwarm
       if null swarmsOfSpiders
         then findEncounterCard lead attrs Cards.swarmOfSpiders
         else chooseOneAtATimeM lead $ targets swarmsOfSpiders \eid -> push $ PlaceSwarmCards lead eid 1

d533c11c5b950deeddf9f0d99b3d41f0778af26a
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 19:41:51 +0900

    Alkaline Rail

--- backend/arkham-api/library/Arkham/Location/Cards/AlkalineRailB.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/AlkalineRailB.hs
@@ -1,7 +1,10 @@
 module Arkham.Location.Cards.AlkalineRailB (alkalineRailB) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
+import Arkham.Matcher
+import Arkham.Trait (Trait (Resident))
 
 newtype AlkalineRailB = AlkalineRailB LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +15,15 @@ alkalineRailB = symbolLabel $ location AlkalineRailB Cards.alkalineRailB 3 (PerP
 
 instance HasAbilities AlkalineRailB where
   getAbilities (AlkalineRailB a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ restricted a 1 (oneOf [Here, exists $ assetAt a <> withTrait Resident])
+      $ forced
+      $ RoundEnds #when
 
 instance RunMessage AlkalineRailB where
-  runMessage msg (AlkalineRailB attrs) = runQueueT $ case msg of
+  runMessage msg l@(AlkalineRailB attrs) = runQueueT $ case msg of
+    UseThisAbility _ (isSource attrs -> True) 1 -> do
+      selectEach (investigatorAt attrs) (assignDamageTo (attrs.ability 1) 1)
+      selectEach (assetAt attrs <> withTrait Resident) \a -> dealAssetDirectDamage a (attrs.ability 1) 1
+      pure l
     _ -> AlkalineRailB <$> liftRunMessage msg attrs

3f4bd250853bfb580103778c5e7688a0954b7637
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 18:00:54 +0900

    Left Turns

--- backend/arkham-api/library/Arkham/Location/Cards/LeftTurnA.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/LeftTurnA.hs
@@ -1,7 +1,14 @@
 module Arkham.Location.Cards.LeftTurnA (leftTurnA) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Grid
 import Arkham.Location.Import.Lifted
+import Arkham.Location.Types (Field (..))
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Projection
+import Arkham.Scenarios.WrittenInRock.Helpers
 
 newtype LeftTurnA = LeftTurnA LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +19,20 @@ leftTurnA = location LeftTurnA Cards.leftTurnA 2 (PerPlayer 1)
 
 instance HasAbilities LeftTurnA where
   getAbilities (LeftTurnA a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ groupLimit PerGame
+      $ restricted a 1 (Here <> HasAdjacentLocations LocationCanBeSwapped) doubleActionAbility
 
 instance RunMessage LeftTurnA where
-  runMessage msg (LeftTurnA attrs) = runQueueT $ case msg of
+  runMessage msg l@(LeftTurnA attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      locations <- select LocationCanBeSwapped
+      chooseTargetM iid locations $ handleTarget iid (attrs.ability 1)
+      pure l
+    HandleTargetChoice iid (isAbilitySource attrs 1 -> True) (LocationTarget lid) -> do
+      pos <- fieldJust LocationPosition lid
+      locations <-
+        select $ mapOneOf LocationInPosition (adjacentPositions pos) <> LocationCanBeSwapped
+      chooseTargetM iid locations (swapLocations lid)
+      pure l
     _ -> LeftTurnA <$> liftRunMessage msg attrs

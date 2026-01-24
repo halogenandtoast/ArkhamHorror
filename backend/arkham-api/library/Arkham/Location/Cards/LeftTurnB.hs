a057341f70468c5d51b52bd4cb6ec3da10aec803
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-24 18:00:54 +0900

    Left Turns

--- backend/arkham-api/library/Arkham/Location/Cards/LeftTurnB.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/LeftTurnB.hs
@@ -1,7 +1,11 @@
 module Arkham.Location.Cards.LeftTurnB (leftTurnB) where
 
+import Arkham.Ability
 import Arkham.Location.Cards qualified as Cards
+import Arkham.Location.Grid
 import Arkham.Location.Import.Lifted
+import Arkham.Message.Lifted.Choose
+import Arkham.Scenarios.WrittenInRock.Helpers
 
 newtype LeftTurnB = LeftTurnB LocationAttrs
   deriving anyclass (IsLocation, HasModifiersFor)
@@ -12,8 +16,24 @@ leftTurnB = location LeftTurnB Cards.leftTurnB 2 (PerPlayer 1)
 
 instance HasAbilities LeftTurnB where
   getAbilities (LeftTurnB a) =
-    extendRevealed a []
+    extendRevealed1 a
+      $ groupLimit PerGame
+      $ restricted a 1 (Here <> exists LocationCanBeSlid)
+      $ actionAbilityWithCost (ResourceCost 2)
 
 instance RunMessage LeftTurnB where
-  runMessage msg (LeftTurnB attrs) = runQueueT $ case msg of
+  runMessage msg l@(LeftTurnB attrs) = runQueueT $ case msg of
+    UseThisAbility iid (isSource attrs -> True) 1 -> do
+      locations <- select LocationCanBeSlid
+      chooseTargetM iid locations $ handleTarget iid (attrs.ability 1)
+      pure l
+    HandleTargetChoice iid (isAbilitySource attrs 1 -> True) (LocationTarget lid) -> do
+      slideLocations <-
+        matches lid LocationCanBeSlid >>= \case
+          False -> pure []
+          True -> getEmptyPositions lid
+      chooseOneM iid do
+        for_ slideLocations \newPos ->
+          gridLabeled (gridLabel newPos) $ push $ PlaceGrid (GridLocation newPos lid)
+      pure l
     _ -> LeftTurnB <$> liftRunMessage msg attrs

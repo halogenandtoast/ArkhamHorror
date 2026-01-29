75407c88330efcc251ffa5be681fe419cdb1d661
Matthew Mongeau <halogenandtoast@gmail.com>
2026-01-29 19:41:26 +0900

    Some more label work

--- backend/arkham-api/library/Arkham/Asset/Assets/BritishBullDog.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/BritishBullDog.hs
@@ -1,4 +1,4 @@
-module Arkham.Asset.Assets.BritishBullDog (britishBullDog, BritishBullDog (..)) where
+module Arkham.Asset.Assets.BritishBullDog (britishBullDog) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards

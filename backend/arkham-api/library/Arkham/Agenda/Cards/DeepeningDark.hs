68a3e93b4f8fa239cdebe9ef4572a7fdb42b38b9
Matthew Mongeau <halogenandtoast@gmail.com>
2026-02-02 15:26:33 +0900

    Fix

--- backend/arkham-api/library/Arkham/Agenda/Cards/DeepeningDark.hs
+++ backend/arkham-api/library/Arkham/Agenda/Cards/DeepeningDark.hs
@@ -57,7 +57,7 @@ instance RunMessage DeepeningDark where
           investigatorDefeated attrs iid
         else do
           selectEach (EnemyAt (LocationWithTrait Dark) <> UnengagedEnemy) (`place` PursuitZone)
-          unrevealed <- select $ UnrevealedLocation <> LocationWithTrait Dark
+          unrevealed <- selectField LocationCard $ UnrevealedLocation <> LocationWithTrait Dark
           unless (null unrevealed) do
             shuffleCardsIntoTopOfDeck WoodsDeck 2 unrevealed
           selectEach (RevealedLocation <> EmptyLocation) \loc -> do

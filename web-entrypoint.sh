790162ff76695bd6550055fca35ecc76e3a8bafd
Matthew Mongeau <halogenandtoast@gmail.com>
2026-03-17 11:58:46 +0900

    Update web-entrypoint

--- web-entrypoint.sh
+++ web-entrypoint.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 set -e
 
-# Only construct DATABASE_URL from Docker secrets if not already provided
-if [ -z "${DATABASE_URL}" ]; then
-  if [ -n "${PGPASSWORD}" ]; then
-    POSTGRES_PASSWORD="${PGPASSWORD}"
-  elif [ -f /run/secrets/postgres_password ]; then
-    POSTGRES_PASSWORD=$(tr -d '\n\r' < /run/secrets/postgres_password)
-  else
-    echo "Postgres password secret not found!" >&2
-    exit 1
-  fi
-
-  POSTGRES_USER="${PGUSER:-arkham_pg_user}"
-  POSTGRES_HOST="${PGHOST:-db}"
-  POSTGRES_PORT="${PGPORT:-5432}"
-  POSTGRES_DB="${PGDATABASE:-arkham-horror-backend}"
-
-  export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
-  unset POSTGRES_PASSWORD
+if [ -n "${PGPASSWORD}" ]; then
+  POSTGRES_PASSWORD="${PGPASSWORD}"
+elif [ -f /run/secrets/postgres_password ]; then
+  POSTGRES_PASSWORD=$(tr -d '\n\r' < /run/secrets/postgres_password)
+else
+  echo "Postgres password secret not found!" >&2
+  exit 1
 fi
+
+export PGUSER="${PGUSER:-arkham_pg_user}"
+export PGHOST="${PGHOST:-db}"
+export PGPORT="${PGPORT:-5432}"
+export PGDATABASE="${PGDATABASE:-arkham-horror-backend}"
+export PGPASSWORD="${POSTGRES_PASSWORD}"
+
+if [ -z "${DATABASE_URL}" ]; then
+  export DATABASE_URL="postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
+fi
+
+unset POSTGRES_PASSWORD
 
 exec "$@"

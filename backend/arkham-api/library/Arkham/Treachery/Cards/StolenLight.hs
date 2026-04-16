fb3d87d1dd5ea14f7905b327e71122d2d3ec8249
Matthew Mongeau <halogenandtoast@gmail.com>
2026-04-16 16:36:19 +0900

    Finish The Twisted Hollow Cards

--- backend/arkham-api/library/Arkham/Treachery/Cards/StolenLight.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/StolenLight.hs
@@ -1,5 +1,13 @@
 module Arkham.Treachery.Cards.StolenLight (stolenLight) where
 
+import Arkham.Asset.Types (Field (..))
+import Arkham.I18n
+import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
+import Arkham.Message.Lifted.Placement
+import Arkham.Projection
+import Arkham.Scenarios.TheTwistedHollow.Helpers
+import Arkham.Trait (Trait (Lit))
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
@@ -12,5 +20,17 @@ stolenLight = treachery StolenLight Cards.stolenLight
 
 instance RunMessage StolenLight where
   runMessage msg t@(StolenLight attrs) = runQueueT $ case msg of
-    Revelation _iid (isSource attrs -> True) -> pure t
+    Revelation iid (isSource attrs -> True) -> do
+      mLantern <- selectOne $ AssetWithTitle "Vale Lantern"
+      mLoc <- runMaybeT $ MaybeT . field AssetLocation =<< hoistMaybe mLantern
+      farthestLocations <- select $ FarthestLocationFromAll EmptyLocation
+
+      chooseOneM iid $ scenarioI18n do
+        unscoped $ countVar 1 $ labeled' "placeAgendaDoomCanAdvance" $ placeDoomOnAgendaAndCheckAdvance 1
+        for_ mLantern \lantern -> do
+          lit <- matches lantern $ AssetWithTrait Lit
+          labeledValidate' (lit || maybe False (`notElem` farthestLocations) mLoc) "stolenLight.lantern" do
+            when lit $ flipOverBy iid attrs lantern
+            chooseTargetM iid farthestLocations $ place lantern . AtLocation
+      pure t
     _ -> StolenLight <$> liftRunMessage msg attrs

3de0d9a7aa6a5cc55684fc5553179348907a2dfd
User <user@example.com>
2026-04-20 20:44:07 +0800

    fix Downpour_c2026

--- backend/arkham-api/library/Arkham/Treachery/Cards/Downpour.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/Downpour.hs
@@ -1,4 +1,4 @@
-module Arkham.Treachery.Cards.Downpour_c2026 (downpour_c2026) where
+module Arkham.Treachery.Cards.Downpour (downpour) where
 
 import Arkham.I18n
 import Arkham.Investigator.Types (Field (..))
@@ -7,18 +7,18 @@ import Arkham.Projection
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
-newtype Downpour_c2026 = Downpour_c2026 TreacheryAttrs
+newtype Downpour = Downpour TreacheryAttrs
   deriving anyclass (IsTreachery, HasModifiersFor, HasAbilities)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
-downpour_c2026 :: TreacheryCard Downpour_c2026
-downpour_c2026 = treachery Downpour_c2026 Cards.downpour_c2026
+downpour :: TreacheryCard Downpour
+downpour = treachery Downpour Cards.downpour
 
-instance RunMessage Downpour_c2026 where
-  runMessage msg t@(Downpour_c2026 attrs) = runQueueT $ case msg of
+instance RunMessage Downpour where
+  runMessage msg t@(Downpour attrs) = runQueueT $ case msg of
     Revelation iid (isSource attrs -> True) -> do
       sid <- getRandom
-      revelationSkillTest sid iid attrs #agility (Fixed 3)
+      revelationSkillTest sid iid attrs #intellect (Fixed 3)
       pure t
     FailedThisSkillTestBy _iid (isSource attrs -> True) n -> do
       doStep n msg
@@ -34,4 +34,4 @@ instance RunMessage Downpour_c2026 where
           placeCluesOnLocation iid attrs 1
           doStep (n - 1) msg'
       pure t
-    _ -> Downpour_c2026 <$> liftRunMessage msg attrs
+    _ -> Downpour <$> liftRunMessage msg attrs

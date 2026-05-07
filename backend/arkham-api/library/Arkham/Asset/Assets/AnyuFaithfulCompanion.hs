3dec66a68e4c03f183cd29daad0acfb7c4fda3c3
Matthew Mongeau <halogenandtoast@gmail.com>
2026-05-07 19:05:27 +0900

    Fix Anyu

--- backend/arkham-api/library/Arkham/Asset/Assets/AnyuFaithfulCompanion.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/AnyuFaithfulCompanion.hs
@@ -3,10 +3,9 @@ module Arkham.Asset.Assets.AnyuFaithfulCompanion (anyuFaithfulCompanion) where
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
-import Arkham.ForMovement
 import Arkham.Helpers.CombatTarget
+import Arkham.Helpers.Location (getAccessibleLocations)
 import Arkham.Helpers.Modifiers (ModifierType (..))
-import Arkham.Matcher
 import Arkham.Message.Lifted.Choose
 import Arkham.Message.Lifted.Move
 
@@ -22,7 +21,7 @@ instance HasAbilities AnyuFaithfulCompanion where
 
 anyuChoices :: ReverseQueue m => AssetAttrs -> InvestigatorId -> ChooseT m ()
 anyuChoices attrs iid = do
-  locations <- select $ AccessibleFrom ForMovement (locationWithInvestigator iid)
+  locations <- getAccessibleLocations iid (attrs.ability 1)
   unless (null locations) do
     labeled "Move to a connecting location" do
       chooseTargetM iid locations $ moveTo (attrs.ability 1) iid

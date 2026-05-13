137d85648553457525ebfb3f0deb3918c0a3a1d3
HyperActros <13617957013x@gmail.com>
2026-05-13 17:24:56 +0900

    Add Chinese translation for JSON files (#4514)

--- frontend/src/locales/zh.ts
+++ frontend/src/locales/zh.ts
@@ -1,7 +1,9 @@
 import base from '@/locales/zh/base.json'
+import log from '@/locales/zh/log.json'
+import cards from '@/locales/zh/cards.json'
 import label from '@/locales/zh/label.json'
-import campaignLog from '@/locales/zh/campaignLog.json'
 import investigators from '@/locales/zh/investigators.json'
+import campaignLog from '@/locales/zh/campaignLog.json'
 import nightOfTheZealot from '@/locales/zh/nightOfTheZealot'
 import theDunwichLegacy from '@/locales/zh/theDunwichLegacy'
 import thePathToCarcosa from '@/locales/zh/thePathToCarcosa'
@@ -10,8 +12,33 @@ import theCircleUndone from '@/locales/zh/theCircleUndone'
 import theDreamEaters from '@/locales/zh/theDreamEaters'
 import theInnsmouthConspiracy from '@/locales/zh/theInnsmouthConspiracy'
 import edgeOfTheEarth from '@/locales/zh/edgeOfTheEarth'
-import brethrenOfAsh from '@/locales/zh/brethrenOfAsh'
+import theScarletKeys from '@/locales/zh/theScarletKeys'
+import theFeastOfHemlockVale from '@/locales/en/theFeastOfHemlockVale'
+import brethrenOfAsh from '@/locales/en/brethrenOfAsh'
 import standalone from '@/locales/zh/standalone'
 import gameBoard from '@/locales/zh/gameBoard/gameBoard'
+import xp from '@/locales/zh/xp.json'
 
-export default {...base, label, ...campaignLog, ...investigators, ...gameBoard, nightOfTheZealot, theDunwichLegacy, thePathToCarcosa, theForgottenAge, theCircleUndone, theDreamEaters, theInnsmouthConspiracy, edgeOfTheEarth, brethrenOfAsh, standalone}
+export default 
+{ ...base,
+  ...campaignLog, 
+  ...gameBoard, 
+  cards,
+  investigators,
+  label: { ...label, cards: cards["label"] },
+  log,
+  xp,
+  nightOfTheZealot, 
+  theDunwichLegacy, 
+  thePathToCarcosa, 
+  theForgottenAge, 
+  returnToTheForgottenAge: theForgottenAge,
+  theCircleUndone, 
+  theDreamEaters, 
+  theInnsmouthConspiracy, 
+  edgeOfTheEarth, 
+  theScarletKeys, 
+  theFeastOfHemlockVale,
+  brethrenOfAsh, 
+  standalone
+}

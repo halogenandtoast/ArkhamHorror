d5ea742b728cf3d459a0da7c51eabe2100b95560
Matthew Mongeau <halogenandtoast@gmail.com>
2026-05-18 07:59:49 +0900

    Fix Blood Curse (3)
    Should be based on the controller

--- backend/arkham-api/library/Arkham/Skill/Cards/BloodCurse3.hs
+++ backend/arkham-api/library/Arkham/Skill/Cards/BloodCurse3.hs
@@ -15,15 +15,16 @@ bloodCurse3 = skill BloodCurse3 Cards.bloodCurse3
 instance RunMessage BloodCurse3 where
   runMessage msg s@(BloodCurse3 attrs) = runQueueT $ case msg of
     PassedSkillTest _iid _ _ (isTarget attrs -> True) _ _ -> do
+      priority $ skillTestCardOption attrs $ do_ msg
+      pure s
+    Do (PassedSkillTest _iid _ _ (isTarget attrs -> True) _ _) -> do
       let controller = attrs.owner
       enemies <- select $ enemyAtLocationWith controller <> EnemyCanBeDamagedBySource (toSource attrs)
       assets <- select $ assetAtLocationWith controller <> AssetWithHealth
       investigators <- select $ colocatedWith controller
-      unless (null enemies && null assets && null investigators) do
-        skillTestCardOption attrs do
-          chooseOneM controller do
-            targets enemies $ nonAttackEnemyDamage (Just controller) attrs 1
-            targets assets \aid -> dealAssetDamage aid attrs 1
-            targets investigators \iid' -> assignDamage iid' attrs 1
+      chooseOneM controller do
+        targets enemies $ nonAttackEnemyDamage (Just controller) attrs 1
+        targets assets \aid -> dealAssetDamage aid attrs 1
+        targets investigators \iid' -> assignDamage iid' attrs 1
       pure s
     _ -> BloodCurse3 <$> liftRunMessage msg attrs

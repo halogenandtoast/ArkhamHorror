1bfa3894d2c9bed006a597c04baa143adfafc941
Matthew Mongeau <halogenandtoast@gmail.com>
2026-05-21 22:17:29 +0900

    Fix Yaotl1

--- backend/arkham-api/library/Arkham/Asset/Assets/Yaotl1.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/Yaotl1.hs
@@ -23,14 +23,14 @@ yaotl1 = ally Yaotl1 Cards.yaotl1 (2, 2)
 
 instance HasAbilities Yaotl1 where
   getAbilities (Yaotl1 a) =
-    [ (cardI18n $ withI18nTooltip "yaotl1.fastExhaustYaotl")
+    [ cardI18n (withI18nTooltip "yaotl1.fastExhaustYaotl")
         $ wantsSkillTest (YourSkillTest #any)
         $ controlled a 1 DuringAnySkillTest
         $ FastAbility
         $ exhaust a
-    , (cardI18n $ withI18nTooltip "yaotl1.fastDiscardThe")
+    , cardI18n (withI18nTooltip "yaotl1.fastDiscardThe")
         $ playerLimit PerPhase
-        $ controlled a 2 (youExist $ can.manipulate.deck <> DeckWith AnyCards)
+        $ controlled a 2 (youExist $ can.manipulate.deck <> not_ DeckIsEmpty)
         $ FastAbility Free
     ]
 

4dfdbe6f7e127b247f9a4cc3c25ebe6e214af681
Matthew Mongeau <halogenandtoast@gmail.com>
2026-06-22 19:23:43 +0900

    Fix Searching for Dad

--- backend/arkham-api/library/Arkham/Act/Cards/SearchingForDad.hs
+++ backend/arkham-api/library/Arkham/Act/Cards/SearchingForDad.hs
@@ -26,30 +26,27 @@ instance HasAbilities SearchingForDad where
     [ restricted
         a
         1
-        ( Negate (oneOf [exists (EnemyWithTrait Suspect), exists (LocationWithTrait Hideout)])
+        ( wrapCriteria a
+            $ Negate (oneOf [exists (InPlayEnemy $ EnemyWithTrait Suspect), exists (LocationWithTrait Hideout)])
             <> ScenarioDeckWithCard LeadsDeck
             <> exists wendyAdams
         )
         $ forced AnyWindow
     , restricted a 2 (Negate $ ScenarioDeckWithCard LeadsDeck) $ Objective $ forced AnyWindow
     ]
+   where
+    wrapCriteria a = if toResultDefault False a.meta then const Never else id
 
 instance RunMessage SearchingForDad where
   runMessage msg a@(SearchingForDad attrs) = runQueueT $ case msg of
     UseThisAbility _ (isSource attrs -> True) 1 -> do
+      getLeadsDeck >>= \case
+        [] -> pure ()
+        (x : _) -> whenJustM getWendyAdams \wendy -> drawCardFrom wendy LeadsDeck x
       doStep 1 msg
-      pure a
-    DoStep 1 msg'@(UseThisAbility _ (isSource attrs -> True) 1) -> do
-      noLeadsInPlay <-
-        andM [selectNone (EnemyWithTrait Suspect), selectNone (LocationWithTrait Hideout)]
-      when noLeadsInPlay do
-        getLeadsDeck >>= \case
-          [] -> pure ()
-          (x : _) -> do
-            getWendyAdams >>= traverse_ \wendy -> do
-              drawCard wendy x
-              doStep 1 msg'
-      pure a
+      pure $ SearchingForDad $ attrs & metaL .~ toJSON True
+    DoStep 1 (UseThisAbility _ (isSource attrs -> True) 1) ->
+      pure $ SearchingForDad $ attrs & metaL .~ toJSON False
     UseThisAbility _ (isSource attrs -> True) 2 -> do
       advancedWithOther attrs
       pure a

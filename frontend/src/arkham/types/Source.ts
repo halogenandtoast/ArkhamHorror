e388c46ccda24388fc01f88a27bc431b42388cac
Matthew Mongeau <halogenandtoast@gmail.com>
2026-06-22 22:30:57 +0900

    Fix source decoding for skill test modifiers

--- frontend/src/arkham/types/Source.ts
+++ frontend/src/arkham/types/Source.ts
@@ -13,6 +13,16 @@ export function sourceKey (source: Source): string | undefined {
       return sourceKey(source.source)
     case "TarotSource":
       return source.contents.arcana
+    case "IndexedSource":
+      return source.contents ? sourceKey(source.contents[1]) : undefined
+    case "AbilitySource":
+      return sourceKey(source.contents[0])
+    case "UseAbilitySource":
+      return sourceKey(source.contents[1])
+    case "PaymentSource":
+      return sourceKey(source.contents)
+    case "BothSource":
+      return sourceKey(source.contents[0]) ?? sourceKey(source.contents[1])
     case "OtherSource":
       return source.contents
   }
@@ -28,6 +38,31 @@ export type ProxySource = {
 export type IndexedSource = {
   sourceTag: "IndexedSource"
   tag: "IndexedSource"
+  contents?: [number, Source]
+}
+
+export type AbilitySource = {
+  sourceTag: "AbilitySource"
+  tag: "AbilitySource"
+  contents: [Source, number]
+}
+
+export type UseAbilitySource = {
+  sourceTag: "UseAbilitySource"
+  tag: "UseAbilitySource"
+  contents: [string, Source, number]
+}
+
+export type PaymentSource = {
+  sourceTag: "PaymentSource"
+  tag: "PaymentSource"
+  contents: Source
+}
+
+export type BothSource = {
+  sourceTag: "BothSource"
+  tag: "BothSource"
+  contents: [Source, Source]
 }
 
 export type OtherSource = {
@@ -42,7 +77,7 @@ export type TarotSource = {
   contents: TarotCard
 }
 
-export type Source = ProxySource | TarotSource | IndexedSource | OtherSource
+export type Source = ProxySource | TarotSource | IndexedSource | AbilitySource | UseAbilitySource | PaymentSource | BothSource | OtherSource
 
 export const proxySourceDecoder: JsonDecoder.Decoder<ProxySource> = JsonDecoder.object<ProxySource>({
   tag: JsonDecoder.literal("ProxySource"),
@@ -54,7 +89,32 @@ export const proxySourceDecoder: JsonDecoder.Decoder<ProxySource> = JsonDecoder.
 export const indexedSourceDecoder: JsonDecoder.Decoder<IndexedSource> = JsonDecoder.object<IndexedSource>({
   tag: JsonDecoder.literal("IndexedSource"),
   sourceTag: JsonDecoder.constant("IndexedSource"),
+  contents: v2Optional(JsonDecoder.tuple([JsonDecoder.number(), JsonDecoder.lazy<Source>(() => sourceDecoder)], 'IndexedSource.contents')),
 }, 'IndexedSource')
+
+export const abilitySourceDecoder: JsonDecoder.Decoder<AbilitySource> = JsonDecoder.object<AbilitySource>({
+  tag: JsonDecoder.literal("AbilitySource"),
+  sourceTag: JsonDecoder.constant("AbilitySource"),
+  contents: JsonDecoder.tuple([JsonDecoder.lazy<Source>(() => sourceDecoder), JsonDecoder.number()], 'AbilitySource.contents'),
+}, 'AbilitySource')
+
+export const useAbilitySourceDecoder: JsonDecoder.Decoder<UseAbilitySource> = JsonDecoder.object<UseAbilitySource>({
+  tag: JsonDecoder.literal("UseAbilitySource"),
+  sourceTag: JsonDecoder.constant("UseAbilitySource"),
+  contents: JsonDecoder.tuple([JsonDecoder.string(), JsonDecoder.lazy<Source>(() => sourceDecoder), JsonDecoder.number()], 'UseAbilitySource.contents'),
+}, 'UseAbilitySource')
+
+export const paymentSourceDecoder: JsonDecoder.Decoder<PaymentSource> = JsonDecoder.object<PaymentSource>({
+  tag: JsonDecoder.literal("PaymentSource"),
+  sourceTag: JsonDecoder.constant("PaymentSource"),
+  contents: JsonDecoder.lazy<Source>(() => sourceDecoder),
+}, 'PaymentSource')
+
+export const bothSourceDecoder: JsonDecoder.Decoder<BothSource> = JsonDecoder.object<BothSource>({
+  tag: JsonDecoder.literal("BothSource"),
+  sourceTag: JsonDecoder.constant("BothSource"),
+  contents: JsonDecoder.tuple([JsonDecoder.lazy<Source>(() => sourceDecoder), JsonDecoder.lazy<Source>(() => sourceDecoder)], 'BothSource.contents'),
+}, 'BothSource')
 
 export const tarotSourceDecoder: JsonDecoder.Decoder<TarotSource> = JsonDecoder.object<TarotSource>({
   tag: JsonDecoder.literal("TarotSource"),
@@ -80,5 +140,9 @@ export const sourceDecoder = JsonDecoder.oneOf<Source>([
   proxySourceDecoder,
   tarotSourceDecoder,
   indexedSourceDecoder,
+  abilitySourceDecoder,
+  useAbilitySourceDecoder,
+  paymentSourceDecoder,
+  bothSourceDecoder,
   otherSourceDecoder
 ], 'Source')

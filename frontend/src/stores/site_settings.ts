5ceb7b01246e5b9fe503f32319ae153b947aecb7
XuHuaLong1 <397860980@qq.com>
2026-07-07 06:19:38 +0000

    feat: 本地安装项目
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- frontend/src/stores/site_settings.ts
+++ frontend/src/stores/site_settings.ts
@@ -21,19 +21,14 @@ export const useSiteSettingsStore = defineStore("site_settings", {
 
   actions: {
     async init() {
-      //const assetHost = localStorage.getItem('asset-host');
-      // if (assetHost !== null && assetHost !== undefined) {
-      //   this.assetHost = assetHost
-      // } else {
+      try {
         const result = await api.get('/site-settings')
         if (result.status === 200) {
           this.assetHost = result.data.assetHost ?? baseUrl
-          //localStorage.setItem('asset-host', this.assetHost)
-        } else {
-          this.assetHost = baseUrl
-          //localStorage.setItem('asset-host', baseUrl)
         }
-      //}
+      } catch {
+        this.assetHost = baseUrl
+      }
     }
   }
 })

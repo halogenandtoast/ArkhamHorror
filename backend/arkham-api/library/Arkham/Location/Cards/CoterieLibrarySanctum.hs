024a6df8f98875d538ea4f9cec6cff77f73747f4
Matthew Mongeau <halogenandtoast@gmail.com>
2026-07-18 21:50:54 +0900

    Fix Coterie Library Sanctum

--- backend/arkham-api/library/Arkham/Location/Cards/CoterieLibrarySanctum.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/CoterieLibrarySanctum.hs
@@ -1,10 +1,12 @@
-module Arkham.Location.Cards.CoterieLibrarySanctum (coterieLibrarySanctum) where
+module Arkham.Location.Cards.CoterieLibrarySanctum (coterieLibrarySanctum, coterieLibrarySanctumEffect) where
 
 import Arkham.Ability
+import Arkham.Card
+import Arkham.Effect.Import
+import Arkham.Helpers.Modifiers (ModifierType (..), modified_)
 import Arkham.Location.Cards qualified as Cards
 import Arkham.Location.Import.Lifted
 import Arkham.Matcher
-import Arkham.Modifier
 import Arkham.Trait (Trait (Conspirator))
 
 newtype CoterieLibrarySanctum = CoterieLibrarySanctum LocationAttrs
@@ -25,9 +27,28 @@ instance HasAbilities CoterieLibrarySanctum where
 instance RunMessage CoterieLibrarySanctum where
   runMessage msg l@(CoterieLibrarySanctum attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
-      roundModifier (attrs.ability 1) iid (ReduceCostOf #asset 2)
+      createCardEffect Cards.coterieLibrarySanctum Nothing (attrs.ability 1) iid
       pure l
     UseThisAbility iid (isSource attrs -> True) 2 -> do
-      roundModifier (attrs.ability 1) iid (ReduceCostOf #asset 2)
+      createCardEffect Cards.coterieLibrarySanctum Nothing (attrs.ability 2) iid
       pure l
     _ -> CoterieLibrarySanctum <$> liftRunMessage msg attrs
+
+newtype CoterieLibrarySanctumEffect = CoterieLibrarySanctumEffect EffectAttrs
+  deriving anyclass (HasAbilities, IsEffect)
+  deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
+
+coterieLibrarySanctumEffect :: EffectArgs -> CoterieLibrarySanctumEffect
+coterieLibrarySanctumEffect = cardEffect CoterieLibrarySanctumEffect Cards.coterieLibrarySanctum
+
+instance HasModifiersFor CoterieLibrarySanctumEffect where
+  getModifiersFor (CoterieLibrarySanctumEffect a) = for_ a.target.investigator \iid ->
+    modified_ a iid [ReduceCostOf #asset 2]
+
+instance RunMessage CoterieLibrarySanctumEffect where
+  runMessage msg e@(CoterieLibrarySanctumEffect attrs) = runQueueT $ case msg of
+    CardEnteredPlay iid card -> do
+      when (attrs.target.investigator == Just iid && cardMatch card (#asset :: CardMatcher)) $ disable attrs
+      pure e
+    EndRound -> disableReturn e
+    _ -> CoterieLibrarySanctumEffect <$> liftRunMessage msg attrs

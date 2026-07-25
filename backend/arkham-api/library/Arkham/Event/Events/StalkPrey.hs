3c59e9349083d6add5b9d65c81960ce9d6ae2789
Matthew Mongeau <halogenandtoast@gmail.com>
2026-07-25 11:19:36 +0900

    Stalk Prey should only search the top 9 cards of the encounter deck (#5248)

--- backend/arkham-api/library/Arkham/Event/Events/StalkPrey.hs
+++ backend/arkham-api/library/Arkham/Event/Events/StalkPrey.hs
@@ -1,12 +1,12 @@
 module Arkham.Event.Events.StalkPrey (stalkPrey) where
 
 import Arkham.Card
-import Arkham.Deck qualified as Deck
 import Arkham.Event.Cards qualified as Cards
 import Arkham.Event.Import.Lifted
 import Arkham.I18n
 import Arkham.Matcher
 import Arkham.Message.Lifted.Move
+import Arkham.Strategy
 import Arkham.Zone
 
 newtype StalkPrey = StalkPrey EventAttrs
@@ -19,15 +19,26 @@ stalkPrey = event StalkPrey Cards.stalkPrey
 instance RunMessage StalkPrey where
   runMessage msg e@(StalkPrey attrs) = runQueueT $ case msg of
     PlayThisEvent iid (is attrs -> True) -> do
-      findEncounterCardIn iid attrs (cardMatcher_ #enemy) [FromEncounterDeck]
+      search iid attrs EncounterDeckTarget [(FromTopOfDeck 9, ShuffleBackIn)] #any (defer attrs IsDraw)
       pure e
-    FoundEncounterCard iid (isTarget attrs -> True) (toCard -> card) -> do
-      drawCardFrom iid Deck.EncounterDeck card
+    SearchNoneFound iid (isTarget attrs -> True) -> do
       drawCards iid attrs 1
       discoverAtYourLocation NotInvestigate iid attrs 1
-      doStep 1 msg
       pure e
-    DoStep 1 (FoundEncounterCard iid (isTarget attrs -> True) (toCard -> card)) -> do
+    SearchFound iid (isTarget attrs -> True) _ cards -> do
+      let enemyCards = filter (`cardMatch` EnemyType) $ onlyEncounterCards cards
+      case enemyCards of
+        [] -> do
+          drawCards iid attrs 1
+          discoverAtYourLocation NotInvestigate iid attrs 1
+        _ -> chooseOneM iid do
+          targets enemyCards \card -> do
+            push $ InvestigatorDrewEncounterCard iid card
+            drawCards iid attrs 1
+            discoverAtYourLocation NotInvestigate iid attrs 1
+            doStep 1 (InvestigatorDrewEncounterCard iid card)
+      pure e
+    DoStep 1 (InvestigatorDrewEncounterCard iid card) -> do
       menemy <- selectOne $ EnemyWithCardId card.id
       for_ menemy \eid -> do
         unlessM (eid <=~> EnemyAt (locationWithInvestigator iid)) do

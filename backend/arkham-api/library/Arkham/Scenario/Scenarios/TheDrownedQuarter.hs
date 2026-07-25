b9d26988ef3171cd53dfa2fad05eab92e38e83ee
Matthew Mongeau <halogenandtoast@gmail.com>
2026-07-25 16:43:16 +0900

    The Drowned Quarter intro

--- backend/arkham-api/library/Arkham/Scenario/Scenarios/TheDrownedQuarter.hs
+++ backend/arkham-api/library/Arkham/Scenario/Scenarios/TheDrownedQuarter.hs
@@ -2,16 +2,18 @@ module Arkham.Scenario.Scenarios.TheDrownedQuarter (theDrownedQuarter) where
 
 import Arkham.Act.Cards qualified as Acts
 import Arkham.Agenda.Cards qualified as Agendas
+import Arkham.Asset.Cards qualified as Assets
 import Arkham.Campaign.Import.Lifted (setNextCampaignStep)
 import Arkham.Campaigns.TheDrownedCity.CampaignSteps (pattern TheApiary, pattern TheWesternWall)
 import Arkham.Campaigns.TheDrownedCity.Import
 import Arkham.ChaosToken
 import Arkham.EncounterSet qualified as Set
+import Arkham.Helpers.FlavorText
 import Arkham.Helpers.Xp
-import Arkham.I18n
 import Arkham.Location.Cards qualified as Locations
 import Arkham.Location.Grid (Pos (..))
 import Arkham.Matcher
+import Arkham.Message.Lifted.Choose
 import Arkham.Message.Lifted.Log
 import Arkham.Resolution
 import Arkham.Scenario.Import.Lifted
@@ -34,21 +36,45 @@ instance HasChaosTokenValue TheDrownedQuarter where
     Tablet -> pure $ toChaosTokenValue attrs Tablet 4 4
     ElderThing -> do
       flooded <- selectAny $ FloodedLocation <> locationWithInvestigator iid
-      pure $ if flooded then toChaosTokenValue attrs ElderThing 3 4 else toChaosTokenValue attrs ElderThing 5 6
+      pure
+        $ if flooded then toChaosTokenValue attrs ElderThing 3 4 else toChaosTokenValue attrs ElderThing 5 6
     otherFace -> getChaosTokenValue iid otherFace attrs
 
 instance RunMessage TheDrownedQuarter where
   runMessage msg s@(TheDrownedQuarter attrs) = runQueueT $ scenarioI18n $ case msg of
+    PreScenarioSetup -> scope "intro" do
+      headedWest <- getHasRecord TheExpeditionHeadedWest
+      storyWithContinue' do
+        setTitle "title"
+        p "drownedQuarter1"
+        p.basic "checkCampaignLog"
+        ul do
+          li.validate headedWest "headedWest"
+          li.validate (not headedWest) "headedEast"
+
+      hasNoPlaceLikeHome <-
+        anyM (`investigatorHasTask` Assets.noPlaceLikeHome)
+          =<< select (IncludeEliminated Anyone)
+      flavor do
+        setTitle "title"
+        p $ if headedWest then "drownedQuarter2" else "drownedQuarter3"
+        p $ if headedWest then "drownedQuarter2Conclusion" else "drownedQuarter3Conclusion"
+        ul $ li.validate hasNoPlaceLikeHome "resolveNoPlaceLikeHome"
+        p.basic "proceedToSetup"
+      pure s
     StandaloneSetup -> do
       setChaosTokens (chaosBagContents attrs.difficulty)
       pure s
     Setup -> runScenarioSetup TheDrownedQuarter attrs do
+      setUsesGrid
+
       gather Set.TheDrownedQuarter
       gather Set.AlienMachinery
       gather Set.CosmicLegacy
       gather Set.DeepOnes
       gather Set.ElderMist
       gather Set.Flood
+      gather Set.Rlyeh
       gather Set.UnderseaCreatures
 
       setActDeck [Acts.reactivateTheCore]

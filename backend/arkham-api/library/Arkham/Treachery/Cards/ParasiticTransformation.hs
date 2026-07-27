0eb9c59b29d45fe7ab7a7569cef6d8a96f1dc797
Matthew Mongeau <halogenandtoast@gmail.com>
2026-07-27 20:17:47 +0900

    Fix Parasitic Transformation

--- backend/arkham-api/library/Arkham/Treachery/Cards/ParasiticTransformation.hs
+++ backend/arkham-api/library/Arkham/Treachery/Cards/ParasiticTransformation.hs
@@ -1,15 +1,32 @@
 module Arkham.Treachery.Cards.ParasiticTransformation (parasiticTransformation) where
 
+import Arkham.Ability
+import Arkham.Matcher
 import Arkham.Treachery.Cards qualified as Cards
 import Arkham.Treachery.Import.Lifted
 
 newtype ParasiticTransformation = ParasiticTransformation TreacheryAttrs
-  deriving anyclass (IsTreachery, HasAbilities, HasModifiersFor)
+  deriving anyclass (IsTreachery, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 parasiticTransformation :: TreacheryCard ParasiticTransformation
 parasiticTransformation = treachery ParasiticTransformation Cards.parasiticTransformation
 
--- TODO: abilities
+instance HasAbilities ParasiticTransformation where
+  getAbilities (ParasiticTransformation a) =
+    [ restricted a 1 (InThreatAreaOf You) $ forced $ EnemyEngaged #after You AnyEnemy
+    , restricted a 2 OnSameLocation doubleActionAbility
+    ]
+
 instance RunMessage ParasiticTransformation where
-  runMessage msg (ParasiticTransformation attrs) = runQueueT $ ParasiticTransformation <$> liftRunMessage msg attrs
+  runMessage msg t@(ParasiticTransformation attrs) = runQueueT $ case msg of
+    Revelation iid (isSource attrs -> True) -> do
+      placeInThreatArea attrs iid
+      pure t
+    UseThisAbility _iid (isSource attrs -> True) 1 -> do
+      placeDoom (attrs.ability 1) attrs 1
+      pure t
+    UseThisAbility iid (isSource attrs -> True) 2 -> do
+      toDiscardBy iid (attrs.ability 2) attrs
+      pure t
+    _ -> ParasiticTransformation <$> liftRunMessage msg attrs

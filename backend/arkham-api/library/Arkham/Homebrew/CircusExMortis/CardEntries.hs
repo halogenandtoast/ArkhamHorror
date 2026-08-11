4f120c9c7ee22040302480f0d4f090635c1a0fea
Matthew Mongeau <halogenandtoast@gmail.com>
2026-08-11 23:44:07 +0900

    Update Circus Ex Mortis Content

---
+++ backend/arkham-api/library/Arkham/Homebrew/CircusExMortis/CardEntries.hs
@@ -0,0 +1,2 @@
+-- Generated aggregate registration for every Circus Ex Mortis card implementation.
+{-# OPTIONS_GHC -F -pgmF cards-discover -optF --dir=. -optF --homebrew-content #-}
--- backend/arkham-api/library/Arkham/Homebrew/CircusExMortis/Content.hs
+++ backend/arkham-api/library/Arkham/Homebrew/CircusExMortis/Content.hs
@@ -1,172 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Arkham.Homebrew.CircusExMortis.Content where
 
-import Arkham.Act.Types (SomeActCard (..))
-import Arkham.Agenda.Types (SomeAgendaCard (..))
-import Arkham.Asset.Types (SomeAssetCard (..))
-import Arkham.Enemy.Types (SomeEnemyCard (..))
-import Arkham.Homebrew.CircusExMortis.Acts.ForestOfIllusion
-import Arkham.Homebrew.CircusExMortis.Acts.OutAndAway
-import Arkham.Homebrew.CircusExMortis.Acts.RatsInACage
-import Arkham.Homebrew.CircusExMortis.Acts.SmokeAndMirrors
-import Arkham.Homebrew.CircusExMortis.Agendas.BloodMoon
-import Arkham.Homebrew.CircusExMortis.Agendas.HouseOfHorrors
-import Arkham.Homebrew.CircusExMortis.Agendas.MesmericMagic
-import Arkham.Homebrew.CircusExMortis.Agendas.SavageNature
-import Arkham.Homebrew.CircusExMortis.Agendas.TheTrueFace
-import Arkham.Homebrew.CircusExMortis.Assets.IllusoryLocus
 import Arkham.Homebrew.CircusExMortis.Campaign (circusExMortis)
-import Arkham.Homebrew.CircusExMortis.Enemies.CircusPredator
-import Arkham.Homebrew.CircusExMortis.Enemies.DisguisedMonstrosity
-import Arkham.Homebrew.CircusExMortis.Enemies.GrotesqueLion
-import Arkham.Homebrew.CircusExMortis.Enemies.Mooncalf
-import Arkham.Homebrew.CircusExMortis.Enemies.NascentDarkYoung
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonAcrobat
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonBeastTamer
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonCarny
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonClown
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonDrudge
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonIllusionist
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonMagician
-import Arkham.Homebrew.CircusExMortis.Enemies.NewMoonStrongman
-import Arkham.Homebrew.CircusExMortis.Enemies.SupplicantOfTheGoat
-import Arkham.Homebrew.CircusExMortis.Enemies.TwistedSatyr
-import Arkham.Homebrew.CircusExMortis.Enemies.UrsineBrute
-import Arkham.Homebrew.CircusExMortis.Locations.AnimalCages
-import Arkham.Homebrew.CircusExMortis.Locations.Carousel
-import Arkham.Homebrew.CircusExMortis.Locations.CircusEncampment
-import Arkham.Homebrew.CircusExMortis.Locations.CircusGatesPathToFreedom
-import Arkham.Homebrew.CircusExMortis.Locations.ForestPassage
-import Arkham.Homebrew.CircusExMortis.Locations.GamesGallery
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestCircularGrove
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestDeadGrove
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestFogBank
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestGlassyLake
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestLabyrinthOfTrees
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestMistyMarsh
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestQuietValley
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestShadowedPath
-import Arkham.Homebrew.CircusExMortis.Locations.MoonlitForestShallowRiver
-import Arkham.Homebrew.CircusExMortis.Locations.PerformerTrailers
-import Arkham.Homebrew.CircusExMortis.Locations.RemoteCabin
-import Arkham.Homebrew.CircusExMortis.Locations.TheBigTopFirstRing
-import Arkham.Homebrew.CircusExMortis.Locations.TheBigTopSecondRing
-import Arkham.Homebrew.CircusExMortis.Locations.TheBigTopThirdRing
-import Arkham.Homebrew.CircusExMortis.Locations.WoodlandOverlook
-import Arkham.Homebrew.CircusExMortis.Scenarios.AllPointsWest
-import Arkham.Homebrew.CircusExMortis.Scenarios.Bacchanalia
-import Arkham.Homebrew.CircusExMortis.Scenarios.HarmsWay
-import Arkham.Homebrew.CircusExMortis.Scenarios.OneNightOnly
-import Arkham.Homebrew.CircusExMortis.Scenarios.PiperAtTheGatesOfDawn
-import Arkham.Homebrew.CircusExMortis.Scenarios.RedSunrise
-import Arkham.Homebrew.CircusExMortis.Scenarios.ThePrimrosePath
-import Arkham.Homebrew.CircusExMortis.Scenarios.ThousandToOne
-import Arkham.Homebrew.CircusExMortis.Sets qualified as Sets
-import Arkham.Homebrew.CircusExMortis.Treacheries.EndlessSpawn
-import Arkham.Homebrew.CircusExMortis.Treacheries.FeralImpulses
-import Arkham.Homebrew.CircusExMortis.Treacheries.LunarInfluence
-import Arkham.Homebrew.CircusExMortis.Treacheries.MaddeningSpectacle
-import Arkham.Homebrew.CircusExMortis.Treacheries.MilkOfShubNiggurath
-import Arkham.Homebrew.CircusExMortis.Treacheries.MoonlightIllusion
-import Arkham.Homebrew.CircusExMortis.Treacheries.OminousMoonlight
-import Arkham.Homebrew.CircusExMortis.Treacheries.QuickerThanTheEye
-import Arkham.Homebrew.CircusExMortis.Treacheries.RecklessStunt
-import Arkham.Homebrew.Types
-import Arkham.Location.Types (SomeLocationCard (..))
-import Arkham.Treachery.Types (SomeTreacheryCard (..))
-
-acts :: [SomeActCard]
-acts =
-  [ SomeActCard forestOfIllusion
-  , SomeActCard outAndAway
-  , SomeActCard ratsInACage_005
-  , SomeActCard ratsInACage_006
-  , SomeActCard ratsInACage_007
-  , SomeActCard ratsInACage_008
-  , SomeActCard smokeAndMirrors
-  ]
-
-agendas :: [SomeAgendaCard]
-agendas =
-  [ SomeAgendaCard bloodMoon
-  , SomeAgendaCard houseOfHorrors
-  , SomeAgendaCard mesmericMagic
-  , SomeAgendaCard savageNature
-  , SomeAgendaCard theTrueFace
-  ]
-
-assets :: [SomeAssetCard]
-assets =
-  [ SomeAssetCard illusoryLocus
-  ]
-
-enemies :: [SomeEnemyCard]
-enemies =
-  [ SomeEnemyCard circusPredator
-  , SomeEnemyCard disguisedMonstrosity
-  , SomeEnemyCard grotesqueLion
-  , SomeEnemyCard mooncalf
-  , SomeEnemyCard nascentDarkYoung
-  , SomeEnemyCard newMoonAcrobat
-  , SomeEnemyCard newMoonBeastTamer
-  , SomeEnemyCard newMoonCarny
-  , SomeEnemyCard newMoonClown
-  , SomeEnemyCard newMoonDrudge
-  , SomeEnemyCard newMoonIllusionist
-  , SomeEnemyCard newMoonMagician
-  , SomeEnemyCard newMoonStrongman
-  , SomeEnemyCard supplicantOfTheGoat
-  , SomeEnemyCard twistedSatyr
-  , SomeEnemyCard ursineBrute
-  ]
-
-locations :: [SomeLocationCard]
-locations =
-  [ SomeLocationCard animalCages
-  , SomeLocationCard carousel
-  , SomeLocationCard circusEncampment
-  , SomeLocationCard circusGatesPathToFreedom
-  , SomeLocationCard forestPassage
-  , SomeLocationCard gamesGallery
-  , SomeLocationCard moonlitForestCircularGrove
-  , SomeLocationCard moonlitForestDeadGrove
-  , SomeLocationCard moonlitForestFogBank
-  , SomeLocationCard moonlitForestGlassyLake
-  , SomeLocationCard moonlitForestLabyrinthOfTrees
-  , SomeLocationCard moonlitForestMistyMarsh
-  , SomeLocationCard moonlitForestQuietValley
-  , SomeLocationCard moonlitForestShadowedPath
-  , SomeLocationCard moonlitForestShallowRiver
-  , SomeLocationCard performerTrailers
-  , SomeLocationCard remoteCabin
-  , SomeLocationCard theBigTopFirstRing
-  , SomeLocationCard theBigTopSecondRing
-  , SomeLocationCard theBigTopThirdRing
-  , SomeLocationCard woodlandOverlook
-  ]
-
-treacheries :: [SomeTreacheryCard]
-treacheries =
-  [ SomeTreacheryCard endlessSpawn
-  , SomeTreacheryCard feralImpulses
-  , SomeTreacheryCard lunarInfluence
-  , SomeTreacheryCard maddeningSpectacle
-  , SomeTreacheryCard milkOfShubNiggurath
-  , SomeTreacheryCard moonlightIllusion
-  , SomeTreacheryCard ominousMoonlight
-  , SomeTreacheryCard quickerThanTheEye
-  , SomeTreacheryCard recklessStunt
-  ]
+import Arkham.Homebrew.CircusExMortis.CardEntries ()
+import Arkham.Homebrew.CircusExMortis.Scenarios.AllPointsWest (allPointsWest)
+import Arkham.Homebrew.CircusExMortis.Scenarios.Bacchanalia (bacchanalia)
+import Arkham.Homebrew.CircusExMortis.Scenarios.HarmsWay (harmsWay)
+import Arkham.Homebrew.CircusExMortis.Scenarios.OneNightOnly (oneNightOnly)
+import Arkham.Homebrew.CircusExMortis.Scenarios.PiperAtTheGatesOfDawn (piperAtTheGatesOfDawn)
+import Arkham.Homebrew.CircusExMortis.Scenarios.RedSunrise (redSunrise)
+import Arkham.Homebrew.CircusExMortis.Scenarios.ThePrimrosePath (thePrimrosePath)
+import Arkham.Homebrew.CircusExMortis.Scenarios.ThousandToOne (thousandToOne)
+import Arkham.Homebrew.CircusExMortis.Sets
+import Arkham.Homebrew.Import
 
 scenarios :: HomebrewScenarios
 scenarios =
-  [ (":circus-ex-mortis:001", HomebrewScenario Sets.OneNightOnly oneNightOnly)
-  , (":circus-ex-mortis:017", HomebrewScenario Sets.ThePrimrosePath thePrimrosePath)
-  , (":circus-ex-mortis:042", HomebrewScenario Sets.HarmsWay harmsWay)
-  , (":circus-ex-mortis:076", HomebrewScenario Sets.AllPointsWest allPointsWest)
-  , (":circus-ex-mortis:110", HomebrewScenario Sets.PiperAtTheGatesOfDawn piperAtTheGatesOfDawn)
-  , (":circus-ex-mortis:124", HomebrewScenario Sets.Bacchanalia bacchanalia)
-  , (":circus-ex-mortis:155", HomebrewScenario Sets.RedSunrise redSunrise)
-  , (":circus-ex-mortis:192", HomebrewScenario Sets.ThousandToOne thousandToOne)
+  [ (":circus-ex-mortis:001", HomebrewScenario OneNightOnly oneNightOnly)
+  , (":circus-ex-mortis:017", HomebrewScenario ThePrimrosePath thePrimrosePath)
+  , (":circus-ex-mortis:042", HomebrewScenario HarmsWay harmsWay)
+  , (":circus-ex-mortis:076", HomebrewScenario AllPointsWest allPointsWest)
+  , (":circus-ex-mortis:110", HomebrewScenario PiperAtTheGatesOfDawn piperAtTheGatesOfDawn)
+  , (":circus-ex-mortis:124", HomebrewScenario Bacchanalia bacchanalia)
+  , (":circus-ex-mortis:155", HomebrewScenario RedSunrise redSunrise)
+  , (":circus-ex-mortis:192", HomebrewScenario ThousandToOne thousandToOne)
   ]
 
 campaigns :: HomebrewCampaigns
@@ -176,14 +34,7 @@ data CircusExMortisContent
 
 instance IsHomebrewContent CircusExMortisContent where
   homebrewContent =
-    HomebrewContent
-      { acts = acts
-      , agendas = agendas
-      , assets = assets
-      , enemies = enemies
-      , locations = locations
-      , stories = []
-      , treacheries = treacheries
-      , scenarios = scenarios
+    $(generateHomebrew)
+      { scenarios = scenarios
       , campaigns = campaigns
       }

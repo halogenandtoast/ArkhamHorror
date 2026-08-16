73abc6c1daa1cb55e24bee86821d4c20e716fa45
Thomas Moeller <tomathomas@mailbox.org>
2026-08-16 10:58:49 +0200

    Load german translation

--- frontend/src/locales/de/gameBoard/gameBoard.ts
+++ frontend/src/locales/de/gameBoard/gameBoard.ts
@@ -1,4 +1,5 @@
+import base from '@/locales/de/gameBoard/base.json'
 import phase from '@/locales/de/gameBoard/phases.json'
 import skillTest from '@/locales/de/gameBoard/skill_test.json'
 
-export default {phase, skillTest}
+export default {...base, phase, skillTest}

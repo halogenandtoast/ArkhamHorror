94cd1985e3ec4d21795147ebb4fa00202bccd0d2
Matthew Mongeau <halogenandtoast@gmail.com>
2026-08-19 22:14:18 +0900

    Fix Field Dressing (1)

--- backend/arkham-api/library/Arkham/Event/Events/FieldDressing1.hs
+++ backend/arkham-api/library/Arkham/Event/Events/FieldDressing1.hs
@@ -16,16 +16,16 @@ instance RunMessage FieldDressing1 where
     PlayThisEvent _iid (is attrs -> True) -> do
       doStep 3 msg
       pure e
-    DoStep n (PlayThisEvent iid (is attrs -> True)) | n > 0 -> do
+    DoStep n msg'@(PlayThisEvent iid (is attrs -> True)) | n > 0 -> do
       let source = toSource attrs
       healableInvestigators <- select $ HealableInvestigator source #damage (colocatedWith iid)
-      healableAllies <- select $ HealableAsset source #damage $ AllyAsset <> assetControlledBy iid
+      healableAllies <- select $ HealableAsset source #damage $ AllyAsset <> assetAtLocationWith iid
       chooseOrRunOneM iid do
         targets healableInvestigators \iid' -> do
           healDamage iid' source 1
-          doStep (n - 1) msg
+          doStep (n - 1) msg'
         targets healableAllies \asset -> do
           healDamage asset source 1
-          doStep (n - 1) msg
+          doStep (n - 1) msg'
       pure e
     _ -> FieldDressing1 <$> liftRunMessage msg attrs

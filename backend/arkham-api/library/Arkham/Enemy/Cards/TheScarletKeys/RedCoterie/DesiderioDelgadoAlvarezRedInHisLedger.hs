6b017e8401ca0874aa28769a6bd53dbdbffddf1f
Matthew Mongeau <halogenandtoast@gmail.com>
2026-08-21 08:54:20 +0900

    Fix Desi Red in His Ledger
    * Do not trigger his ability on himself
    * Also shift his keys

--- backend/arkham-api/library/Arkham/Enemy/Cards/TheScarletKeys/RedCoterie/DesiderioDelgadoAlvarezRedInHisLedger.hs
+++ backend/arkham-api/library/Arkham/Enemy/Cards/TheScarletKeys/RedCoterie/DesiderioDelgadoAlvarezRedInHisLedger.hs
@@ -4,9 +4,9 @@ import Arkham.Ability
 import Arkham.Campaigns.TheScarletKeys.Helpers
 import Arkham.Enemy.CardDefs.TheScarletKeys.RedCoterie qualified as Cards
 import Arkham.Enemy.Import.Lifted
-import Arkham.Helpers.Window
+import Arkham.Helpers.Window (damagedEnemy, damagedEnemyAmount)
 import Arkham.Matcher
-import Arkham.Modifier
+import Arkham.Message.Lifted.Choose
 import Arkham.Placement
 import Arkham.Trait (Trait (Coterie))
 
@@ -25,15 +25,20 @@ instance HasAbilities DesiderioDelgadoAlvarezRedInHisLedger where
     extend1 a
       $ mkAbility a 1
       $ forced
-      $ EnemyWouldTakeDamage #when AnySource (EnemyAt (locationWithEnemy a) <> EnemyWithTrait Coterie)
+      $ EnemyWouldTakeDamage
+        #when
+        AnySource
+        (EnemyAt (locationWithEnemy a) <> EnemyWithTrait Coterie <> not_ (be a))
 
 instance RunMessage DesiderioDelgadoAlvarezRedInHisLedger where
   runMessage msg e@(DesiderioDelgadoAlvarezRedInHisLedger attrs) = runQueueT $ case msg of
     InvestigatorDrawEnemy _ eid | eid == attrs.id -> do
       keysFor attrs >>= traverse_ (`createScarletKeyAt_` AttachedToEnemy attrs.id)
       DesiderioDelgadoAlvarezRedInHisLedger <$> liftRunMessage msg attrs
-    UseCardAbility _ (isSource attrs -> True) 1 (damagedEnemy -> eid) _ -> do
-      damageModifier (attrs.ability 1) eid (DamageTaken (-1))
-      nonAttackEnemyDamage Nothing (attrs.ability 1) 1 attrs
+    UseCardAbility iid (isSource attrs -> True) 1 (damagedEnemy &&& damagedEnemyAmount -> (eid, n)) _ -> do
+      reduceDamageTaken (attrs.ability 1) eid n
+      nonAttackEnemyDamage Nothing (attrs.ability 1) n attrs
+      skeys <- select $ scarletKeyWithEnemy attrs.id
+      chooseOneAtATimeM iid $ targets skeys shift
       pure e
     _ -> DesiderioDelgadoAlvarezRedInHisLedger <$> liftRunMessage msg attrs

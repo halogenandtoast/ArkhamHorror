15621c42cad341d95a83a5be2ac9c18f04b240e6
Matthew Mongeau <halogenandtoast@gmail.com>
2026-08-28 01:54:48 +0900

    Cleanup Cleaning Kit

--- backend/arkham-api/library/Arkham/Asset/Assets/CleaningKit.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/CleaningKit.hs
@@ -1,4 +1,4 @@
-module Arkham.Asset.Assets.CleaningKit (cleaningKit, CleaningKit (..)) where
+module Arkham.Asset.Assets.CleaningKit (cleaningKit) where
 
 import Arkham.Asset.Cards qualified as Cards
 import Arkham.Asset.Import.Lifted
@@ -7,22 +7,16 @@ import Arkham.Helpers.Modifiers
 import Arkham.Matcher
 
 newtype CleaningKit = CleaningKit AssetAttrs
-  deriving anyclass (IsAsset, HasAbilities)
+  deriving anyclass (IsAsset, HasAbilities, RunMessage)
   deriving newtype (Show, Eq, ToJSON, FromJSON, Entity)
 
 cleaningKit :: AssetCard CleaningKit
 cleaningKit = asset CleaningKit Cards.cleaningKit
 
 instance HasModifiersFor CleaningKit where
-  getModifiersFor (CleaningKit a) = case a.controller of
-    Nothing -> pure mempty
-    Just iid ->
-      modifySelectWhen
-        a
-        (a.use Supply > 0)
-        (not_ (AssetWithId a.id) <> assetControlledBy iid)
-        [ProvidesUses Supply (toSource a), ProvidesProxyUses Supply Ammo (toSource a)]
-
-instance RunMessage CleaningKit where
-  runMessage msg (CleaningKit attrs) = runQueueT $ case msg of
-    _ -> CleaningKit <$> liftRunMessage msg attrs
+  getModifiersFor (CleaningKit a) = for_ a.controller \iid ->
+    modifySelectWhen
+      a
+      (a.use Supply > 0)
+      (not_ (AssetWithId a.id) <> assetControlledBy iid)
+      [ProvidesUses Supply (toSource a), ProvidesProxyUses Supply Ammo (toSource a)]

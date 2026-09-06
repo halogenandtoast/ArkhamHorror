877d9661cd18235b35a036f30e0cb871979be89e
Matthew Mongeau <halogenandtoast@gmail.com>
2026-09-06 20:07:42 +0900

    Award victory points for story-side victory cards (#5624)
    Seafloor Frieze, Eroded Frieze, and the six Shades of Suffering shades put
    their story side into the victory display, but those CardDefs never declared
    cdVictoryPoints, so each scored 0 XP despite showing in the display.

--- backend/arkham-api/library/Arkham/Story/CardDefs/TheDrownedCity/TheWesternWall.hs
+++ backend/arkham-api/library/Arkham/Story/CardDefs/TheDrownedCity/TheWesternWall.hs
@@ -3,7 +3,8 @@ module Arkham.Story.CardDefs.TheDrownedCity.TheWesternWall where
 import Arkham.Story.CardDefs.Import
 
 seafloorFrieze :: CardDef
-seafloorFrieze = doubleSided $ story "11531b" "Seafloor Frieze" TheWesternWall
+seafloorFrieze =
+  (doubleSided $ story "11531b" "Seafloor Frieze" TheWesternWall) {cdVictoryPoints = Just 1}
 
 theUnderseaVault :: CardDef
 theUnderseaVault = doubleSided $ story "11532b" "The Undersea Vault" TheWesternWall

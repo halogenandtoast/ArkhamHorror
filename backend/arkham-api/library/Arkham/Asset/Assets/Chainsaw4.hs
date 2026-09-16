ff438da8d29cdef2553dc91e3383984e435f4673
Matthew Mongeau <halogenandtoast@gmail.com>
2026-09-16 20:53:07 +0900

    Fix Chainsaw (4)
    Live and learn needs the correct modifier source

--- backend/arkham-api/library/Arkham/Asset/Assets/Chainsaw4.hs
+++ backend/arkham-api/library/Arkham/Asset/Assets/Chainsaw4.hs
@@ -2,12 +2,13 @@ module Arkham.Asset.Assets.Chainsaw4 (chainsaw4) where
 
 import Arkham.Ability
 import Arkham.Asset.Cards qualified as Cards
-import Arkham.Asset.Runner
-import Arkham.DamageEffect
-import Arkham.Fight
-import Arkham.Helpers.Modifiers
+import Arkham.Asset.Import.Lifted
+import Arkham.Asset.Uses
+import Arkham.Helpers.SkillTest (getSkillTestTargetedEnemy)
+import Arkham.I18n
 import Arkham.Matcher
-import Arkham.Prelude
+import Arkham.Message.Lifted.Choose
+import Arkham.Modifier
 
 newtype Chainsaw4 = Chainsaw4 AssetAttrs
   deriving anyclass (IsAsset, HasModifiersFor)
@@ -17,30 +18,22 @@ chainsaw4 :: AssetCard Chainsaw4
 chainsaw4 = asset Chainsaw4 Cards.chainsaw4
 
 instance HasAbilities Chainsaw4 where
-  getAbilities (Chainsaw4 a) = [restricted a 1 ControlsThis $ fightAction $ assetUseCost a Supply 1]
+  getAbilities (Chainsaw4 a) = [controlled_ a 1 $ fightAction $ assetUseCost a Supply 1]
 
 instance RunMessage Chainsaw4 where
-  runMessage msg a@(Chainsaw4 attrs) = case msg of
+  runMessage msg a@(Chainsaw4 attrs) = runQueueT $ case msg of
     UseThisAbility iid (isSource attrs -> True) 1 -> do
       sid <- getRandom
-      chooseFight <- toMessage <$> mkChooseFight sid iid (attrs.ability 1)
-      enabled <- skillTestModifiers sid attrs iid [SkillModifier #combat 2, DamageDealt 2]
-      pushAll [enabled, chooseFight]
+      skillTestModifiers sid (attrs.ability 1) iid [SkillModifier #combat 2, DamageDealt 2]
+      chooseFightEnemy sid iid (attrs.ability 1)
       pure a
     FailedThisSkillTest iid (isAbilitySource attrs 1 -> True) -> do
-      mEnemy <- (.enemy) <$> getSkillTestTarget
-      canDamage <- maybe (pure False) (selectAny . EnemyWithId) mEnemy
-      player <- getPlayer iid
-      push
-        $ chooseOne player
-        $ Label
-          "$label.cards.chainsaw4.place1SupplyOnChainsaw"
-          [AddUses (attrs.ability 1) (toId attrs) Supply 1]
-        : [ Label
-              "$label.cards.chainsaw4.deal1DamageToTheAttackedEnemy"
-              [DealDamage (EnemyTarget eid) $ nonAttack (Just iid) (attrs.ability 1) 1]
-          | canDamage
-          , eid <- maybeToList mEnemy
-          ]
+      mEnemy <- runMaybeT $ selectMaybeT . EnemyWithId =<< MaybeT getSkillTestTargetedEnemy
+      chooseOneM iid $ withI18n do
+        labeled "cards.chainsaw4.place1SupplyOnChainsaw" do
+          addUses (attrs.ability 1) (toId attrs) Supply 1
+        for_ mEnemy \eid -> do
+          labeled "cards.chainsaw4.deal1DamageToTheAttackedEnemy" do
+            nonAttackEnemyDamage (Just iid) (attrs.ability 1) 1 eid
       pure a
-    _ -> Chainsaw4 <$> runMessage msg attrs
+    _ -> Chainsaw4 <$> liftRunMessage msg attrs
